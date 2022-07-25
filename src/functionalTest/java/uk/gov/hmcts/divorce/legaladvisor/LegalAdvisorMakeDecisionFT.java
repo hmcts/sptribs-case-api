8b2b6f2b24ac0df8868d98dbe42c80f51996a2bf
Jeff Nayler <jeff.nayler@version1.com>
2022-07-25 10:40:33 +0100

    ST-323 CIC Create Case (#8)
    * added objects in model
    
    * Temporarily revert secrets change which is breaking build
    
    * 1) Developed a create case page
    2) All the drop down fields ahve been created for create case.
    
    * Update Jenkinsfile_CNP (#7)
    
    Co-authored-by: jaberelfasi <jaberelfasi@gmail.com>
    Co-authored-by: version1rhel <murad.alam@version1.com>
    Co-authored-by: Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>

---
+++ src/functionalTest/java/uk/gov/hmcts/divorce/legaladvisor/LegalAdvisorMakeDecisionFT.java
@@ -0,0 +1,56 @@
+package uk.gov.hmcts.divorce.legaladvisor;
+
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import java.util.Map;
+
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.json;
+import static net.javacrumbs.jsonunit.core.Option.IGNORING_ARRAY_ORDER;
+import static net.javacrumbs.jsonunit.core.Option.IGNORING_EXTRA_FIELDS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpStatus.OK;
+import static uk.gov.hmcts.divorce.legaladvisor.event.LegalAdvisorMakeDecision.LEGAL_ADVISOR_MAKE_DECISION;
+import static uk.gov.hmcts.divorce.testutil.CaseDataUtil.caseData;
+import static uk.gov.hmcts.divorce.testutil.TestConstants.ABOUT_TO_SUBMIT_URL;
+import static uk.gov.hmcts.divorce.testutil.TestResourceUtil.expectedResponse;
+
+@SpringBootTest
+public class LegalAdvisorMakeDecisionFT extends FunctionalTestSuite {
+
+    private static final String REQUEST = "classpath:request/casedata/ccd-callback-casedata-legal-advisor-make-decision.json";
+    private static final String CO_MORE_INFO_RESPONSE = "classpath:responses/response-legal-advisor-make-decision-co-more-info.json";
+    private static final String CO_REJECTED_RESPONSE = "classpath:responses/response-legal-advisor-make-decision-co-rejected.json";
+
+    @Test
+    public void shouldSendEmailToApp1SolicitorAndGenerateRefusalOrderWhenMoreInfoSelected() throws IOException {
+        Map<String, Object> request = caseData(REQUEST);
+
+        Response response = triggerCallback(request, LEGAL_ADVISOR_MAKE_DECISION, ABOUT_TO_SUBMIT_URL);
+
+        assertThat(response.getStatusCode()).isEqualTo(OK.value());
+
+        assertThatJson(response.asString())
+            .when(IGNORING_EXTRA_FIELDS)
+            .when(IGNORING_ARRAY_ORDER)
+            .isEqualTo(json(expectedResponse(CO_MORE_INFO_RESPONSE)));
+    }
+
+    @Test
+    public void shouldGenerateRefusalOrderWhenRejectedSelected() throws IOException {
+        Map<String, Object> request = caseData(REQUEST);
+        request.put("coRefusalDecision", "reject");
+
+        Response response = triggerCallback(request, LEGAL_ADVISOR_MAKE_DECISION, ABOUT_TO_SUBMIT_URL);
+
+        assertThat(response.getStatusCode()).isEqualTo(OK.value());
+
+        assertThatJson(response.asString())
+            .when(IGNORING_EXTRA_FIELDS)
+            .when(IGNORING_ARRAY_ORDER)
+            .isEqualTo(json(expectedResponse(CO_REJECTED_RESPONSE)));
+    }
+}
---
+++ src/main/java/uk/gov/hmcts/divorce/ciccase/model/CaseSubcategory.java
@@ -0,0 +1,21 @@
+package uk.gov.hmcts.divorce.ciccase.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import uk.gov.hmcts.ccd.sdk.api.HasLabel;
+
+@Getter
+@AllArgsConstructor
+public enum CaseSubcategory implements HasLabel {
+    @JsonProperty("Fatal")
+    FATAL("Fatal"),
+
+    @JsonProperty("sexualAbuse")
+    SEXUAL_ABUSE("Sexual Abuse"),
+
+    @JsonProperty("other")
+    OTHER("Other");
+
+    private final String label;
+}
