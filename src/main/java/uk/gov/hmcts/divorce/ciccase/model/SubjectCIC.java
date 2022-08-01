b0af4032015ead1e0e42ebd1a675603c6cf5f5d9
muradalamv1 <107861321+muradalamv1@users.noreply.github.com>
2022-08-01 11:56:16 +0100

    Feat/st 337 (#11)
    * 1)Created Category and sub category selection.
    2) Created a text box for comments.
    
    * 1)Created checkboxes for Applicant , subject and representative.
    
    * 1)Created a Contact preferences and applicant details page for Applicant
    
    * 1)Created a Contact preferences and applicant details page for Applicant

---
+++ src/main/java/uk/gov/hmcts/divorce/ciccase/model/SubjectCIC.java
@@ -0,0 +1,20 @@
+package uk.gov.hmcts.divorce.ciccase.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import uk.gov.hmcts.ccd.sdk.api.HasLabel;
+
+@Getter
+@AllArgsConstructor
+public enum SubjectCIC implements HasLabel {
+    @JsonProperty("SubjectCIC")
+    SUBJECT("Subject");
+
+
+    private final String label;
+
+    public boolean isSubject() {
+        return SUBJECT.name().equalsIgnoreCase(this.name());
+    }
+}
