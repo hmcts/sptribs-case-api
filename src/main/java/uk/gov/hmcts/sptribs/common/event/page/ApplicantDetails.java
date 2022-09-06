362c4cfc44cfb2bad694c2704b31a757a2935a30
jaberelfasi <jaber.elfasi@hmcts.net>
2022-09-06 09:37:08 +0100

    change for ST-343 (#41)
    * change for ST-343
    
    * change for ST-343

--- src/main/java/uk/gov/hmcts/sptribs/common/event/page/ApplicantDetails.java
+++ src/main/java/uk/gov/hmcts/sptribs/common/event/page/ApplicantDetails.java
@@ -1,15 +1,15 @@
-package uk.gov.hmcts.sptribs.caseworker.event.page;
+package uk.gov.hmcts.sptribs.common.event.page;
 
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.CicCase;
 import uk.gov.hmcts.sptribs.common.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
 
-public class EditApplicantDetails implements CcdPageConfiguration {
+public class ApplicantDetails implements CcdPageConfiguration {
 
     @Override
     public void addTo(PageBuilder pageBuilder) {
-        pageBuilder.page("editApplicantDetails")
+        pageBuilder.page("applicantDetails")
             .pageLabel("Who is the applicant in this case?")
             .label("applicantDetailsObject", "")
             .complex(CaseData::getCicCase)
