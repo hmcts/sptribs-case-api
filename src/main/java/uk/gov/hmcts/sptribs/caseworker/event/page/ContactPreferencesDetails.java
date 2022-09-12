87624ac86ae8ecf54232a7cddc81a8b29d8767d4
version1rhel <murad.alam@version1.com>
2022-09-12 14:17:18 +0100

    Changed  super user from sptribs to nfdiv

---
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/page/ContactPreferencesDetails.java
@@ -0,0 +1,61 @@
+package uk.gov.hmcts.sptribs.caseworker.event.page;
+
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
+import uk.gov.hmcts.sptribs.ciccase.model.CicCase;
+import uk.gov.hmcts.sptribs.ciccase.model.ContactPreferencesDetailsCIC;
+import uk.gov.hmcts.sptribs.ciccase.model.PartiesCIC;
+import uk.gov.hmcts.sptribs.ciccase.model.State;
+import uk.gov.hmcts.sptribs.common.ccd.CcdPageConfiguration;
+import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ContactPreferencesDetails implements CcdPageConfiguration {
+
+
+    @Override
+    public void addTo(PageBuilder pageBuilder) {
+
+        pageBuilder.page("editContactPreferencesDetails")
+            .label("objectContact", "Who should receive information about the case?")
+            .complex(CaseData::getCicCase)
+            .complex(CicCase::getContactPreferenceCic, "")
+            .optional(ContactPreferencesDetailsCIC::getSubjectCIC, "cicCasePartiesCICCONTAINS \"SubjectCIC\"")
+            .optional(ContactPreferencesDetailsCIC::getApplicantCIC, "cicCasePartiesCICCONTAINS \"ApplicantCIC\"")
+            .optional(ContactPreferencesDetailsCIC::getRepresentativeCic, "cicCasePartiesCICCONTAINS \"RepresentativeCIC\"")
+           // .mandatoryWithLabel(CicCase::getPartiesCIC, "")
+            .done();
+
+
+//        pageBuilder
+//            .page("objectSubjects", this::midEvent)
+//            .label("subjectObject", "Which parties are named on the tribunal form?\r\n" + "\r\nCase record for [DRAFT]")
+//            .pageLabel("Who are the parties in this case?")
+//            .complex(CaseData::getCicCase)
+//            .mandatoryWithLabel(CicCase::getPartiesCIC, "")
+//            .done();
+    }
+
+
+
+//    private AboutToStartOrSubmitResponse<CaseData, State> midEvent(CaseDetails<CaseData, State> details,
+//                                                                   CaseDetails<CaseData, State> detailsBefore) {
+//        final CaseData data = details.getData();
+//        final List<String> errors = new ArrayList<>();
+//
+//        if (null != data.getCicCase() && !data.getCicCase().getPartiesCIC().contains(PartiesCIC.SUBJECT)) {
+//            errors.add("Subject is mandatory.");
+//        }
+//
+//        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+//            .data(data)
+//            .errors(errors)
+//            .build();
+//    }
+}
+
+
