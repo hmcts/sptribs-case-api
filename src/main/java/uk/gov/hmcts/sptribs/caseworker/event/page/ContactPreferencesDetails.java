a86840628454704a78edf18a872cbdf12d0645b2
jaberelfasi <jaberelfasi@gmail.com>
2022-09-12 19:21:04 +0100

    fixed checkstyle

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/page/ContactPreferencesDetails.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/page/ContactPreferencesDetails.java
@@ -1,17 +1,10 @@
 package uk.gov.hmcts.sptribs.caseworker.event.page;
 
-import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
-import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.CicCase;
 import uk.gov.hmcts.sptribs.ciccase.model.ContactPreferencesDetailsCIC;
-import uk.gov.hmcts.sptribs.ciccase.model.PartiesCIC;
-import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.common.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class ContactPreferencesDetails implements CcdPageConfiguration {
@@ -27,35 +20,34 @@ public class ContactPreferencesDetails implements CcdPageConfiguration {
             .optional(ContactPreferencesDetailsCIC::getSubjectCIC, "cicCasePartiesCICCONTAINS \"SubjectCIC\"")
             .optional(ContactPreferencesDetailsCIC::getApplicantCIC, "cicCasePartiesCICCONTAINS \"ApplicantCIC\"")
             .optional(ContactPreferencesDetailsCIC::getRepresentativeCic, "cicCasePartiesCICCONTAINS \"RepresentativeCIC\"")
-           // .mandatoryWithLabel(CicCase::getPartiesCIC, "")
             .done();
 
 
-//        pageBuilder
-//            .page("objectSubjects", this::midEvent)
-//            .label("subjectObject", "Which parties are named on the tribunal form?\r\n" + "\r\nCase record for [DRAFT]")
-//            .pageLabel("Who are the parties in this case?")
-//            .complex(CaseData::getCicCase)
-//            .mandatoryWithLabel(CicCase::getPartiesCIC, "")
-//            .done();
+        /*pageBuilder
+            .page("objectSubjects", this::midEvent)
+            .label("subjectObject", "Which parties are named on the tribunal form?\r\n" + "\r\nCase record for [DRAFT]")
+            .pageLabel("Who are the parties in this case?")
+            .complex(CaseData::getCicCase)
+            .mandatoryWithLabel(CicCase::getPartiesCIC, "")
+            .done();*/
     }
 
 
 
-//    private AboutToStartOrSubmitResponse<CaseData, State> midEvent(CaseDetails<CaseData, State> details,
-//                                                                   CaseDetails<CaseData, State> detailsBefore) {
-//        final CaseData data = details.getData();
-//        final List<String> errors = new ArrayList<>();
-//
-//        if (null != data.getCicCase() && !data.getCicCase().getPartiesCIC().contains(PartiesCIC.SUBJECT)) {
-//            errors.add("Subject is mandatory.");
-//        }
-//
-//        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
-//            .data(data)
-//            .errors(errors)
-//            .build();
-//    }
+    /*private AboutToStartOrSubmitResponse<CaseData, State> midEvent(CaseDetails<CaseData, State> details,
+                                                                   CaseDetails<CaseData, State> detailsBefore) {
+        final CaseData data = details.getData();
+        final List<String> errors = new ArrayList<>();
+
+        if (null != data.getCicCase() && !data.getCicCase().getPartiesCIC().contains(PartiesCIC.SUBJECT)) {
+            errors.add("Subject is mandatory.");
+        }
+
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .data(data)
+            .errors(errors)
+            .build();
+    }*/
 }
 
 
