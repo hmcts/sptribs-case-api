c7bb011389739a33c8f519ba25c8c9e5d1cae960
ElifDenizEsea <61947470+ElifDenizEsea@users.noreply.github.com>
2022-09-13 13:23:22 +0100

    ST-606 Stay the case conformation page (#54)
    * Stay the case conformation page
    
    * Stay the case conformation page
    
    Co-authored-by: hmctscivil <version1rhel@hmctscivil.mshome.net>

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerStayTheCase.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerStayTheCase.java
@@ -6,12 +6,14 @@ import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import uk.gov.hmcts.sptribs.caseworker.model.CaseStay;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
 import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
 
+import static java.lang.String.format;
 import static uk.gov.hmcts.sptribs.ciccase.model.State.CaseStayed;
 import static uk.gov.hmcts.sptribs.ciccase.model.State.POST_SUBMISSION_STATES_WITH_WITHDRAWN_AND_REJECTED;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.COURT_ADMIN_CIC;
@@ -33,6 +35,7 @@ public class CaseworkerStayTheCase implements CCDConfig<CaseData, State, UserRol
             .name("Stay the Case")
             .description("Stay the Case")
             .aboutToSubmitCallback(this::aboutToSubmit)
+            .submittedCallback(this::stayed)
             .showEventNotes()
             .grant(CREATE_READ_UPDATE_DELETE, COURT_ADMIN_CIC, SUPER_USER)
             .grantHistoryOnly(SOLICITOR))
@@ -56,6 +59,13 @@ public class CaseworkerStayTheCase implements CCDConfig<CaseData, State, UserRol
         return AboutToStartOrSubmitResponse.<CaseData, State>builder()
             .data(caseData)
             .state(CaseStayed)
+            .build();
+    }
+
+    public SubmittedCallbackResponse stayed(CaseDetails<CaseData, State> details,
+                                            CaseDetails<CaseData, State> beforeDetails) {
+        return SubmittedCallbackResponse.builder()
+            .confirmationHeader(format("# Stay Added to Case"))
             .build();
     }
 }
