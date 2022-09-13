057ac3ee6f96735ee58bd9d0de50d2f352a4ee44
sjamihmcts <110464983+sjamihmcts@users.noreply.github.com>
2022-09-13 14:31:49 +0100

    Feat/st 525: Removed old roles from config files (#58)
    * ST-525: initial changes
    
    * ST-525: Updated roles SUPER_USER, LEGAL_ADVISOR and few more
    
    * ST-525: Fixed tests
    
    * ST-525: Removed LEGAL_ADVISOR and APPLICANT_1_SOLICITOR users
    
    * ST-525: Removed ORGANISATION_CASE_ACCESS_ADMINISTRATOR users
    
    * ST-525: Renamed CASE_WORKER to COURT_ADMIN_CIC
    
    * ST-525: Organised imports
    
    * ST-525: Removed UserRoleCIC class as these users are moved to UserRole class
    
    * ST-525: Added roles to config files
    
    * ST-525: Updated comments
    
    * ST-525: Added missing file
    
    * ST-525: Removed old roles from config files

--- bin/add-roles.sh
+++ bin/add-roles.sh
@@ -3,17 +3,10 @@
 scriptPath=$(dirname $(realpath $0))
 
 # Roles used during the CCD import
-${scriptPath}/add-ccd-role.sh "caseworker-divorce-courtadmin_beta"
 ${scriptPath}/add-ccd-role.sh "caseworker-divorce-superuser"
 ${scriptPath}/add-ccd-role.sh "caseworker-sptribs-superuser"
-${scriptPath}/add-ccd-role.sh "caseworker-divorce-courtadmin-la"
-${scriptPath}/add-ccd-role.sh "caseworker-divorce-courtadmin"
 ${scriptPath}/add-ccd-role.sh "caseworker-divorce-solicitor"
-${scriptPath}/add-ccd-role.sh "caseworker-divorce-pcqextractor"
 ${scriptPath}/add-ccd-role.sh "caseworker-divorce-systemupdate"
-${scriptPath}/add-ccd-role.sh "caseworker-divorce-bulkscan"
-${scriptPath}/add-ccd-role.sh "caseworker-caa"
-${scriptPath}/add-ccd-role.sh "citizen"
 ${scriptPath}/add-ccd-role.sh "payments"
 ${scriptPath}/add-ccd-role.sh "caseworker-sptribs-cic-courtadmin"
 ${scriptPath}/add-ccd-role.sh "caseworker-sptribs-cic-caseofficer"
