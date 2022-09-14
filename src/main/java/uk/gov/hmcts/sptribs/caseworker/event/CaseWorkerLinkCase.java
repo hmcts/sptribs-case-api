dd73f1dde2a98fcf83cdbc3453a99d33692e66ee
sjamihmcts <santoshini.jami@hmcts.net>
2022-09-14 14:40:22 +0100

    ST-458: Updated submit callback event and states

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerLinkCase.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerLinkCase.java
@@ -1,15 +1,22 @@
 package uk.gov.hmcts.sptribs.caseworker.event;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
 import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
+import uk.gov.hmcts.sptribs.common.service.SubmissionService;
 
-import static uk.gov.hmcts.sptribs.ciccase.model.State.POST_SUBMISSION_STATES_WITH_WITHDRAWN_AND_REJECTED;
+import static java.lang.String.format;
+import static uk.gov.hmcts.sptribs.ciccase.model.State.POST_SUBMISSION_STATES;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.COURT_ADMIN_CIC;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.SOLICITOR;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.SUPER_USER;
@@ -20,11 +27,14 @@ import static uk.gov.hmcts.sptribs.ciccase.model.access.Permissions.CREATE_READ_
 public class CaseWorkerLinkCase implements CCDConfig<CaseData, State, UserRole> {
     public static final String CASEWORKER_LINK_CASE = "caseworker-link-case";
 
+    @Autowired
+    private SubmissionService submissionService;
+
     @Override
     public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
         new PageBuilder(configBuilder
             .event(CASEWORKER_LINK_CASE)
-            .forStates(POST_SUBMISSION_STATES_WITH_WITHDRAWN_AND_REJECTED)
+            .forStates(POST_SUBMISSION_STATES)
             .name("Link case")
             .showSummary()
             .description("Link case")
@@ -33,6 +43,27 @@ public class CaseWorkerLinkCase implements CCDConfig<CaseData, State, UserRole> 
             .grantHistoryOnly(SOLICITOR))
             .page("linkCase")
             .pageLabel("Link Case");
+    }
+
+    @SneakyThrows
+    public AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmit(CaseDetails<CaseData, State> details,
+                                                                       CaseDetails<CaseData, State> beforeDetails) {
+
+        var submittedDetails = submissionService.submitApplication(details);
+        CaseData data = submittedDetails.getData();
+        State state = submittedDetails.getState();
+
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .data(data)
+            .state(state)
+            .build();
+    }
+
+    public SubmittedCallbackResponse submitted(CaseDetails<CaseData, State> details,
+                                               CaseDetails<CaseData, State> beforeDetails) {
+        return SubmittedCallbackResponse.builder()
+            .confirmationHeader(format("Case Updated"))
+            .build();
     }
 
 }
