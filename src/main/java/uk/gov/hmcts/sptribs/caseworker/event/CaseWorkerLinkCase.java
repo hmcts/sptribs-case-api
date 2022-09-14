73745c716000b3e776add59c84f02d989d905467
sjamihmcts <santoshini.jami@hmcts.net>
2022-09-14 12:13:33 +0100

    ST-458: Remove unwanted code

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerLinkCase.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerLinkCase.java
@@ -3,17 +3,12 @@ package uk.gov.hmcts.sptribs.caseworker.event;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
-import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
-import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
-import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
 import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
 
-import static java.lang.String.format;
-import static uk.gov.hmcts.sptribs.ciccase.model.State.CaseStayed;
 import static uk.gov.hmcts.sptribs.ciccase.model.State.POST_SUBMISSION_STATES_WITH_WITHDRAWN_AND_REJECTED;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.COURT_ADMIN_CIC;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.SOLICITOR;
@@ -33,8 +28,6 @@ public class CaseWorkerLinkCase implements CCDConfig<CaseData, State, UserRole> 
             .name("Link case")
             .showSummary()
             .description("Link case")
-            //.aboutToSubmitCallback(this::aboutToSubmit)
-            //.submittedCallback(this::stayed)
             .showEventNotes()
             .grant(CREATE_READ_UPDATE_DELETE, COURT_ADMIN_CIC, SUPER_USER)
             .grantHistoryOnly(SOLICITOR))
@@ -42,24 +35,4 @@ public class CaseWorkerLinkCase implements CCDConfig<CaseData, State, UserRole> 
             .pageLabel("Link Case");
     }
 
-    public AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmit(
-        final CaseDetails<CaseData, State> details,
-        final CaseDetails<CaseData, State> beforeDetails
-    ) {
-        log.info("Caseworker stay the case callback invoked for Case Id: {}", details.getId());
-
-        var caseData = details.getData();
-
-        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
-            .data(caseData)
-            .state(CaseStayed)
-            .build();
-    }
-
-    public SubmittedCallbackResponse stayed(CaseDetails<CaseData, State> details,
-                                            CaseDetails<CaseData, State> beforeDetails) {
-        return SubmittedCallbackResponse.builder()
-            .confirmationHeader(format("# Stay Added to Case"))
-            .build();
-    }
 }
