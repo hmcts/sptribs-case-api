8a2dffb8ac9b481690eba958bbf5a7ca701dc36a
sjamihmcts <santoshini.jami@hmcts.net>
2022-09-14 15:32:54 +0100

    ST-458: Updated states as per state diagram

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerLinkCase.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerLinkCase.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.sptribs.caseworker.event;
 
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
@@ -13,10 +12,9 @@ import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
 import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
-import uk.gov.hmcts.sptribs.common.service.SubmissionService;
 
 import static java.lang.String.format;
-import static uk.gov.hmcts.sptribs.ciccase.model.State.POST_SUBMISSION_STATES;
+import static uk.gov.hmcts.sptribs.ciccase.model.State.NewCaseReceived;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.COURT_ADMIN_CIC;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.SOLICITOR;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.SUPER_USER;
@@ -27,14 +25,11 @@ import static uk.gov.hmcts.sptribs.ciccase.model.access.Permissions.CREATE_READ_
 public class CaseWorkerLinkCase implements CCDConfig<CaseData, State, UserRole> {
     public static final String CASEWORKER_LINK_CASE = "caseworker-link-case";
 
-    @Autowired
-    private SubmissionService submissionService;
-
     @Override
     public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
         new PageBuilder(configBuilder
             .event(CASEWORKER_LINK_CASE)
-            .forStates(POST_SUBMISSION_STATES)
+            .forStates(NewCaseReceived)
             .name("Link case")
             .showSummary()
             .description("Link case")
@@ -48,14 +43,11 @@ public class CaseWorkerLinkCase implements CCDConfig<CaseData, State, UserRole> 
     @SneakyThrows
     public AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmit(CaseDetails<CaseData, State> details,
                                                                        CaseDetails<CaseData, State> beforeDetails) {
-
-        var submittedDetails = submissionService.submitApplication(details);
-        CaseData data = submittedDetails.getData();
-        State state = submittedDetails.getState();
+        log.info("Caseworker link the case callback invoked for Case Id: {}", details.getId());
 
         return AboutToStartOrSubmitResponse.<CaseData, State>builder()
-            .data(data)
-            .state(state)
+            .data(details.getData())
+            .state(NewCaseReceived)
             .build();
     }
 
