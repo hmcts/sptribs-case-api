20572d644accdfa4749e104c29ae465991d8ab40
ElifDenizEsea <61947470+ElifDenizEsea@users.noreply.github.com>
2022-09-15 16:06:33 +0100

    ST-478 Labels changed (#72)
    Co-authored-by: hmctscivil <version1rhel@hmctscivil.mshome.net>

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/model/CaseStay.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/model/CaseStay.java
@@ -25,7 +25,7 @@ import static uk.gov.hmcts.ccd.sdk.type.FieldType.TextArea;
 public class CaseStay {
 
     @CCD(
-        label = "Stay Reason",
+        label = "Why is a stay being added to this case?",
         access = {DefaultAccess.class, CaseworkerWithCAAAccess.class}
     )
     private StayReason stayReason;
@@ -44,7 +44,7 @@ public class CaseStay {
 
 
     @CCD(
-        label = "Enter a Flag TYpe",
+        label = " Enter a flag type ",
         typeOverride = TextArea
     )
     private String flagType;
