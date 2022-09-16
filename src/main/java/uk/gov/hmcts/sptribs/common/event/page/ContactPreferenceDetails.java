c86e0e3d68039db4cf49b959423bbb6c08c88a9c
muradalamv1 <107861321+muradalamv1@users.noreply.github.com>
2022-09-16 12:13:20 +0100

    Feat/st 347 (#73)
    * Changed the case category and sub category to dropdown.(ST-492)
    Created a page for document upload.
    Created a page for contact preferences.
    
    * Updated Documents upload page.
    Created page for further details.
    Made email a mandatory field if the user select Email as a preference type.
    
    * Updated Documents upload page.
    Created page for further details.
    Made email a mandatory field if the user select Email as a preference type.
    
    * Merge remote-tracking branch 'origin/master' into feat/ST-399
    
    # Conflicts:
    #	src/main/java/uk/gov/hmcts/sptribs/common/event/CreateTestCase.java
    
    * Merge remote-tracking branch 'origin/master' into feat/ST-399
    
    # Conflicts:
    #	src/main/java/uk/gov/hmcts/sptribs/common/event/CreateTestCase.java
    
    * ST-335: Initial changes
    
    * ST-335: Changes to add Named parties into single MultiselectList and also the same with contactDetailPreference list. Also add code for Summary tab in overview page
    
    * made Further details fields mandatory
    Csae parties tab created
    
    * Changed  super user from sptribs to nfdiv
    
    * added new branch
    
    * Contact Preference details has been changed and also ahowed on edit page.
    Case typ details has also been uodate as only the data coming from the forms filled will be shown.
    
    * Summary tab details has changed
    
    * Contact preferences Lable changed
    Added subject details to case parties tab
    
    * Contact preferences Lable changed
    Added subject details to case parties tab
    All parties details shown on case parties tab
    
    * Contact preferences page is updated and will not proceed to next page untill one option selected
    
    * Contact preferences page is updated and will not proceed to next page untill one option selected
    
    Co-authored-by: sjamihmcts <santoshini.jami@hmcts.net>
    Co-authored-by: jaberelfasi <jaberelfasi@gmail.com>

--- src/main/java/uk/gov/hmcts/sptribs/common/event/page/ContactPreferenceDetails.java
+++ src/main/java/uk/gov/hmcts/sptribs/common/event/page/ContactPreferenceDetails.java
@@ -1,23 +1,47 @@
 package uk.gov.hmcts.sptribs.common.event.page;
 
 
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.CicCase;
+import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.common.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ContactPreferenceDetails implements CcdPageConfiguration {
     @Override
     public void addTo(PageBuilder pageBuilder) {
-        pageBuilder.page("objectContacts")
+        pageBuilder
+            .page("objectContacts", this::midEvent)
             .label("objectContact", "Who should receive information about the case?")
             .complex(CaseData::getCicCase)
-            .optional(CicCase::getSubjectCIC,"cicCasePartiesCICCONTAINS \"SubjectCIC\"")
-            .optional(CicCase::getApplicantCIC,"cicCasePartiesCICCONTAINS \"ApplicantCIC\"")
-            .optional(CicCase::getRepresentativeCIC,"cicCasePartiesCICCONTAINS \"RepresentativeCIC\"")
+            .optional(CicCase::getSubjectCIC, "cicCasePartiesCICCONTAINS \"SubjectCIC\"")
+            .optional(CicCase::getApplicantCIC, "cicCasePartiesCICCONTAINS \"ApplicantCIC\"")
+            .optional(CicCase::getRepresentativeCIC, "cicCasePartiesCICCONTAINS \"RepresentativeCIC\"")
             .done();
+    }
+
+
+    private AboutToStartOrSubmitResponse<CaseData, State> midEvent(CaseDetails<CaseData, State> details,
+                                                                   CaseDetails<CaseData, State> detailsBefore) {
+        final CaseData data = details.getData();
+        final List<String> errors = new ArrayList<>();
+
+        if (null != data.getCicCase() && (null == data.getCicCase().getSubjectCIC() || data.getCicCase().getSubjectCIC().size() == 0)
+            && (null == data.getCicCase().getRepresentativeCIC() || data.getCicCase().getRepresentativeCIC().size() == 0)
+            && (null == data.getCicCase().getApplicantCIC() || data.getCicCase().getApplicantCIC().size() == 0)) {
+            errors.add("One field must be selected.");
+        }
+
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .data(data)
+            .errors(errors)
+            .build();
     }
 
 }
