c03dcc12c225d44c065ef68d1a1029c38405f98a
muradalamv1 <107861321+muradalamv1@users.noreply.github.com>
2022-09-21 14:05:31 +0100

    Feat/st 549 (#85)
    * Changed the case category and sub category to dropdown.(ST-492)
    Created a page for document upload.
    Created a page for contact preferences.
    
    * Updated Documents upload page.
    Created page for further details.
    Made email a mandatory field if the user select Email as a preference type.
    
    * Updated Documents upload page.
    Created page for further details.
    Made email a mandatory field if the user select Email as a preference type.
    
    * Merge remote-tracking branch 'origin/master' into feat/ST-399
    
    # Conflicts:
    #	src/main/java/uk/gov/hmcts/sptribs/common/event/CreateTestCase.java
    
    * Merge remote-tracking branch 'origin/master' into feat/ST-399
    
    # Conflicts:
    #	src/main/java/uk/gov/hmcts/sptribs/common/event/CreateTestCase.java
    
    * ST-335: Initial changes
    
    * ST-335: Changes to add Named parties into single MultiselectList and also the same with contactDetailPreference list. Also add code for Summary tab in overview page
    
    * made Further details fields mandatory
    Csae parties tab created
    
    * Changed  super user from sptribs to nfdiv
    
    * added new branch
    
    * Contact Preference details has been changed and also ahowed on edit page.
    Case typ details has also been uodate as only the data coming from the forms filled will be shown.
    
    * Summary tab details has changed
    
    * Contact preferences Lable changed
    Added subject details to case parties tab
    
    * Contact preferences Lable changed
    Added subject details to case parties tab
    All parties details shown on case parties tab
    
    * Contact preferences page is updated and will not proceed to next page untill one option selected
    
    * Contact preferences page is updated and will not proceed to next page untill one option selected
    
    * Case worker draft order created
    
    * Created draft case start
    ammended feat/ST-420 (removed comment text area)
    
    * created a dropdown for draft order
    
    Co-authored-by: sjamihmcts <santoshini.jami@hmcts.net>
    Co-authored-by: jaberelfasi <jaberelfasi@gmail.com>

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/model/DraftOrderCIC.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/model/DraftOrderCIC.java
@@ -3,17 +3,33 @@ package uk.gov.hmcts.sptribs.caseworker.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import uk.gov.hmcts.ccd.sdk.api.CCD;
+import uk.gov.hmcts.sptribs.ciccase.model.OrderTemplate;
+import uk.gov.hmcts.sptribs.ciccase.model.access.CaseworkerWithCAAAccess;
+import uk.gov.hmcts.sptribs.ciccase.model.access.DefaultAccess;
+
+import static uk.gov.hmcts.ccd.sdk.type.FieldType.FixedList;
 
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @JsonNaming(PropertyNamingStrategies.UpperCamelCaseStrategy.class)
 @Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class DraftOrderCIC {
+
+    @CCD(
+        access = {DefaultAccess.class, CaseworkerWithCAAAccess.class},
+        typeOverride = FixedList,
+        typeParameterOverride = "OrderTemplate"
+    )
+    private OrderTemplate orderTemplate;
+
 
 
 }
