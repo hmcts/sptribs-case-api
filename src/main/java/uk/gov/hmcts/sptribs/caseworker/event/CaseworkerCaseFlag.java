2bd44249693c6f0c7ad8baa0bf27f32e5e69ee72
ElifDenizEsea <61947470+ElifDenizEsea@users.noreply.github.com>
2022-09-26 15:59:43 +0100

    ST-446 Conformation page (#88)
    Co-authored-by: hmctscivil <version1rhel@hmctscivil.mshome.net>

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerCaseFlag.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerCaseFlag.java
@@ -3,12 +3,16 @@ package uk.gov.hmcts.sptribs.caseworker.event;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
 import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
 
+import static java.lang.String.format;
 import static uk.gov.hmcts.sptribs.ciccase.model.State.POST_SUBMISSION_STATES;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.COURT_ADMIN_CIC;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.SOLICITOR;
@@ -29,7 +33,32 @@ public class CaseworkerCaseFlag implements CCDConfig<CaseData, State, UserRole> 
             .showSummary()
             .description("Create a case flag")
             .showEventNotes()
+            .aboutToSubmitCallback(this::aboutToSubmit)
+            .submittedCallback(this::flagCreated)
             .grant(CREATE_READ_UPDATE_DELETE, COURT_ADMIN_CIC, SUPER_USER)
-            .grantHistoryOnly(SOLICITOR));
+            .grantHistoryOnly(SOLICITOR))
+            .page("addFlag")
+            .label("addFlag", "<H2>Add a Flag to this case</H2>")
+        ;
+    }
+
+    public AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmit(
+        final CaseDetails<CaseData, State> details,
+        final CaseDetails<CaseData, State> beforeDetails
+    ) {
+        log.info("Caseworker stay the case callback invoked for Case Id: {}", details.getId());
+
+        var caseData = details.getData();
+
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .data(caseData)
+            .build();
+    }
+
+    public SubmittedCallbackResponse flagCreated(CaseDetails<CaseData, State> details,
+                                                 CaseDetails<CaseData, State> beforeDetails) {
+        return SubmittedCallbackResponse.builder()
+            .confirmationHeader(format("# Case Flag created %n## This Flag has been added to case"))
+            .build();
     }
 }
