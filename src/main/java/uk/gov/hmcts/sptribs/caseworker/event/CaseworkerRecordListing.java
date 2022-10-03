45fde034d81411b4f9d719a25c7d58122dc2ce7f
sjamihmcts <santoshini.jami@hmcts.net>
2022-10-03 11:07:32 +0100

    ST-656: Added changes for Record Listing

---
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerRecordListing.java
@@ -0,0 +1,48 @@
+package uk.gov.hmcts.sptribs.caseworker.event;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
+import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.sptribs.caseworker.model.RecordListing;
+import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
+import uk.gov.hmcts.sptribs.ciccase.model.State;
+import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
+import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
+
+
+import static uk.gov.hmcts.sptribs.ciccase.model.State.POST_SUBMISSION_STATES;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.COURT_ADMIN_CIC;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.SOLICITOR;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.SUPER_USER;
+import static uk.gov.hmcts.sptribs.ciccase.model.access.Permissions.CREATE_READ_UPDATE_DELETE;
+
+@Component
+@Slf4j
+public class CaseworkerRecordListing implements CCDConfig<CaseData, State, UserRole> {
+    public static final String CASEWORKER_RECORD_LISTING = "caseworker-record-listing";
+
+    @Override
+    public void configure(ConfigBuilder<CaseData, State, UserRole> configBuilder) {
+        PageBuilder pageBuilder = new PageBuilder(configBuilder
+            .event(CASEWORKER_RECORD_LISTING)
+            .forStates(POST_SUBMISSION_STATES)
+            .name("Record listing")
+            .showSummary()
+            .description("Record listing")
+            .showEventNotes()
+            .grant(CREATE_READ_UPDATE_DELETE, COURT_ADMIN_CIC, SUPER_USER)
+            .grantHistoryOnly(SOLICITOR));
+
+        addHearingTypeAndFormat(pageBuilder);
+    }
+
+    private void addHearingTypeAndFormat(PageBuilder pageBuilder) {
+        pageBuilder.page("hearingTypeAndFormat")
+            .label("hearingTypeAndFormatObj", "<h1>Hearing type and format</h1>")
+            .complex(CaseData::getRecordListing)
+            .mandatory(RecordListing::getHearingType)
+            .mandatory(RecordListing::getHearingFormat)
+            .done();
+    }
+}
