f8b9730f7a162be95bd307b08cb747444fe67fbb
sjamihmcts <santoshini.jami@hmcts.net>
2022-10-05 12:23:33 +0100

    ST-678: Record listing created message

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerRecordListing.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerRecordListing.java
@@ -3,14 +3,18 @@ package uk.gov.hmcts.sptribs.caseworker.event;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import uk.gov.hmcts.sptribs.caseworker.model.RecordListing;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
 import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
 
-import static uk.gov.hmcts.sptribs.ciccase.model.State.POST_SUBMISSION_STATES;
+import static java.lang.String.format;
+import static uk.gov.hmcts.sptribs.ciccase.model.State.*;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.COURT_ADMIN_CIC;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.SOLICITOR;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.SUPER_USER;
@@ -31,9 +35,36 @@ public class CaseworkerRecordListing implements CCDConfig<CaseData, State, UserR
             .description("Record listing")
             .showEventNotes()
             .grant(CREATE_READ_UPDATE_DELETE, COURT_ADMIN_CIC, SUPER_USER)
+            .aboutToSubmitCallback(this::aboutToSubmit)
+            .submittedCallback(this::submitted)
             .grantHistoryOnly(SOLICITOR));
 
         addHearingTypeAndFormat(pageBuilder);
+    }
+
+    public AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmit(
+        final CaseDetails<CaseData, State> details,
+        final CaseDetails<CaseData, State> beforeDetails
+    ) {
+        log.info("Caseworker record listing callback invoked for Case Id: {}", details.getId());
+
+        var caseData = details.getData();
+
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .data(caseData)
+            .state(CaseManagement)
+            .build();
+    }
+
+    public SubmittedCallbackResponse submitted(CaseDetails<CaseData, State> details,
+                                               CaseDetails<CaseData, State> beforeDetails) {
+        var data = details.getData();
+
+        return SubmittedCallbackResponse.builder()
+            .confirmationHeader(format("# Stay Added to Case"))
+            /*.confirmationHeader(format("# Listing Record Created %n## A notification has been sent via email to: Subject, Representative, Respondent %n##" +
+                " If any changes are made to this hearing, remember to make those changes in this listing record %n##"))*/
+            .build();
     }
 
     private void addHearingTypeAndFormat(PageBuilder pageBuilder) {
