f891126aa564437845d4f5887f9ad6bd09841b4a
ElifDenizEsea <61947470+ElifDenizEsea@users.noreply.github.com>
2022-10-26 12:02:27 +0100

    ST-782 Reinstate Case Check Answers (#142)
    Co-authored-by: hmctscivil <version1rhel@hmctscivil.mshome.net>

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/ReinstateCase.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/ReinstateCase.java
@@ -50,6 +50,7 @@ public class ReinstateCase implements CCDConfig<CaseData, State, UserRole> {
             .aboutToSubmitCallback(this::aboutToSubmit)
             .submittedCallback(this::closed)
             .showEventNotes()
+            .showSummary()
             .grant(CREATE_READ_UPDATE_DELETE, COURT_ADMIN_CIC, SUPER_USER)
             .grantHistoryOnly(SOLICITOR));
     }
