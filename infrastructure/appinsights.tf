dc63fbf510043643505c678363ab8d6a752af02e
Serkan Geçici <geciciserkan@gmail.com>
2022-10-28 15:50:28 +0100

    ST-385- build problems  (#149)
    * tf update
    
    * tf update

--- infrastructure/appinsights.tf
+++ infrastructure/appinsights.tf
@@ -1,10 +1,1 @@
-resource "azurerm_key_vault_secret" "app_insights_connection_string" {
-  name         = "app-insights-connection-string"
-  value        = data.azurerm_application_insights.ai.connection_string
-  key_vault_id = data.azurerm_key_vault.key_vault.id
 
-  content_type = "terraform-managed"
-  tags = merge(var.common_tags, {
-    "source" : "app insights ${data.azurerm_application_insights.ai.name}"
-  })
-}
