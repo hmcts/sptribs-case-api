0bd67f1ac1bb6fd52ae507cbc441e4e59bcd0140
Serkan Geçici <geciciserkan@gmail.com>
2022-10-28 15:15:28 +0100

    ST-385- build problems (#148)
    tf update

--- infrastructure/appinsights.tf
+++ infrastructure/appinsights.tf
@@ -1,13 +1,3 @@
-data "azurerm_application_insights" "ai" {
-  name                = "${var.product}-appinsights-${var.env}"
-  resource_group_name = "${var.product}-${var.env}"
-}
-
-data "azurerm_key_vault" "key_vault" {
-  name                = "${var.product}-${var.env}"
-  resource_group_name = "${var.product}-${var.env}"
-}
-
 resource "azurerm_key_vault_secret" "app_insights_connection_string" {
   name         = "app-insights-connection-string"
   value        = data.azurerm_application_insights.ai.connection_string
