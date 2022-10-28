ca7269e11510fd43c51fa7b70995ab52fd186f9c
osheaf01 <fergal.oshea@version1.com>
2022-10-28 17:05:19 +0100

    ST-815 Unit test for OcrDataField

---
+++ src/test/java/uk/gov/hmcts/sptribs/endpoint/model/input/OcrDataFieldTest.java
@@ -0,0 +1,36 @@
+package uk.gov.hmcts.sptribs.endpoint.model.input;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.sptribs.endpoint.data.ValidationStatus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.hmcts.sptribs.endpoint.data.ValidationStatus.*;
+
+@ExtendWith(MockitoExtension.class)
+class OcrDataFieldTest {
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @Test
+    void shouldOCRDataFieldJsonMapping() throws JsonProcessingException {
+        //Given
+        String json = "{\"name\":\"NAME\", \"value\":\"VALUE\"}";
+        OcrDataField constructedOcrDataField = new OcrDataField("NAME", "VALUE");
+
+        //When
+        OcrDataField ocrDataField = mapper.readValue(json, OcrDataField.class);
+
+        //Then
+        assertThat(ocrDataField.getName()).isEqualTo(constructedOcrDataField.getName());
+        assertThat(ocrDataField.getValue()).isEqualTo(constructedOcrDataField.getValue());
+    }
+
+
+}
