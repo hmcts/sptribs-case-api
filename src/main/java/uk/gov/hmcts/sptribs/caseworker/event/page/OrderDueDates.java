eb61597e81c1d4ca9dd5c2a90d54bcb8d77750fd
ElifDenizEsea <61947470+ElifDenizEsea@users.noreply.github.com>
2022-11-10 12:57:16 +0000

    ST-581 Send Order Add Due Date (#183)
    Co-authored-by: hmctscivil <version1rhel@hmctscivil.mshome.net>

---
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/page/OrderDueDates.java
@@ -0,0 +1,19 @@
+package uk.gov.hmcts.sptribs.caseworker.event.page;
+
+import uk.gov.hmcts.sptribs.caseworker.model.SendOrder;
+import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
+import uk.gov.hmcts.sptribs.common.ccd.CcdPageConfiguration;
+import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
+
+public class OrderDueDates implements CcdPageConfiguration {
+
+    @Override
+    public void addTo(PageBuilder pageBuilder) {
+
+        pageBuilder.page("orderDueDates")
+            .label("orderDueDates", "<h1>Add a due date\n</h1>")
+            .complex(CaseData::getSendOrder, "", "", "")
+            .optional(SendOrder::getDueDates)
+            .done();
+    }
+}
