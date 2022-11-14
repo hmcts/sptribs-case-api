c1b6fc60457d0cfddf6ab5b646b15aabb6a5b906
ElifDenizEsea <61947470+ElifDenizEsea@users.noreply.github.com>
2022-11-14 13:01:36 +0000

    ST-580 Select draft order (#188)
    Co-authored-by: hmctscivil <version1rhel@hmctscivil.mshome.net>

---
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/page/SendOrderAddDraftOrder.java
@@ -0,0 +1,27 @@
+package uk.gov.hmcts.sptribs.caseworker.event.page;
+
+
+import uk.gov.hmcts.sptribs.caseworker.model.DraftOrderCIC;
+import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
+import uk.gov.hmcts.sptribs.common.ccd.CcdPageConfiguration;
+import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SendOrderAddDraftOrder implements CcdPageConfiguration {
+
+    @Override
+    public void addTo(PageBuilder pageBuilder) {
+        String pageName = "selectDraftOrder";
+        Map<String, String> map = new HashMap<>();
+        map.put(pageName, "sendOrderOrderIssuingType = \"Issue and send an existing draft\"");
+        pageBuilder.page(pageName)
+            .label(pageName, "<h1>Select order\n</h1>")
+            .pageShowConditions(map)
+            .complex(CaseData::getDraftOrderCIC, "", "", "")
+            .readonly(DraftOrderCIC::getOrderTemplate)
+            .mandatory(DraftOrderCIC::getMainContentToBeEdited)
+            .done();
+    }
+}
