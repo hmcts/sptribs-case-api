9902be8e1e3b7cb5e2e9b4183821db360a544c4d
ElifDenizEsea <61947470+ElifDenizEsea@users.noreply.github.com>
2022-11-17 10:33:00 +0000

    ST-783  conformation page (#203)
    * ST-783  conformation page
    
    * ST-783 Message creation moved to methods
    
    Co-authored-by: hmctscivil <version1rhel@hmctscivil.mshome.net>

--- src/test/java/uk/gov/hmcts/sptribs/caseworker/event/ReinstateCaseTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/caseworker/event/ReinstateCaseTest.java
@@ -29,7 +29,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.hmcts.sptribs.caseworker.event.ReinstateCase.CASEWORKER_REINSTATE_CASE;
 import static uk.gov.hmcts.sptribs.testutil.ConfigTestUtil.createCaseDataConfigBuilder;
 import static uk.gov.hmcts.sptribs.testutil.ConfigTestUtil.getEventsFrom;
+import static uk.gov.hmcts.sptribs.testutil.TestConstants.SOLICITOR_ADDRESS;
+import static uk.gov.hmcts.sptribs.testutil.TestConstants.SUBJECT_ADDRESS;
+import static uk.gov.hmcts.sptribs.testutil.TestConstants.TEST_CASEWORKER_USER_EMAIL;
 import static uk.gov.hmcts.sptribs.testutil.TestConstants.TEST_CASE_ID;
+import static uk.gov.hmcts.sptribs.testutil.TestConstants.TEST_FIRST_NAME;
+import static uk.gov.hmcts.sptribs.testutil.TestConstants.TEST_SOLICITOR_EMAIL;
+import static uk.gov.hmcts.sptribs.testutil.TestConstants.TEST_SOLICITOR_NAME;
+import static uk.gov.hmcts.sptribs.testutil.TestConstants.TEST_SUBJECT_EMAIL;
 import static uk.gov.hmcts.sptribs.testutil.TestDataHelper.LOCAL_DATE_TIME;
 import static uk.gov.hmcts.sptribs.testutil.TestDataHelper.caseData;
 
@@ -55,7 +62,7 @@ class ReinstateCaseTest {
     }
 
     @Test
-    void shouldSuccessfullyReinstateTheCase() {
+    void shouldSuccessfullyReinstateTheCaseEmail() {
         //Given
         final CaseData caseData = caseData();
         final CICDocument document = CICDocument.builder()
@@ -68,6 +75,11 @@ class ReinstateCaseTest {
         CicCase cicCase = CicCase.builder()
             .reinstateReason(ReinstateReason.CASE_HAD_BEEN_CLOSED_IN_ERROR)
             .reinstateAdditionalDetail("some detail")
+            .fullName(TEST_FIRST_NAME)
+            .email(TEST_SUBJECT_EMAIL)
+            .respondantEmail(TEST_CASEWORKER_USER_EMAIL)
+            .representativeFullName(TEST_SOLICITOR_NAME)
+            .representativeEmailAddress(TEST_SOLICITOR_EMAIL)
             .notifyPartyRepresentative(Set.of(RepresentativeCIC.REPRESENTATIVE))
             .notifyPartyRespondent(Set.of(RespondentCIC.RESPONDENT))
             .notifyPartySubject(Set.of(SubjectCIC.SUBJECT))
@@ -98,5 +110,52 @@ class ReinstateCaseTest {
 
     }
 
+    @Test
+    void shouldSuccessfullyReinstateTheCasePost() {
+        //Given
+        final CaseData caseData = caseData();
+        final CICDocument document = CICDocument.builder()
+            .documentLink(Document.builder().build())
+            .documentEmailContent("content")
+            .build();
+        ListValue<CICDocument> documentListValue = new ListValue<>();
+        documentListValue.setValue(document);
+        CaseDocumentsCIC caseDocumentsCIC = CaseDocumentsCIC.builder().applicantDocumentsUploaded(List.of(documentListValue)).build();
+        CicCase cicCase = CicCase.builder()
+            .reinstateReason(ReinstateReason.CASE_HAD_BEEN_CLOSED_IN_ERROR)
+            .reinstateAdditionalDetail("some detail")
+            .fullName(TEST_FIRST_NAME)
+            .address(SUBJECT_ADDRESS)
+            .respondantEmail(TEST_CASEWORKER_USER_EMAIL)
+            .representativeFullName(TEST_SOLICITOR_NAME)
+            .representativeAddress(SOLICITOR_ADDRESS)
+            .notifyPartyRepresentative(Set.of(RepresentativeCIC.REPRESENTATIVE))
+            .notifyPartyRespondent(Set.of(RespondentCIC.RESPONDENT))
+            .notifyPartySubject(Set.of(SubjectCIC.SUBJECT))
+            .reinstateDocuments(caseDocumentsCIC)
+            .build();
+        caseData.setCicCase(cicCase);
+        final CaseDetails<CaseData, State> updatedCaseDetails = new CaseDetails<>();
+        final CaseDetails<CaseData, State> beforeDetails = new CaseDetails<>();
+        updatedCaseDetails.setData(caseData);
+        updatedCaseDetails.setId(TEST_CASE_ID);
+        updatedCaseDetails.setCreatedDate(LOCAL_DATE_TIME);
 
+        //When
+        AboutToStartOrSubmitResponse<CaseData, State> response =
+            reinstateCase.aboutToSubmit(updatedCaseDetails, beforeDetails);
+        SubmittedCallbackResponse responseReinstate =
+            reinstateCase.reinstated(updatedCaseDetails, beforeDetails);
+
+        //Then
+        assertThat(responseReinstate).isNotNull();
+        assertThat(responseReinstate.getConfirmationHeader()).contains("Subject");
+        assertThat(responseReinstate.getConfirmationHeader()).contains("Respondent");
+        assertThat(responseReinstate.getConfirmationHeader()).contains("Representative");
+        assertThat(response.getData().getCicCase().getReinstateReason()).isNotNull();
+        assertThat(response.getState()).isEqualTo(State.CaseManagement);
+        assertThat(response.getData().getCicCase().getReinstateDocuments().getApplicantDocumentsUploaded()
+            .get(0).getValue().getDocumentEmailContent()).isNotNull();
+
+    }
 }
