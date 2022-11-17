9405ff10786abfd7c641a0712ffc96e7893545e4
hmctscivil <version1rhel@hmctscivil.mshome.net>
2022-11-17 13:19:56 +0000

    ST-802 Send order send reminder

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerSendOrder.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerSendOrder.java
@@ -2,9 +2,6 @@ package uk.gov.hmcts.sptribs.caseworker.event;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import org.springframework.util.CollectionUtils;
-import org.springframework.util.ObjectUtils;
-import org.springframework.util.StringUtils;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
@@ -17,13 +14,14 @@ import uk.gov.hmcts.sptribs.caseworker.event.page.SendOrderOrderIssuingSelect;
 import uk.gov.hmcts.sptribs.caseworker.event.page.SendOrderSendReminder;
 import uk.gov.hmcts.sptribs.caseworker.event.page.SendOrderUploadOrder;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
-import uk.gov.hmcts.sptribs.ciccase.model.CicCase;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
 import uk.gov.hmcts.sptribs.common.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
 
 import static java.lang.String.format;
+import static uk.gov.hmcts.sptribs.caseworker.util.MessageUtil.getEmailMessage;
+import static uk.gov.hmcts.sptribs.caseworker.util.MessageUtil.getPostMessage;
 import static uk.gov.hmcts.sptribs.ciccase.model.State.AwaitingApplicant1Response;
 import static uk.gov.hmcts.sptribs.ciccase.model.State.AwaitingApplicant2Response;
 import static uk.gov.hmcts.sptribs.ciccase.model.State.AwaitingConditionalOrder;
@@ -117,45 +115,6 @@ public class CaseworkerSendOrder implements CCDConfig<CaseData, State, UserRole>
             .build();
     }
 
-    private StringBuilder getPostMessage(final CicCase cicCase) {
-        boolean post = false;
-        StringBuilder postMessage = new StringBuilder(100);
-        postMessage.append("It will be sent via post to:");
-        if (!CollectionUtils.isEmpty(cicCase.getNotifyPartySubject())
-            && !ObjectUtils.isEmpty(cicCase.getAddress())) {
-            postMessage.append("Subject, ");
-            post = true;
-        }
-        if (!CollectionUtils.isEmpty(cicCase.getNotifyPartyRepresentative())
-            && !ObjectUtils.isEmpty(cicCase.getRepresentativeAddress())) {
-            postMessage.append("Representative, ");
-            post = true;
-        }
-        if (post) {
-            return postMessage;
-        }
-        return null;
-    }
 
-    private StringBuilder getEmailMessage(final CicCase cicCase) {
-        final StringBuilder messageLine = new StringBuilder(100);
-        messageLine.append(" A notification will be sent via email to: ");
-
-        if (!CollectionUtils.isEmpty(cicCase.getNotifyPartySubject())
-            && StringUtils.hasText(cicCase.getEmail())) {
-            messageLine.append("Subject, ");
-            cicCase.setNotifyPartySubject(null);
-        }
-        if (!CollectionUtils.isEmpty(cicCase.getNotifyPartyRespondent())) {
-            messageLine.append("Respondent, ");
-            cicCase.setNotifyPartyRespondent(null);
-        }
-        if (!CollectionUtils.isEmpty(cicCase.getNotifyPartyRepresentative())
-            && StringUtils.hasText(cicCase.getRepresentativeEmailAddress())) {
-            messageLine.append("Representative, ");
-            cicCase.setNotifyPartyRepresentative(null);
-        }
-        return messageLine;
-    }
 
 }
