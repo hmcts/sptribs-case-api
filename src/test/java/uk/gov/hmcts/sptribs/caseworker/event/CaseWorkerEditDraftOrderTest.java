c7355a069dca226b306ee19ea7b9fa5a95f43be0
version1rhel <murad.alam@version1.com>
2022-11-23 11:54:59 +0000

    603

--- src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerEditDraftOrderTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerEditDraftOrderTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.Event;
+import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
@@ -14,6 +16,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.hmcts.sptribs.caseworker.event.CaseWorkerEditDraftOrder.CASEWORKER_EDIT_DRAFT_ORDER;
 import static uk.gov.hmcts.sptribs.testutil.ConfigTestUtil.createCaseDataConfigBuilder;
 import static uk.gov.hmcts.sptribs.testutil.ConfigTestUtil.getEventsFrom;
+import static uk.gov.hmcts.sptribs.testutil.TestConstants.TEST_CASE_ID;
+import static uk.gov.hmcts.sptribs.testutil.TestDataHelper.LOCAL_DATE_TIME;
+import static uk.gov.hmcts.sptribs.testutil.TestDataHelper.caseData;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -33,6 +38,26 @@ class CaseWorkerEditDraftOrderTest {
         assertThat(getEventsFrom(configBuilder).values())
             .extracting(Event::getId)
             .contains(CASEWORKER_EDIT_DRAFT_ORDER);
+    }
+
+    @Test
+    void shouldSuccessfullySaveDraftOrder() {
+        //Given
+        final CaseData caseData = caseData();
+        final CaseDetails<CaseData, State> updatedCaseDetails = new CaseDetails<>();
+        final CaseDetails<CaseData, State> beforeDetails = new CaseDetails<>();
+
+        updatedCaseDetails.setData(caseData);
+        updatedCaseDetails.setId(TEST_CASE_ID);
+        updatedCaseDetails.setCreatedDate(LOCAL_DATE_TIME);
+
+        //When
+
+
+        SubmittedCallbackResponse draftCreatedResponse = caseWorkerEditDraftOrder.draftCreated(updatedCaseDetails, beforeDetails);
+        //  Then
+        assertThat(draftCreatedResponse).isNotNull();
+
     }
 
 }
