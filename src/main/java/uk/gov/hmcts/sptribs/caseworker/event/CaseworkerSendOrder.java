408f56cb012d60748d899b47f42c1c8a0e014621
hmctscivil <version1rhel@hmctscivil.mshome.net>
2022-11-24 10:52:02 +0000

    ST-595 Message changed

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerSendOrder.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerSendOrder.java
@@ -91,7 +91,7 @@ public class CaseworkerSendOrder implements CCDConfig<CaseData, State, UserRole>
         StringBuilder postMessage = getPostMessage(cicCase);
         String message = "";
         if (null != postMessage && null != emailMessage) {
-            message = format("# Order sent  %n ## "
+            message = format("# Order sent  %n"
                 + " %s  %n  %s", emailMessage.substring(0, emailMessage.length() - 2), postMessage.substring(0, postMessage.length() - 2));
         } else if (null != emailMessage) {
             message = format("# Order sent %n ## "
