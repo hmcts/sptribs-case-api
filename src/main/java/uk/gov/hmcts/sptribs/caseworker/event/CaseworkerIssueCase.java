49edc72a874029933c6cffed227a5ebc7f9287c4
ElifDenizEsea <61947470+ElifDenizEsea@users.noreply.github.com>
2022-11-28 11:18:35 +0000

    ST-655 Issue case conformation page (#238)
    Co-authored-by: hmctscivil <version1rhel@hmctscivil.mshome.net>

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerIssueCase.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerIssueCase.java
@@ -1,9 +1,13 @@
 package uk.gov.hmcts.sptribs.caseworker.event;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import uk.gov.hmcts.sptribs.caseworker.event.page.IssueCaseAdditionalDocument;
 import uk.gov.hmcts.sptribs.caseworker.event.page.IssueCaseNotifyParties;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
@@ -12,6 +16,7 @@ import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
 import uk.gov.hmcts.sptribs.common.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
 
+import static java.lang.String.format;
 import static uk.gov.hmcts.sptribs.ciccase.model.State.CaseManagement;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.COURT_ADMIN_CIC;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.SOLICITOR;
@@ -35,10 +40,43 @@ public class CaseworkerIssueCase implements CCDConfig<CaseData, State, UserRole>
             .description("Issue case to respondent\"")
             .showEventNotes()
             .showSummary()
+            .aboutToSubmitCallback(this::aboutToSubmit)
+            .submittedCallback(this::issued)
             .grant(CREATE_READ_UPDATE_DELETE, COURT_ADMIN_CIC, SUPER_USER)
             .grantHistoryOnly(SOLICITOR));
         issueCaseAdditionalDocument.addTo(pageBuilder);
         issueCaseNotifyParties.addTo(pageBuilder);
     }
 
+    public AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmit(
+        final CaseDetails<CaseData, State> details,
+        final CaseDetails<CaseData, State> beforeDetails
+    ) {
+        var caseData = details.getData();
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .data(caseData)
+            .state(details.getState())
+            .build();
+    }
+
+    public SubmittedCallbackResponse issued(CaseDetails<CaseData, State> details,
+                                            CaseDetails<CaseData, State> beforeDetails) {
+        var cicCase = details.getData().getCicCase();
+        final StringBuilder messageLine2 = new StringBuilder(100);
+        messageLine2.append(" A notification will be sent  to: ");
+        if (!CollectionUtils.isEmpty(cicCase.getNotifyPartySubject())) {
+            messageLine2.append("Subject, ");
+        }
+        if (!CollectionUtils.isEmpty(cicCase.getNotifyPartyApplicant())) {
+            messageLine2.append("Applicant, ");
+        }
+        if (!CollectionUtils.isEmpty(cicCase.getNotifyPartyRepresentative())) {
+            messageLine2.append("Representative, ");
+        }
+
+        return SubmittedCallbackResponse.builder()
+            .confirmationHeader(format("# Case issued %n##  This case has now been issued. "
+                + ". %n## %s ", messageLine2.substring(0, messageLine2.length() - 2)))
+            .build();
+    }
 }
