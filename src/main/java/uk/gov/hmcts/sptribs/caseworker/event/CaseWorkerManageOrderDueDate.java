9f5da35f39a52c268d689e5517f735be1b4ad192
version1rhel <murad.alam@version1.com>
2022-11-30 15:35:56 +0000

    Added order templates names Managed order templated content

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerManageOrderDueDate.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerManageOrderDueDate.java
@@ -6,6 +6,7 @@ import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import uk.gov.hmcts.sptribs.caseworker.event.page.ManageSelectOrderTemplates;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
@@ -61,4 +62,10 @@ public class CaseWorkerManageOrderDueDate implements CCDConfig<CaseData, State, 
 
     }
 
+    public SubmittedCallbackResponse orderDatesManaged(CaseDetails<CaseData, State> details,
+                                                  CaseDetails<CaseData, State> beforeDetails) {
+        return SubmittedCallbackResponse.builder()
+            .confirmationHeader("# Due dates amended.")
+            .build();
+    }
 }
--- src/test/java/uk/gov/hmcts/sptribs/caseworker/CaseWorkerManageDueDatesTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/caseworker/CaseWorkerManageDueDatesTest.java
@@ -8,6 +8,7 @@ import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.Event;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import uk.gov.hmcts.sptribs.caseworker.event.CaseWorkerManageOrderDueDate;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
@@ -56,10 +57,10 @@ class CaseWorkerManageDueDatesTest {
         AboutToStartOrSubmitResponse<CaseData, State> response =
             caseWorkerManageOrderDueDate.aboutToSubmit(updatedCaseDetails, beforeDetails);
 
-        //  SubmittedCallbackResponse draftCreatedResponse =
-        //  caseWorkerManageOrderDueDate.orderDatesManaged(updatedCaseDetails, beforeDetails);
-        //  Then
-        // assertThat(draftCreatedResponse).isNotNull();
+        SubmittedCallbackResponse draftCreatedResponse =
+            caseWorkerManageOrderDueDate.orderDatesManaged(updatedCaseDetails, beforeDetails);
+        //Then
+        assertThat(draftCreatedResponse).isNotNull();
 
     }
 
