cddfbbb361233b6c47e2f450eb75a519ac111753
ElifDenizEsea <61947470+ElifDenizEsea@users.noreply.github.com>
2022-12-05 13:01:21 +0000

    ST-761 Select decision notice (#250)
    Co-authored-by: hmctscivil <version1rhel@hmctscivil.mshome.net>

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/model/CaseIssueFinalDecision.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/model/CaseIssueFinalDecision.java
@@ -1,12 +1,17 @@
 package uk.gov.hmcts.sptribs.caseworker.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import uk.gov.hmcts.ccd.sdk.api.CCD;
+import uk.gov.hmcts.sptribs.ciccase.model.FinalDecisionTemplate;
 import uk.gov.hmcts.sptribs.ciccase.model.access.CaseworkerWithCAAAccess;
 import uk.gov.hmcts.sptribs.ciccase.model.access.DefaultAccess;
 
+import static uk.gov.hmcts.ccd.sdk.type.FieldType.FixedList;
+
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class CaseIssueFinalDecision {
     @CCD(
@@ -14,4 +19,12 @@ public class CaseIssueFinalDecision {
         access = {DefaultAccess.class, CaseworkerWithCAAAccess.class}
     )
     private NoticeOption decisionNotice;
+
+    @CCD(
+        label = "Templates",
+        access = {DefaultAccess.class, CaseworkerWithCAAAccess.class},
+        typeOverride = FixedList,
+        typeParameterOverride = "FinalDecisionTemplate"
+    )
+    private FinalDecisionTemplate issueFinalDecisionTemplate;
 }
