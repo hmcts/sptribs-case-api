66ea8c9fc7f14c9b868a3c93a9ca076c9fc5b0be
ElifDenizEsea <61947470+ElifDenizEsea@users.noreply.github.com>
2022-12-08 10:14:24 +0000

    ST-580 Multiple orders (#259)
    * ST-580 Multiple orders
    
    * ST-580 Multiple orders
    
    Co-authored-by: hmctscivil <version1rhel@hmctscivil.mshome.net>
    Co-authored-by: Jeff Nayler <jeff.nayler@version1.com>

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/page/AmendOrderDueDates.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/page/AmendOrderDueDates.java
@@ -1,7 +1,7 @@
 package uk.gov.hmcts.sptribs.caseworker.event.page;
 
-import uk.gov.hmcts.sptribs.caseworker.model.SendOrder;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
+import uk.gov.hmcts.sptribs.ciccase.model.CicCase;
 import uk.gov.hmcts.sptribs.common.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
 
@@ -10,9 +10,9 @@ public class AmendOrderDueDates implements CcdPageConfiguration {
     @Override
     public void addTo(PageBuilder pageBuilder) {
         pageBuilder.page("amendOrderDueDates")
-            .complex(CaseData::getSendOrder, "", "", "")
-            .optional(SendOrder::getDueDates)
-            .optional(SendOrder::getMarkAsCompleted)
+            .complex(CaseData::getCicCase)
+            .optional(CicCase::getOrderDueDates)
+            .optional(CicCase::getOrderMarkAsCompleted)
             .done();
     }
 }
