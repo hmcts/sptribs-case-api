c7b401dca3572f1a2a1d492a44a94e33fac8cbef
Fergal O'Shea <94917860+osheaf01@users.noreply.github.com>
2022-12-09 10:59:34 +0000

    St 805 v2 (#267)
    * ST-805 Pushing separate service classes
    
    * ST-805 Renaming of Services and Case Types
    
    * ST-805 Renaming of Services and Case Types
    
    * ST-805 Renaming of Services and Case Types
    
    * Fixing compile issue
    
    * SEN to SEND
    
    * Moving classes
    
    * ST-805 SEN to SEND
    
    * ST-805 Adding service based CaseData subclasses
    
    * ST-805 Separating out caseTypes for SEN and DD and importing all definitions.
    
    * ST-805 Incorporating Alex's changes locally
    
    * ST-805 Adding mandatory config for all case types
    
    * ST-805 Adding events for all case types just to get bootWithCCD to run.
    
    * ST-805 Tidy up imports
    
    * ST-805 Refactor how jurisdiction is passed in - ST_CIC - and caseTypes - CIC, MH, PHL, CS, SEN, DD.
    
    * ST-805 Load the definitions by caseType
    
    * ST-805 Adding ST_CIC role. Refactoring spreadsheet names.
    
    * ST-805 Adding role for caseworker-st_cic
    
    * ST-805 Removing DIVORCE as caseType from ApplicationTab. Configuring each caseType with its own jurisdiction.
    
    * ST-805 Adding role-assignments for each caseType.
    
    * ST-805 Adding a unit test and tinkering with the roles.
    
    * ST-805 Changing jurisdiction to SEND.
    
    * ST-805 Changing jurisdiction to SEND.
    
    * ST-805 Adding DD caseType as SEND.
    
    * ST-805 Refactoring to clarify JURISDICTION_NAME, and refactor CcdCaseType references elsewhere to the CcdCaseType.CIC enum
    
    * ST-805 Adding unit tests
    
    * ST-805 Refactoring: removing redundant spreadsheets, pulling common code into a Helper class.
    
    * ST-805 Removing local debugging entries in settings.gradle and build.gradle.
    
    * ST-805 Fixing checkstyle errors
    
    * ST-805 Correct version of CCD Generator
    
    * ST-805 SonarLint changes to remove some code smells. Going to leave in commented code about shuttering, despite SonarLint
    
    * ST-805 Commenting out add-roles.sh to make same as master
    
    Co-authored-by: osheaf01 <fergal.oshea@version1.com>

--- src/main/java/uk/gov/hmcts/sptribs/ciccase/PrimaryHealthLists.java
+++ src/main/java/uk/gov/hmcts/sptribs/ciccase/PrimaryHealthLists.java
@@ -8,7 +8,7 @@ import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
-import uk.gov.hmcts.sptribs.ciccase.model.casetype.CriminalInjuriesCompensationData;
+import uk.gov.hmcts.sptribs.ciccase.model.casetype.PrimaryHealthListsData;
 import uk.gov.hmcts.sptribs.common.ccd.CcdCaseType;
 import uk.gov.hmcts.sptribs.common.ccd.CcdServiceCode;
 
@@ -18,20 +18,20 @@ import java.util.List;
 
 @Component
 @Slf4j
-public class CriminalInjuriesCompensation implements CCDConfig<CriminalInjuriesCompensationData, State, UserRole> {
+public class PrimaryHealthLists implements CCDConfig<PrimaryHealthListsData, State, UserRole> {
 
-    public static final String JURISDICTION_NAME = "CIC";
-    public static final String JURISDICTION = "ST_CIC";
+    public static final String JURISDICTION_NAME = "PHL";
+    public static final String JURISDICTION = "ST_PHL";
 
     @Autowired
     private List<CCDConfig<CaseData, State, UserRole>> cfgs;
 
     @Override
-    public void configure(final ConfigBuilder<CriminalInjuriesCompensationData, State, UserRole> configBuilder) {
+    public void configure(final ConfigBuilder<PrimaryHealthListsData, State, UserRole> configBuilder) {
         ConfigBuilderHelper.configureWithMandatoryConfig(configBuilder);
 
-        configBuilder.caseType(CcdCaseType.CIC.name(), "CIC Case Type", CcdCaseType.CIC.getDescription());
-        configBuilder.jurisdiction(JURISDICTION, JURISDICTION_NAME, CcdServiceCode.ST_CIC.getCcdServiceDescription());
+        configBuilder.caseType(CcdCaseType.PHL.name(), "PHL Case Type", CcdCaseType.PHL.getDescription());
+        configBuilder.jurisdiction(JURISDICTION, JURISDICTION_NAME, CcdServiceCode.ST_PHL.getCcdServiceDescription());
 
         ConfigBuilderHelper.configure(configBuilder, cfgs);
 
