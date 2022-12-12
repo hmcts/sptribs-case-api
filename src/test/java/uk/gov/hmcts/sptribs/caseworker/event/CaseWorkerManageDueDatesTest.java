fa1a6e7ca23faa527d6c39f6659cabc9141c94ba
ElifDenizEsea <61947470+ElifDenizEsea@users.noreply.github.com>
2022-12-12 16:25:43 +0000

    ST-604 Manage order due dates (#274)
    Co-authored-by: hmctscivil <version1rhel@hmctscivil.mshome.net>

--- src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerManageDueDatesTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerManageDueDatesTest.java
@@ -1,20 +1,32 @@
-package uk.gov.hmcts.sptribs.caseworker;
+package uk.gov.hmcts.sptribs.caseworker.event;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.Event;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.ccd.sdk.type.DynamicList;
+import uk.gov.hmcts.ccd.sdk.type.DynamicListElement;
+import uk.gov.hmcts.ccd.sdk.type.ListValue;
 import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
-import uk.gov.hmcts.sptribs.caseworker.event.CaseWorkerManageOrderDueDate;
+import uk.gov.hmcts.sptribs.caseworker.model.Order;
+import uk.gov.hmcts.sptribs.caseworker.service.OrderService;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
+import uk.gov.hmcts.sptribs.ciccase.model.CicCase;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
 
+import java.util.List;
+import java.util.UUID;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.sptribs.caseworker.event.CaseWorkerManageOrderDueDate.CASEWORKER_AMEND_DUE_DATE;
 import static uk.gov.hmcts.sptribs.testutil.ConfigTestUtil.createCaseDataConfigBuilder;
 import static uk.gov.hmcts.sptribs.testutil.ConfigTestUtil.getEventsFrom;
 import static uk.gov.hmcts.sptribs.testutil.TestConstants.TEST_CASE_ID;
@@ -27,6 +39,8 @@ class CaseWorkerManageDueDatesTest {
     @InjectMocks
     private CaseWorkerManageOrderDueDate caseWorkerManageOrderDueDate;
 
+    @Mock
+    private OrderService orderService;
 
     @Test
     void shouldAddConfigurationToConfigBuilder() {
@@ -39,7 +53,7 @@ class CaseWorkerManageDueDatesTest {
         //Then
         assertThat(getEventsFrom(configBuilder).values())
             .extracting(Event::getId)
-            .contains("Manage order due dates");
+            .contains(CASEWORKER_AMEND_DUE_DATE);
     }
 
     @Test
@@ -48,7 +62,15 @@ class CaseWorkerManageDueDatesTest {
         final CaseData caseData = caseData();
         final CaseDetails<CaseData, State> updatedCaseDetails = new CaseDetails<>();
         final CaseDetails<CaseData, State> beforeDetails = new CaseDetails<>();
-
+        final Order order = new Order();
+        ListValue<Order> listValue = new ListValue<>();
+        listValue.setValue(order);
+        listValue.setId("0");
+        final CicCase cicCase = CicCase.builder()
+            .orderList(List.of(listValue))
+            .orderDynamicList(getOrderList())
+            .build();
+        caseData.setCicCase(cicCase);
         updatedCaseDetails.setData(caseData);
         updatedCaseDetails.setId(TEST_CASE_ID);
         updatedCaseDetails.setCreatedDate(LOCAL_DATE_TIME);
@@ -61,9 +83,40 @@ class CaseWorkerManageDueDatesTest {
             caseWorkerManageOrderDueDate.orderDatesManaged(updatedCaseDetails, beforeDetails);
         //Then
         assertThat(draftCreatedResponse).isNotNull();
+        assertThat(response).isNotNull();
 
     }
 
+    @Test
+    void shouldRunAboutToStart() {
+        //Given
+        final CaseDetails<CaseData, State> updatedCaseDetails = new CaseDetails<>();
+        final CicCase cicCase = CicCase.builder().build();
+        final CaseData caseData = CaseData.builder()
+            .cicCase(cicCase)
+            .build();
+        updatedCaseDetails.setData(caseData);
+        when(orderService.getOrderDynamicList(any())).thenReturn(null);
+
+        //When
+        AboutToStartOrSubmitResponse<CaseData, State> response = caseWorkerManageOrderDueDate.aboutToStart(updatedCaseDetails);
+
+        //Then
+        assertThat(response).isNotNull();
+    }
+
+    private DynamicList getOrderList() {
+        final DynamicListElement listItem = DynamicListElement
+            .builder()
+            .label("0")
+            .code(UUID.randomUUID())
+            .build();
+        return DynamicList
+            .builder()
+            .value(listItem)
+            .listItems(List.of(listItem))
+            .build();
+    }
 
 }
 
