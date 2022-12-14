c80cace3526f5a7bba896a1fa84958912dff4e27
Fergal O'Shea <94917860+osheaf01@users.noreply.github.com>
2022-12-14 10:21:42 +0000

    St 932 v2 (#277)
    * ST-932 Fixing error in setting jurisdiction, and adding jurisdiction enum to avoid confusion with CcdServiceCode.
    
    * ST-932 Removing redundant static fields.
    
    * ST-932 Moving literal into static field.
    
    * ST-932 Moving literal into static field.
    
    Co-authored-by: osheaf01 <fergal.oshea@version1.com>

--- src/cftlib/java/uk/gov/hmcts/sptribs/cftlib/CftLibConfig.java
+++ src/cftlib/java/uk/gov/hmcts/sptribs/cftlib/CftLibConfig.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.CCDDefinitionGenerator;
 import uk.gov.hmcts.rse.ccd.lib.api.CFTLib;
 import uk.gov.hmcts.rse.ccd.lib.api.CFTLibConfigurer;
+import uk.gov.hmcts.sptribs.common.ccd.CcdJurisdiction;
 import uk.gov.hmcts.sptribs.common.ccd.CcdServiceCode;
 
 import java.io.File;
@@ -18,6 +19,7 @@ import java.util.List;
 @Component
 public class CftLibConfig implements CFTLibConfigurer {
 
+    private static final String BUILD_DEFINITIONS = "build/definitions/";
     @Value("Submitted")
     String state;
 
@@ -32,12 +34,18 @@ public class CftLibConfig implements CFTLibConfigurer {
             "TEST_CASE_WORKER_USER@mailinator.com",
             "TEST_SOLICITOR@mailinator.com",
             "divorce_as_caseworker_admin@mailinator.com")) {
-            lib.createProfile(p, CcdServiceCode.ST_CIC.name(), CcdServiceCode.ST_CIC.getCaseType().getCaseName(), state);
-            lib.createProfile(p, CcdServiceCode.ST_CS.name(), CcdServiceCode.ST_CS.getCaseType().getCaseName(), state);
-            lib.createProfile(p, CcdServiceCode.ST_MH.name(), CcdServiceCode.ST_MH.getCaseType().getCaseName(), state);
-            lib.createProfile(p, CcdServiceCode.ST_PHL.name(), CcdServiceCode.ST_PHL.getCaseType().getCaseName(), state);
-            lib.createProfile(p, CcdServiceCode.ST_SEN.name(), CcdServiceCode.ST_SEN.getCaseType().getCaseName(), state);
-            lib.createProfile(p, CcdServiceCode.ST_DD.name(), CcdServiceCode.ST_DD.getCaseType().getCaseName(), state);
+            lib.createProfile(p, CcdJurisdiction.CRIMINAL_INJURIES_COMPENSATION.getJurisdictionId(),
+                CcdServiceCode.ST_CIC.getCaseType().getCaseTypeName(), state);
+            lib.createProfile(p, CcdJurisdiction.CARE_STANDARDS.getJurisdictionId(),
+                CcdServiceCode.ST_CS.getCaseType().getCaseTypeName(), state);
+            lib.createProfile(p, CcdJurisdiction.MENTAL_HEALTH.getJurisdictionId(),
+                CcdServiceCode.ST_MH.getCaseType().getCaseTypeName(), state);
+            lib.createProfile(p, CcdJurisdiction.PRIMARY_HEALTH_LISTS.getJurisdictionId(),
+                CcdServiceCode.ST_PHL.getCaseType().getCaseTypeName(), state);
+            lib.createProfile(p, CcdJurisdiction.SPECIAL_EDUCATIONAL_NEEDS_AND_DISCRIMINATION.getJurisdictionId(),
+                CcdServiceCode.ST_SEN.getCaseType().getCaseTypeName(), state);
+            lib.createProfile(p, CcdJurisdiction.SPECIAL_EDUCATIONAL_NEEDS_AND_DISCRIMINATION.getJurisdictionId(),
+                CcdServiceCode.ST_DD.getCaseType().getCaseTypeName(), state);
         }
 
         lib.createRoles(
@@ -62,13 +70,13 @@ public class CftLibConfig implements CFTLibConfigurer {
             .getInputStream(), Charset.defaultCharset());
         lib.configureRoleAssignments(json);
 
-        configWriter.generateAllCaseTypesToJSON(new File("build/definitions"));
+        configWriter.generateAllCaseTypesToJSON(new File(BUILD_DEFINITIONS));
         // Load the JSON definitions for each caseType.
-        lib.importJsonDefinition(new File("build/definitions/" + CcdServiceCode.ST_CIC.getCaseType().getCaseName()));
-        lib.importJsonDefinition(new File("build/definitions/" + CcdServiceCode.ST_CS.getCaseType().getCaseName()));
-        lib.importJsonDefinition(new File("build/definitions/" + CcdServiceCode.ST_DD.getCaseType().getCaseName()));
-        lib.importJsonDefinition(new File("build/definitions/" + CcdServiceCode.ST_MH.getCaseType().getCaseName()));
-        lib.importJsonDefinition(new File("build/definitions/" + CcdServiceCode.ST_PHL.getCaseType().getCaseName()));
-        lib.importJsonDefinition(new File("build/definitions/" + CcdServiceCode.ST_SEN.getCaseType().getCaseName()));
+        lib.importJsonDefinition(new File(BUILD_DEFINITIONS + CcdServiceCode.ST_CIC.getCaseType().getCaseTypeName()));
+        lib.importJsonDefinition(new File(BUILD_DEFINITIONS + CcdServiceCode.ST_CS.getCaseType().getCaseTypeName()));
+        lib.importJsonDefinition(new File(BUILD_DEFINITIONS + CcdServiceCode.ST_DD.getCaseType().getCaseTypeName()));
+        lib.importJsonDefinition(new File(BUILD_DEFINITIONS + CcdServiceCode.ST_MH.getCaseType().getCaseTypeName()));
+        lib.importJsonDefinition(new File(BUILD_DEFINITIONS + CcdServiceCode.ST_PHL.getCaseType().getCaseTypeName()));
+        lib.importJsonDefinition(new File(BUILD_DEFINITIONS + CcdServiceCode.ST_SEN.getCaseType().getCaseTypeName()));
     }
 }
