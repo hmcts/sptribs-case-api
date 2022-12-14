97f105c7c032305fce2dfa9dfaf3a9316cd9e6a0
sjamihmcts <santoshini.jami@hmcts.net>
2022-12-14 22:51:09 +0000

    ST-803: Initial commit

---
+++ src/main/java/uk/gov/hmcts/sptribs/common/notification/CaseUnstayedNotification.java
@@ -0,0 +1,109 @@
+package uk.gov.hmcts.sptribs.common.notification;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.sptribs.caseworker.model.CaseStay;
+import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
+import uk.gov.hmcts.sptribs.ciccase.model.CicCase;
+import uk.gov.hmcts.sptribs.ciccase.model.NotificationResponse;
+import uk.gov.hmcts.sptribs.notification.EmailTemplateName;
+import uk.gov.hmcts.sptribs.notification.NotificationHelper;
+import uk.gov.hmcts.sptribs.notification.NotificationServiceCIC;
+import uk.gov.hmcts.sptribs.notification.PartiesNotification;
+import uk.gov.hmcts.sptribs.notification.model.NotificationRequest;
+
+import java.util.Map;
+
+import static uk.gov.hmcts.sptribs.common.CommonConstants.*;
+
+@Component
+@Slf4j
+public class CaseUnstayedNotification implements PartiesNotification {
+
+    @Autowired
+    private NotificationServiceCIC notificationService;
+
+    @Autowired
+    private NotificationHelper notificationHelper;
+
+    @Override
+    public void sendToSubject(final CaseData caseData, final String caseNumber) {
+        CicCase cicCase = caseData.getCicCase();
+        CaseStay caseStay = caseData.getCaseStay();
+
+        Map<String, Object> templateVars = notificationHelper.commonTemplateVars(cicCase, caseNumber);
+        templateVars.put(CONTACT_NAME, cicCase.getFullName());
+
+        if (cicCase.getContactPreferenceType().isEmail()) {
+            NotificationResponse notificationResponse = sendEmailNotification(cicCase.getEmail(), templateVars);
+            cicCase.setSubjectNotifyList(notificationResponse);
+        } else {
+            addCaseUnStayTemplateVars(caseStay, templateVars);
+            notificationHelper.addAddressTemplateVars(cicCase.getAddress(), templateVars);
+            sendLetterNotification(templateVars);
+        }
+    }
+
+    @Override
+    public void sendToApplicant(final CaseData caseData, final String caseNumber) {
+        CicCase cicCase = caseData.getCicCase();
+        CaseStay caseStay = caseData.getCaseStay();
+
+        Map<String, Object> templateVars = notificationHelper.commonTemplateVars(cicCase, caseNumber);
+        templateVars.put(CONTACT_NAME, cicCase.getApplicantFullName());
+
+        if (cicCase.getApplicantContactDetailsPreference().isEmail()) {
+            NotificationResponse notificationResponse = sendEmailNotification(cicCase.getApplicantEmailAddress(), templateVars);
+            cicCase.setAppNotificationResponse(notificationResponse);
+        } else {
+            addCaseUnStayTemplateVars(caseStay, templateVars);
+            notificationHelper.addAddressTemplateVars(cicCase.getApplicantAddress(), templateVars);
+            sendLetterNotification(templateVars);
+        }
+    }
+
+    @Override
+    public void sendToRepresentative(final CaseData caseData, final String caseNumber) {
+        CicCase cicCase = caseData.getCicCase();
+        CaseStay caseStay = caseData.getCaseStay();
+
+        Map<String, Object> templateVars = notificationHelper.commonTemplateVars(cicCase, caseNumber);
+        templateVars.put(CONTACT_NAME, cicCase.getRepresentativeFullName());
+
+        if (cicCase.getRepresentativeContactDetailsPreference().isEmail()) {
+            NotificationResponse notificationResponse = sendEmailNotification(cicCase.getRepresentativeEmailAddress(), templateVars);
+            cicCase.setRepNotificationResponse(notificationResponse);
+        } else {
+            addCaseUnStayTemplateVars(caseStay, templateVars);
+            notificationHelper.addAddressTemplateVars(cicCase.getRepresentativeAddress(), templateVars);
+            sendLetterNotification(templateVars);
+        }
+    }
+
+    private NotificationResponse sendEmailNotification(final String destinationAddress, final Map<String, Object> templateVars) {
+        NotificationRequest request = NotificationRequest.builder()
+            .destinationAddress(destinationAddress)
+            .template(EmailTemplateName.CASE_UNSTAYED_EMAIL)
+            .templateVars(templateVars)
+            .build();
+
+        notificationService.setNotificationRequest(request);
+        return notificationService.sendEmail();
+    }
+
+    private void sendLetterNotification(Map<String, Object> templateVarsLetter) {
+        NotificationRequest letterRequest = NotificationRequest.builder()
+            .template(EmailTemplateName.CASE_UNSTAYED_POST)
+            .templateVars(templateVarsLetter)
+            .build();
+
+        notificationService.setNotificationRequest(letterRequest);
+        notificationService.sendLetter();
+    }
+
+    private void addCaseUnStayTemplateVars(CaseStay caseStay, Map<String, Object> templateVars) {
+        templateVars.put(STAY_EXPIRATION_DATE, caseStay.getExpirationDate());
+        templateVars.put(STAY_REASON, caseStay.getStayReason().getLabel());
+    }
+}
