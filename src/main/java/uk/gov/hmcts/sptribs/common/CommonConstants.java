acce86417bb6f2c047fdbcd5b3bf1641c18dde5c
sjamihmcts <110464983+sjamihmcts@users.noreply.github.com>
2022-12-15 11:46:01 +0000

    Feat/st 793: Case stay Notification (#280)
    * ST-668: Initial changes for POC
    
    * ST-668: Working changes for sendEmail
    
    * ST-675: in progress sendLetter
    
    * ST-783: Extracted the email and letter logic to methods
    
    * ST-783: Added POC for sending email notification with attachment
    
    * ST-783: POC for multiple file attachments
    
    * ST-793: Initial changes
    
    * ST-831: Set up notification for case issued
    
    * ST-793: Added tests for email notification
    
    * Renaming UK_GOV_NOTIFY_API_KEY to UK_GOV_NOTIFY_API_KEY_1 and key to sptribskey
    
    * ST-793: Updated code for letter notification
    
    * ST-793: Fixed checkstyle issues
    
    * ST-793: Added default data to Address and stayAdditionalDetail fields
    
    * ST-793: Remove wildcard entries
    
    * ST-793: Use None Provided value for null/empty address fields. Also added midevent validation for country and postcode fields
    
    * ST-793: Added validations for Country and post code fields
    
    * ST-793: Fixed checkstyle errors
    
    * ST-793: Removed None Provided default value
    
    * ST-793: Fixed checkstyle issues
    
    * ST-793: Added tests to improve code coverage
    
    * ST-793: Removed wildcard imports
    
    Co-authored-by: Deepesh Sharma <deepesh.sharma@HMCTS.NET>

--- src/main/java/uk/gov/hmcts/sptribs/common/CommonConstants.java
+++ src/main/java/uk/gov/hmcts/sptribs/common/CommonConstants.java
@@ -10,6 +10,20 @@ public final class CommonConstants {
     public static final String CIC_CASE_REPRESENTATIVE_NAME = "CicCaseRepresentativeFullName";
     public static final String CIC_CASE_RESPONDENT_NAME = "CicCaseRespondentFullName";
 
+    public static final String STAY_EXPIRATION_DATE = "StayExpirationDate";
+    public static final String STAY_REASON = "stayStayReason";
+    public static final String STAY_ADDITIONAL_DETAIL = "stayAdditionalDetail";
+    public static final String NONE_PROVIDED = "None provided";
+    public static final String ADDRESS_LINE_1 = "address_line_1";
+    public static final String ADDRESS_LINE_2 = "address_line_2";
+    public static final String ADDRESS_LINE_3 = "address_line_3";
+    public static final String ADDRESS_LINE_4 = "address_line_4";
+    public static final String ADDRESS_LINE_5 = "address_line_5";
+    public static final String ADDRESS_LINE_6 = "address_line_6";
+    public static final String ADDRESS_LINE_7 = "address_line_7";
+
+    public static final String CIC = "Criminal Injuries Compensation Tribunal";
+
     private CommonConstants() {
     }
 }
