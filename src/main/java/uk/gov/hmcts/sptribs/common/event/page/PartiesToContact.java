442a7bcd3ce5544269b58e3967a01b87749e3937
version1rhel <murad.alam@version1.com>
2022-12-16 11:13:36 +0000

    Merge branch 'master' into feat/ST-664
    # Conflicts:
    #	src/main/java/uk/gov/hmcts/sptribs/ciccase/model/CicCase.java

--- src/main/java/uk/gov/hmcts/sptribs/common/event/page/PartiesToContact.java
+++ src/main/java/uk/gov/hmcts/sptribs/common/event/page/PartiesToContact.java
@@ -2,11 +2,15 @@ package uk.gov.hmcts.sptribs.common.event.page;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.sptribs.caseworker.model.ContactParties;
-import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
-import uk.gov.hmcts.sptribs.ciccase.model.CicCase;
+import uk.gov.hmcts.sptribs.ciccase.model.*;
 import uk.gov.hmcts.sptribs.common.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Component
@@ -16,7 +20,7 @@ public class PartiesToContact implements CcdPageConfiguration {
     @Override
     public void addTo(PageBuilder pageBuilder) {
         pageBuilder
-            .page("partiesToContact")
+            .page("partiesToContact",this::midEvent)
             .pageLabel("Contact Parties")
             .complex(CaseData::getCicCase)
             .optional(CicCase::getContactPartiesCIC)
@@ -25,7 +29,22 @@ public class PartiesToContact implements CcdPageConfiguration {
             .mandatory(ContactParties::getMessage)
             .done();
     }
+    public AboutToStartOrSubmitResponse<CaseData, State> midEvent(CaseDetails<CaseData, State> details,
+                                                                  CaseDetails<CaseData, State> detailsBefore) {
+        final CaseData data = details.getData();
+        final List<String> errors = new ArrayList<>();
 
+
+
+        if (null != data.getCicCase() &&  data.getCicCase().getContactPartiesCIC().size() == 0) {
+            errors.add("One field  is mandatory.");
+        }
+
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .data(data)
+            .errors(errors)
+            .build();
+    }
 
 }
 
--- src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerContactPartiesTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerContactPartiesTest.java
@@ -8,9 +8,10 @@ import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.Event;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
-import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
-import uk.gov.hmcts.sptribs.ciccase.model.State;
-import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
+import uk.gov.hmcts.sptribs.ciccase.model.*;
+import uk.gov.hmcts.sptribs.common.event.page.PartiesToContact;
+
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.hmcts.sptribs.caseworker.event.CaseWorkerContactParties.CASEWORKER_CONTACT_PARTIES;
@@ -24,6 +25,9 @@ import static uk.gov.hmcts.sptribs.testutil.TestDataHelper.caseData;
 class CaseworkerContactPartiesTest {
     @InjectMocks
     private CaseWorkerContactParties caseWorkerContactParties;
+
+    @InjectMocks
+    private PartiesToContact partiesToContact;
 
 
     @Test
@@ -59,6 +63,22 @@ class CaseworkerContactPartiesTest {
 
     }
 
+    @Test
+    void shouldSuccessfullyMoveToNextPage() {
+        final CaseData caseData = caseData();
+        CicCase cicCase = CicCase.builder().contactPartiesCIC(Set.of(ContactPartiesCIC.APPLICANTTOCONTACT)).build();
+        caseData.setCicCase(cicCase);
+        final CaseDetails<CaseData, State> updatedCaseDetails = new CaseDetails<>();
+        final CaseDetails<CaseData, State> beforeDetails = new CaseDetails<>();
+        updatedCaseDetails.setData(caseData);
+        updatedCaseDetails.setId(TEST_CASE_ID);
+        updatedCaseDetails.setCreatedDate(LOCAL_DATE_TIME);
+        AboutToStartOrSubmitResponse<CaseData, State> response =
+            partiesToContact.midEvent(updatedCaseDetails, beforeDetails);
+        assertThat(response).isNotNull();
+
+
+    }
 
 }
 
