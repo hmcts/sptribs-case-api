e64bee28989c9ad884d5ec552c491159bfb49756
muradalamv1 <107861321+muradalamv1@users.noreply.github.com>
2022-12-19 17:13:32 +0000

    Feat/st 680 (#291)
    Created confirmation page for contact parties

--- src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerContactPartiesTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerContactPartiesTest.java
@@ -8,6 +8,7 @@ import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.Event;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.CicCase;
 import uk.gov.hmcts.sptribs.ciccase.model.ContactPartiesCIC;
@@ -49,7 +50,7 @@ class CaseworkerContactPartiesTest {
     }
 
     @Test
-    void shouldSuccessfullySaveDraftOrder() {
+    void shouldSuccessfullySaveContactParties() {
         //Given
         final CaseData caseData = caseData();
         final CaseDetails<CaseData, State> updatedCaseDetails = new CaseDetails<>();
@@ -63,6 +64,9 @@ class CaseworkerContactPartiesTest {
         AboutToStartOrSubmitResponse<CaseData, State> response =
             caseWorkerContactParties.abutToSubmit(updatedCaseDetails, beforeDetails);
         assertThat(response).isNotNull();
+
+        SubmittedCallbackResponse contactPartiesResponse = caseWorkerContactParties.partiesContacted(updatedCaseDetails,beforeDetails);
+        assertThat(contactPartiesResponse).isNotNull();
 
 
     }
