cb974174863752670bc977cadd83041b82878977
hmctscivil <version1rhel@hmctscivil.mshome.net>
2022-12-19 14:43:44 +0000

    ST-762 Initial Commit

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/page/IssueFinalDecisionSelectTemplate.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/page/IssueFinalDecisionSelectTemplate.java
@@ -1,27 +1,80 @@
 package uk.gov.hmcts.sptribs.caseworker.event.page;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.ccd.sdk.type.Document;
 import uk.gov.hmcts.sptribs.caseworker.model.CaseIssueFinalDecision;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
+import uk.gov.hmcts.sptribs.ciccase.model.LanguagePreference;
+import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.common.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
+import uk.gov.hmcts.sptribs.document.CaseDataDocumentService;
+import uk.gov.hmcts.sptribs.document.content.FinalDecisionTemplateContent;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 
+import static uk.gov.hmcts.sptribs.document.DocumentConstants.FINAL_DECISION_FILE;
+
+@Slf4j
+@Component
 public class IssueFinalDecisionSelectTemplate implements CcdPageConfiguration {
+
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    @Autowired
+    private CaseDataDocumentService caseDataDocumentService;
+
+    @Autowired
+    private FinalDecisionTemplateContent finalDecisionTemplateContent;
+
 
     @Override
     public void addTo(PageBuilder pageBuilder) {
         String pageNameSelectTemplate = "issueFinalDecisionSelectTemplate";
         String pageNameUpload = "issueFinalDecisionUpload";
         Map<String, String> map = new HashMap<>();
-        map.put(pageNameSelectTemplate, "caseIssueFinalDecisionDecisionNotice = \"Create from a template\"");
-        map.put(pageNameUpload, "caseIssueFinalDecisionDecisionNotice = \"Upload from your computer\"");
-        pageBuilder.page(pageNameSelectTemplate)
+        map.put(pageNameSelectTemplate, "caseIssueFinalDecisionFinalDecisionNotice = \"Create from a template\"");
+        map.put(pageNameUpload, "caseIssueFinalDecisionFinalDecisionNotice = \"Upload from your computer\"");
+        pageBuilder.page(pageNameSelectTemplate, this::midEvent)
             .pageLabel("Select a template")
             .pageShowConditions(map)
             .complex(CaseData::getCaseIssueFinalDecision)
-            .mandatory(CaseIssueFinalDecision::getIssueFinalDecisionTemplate)
+            .mandatory(CaseIssueFinalDecision::getFinalDecisionTemplate)
             .done();
+    }
+
+    public AboutToStartOrSubmitResponse<CaseData, State> midEvent(
+        CaseDetails<CaseData, State> details,
+        CaseDetails<CaseData, State> detailsBefore
+    ) {
+
+        CaseData caseData = details.getData();
+        var finalDecision = caseData.getCaseIssueFinalDecision();
+
+        final Long caseId = details.getId();
+
+        final String filename = FINAL_DECISION_FILE + LocalDateTime.now().format(formatter);
+
+        Document generalOrderDocument = caseDataDocumentService.renderDocument(
+            finalDecisionTemplateContent.apply(caseData, caseId),
+            caseId,
+            finalDecision.getFinalDecisionTemplate().getId(),
+            LanguagePreference.ENGLISH,
+            filename
+        );
+
+        finalDecision.setFinalDecisionDraft(generalOrderDocument);
+        caseData.setCaseIssueFinalDecision(finalDecision);
+
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .data(caseData)
+            .build();
     }
 }
