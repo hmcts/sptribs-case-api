3564445ac3001321397d0edc8dce0611e443d6ba
Shailesh Walke <117093807+Shailesh-walke@users.noreply.github.com>
2022-12-21 15:00:37 +0000

    Create hearing summary journey start (#294)
    * Initial commit for create hearing summary
    
    * Changes for create Hearing Summary
    
    * removed unwanted jsonIgnoreProperties

---
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/page/HearingTypeAndFormat.java
@@ -0,0 +1,24 @@
+package uk.gov.hmcts.sptribs.caseworker.event.page;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
+import uk.gov.hmcts.sptribs.common.ccd.CcdPageConfiguration;
+import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
+
+@Slf4j
+@Component
+public class HearingTypeAndFormat implements CcdPageConfiguration {
+
+
+    @Override
+    public void addTo(PageBuilder pageBuilder) {
+        pageBuilder.page("hearingSummaryHearingTypeAndFormat")
+            .pageLabel("Hearing type and format")
+            .complex(CaseData::getCicCase)
+            .label("LabelHearingSummaryHearingTypeAndFormat", "")
+            .done();
+    }
+
+
+}
