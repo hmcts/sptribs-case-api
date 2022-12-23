64e882bb97ef13fad4b8109f2a508f7ee9fd3c18
tamir-b <105726368+tamir-b@users.noreply.github.com>
2022-12-23 12:33:04 +0000

    Feat/st 703 (#301)
    * Issue Decision choice
    
    * ST-703, issue decision
    
    * ST-703 create a decision notice
    
    Co-authored-by: Jeff Nayler <jeff.nayler@hmcts.net>

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerIssueDecision.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerIssueDecision.java
@@ -4,9 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.sptribs.caseworker.event.page.IssueDecisionNotice;
+import uk.gov.hmcts.sptribs.caseworker.event.page.IssueDecisionSelectTemplate;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
+import uk.gov.hmcts.sptribs.common.ccd.CcdPageConfiguration;
+import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
 
 import static uk.gov.hmcts.sptribs.ciccase.model.State.AwaitingOutcome;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.COURT_ADMIN_CIC;
@@ -19,17 +23,21 @@ import static uk.gov.hmcts.sptribs.ciccase.model.access.Permissions.CREATE_READ_
 public class CaseWorkerIssueDecision implements CCDConfig<CaseData, State, UserRole> {
     public static final String CASEWORKER_ISSUE_DECISION = "caseworker-issue-decision";
 
+    private static final CcdPageConfiguration issueDecisionNotice = new IssueDecisionNotice();
+    private static final CcdPageConfiguration issueDecisionSelectTemplate = new IssueDecisionSelectTemplate();
+
     @Override
     public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
-        /*PageBuilder pageBuilder = new PageBuilder(*/
-        configBuilder
+        PageBuilder pageBuilder = new PageBuilder(configBuilder
             .event(CASEWORKER_ISSUE_DECISION)
             .forStates(AwaitingOutcome)
-            .name("Issue decision")
-            .description("Issue decision")
+            .name("Issue a decision")
+            .description("Issue a decision")
             .showEventNotes()
             .grant(CREATE_READ_UPDATE_DELETE, COURT_ADMIN_CIC, SUPER_USER)
-            .grantHistoryOnly(SOLICITOR);//);
+            .grantHistoryOnly(SOLICITOR));
+        issueDecisionNotice.addTo(pageBuilder);
+        issueDecisionSelectTemplate.addTo(pageBuilder);
     }
 
 }
