b8af081bb1dda527d0f4d3903b27f9163ed91b6c
Shailesh Walke <117093807+Shailesh-walke@users.noreply.github.com>
2022-12-23 10:17:25 +0000

    Correct hearing date format (#306)
    * updated format of the hearing date list, removed additional hearing dates from dropdown list

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/service/HearingService.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/service/HearingService.java
@@ -2,13 +2,10 @@ package uk.gov.hmcts.sptribs.caseworker.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.type.DynamicList;
 import uk.gov.hmcts.ccd.sdk.type.DynamicListElement;
-import uk.gov.hmcts.ccd.sdk.type.ListValue;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
-import uk.gov.hmcts.sptribs.ciccase.model.HearingDate;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 
 import java.time.format.DateTimeFormatter;
@@ -19,6 +16,7 @@ import java.util.stream.Collectors;
 
 import static java.time.format.DateTimeFormatter.ofPattern;
 import static java.util.Locale.UK;
+import static uk.gov.hmcts.sptribs.caseworker.util.EventConstants.HYPHEN;
 import static uk.gov.hmcts.sptribs.caseworker.util.EventConstants.SPACE;
 
 @Service
@@ -27,20 +25,14 @@ public class HearingService {
     public DynamicList getHearingDateDynamicList(final CaseDetails<CaseData, State> caseDetails) {
         final DateTimeFormatter dateFormatter = ofPattern("dd MMM yyyy", UK);
         CaseData data = caseDetails.getData();
-        List<ListValue<HearingDate>> additionalHearingDateListValueList = data.getRecordListing().getAdditionalHearingDate();
-        String hearingDate = data.getRecordListing().getHearingDate().format(dateFormatter)
-            + SPACE
-            + data.getRecordListing().getHearingTime();
+        String hearingDate =
+            data.getRecordListing().getHearingType().getLabel()
+                + SPACE + HYPHEN + SPACE
+                + data.getRecordListing().getHearingDate().format(dateFormatter)
+                + SPACE
+                + data.getRecordListing().getHearingTime();
         List<String> hearingDateList = new ArrayList<>();
         hearingDateList.add(hearingDate);
-        if (!CollectionUtils.isEmpty(additionalHearingDateListValueList)) {
-            for (ListValue<HearingDate> additionalHearingDate : additionalHearingDateListValueList) {
-                String date = additionalHearingDate.getValue().getHearingVenueDate().format(dateFormatter)
-                    + SPACE
-                    + additionalHearingDate.getValue().getHearingVenueTime();
-                hearingDateList.add(date);
-            }
-        }
         List<DynamicListElement> dynamicListElements = hearingDateList
             .stream()
             .sorted()
