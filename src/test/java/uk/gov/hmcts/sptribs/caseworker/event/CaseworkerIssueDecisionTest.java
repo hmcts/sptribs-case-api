0fc82d1b0fcc4154657eed647ab3c742e6948615
tamir-b <tamir.bolor@hmcts.net>
2023-01-10 16:40:15 +0000

    reworked failing test cases 2

--- src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerIssueDecisionTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerIssueDecisionTest.java
@@ -9,7 +9,10 @@ import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.Event;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.sptribs.caseworker.model.CaseIssueDecision;
-import uk.gov.hmcts.sptribs.ciccase.model.*;
+import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
+import uk.gov.hmcts.sptribs.ciccase.model.ContactPartiesCIC;
+import uk.gov.hmcts.sptribs.ciccase.model.State;
+import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
 
 import java.util.HashSet;
 import java.util.Set;
