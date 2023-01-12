ad866f60046cfad40c08f55dff060ce64d5d7232
Paul Pearson <46532983+paul-pearson@users.noreply.github.com>
2023-01-12 17:51:51 +0000

    Feat/st 689 (#352)
    * ST-684 Add listing change reason page
    
    * ST-689 Edit record listing notify parties
    
    * ST-689 Add check for at least 1 party
    
    * ST-689 Unique label name

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/page/RecordNotifyParties.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/page/RecordNotifyParties.java
@@ -1,27 +1,51 @@
 package uk.gov.hmcts.sptribs.caseworker.event.page;
 
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.CicCase;
+import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.common.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static uk.gov.hmcts.sptribs.caseworker.util.CheckRequiredUtil.checkNullRecordSubjectRepresentativeRespondent;
+
 public class RecordNotifyParties implements CcdPageConfiguration {
+
+    private static final String NEVER_SHOW = "recordHearingType=\"NEVER_SHOW\"";
 
     @Override
     public void addTo(PageBuilder pageBuilder) {
-        pageBuilder.page("recordListingNotifyPage")
-            .label("recordListingNotifyPageLabel", "<h1>Notify page</h1>")
+        pageBuilder.page("recordListingNotifyPage", this::midEvent)
+            .pageLabel("Notify parties")
+            .label("labelNotifyParties", "")
             .complex(CaseData::getCicCase)
-            .label("recordListingNotifyPageMessage", "Which parties would be notified of this listing?")
-            .readonlyWithLabel(CicCase::getFullName, " ")
+            .label("recordListingNotifyPageMessage", "Which parties should be notified about this listing?")
+            .readonly(CicCase::getFullName, NEVER_SHOW)
             .optional(CicCase::getRecordNotifyPartySubject, "cicCaseFullName!=\"\" ")
             .label("recordListingNotifyPageRepresentative", "")
-            .readonlyWithLabel(CicCase::getRepresentativeFullName, " ")
+            .readonly(CicCase::getRepresentativeFullName, NEVER_SHOW)
             .optional(CicCase::getRecordNotifyPartyRepresentative, "cicCaseRepresentativeFullName!=\"\" ")
             .label("recordListingNotifyPageRespondent", "")
-            .readonlyWithLabel(CicCase::getRespondantName, " ")
+            .readonly(CicCase::getRespondantName, NEVER_SHOW)
             .optional(CicCase::getRecordNotifyPartyRespondent, "cicCaseRespondantName!=\"\" ")
             .done();
     }
 
+    public AboutToStartOrSubmitResponse<CaseData, State> midEvent(CaseDetails<CaseData, State> details,
+                                                                  CaseDetails<CaseData, State> detailsBefore) {
+        final CaseData data = details.getData();
+        final List<String> errors = new ArrayList<>();
+
+        if (checkNullRecordSubjectRepresentativeRespondent(data)) {
+            errors.add("One recipient must be selected.");
+        }
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .data(data)
+            .errors(errors)
+            .build();
+    }
 }
