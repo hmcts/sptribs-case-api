24d16e3465b87f6bf9908253ab857f623f7d13d1
tamir-b <tamir.bolor@hmcts.net>
2023-01-13 15:39:57 +0000

    fixing pipeline failure 3

--- src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerIssueDecisionTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerIssueDecisionTest.java
@@ -8,6 +8,7 @@ import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.Event;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import uk.gov.hmcts.sptribs.caseworker.model.CaseIssueDecision;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.ContactPartiesCIC;
@@ -63,4 +64,26 @@ class CaseworkerIssueDecisionTest {
         assertThat(response.getState()).isEqualTo(CaseManagement);
         assertThat(response.getData().getCaseIssueDecision().getRecipients().contains(ContactPartiesCIC.SUBJECTTOCONTACT));
     }
+
+    @Test
+    void shouldShowCorrectMessageWhenSubmitted() {
+        //Given
+        final CaseDetails<CaseData, State> details = new CaseDetails<>();
+        final CaseDetails<CaseData, State> beforeDetails = new CaseDetails<>();
+        final CaseData caseData = caseData();
+        //final CaseIssueDecision decision = caseData.getCaseIssueDecision();
+        final CaseIssueDecision decision = new CaseIssueDecision();
+        Set<ContactPartiesCIC> subjectSet = new HashSet<>();
+        subjectSet.add(ContactPartiesCIC.SUBJECTTOCONTACT);
+        decision.setRecipients(subjectSet);
+        caseData.setCaseIssueDecision(decision);
+        details.setData(caseData);
+
+        //When
+        SubmittedCallbackResponse response = issueDecision.submitted(details, beforeDetails);
+
+        //Then
+        assertThat(response.getConfirmationHeader()).contains("# Decision notice issued");
+    }
+
 }
