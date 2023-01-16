4372a075bdc6d0153a2506667b56fceba39095a7
sjamihmcts <santoshini.jami@hmcts.net>
2023-01-16 16:48:15 +0000

    ST-844: Removed import

--- src/main/java/uk/gov/hmcts/sptribs/CaseApiApplication.java
+++ src/main/java/uk/gov/hmcts/sptribs/CaseApiApplication.java
@@ -13,7 +13,6 @@ import uk.gov.hmcts.reform.ccd.client.CaseAssignmentApi;
 import uk.gov.hmcts.reform.ccd.client.CaseUserApi;
 import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
 import uk.gov.hmcts.reform.ccd.client.CoreCaseDataClientAutoConfiguration;
-import uk.gov.hmcts.reform.ccd.document.am.feign.CaseDocumentClientApi;
 import uk.gov.hmcts.reform.idam.client.IdamApi;
 import uk.gov.hmcts.sptribs.document.CaseDocumentClient;
 import uk.gov.hmcts.sptribs.document.DocAssemblyClient;
