9e6f0935e5fc62e7f2f604da558c0983b6519a0a
Deepesh Sharma <deepesh.sharma@HMCTS.NET>
2023-01-19 15:11:29 +0000

    ST-833: fixed codestyle issue

--- src/main/java/uk/gov/hmcts/sptribs/notification/NotificationHelper.java
+++ src/main/java/uk/gov/hmcts/sptribs/notification/NotificationHelper.java
@@ -123,6 +123,6 @@ public class NotificationHelper {
 
     private boolean isFaceToFaceFormat(RecordListing recordListing) {
 
-        return (null != recordListing.getHearingFormat() && recordListing.getHearingFormat().equals(HearingFormat.FACE_TO_FACE));
+        return null != recordListing.getHearingFormat() && recordListing.getHearingFormat().equals(HearingFormat.FACE_TO_FACE);
     }
 }
