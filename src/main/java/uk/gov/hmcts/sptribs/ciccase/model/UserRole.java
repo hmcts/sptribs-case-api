507cdf9c9eecb9e92061b702dac95270130642fc
ATedy <weditedy@gmail.com>
2023-01-19 12:39:32 +0000

    cvs is populating right data and roles name fixed

--- src/main/java/uk/gov/hmcts/sptribs/ciccase/model/UserRole.java
+++ src/main/java/uk/gov/hmcts/sptribs/ciccase/model/UserRole.java
@@ -11,18 +11,18 @@ public enum UserRole implements HasRole {
 
     // Common User roles
     //TODO: update : SUPER_USER, SOLICITOR before prod deploy
-    SUPER_USER("IDAM:caseworker-divorce-superuser", "CRU"),
-    SYSTEMUPDATE("IDAM:caseworker-divorce-systemupdate", "CRU"),
-    SOLICITOR("IDAM:caseworker-divorce-solicitor", "CRU"),
+    SUPER_USER("caseworker-divorce-superuser", "CRU"),
+    SYSTEMUPDATE("caseworker-divorce-systemupdate", "CRU"),
+    SOLICITOR("caseworker-divorce-solicitor", "CRU"),
     CREATOR("[CREATOR]", "CRU"),
 
     // CIC User roles
-    SUPER_USER_CIC("IDAM:caseworker-sptribs-superuser", "CRU"),
-    COURT_ADMIN_CIC("IDAM:caseworker-sptribs-cic-courtadmin", "CRU"),
-    CASE_OFFICER_CIC("IDAM:caseworker-sptribs-cic-caseofficer", "CRU"),
-    DISTRICT_REGISTRAR_CIC("IDAM:caseworker-sptribs-cic-districtregistrar", "CRU"),
-    DISTRICT_JUDGE_CIC("IDAM:caseworker-sptribs-cic-districtjudge", "CRU"),
-    RESPONDENT_CIC("IDAM:caseworker-sptribs-cic-respondent", "CRU"),
+    SUPER_USER_CIC("caseworker-sptribs-superuser", "CRU"),
+    COURT_ADMIN_CIC("caseworker-sptribs-cic-courtadmin", "CRU"),
+    CASE_OFFICER_CIC("caseworker-sptribs-cic-caseofficer", "CRU"),
+    DISTRICT_REGISTRAR_CIC("caseworker-sptribs-cic-districtregistrar", "CRU"),
+    DISTRICT_JUDGE_CIC("caseworker-sptribs-cic-districtjudge", "CRU"),
+    RESPONDENT_CIC("caseworker-sptribs-cic-respondent", "CRU"),
 
     HMCTS_STAFF("hmcts-staff", "CRU"),
     HMCTS_JUDICIARY("hmcts-judiciary", "CRU"),
@@ -32,19 +32,19 @@ public enum UserRole implements HasRole {
     private final String role;
     private final String caseTypePermissions;
 
-//    public static String getAccessProfileName(UserRole userRole) {
-//        for (UserRole role : UserRole.values()) {
-//            if (role.equals(userRole)) {
-//                if (role.getRole().startsWith("IDAM:")) {
-//                    String tempString = role.getRole();
-//                    return tempString.substring("IDAM:".length());
-//                }
-//                else {
-//                    return role.getRole();
-//                }
-//            }
-//        }
-//        return null;
-//    }
+    public static String getAccessProfileName(UserRole userRole) {
+        for (UserRole role : UserRole.values()) {
+            if (role.equals(userRole)) {
+                if (role.getRole().startsWith("IDAM:")) {
+                    String tempString = role.getRole();
+                    return tempString.substring("IDAM:".length());
+                }
+                else {
+                    return role.getRole();
+                }
+            }
+        }
+        return null;
+    }
 
 }
