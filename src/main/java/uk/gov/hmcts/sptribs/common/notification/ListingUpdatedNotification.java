c5dcf1b32f8a81d32aa6d3dbb997159773347a5e
Deepesh Sharma <deepesh.sharma@HMCTS.NET>
2023-01-19 11:45:04 +0000

    ST-833: Refactored code to fix sonar code duplicate issue

--- src/main/java/uk/gov/hmcts/sptribs/common/notification/ListingUpdatedNotification.java
+++ src/main/java/uk/gov/hmcts/sptribs/common/notification/ListingUpdatedNotification.java
@@ -32,7 +32,7 @@ public class ListingUpdatedNotification implements PartiesNotification {
         CicCase cicCase = caseData.getCicCase();
         final Map<String, Object> templateVarsSubject = notificationHelper.getSubjectCommonVars(caseNumber, cicCase);
         RecordListing recordListing = caseData.getRecordListing();
-        setRecordingTemplateVars(templateVarsSubject, recordListing);
+        notificationHelper.setRecordingTemplateVars(templateVarsSubject, recordListing);
         if (cicCase.getContactPreferenceType().isEmail()) {
             // Send Email
             NotificationResponse notificationResponse =  sendEmailNotification(templateVarsSubject,
@@ -52,7 +52,7 @@ public class ListingUpdatedNotification implements PartiesNotification {
         final Map<String, Object> templateVarsRepresentative  = notificationHelper.getRepresentativeCommonVars(caseNumber, cicCase);
         templateVarsRepresentative.put(CommonConstants.CIC_CASE_REPRESENTATIVE_NAME, cicCase.getRepresentativeFullName());
         RecordListing recordListing = caseData.getRecordListing();
-        setRecordingTemplateVars(templateVarsRepresentative, recordListing);
+        notificationHelper.setRecordingTemplateVars(templateVarsRepresentative, recordListing);
 
         if (cicCase.getRepresentativeContactDetailsPreference().isEmail()) {
             // Send Email
@@ -73,7 +73,7 @@ public class ListingUpdatedNotification implements PartiesNotification {
         final Map<String, Object> templateVarsRespondent = notificationHelper.getRespondentCommonVars(caseNumber, cicCase);
         templateVarsRespondent.put(CommonConstants.CIC_CASE_RESPONDENT_NAME, caseData.getCicCase().getRespondantName());
         RecordListing recordListing = caseData.getRecordListing();
-        setRecordingTemplateVars(templateVarsRespondent, recordListing);
+        notificationHelper.setRecordingTemplateVars(templateVarsRespondent, recordListing);
         // Send Email
         NotificationResponse notificationResponse = sendEmailNotification(templateVarsRespondent,
             cicCase.getRespondantEmail(), TemplateName.LISTING_UPDATED_CITIZEN_EMAIL);
@@ -93,38 +93,5 @@ public class ListingUpdatedNotification implements PartiesNotification {
         NotificationRequest letterRequest = notificationHelper.buildLetterNotificationRequest(templateVarsLetter, emailTemplateName);
         notificationService.setNotificationRequest(letterRequest);
         return notificationService.sendLetter();
-    }
-
-    private void setRecordingTemplateVars(Map<String, Object> templateVars, RecordListing recordListing) {
-        templateVars.put(CommonConstants.CIC_CASE_HEARING_TYPE, recordListing.getHearingType());
-        templateVars.put(CommonConstants.CIC_CASE_HEARING_DATE, recordListing.getHearingDate());
-        templateVars.put(CommonConstants.CIC_CASE_HEARING_TIME, recordListing.getHearingTime());
-
-        if (null != recordListing.getHearingVenueName()) {
-            templateVars.put(CommonConstants.CIC_CASE_HEARING_VENUE, recordListing.getHearingVenueName());
-        } else {
-            templateVars.put(CommonConstants.CIC_CASE_HEARING_VENUE, CommonConstants.CIC_CASE_RECORD_REMOTE_HEARING);
-        }
-        templateVars.put(CommonConstants.CIC_CASE_HEARING_INFO, recordListing.getImportantInfoDetails());
-
-        if (null != recordListing.getVideoCallLink()) {
-            templateVars.put(CommonConstants.CIC_CASE_RECORD_HEARING_FORMAT_VIDEO, true);
-            templateVars.put(CommonConstants.CIC_CASE_RECORD_VIDEO_CALL_LINK, recordListing.getVideoCallLink());
-        } else {
-            templateVars.put(CommonConstants.CIC_CASE_RECORD_HEARING_FORMAT_VIDEO, false);
-            templateVars.put(CommonConstants.CIC_CASE_RECORD_VIDEO_CALL_LINK, " ");
-        }
-        if (null != recordListing.getConferenceCallNumber()) {
-            templateVars.put(CommonConstants.CIC_CASE_RECORD_FORMAT_TEL, true);
-            templateVars.put(CommonConstants.CIC_CASE_RECORD_CONF_CALL_NUM, recordListing.getConferenceCallNumber());
-        } else {
-            templateVars.put(CommonConstants.CIC_CASE_RECORD_FORMAT_TEL, false);
-            templateVars.put(CommonConstants.CIC_CASE_RECORD_CONF_CALL_NUM, " ");
-        }
-        if (null != recordListing.getHearingFormat() && recordListing.getHearingFormat().equals(HearingFormat.FACE_TO_FACE)) {
-            templateVars.put(CommonConstants.CIC_CASE_RECORD_HEARING_1FACE_TO_FACE, true);
-        } else {
-            templateVars.put(CommonConstants.CIC_CASE_RECORD_HEARING_1FACE_TO_FACE, false);
-        }
     }
 }
