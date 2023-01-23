3e6424f7c1eb5d96e094890fbe15f47ce4b4248f
muradalamv1 <107861321+muradalamv1@users.noreply.github.com>
2023-01-23 16:31:37 +0000

    Feat/st 940 (#395)
    * Created dropdown for edit draft order
    
    * Merge branch 'master' into feat/ST-940
    
    # Conflicts:
    #	src/main/java/uk/gov/hmcts/sptribs/ciccase/model/CicCase.java
    
    * Committed all
    
    * Merge branch 'master' into feat/ST-940
    
    # Conflicts:
    #	src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerEditRecordListing.java
    #	src/main/java/uk/gov/hmcts/sptribs/caseworker/helper/RecordListHelper.java
    
    * Created a xTemplates drop down list with current date for EditDraftOrder
    
    * Created a xTemplates drop down list with current date for EditDraftOrder
    
    * Merge branch 'master' into feat/ST-940
    
    # Conflicts:
    #	src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerIssueDecision.java
    #	src/main/java/uk/gov/hmcts/sptribs/common/event/page/HearingVenues.java
    #	src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerIssueDecisionTest.java
    
    * 553 docmosis changes
    
    * 553 docmosis changes
    
    * created templates for create and edit draft order

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerEditDraftOrder.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerEditDraftOrder.java
@@ -56,6 +56,7 @@ public class CaseWorkerEditDraftOrder implements CCDConfig<CaseData, State, User
 
     }
 
+
     public AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmit(
         final CaseDetails<CaseData, State> details,
         final CaseDetails<CaseData, State> beforeDetails
