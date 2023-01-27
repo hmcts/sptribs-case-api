e8aa0043ff129579023bfb4833c06d1b79f0a7ca
sjamihmcts <santoshini.jami@hmcts.net>
2023-01-27 11:35:13 +0000

    ST-844: Unused imports

--- src/main/java/uk/gov/hmcts/sptribs/common/notification/DecisionIssuedNotification.java
+++ src/main/java/uk/gov/hmcts/sptribs/common/notification/DecisionIssuedNotification.java
@@ -21,7 +21,6 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-import java.util.stream.Collectors;
 import javax.servlet.http.HttpServletRequest;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
