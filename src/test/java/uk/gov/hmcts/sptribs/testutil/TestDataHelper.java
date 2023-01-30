2bb709360d5e7eafd2736fa295450526742e5db2
Paul Pearson <46532983+paul-pearson@users.noreply.github.com>
2023-01-30 14:47:14 +0000

    St 1033 (#446)
    * ST-1033 Delete unused classes from endpoint package
    
    * ST-1033 Delete tests referencing deleted classes

--- src/test/java/uk/gov/hmcts/sptribs/testutil/TestDataHelper.java
+++ src/test/java/uk/gov/hmcts/sptribs/testutil/TestDataHelper.java
@@ -6,10 +6,7 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import feign.FeignException;
 import feign.Request;
 import feign.Response;
-import uk.gov.hmcts.ccd.sdk.type.Fee;
-import uk.gov.hmcts.ccd.sdk.type.KeyValue;
 import uk.gov.hmcts.ccd.sdk.type.ListValue;
-import uk.gov.hmcts.ccd.sdk.type.OrderSummary;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.idam.client.models.User;
@@ -23,7 +20,6 @@ import uk.gov.hmcts.sptribs.ciccase.model.HearingFormat;
 import uk.gov.hmcts.sptribs.ciccase.model.HearingType;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.common.ccd.CcdCaseType;
-import uk.gov.hmcts.sptribs.endpoint.data.OcrDataValidationRequest;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -35,7 +31,6 @@ import java.util.Map;
 
 import static feign.Request.HttpMethod.GET;
 import static java.nio.charset.StandardCharsets.UTF_8;
-import static java.util.Collections.singletonList;
 import static org.apache.commons.lang3.StringUtils.EMPTY;
 import static uk.gov.hmcts.sptribs.testutil.TestConstants.HEARING_DATE_1;
 import static uk.gov.hmcts.sptribs.testutil.TestConstants.HEARING_DATE_2;
@@ -154,57 +149,6 @@ public class TestDataHelper {
             .caseTypeId(CcdCaseType.CIC.getCaseTypeName())
             .build();
     }
-
-    public static ListValue<Fee> getFeeListValue() {
-        return ListValue
-            .<Fee>builder()
-            .value(Fee
-                .builder()
-                .amount("550")
-                .description("fees for divorce")
-                .code("FEE002")
-                .build()
-            )
-            .build();
-    }
-
-    public static OrderSummary orderSummaryWithFee() {
-        return OrderSummary
-            .builder()
-            .paymentTotal("55000")
-            .fees(singletonList(getFeeListValue()))
-            .build();
-    }
-
-
-
-    public static OcrDataValidationRequest ocrDataValidationRequest() {
-        return OcrDataValidationRequest.builder()
-            .ocrDataFields(
-                List.of(
-                    KeyValue.builder()
-                        .key("applicant1Name")
-                        .value("bob")
-                        .build())
-            )
-            .build();
-    }
-
-
-
-
-
-    public static KeyValue populateKeyValue(String key, String value) {
-        return KeyValue.builder()
-            .key(key)
-            .value(value)
-            .build();
-    }
-
-
-
-
-
 
     public static User getUser() {
         UserDetails userDetails = UserDetails
