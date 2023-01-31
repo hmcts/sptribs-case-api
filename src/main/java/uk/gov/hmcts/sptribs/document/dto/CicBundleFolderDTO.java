c2a6b837684e3770e186953181ce20707ed474c6
Deepesh Sharma <deepesh.sharma@HMCTS.NET>
2023-01-31 16:02:43 +0000

    ST-855: fixed review comments

--- src/main/java/uk/gov/hmcts/sptribs/document/dto/CicBundleFolderDTO.java
+++ src/main/java/uk/gov/hmcts/sptribs/document/dto/CicBundleFolderDTO.java
@@ -2,10 +2,14 @@ package uk.gov.hmcts.sptribs.document.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
+@Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CicBundleFolderDTO {
 
@@ -13,37 +17,4 @@ public class CicBundleFolderDTO {
     private List<CicValue<CicBundleDocumentDTO>> documents = new ArrayList<>();
     private List<CicValue<CicBundleFolderDTO>> folders = new ArrayList<>();
     private int sortIndex;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<CicValue<CicBundleDocumentDTO>> getDocuments() {
-        return documents;
-    }
-
-    public void setDocuments(List<CicValue<CicBundleDocumentDTO>> documents) {
-        this.documents = documents;
-    }
-
-    @JsonInclude(JsonInclude.Include.NON_EMPTY)
-    public List<CicValue<CicBundleFolderDTO>> getFolders() {
-        return folders;
-    }
-
-    public void setFolders(List<CicValue<CicBundleFolderDTO>> folders) {
-        this.folders = folders;
-    }
-
-    public int getSortIndex() {
-        return sortIndex;
-    }
-
-    public void setSortIndex(int sortIndex) {
-        this.sortIndex = sortIndex;
-    }
 }
