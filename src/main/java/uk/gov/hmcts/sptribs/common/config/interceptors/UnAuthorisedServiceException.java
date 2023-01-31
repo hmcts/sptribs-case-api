3456af1a5879be46c11ea1ef9340a6f3017cf539
Fergal O'Shea <94917860+osheaf01@users.noreply.github.com>
2023-01-31 16:48:52 +0000

    St1028 common folder (#457)
    * ST-1028 Removing some redundant classes from the common folder.
    
    * ST-1028 Removing redundant static fields from DocmosisTemplateConstants
    
    * ST-1028 Removing more redundant fields
    
    ---------
    
    Co-authored-by: osheaf01 <fergal.oshea@version1.com>
    Co-authored-by: ElifDenizEsea <61947470+ElifDenizEsea@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/sptribs/common/config/interceptors/UnAuthorisedServiceException.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.hmcts.sptribs.common.config.interceptors;
-
-public class UnAuthorisedServiceException extends RuntimeException {
-    private static final long serialVersionUID = -2047737262969335485L;
-
-    public UnAuthorisedServiceException(String message) {
-        super(message);
-    }
-}
