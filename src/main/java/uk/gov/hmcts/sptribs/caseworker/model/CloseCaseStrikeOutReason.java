b739f9d57040468e45cb97c8f7fcfef544f6377b
Paul Pearson <46532983+paul-pearson@users.noreply.github.com>
2023-02-02 14:00:48 +0000

    ST-794 Close case strike out details (#463)
    * ST-794 Close case strike out details
    
    * ST-794 Correct import order for checkstyle
    
    * ST-794 Add test for aboutToStart()
    
    ---------
    
    Co-authored-by: ElifDenizEsea <61947470+ElifDenizEsea@users.noreply.github.com>

---
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/model/CloseCaseStrikeOutReason.java
@@ -0,0 +1,19 @@
+package uk.gov.hmcts.sptribs.caseworker.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import uk.gov.hmcts.ccd.sdk.api.HasLabel;
+
+@Getter
+@AllArgsConstructor
+public enum CloseCaseStrikeOutReason implements HasLabel {
+
+    @JsonProperty("noncomplianceWithDirections")
+    NO_JURISDICTION("Noncompliance with Directions"),
+
+    @JsonProperty("other")
+    OTHER("Other");
+
+    private final String label;
+}
