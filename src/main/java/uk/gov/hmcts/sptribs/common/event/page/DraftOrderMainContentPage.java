29fbb6b445a9c41f8741b2a0ba5574629121036f
muradalamv1 <107861321+muradalamv1@users.noreply.github.com>
2023-02-07 12:19:55 +0000

    Feat/st 553 rework (#481)
    * 553 docmosis changes
    
    * 553 docmosis changes
    
    * 553 docmosis changes
    
    * 553 docmosis changes
    
    * 553 docmosis changes
    
    * Merge branch 'master' into feat/ST-940
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.
    
    * Merge branch 'master' into feat/ST-940
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.
    
    * Merge branch 'master' into feat/ST-940
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.
    
    * Merge branch 'master' into feat/ST-940
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.
    
    * created a preview page for draft order
    
    * created a preview page for draft order
    
    * latest commit of preview order template
    
    * latest commit of preview order template
    
    * latest commit of preview order template3
    
    * latest commit of preview order template3
    
    * latest commit of preview order template3

--- src/main/java/uk/gov/hmcts/sptribs/common/event/page/DraftOrderMainContentPage.java
+++ src/main/java/uk/gov/hmcts/sptribs/common/event/page/DraftOrderMainContentPage.java
@@ -1,15 +1,39 @@
 package uk.gov.hmcts.sptribs.common.event.page;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.ccd.sdk.type.Document;
 import uk.gov.hmcts.sptribs.caseworker.model.DraftOrderMainContentCIC;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
+import uk.gov.hmcts.sptribs.ciccase.model.LanguagePreference;
+import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.common.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
+import uk.gov.hmcts.sptribs.document.CaseDataDocumentService;
+import uk.gov.hmcts.sptribs.document.content.PreviewDraftOrderTemplateContent;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Slf4j
+@Component
 public class DraftOrderMainContentPage implements CcdPageConfiguration {
+
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    @Autowired
+    private CaseDataDocumentService caseDataDocumentService;
+
+    @Autowired
+    private PreviewDraftOrderTemplateContent previewDraftOrderTemplateContent;
+
+
     @Override
     public void addTo(PageBuilder pageBuilder) {
         pageBuilder
-            .page("mainContent")
+            .page("mainContent",this::midEvent)
             .pageLabel("Edit order")
             .label("EditDraftOrderMainContent", "<hr>" + "\n<h3>Header</h3>" + "\nThe header will be automatically generated."
                 + "You can preview this in pdf document on the next screen.\n\n"
@@ -26,5 +50,32 @@ public class DraftOrderMainContentPage implements CcdPageConfiguration {
             .done();
     }
 
+    public AboutToStartOrSubmitResponse<CaseData, State> midEvent(
+        CaseDetails<CaseData, State> details,
+        CaseDetails<CaseData, State> detailsBefore
+    ) {
+
+
+        CaseData caseData = details.getData();
+        var template = caseData.getCicCase().getAnOrderTemplates();
+        String subjectName = caseData.getCicCase().getFullName();
+        final Long caseId = details.getId();
+        final String filename = "Order-[" +  subjectName + "]-" + LocalDateTime.now().format(formatter);
+
+        Document generalOrderDocument = caseDataDocumentService.renderDocument(
+            previewDraftOrderTemplateContent.apply(caseData, caseId),
+            caseId,
+            caseData.getCicCase().getAnOrderTemplates().getId(),
+            LanguagePreference.ENGLISH,
+            filename
+        );
+
+        caseData.getCicCase().setAnOrderTemplates(template);
+        caseData.getCicCase().setOrderTemplateIssued(generalOrderDocument);
+
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .data(caseData)
+            .build();
+    }
 
 }
