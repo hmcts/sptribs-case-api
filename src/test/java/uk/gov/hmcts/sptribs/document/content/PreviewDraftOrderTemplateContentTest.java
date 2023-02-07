9fbacd8322b034ce9fc6a6dbdca15204595a0012
version1rhel <murad.alam@version1.com>
2023-02-07 11:31:17 +0000

    latest commit of preview order template

---
+++ src/test/java/uk/gov/hmcts/sptribs/document/content/PreviewDraftOrderTemplateContentTest.java
@@ -0,0 +1,61 @@
+package uk.gov.hmcts.sptribs.document.content;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import uk.gov.hmcts.ccd.sdk.type.DynamicList;
+import uk.gov.hmcts.ccd.sdk.type.DynamicListElement;
+import uk.gov.hmcts.ccd.sdk.type.ListValue;
+import uk.gov.hmcts.sptribs.caseworker.model.HearingSummary;
+import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
+import uk.gov.hmcts.sptribs.ciccase.model.CicCase;
+import uk.gov.hmcts.sptribs.ciccase.model.PanelMember;
+import uk.gov.hmcts.sptribs.ciccase.model.SchemeCic;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
+import static uk.gov.hmcts.sptribs.testutil.TestConstants.TEST_CASE_ID;
+
+
+@ExtendWith(MockitoExtension.class)
+public class PreviewDraftOrderTemplateContentTest {
+
+
+    @InjectMocks
+    private FinalDecisionTemplateContent templateContent;
+    @InjectMocks
+    private PreviewDraftOrderTemplateContent previewDraftOrderTemplateContent;
+
+    private static final LocalDate ISSUE_DATE = LocalDate.of(2022, 2, 2);
+
+    @Test
+    public void shouldSuccessfullyPreviewDraftOrderContent() {
+        //Given
+        CaseData caseData = buildCaseData();
+
+        //When
+        Map<String, Object> result = previewDraftOrderTemplateContent.apply(caseData, TEST_CASE_ID);
+
+        //Then
+        assertThat(result).contains(
+            entry("cicCaseSchemeCic", SchemeCic.Year1996.getLabel())
+        );
+    }
+
+    private CaseData buildCaseData() {
+        final CicCase cicCase = CicCase.builder().schemeCic(SchemeCic.Year1996).build();
+
+        return CaseData.builder()
+            .cicCase(cicCase)
+            .build();
+    }
+
+}
