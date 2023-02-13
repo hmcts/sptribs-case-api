1898e4a3b24caae3bb7636a2dda06aaf056c3bf8
Shailesh Walke <117093807+Shailesh-walke@users.noreply.github.com>
2023-02-13 15:50:17 +0000

    St 702 (#515)
    * Changes for ST-702, ST-788, ST-704
    * fix for selecting multiple checkboxes

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerCreateHearingSummary.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerCreateHearingSummary.java
@@ -33,8 +33,6 @@ import java.util.List;
 import static uk.gov.hmcts.sptribs.caseworker.util.EventConstants.CASEWORKER_CREATE_HEARING_SUMMARY;
 import static uk.gov.hmcts.sptribs.ciccase.model.State.AwaitingHearing;
 import static uk.gov.hmcts.sptribs.ciccase.model.State.AwaitingOutcome;
-import static uk.gov.hmcts.sptribs.ciccase.model.State.CaseClosed;
-import static uk.gov.hmcts.sptribs.ciccase.model.State.CaseStayed;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.COURT_ADMIN_CIC;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.SOLICITOR;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.SUPER_USER;
@@ -61,7 +59,7 @@ public class CaseWorkerCreateHearingSummary implements CCDConfig<CaseData, State
         PageBuilder pageBuilder = new PageBuilder(
             configBuilder
                 .event(CASEWORKER_CREATE_HEARING_SUMMARY)
-                .forStates(AwaitingHearing, AwaitingOutcome, CaseStayed, CaseClosed)
+                .forStates(AwaitingHearing)
                 .name("Hearings: Create summary")
                 .showSummary()
                 .aboutToStartCallback(this::aboutToStart)
