bf9cb1a915c23fbbdbaff6a41d273fd6568f5708
ElifDenizEsea <61947470+ElifDenizEsea@users.noreply.github.com>
2023-02-16 17:03:27 +0000

    ST-967 Main content prefilled (#546)
    Co-authored-by: hmctscivil <version1rhel@hmctscivil.mshome.net>

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/page/IssueDecisionSelectTemplate.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/page/IssueDecisionSelectTemplate.java
@@ -1,7 +1,12 @@
 package uk.gov.hmcts.sptribs.caseworker.event.page;
 
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.sptribs.caseworker.model.CaseIssueDecision;
+import uk.gov.hmcts.sptribs.caseworker.util.EventUtil;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
+import uk.gov.hmcts.sptribs.ciccase.model.DecisionTemplate;
+import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.common.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
 
@@ -11,11 +16,25 @@ public class IssueDecisionSelectTemplate implements CcdPageConfiguration {
 
     @Override
     public void addTo(PageBuilder pageBuilder) {
-        pageBuilder.page("issueDecisionSelectTemplate")
+        pageBuilder.page("issueDecisionSelectTemplate", this::midEvent)
             .pageLabel("Select a template")
             .pageShowConditions(issueDecisionShowConditions())
             .complex(CaseData::getCaseIssueDecision)
             .mandatory(CaseIssueDecision::getIssueDecisionTemplate)
             .done();
     }
+
+    public AboutToStartOrSubmitResponse<CaseData, State> midEvent(
+        CaseDetails<CaseData, State> details,
+        CaseDetails<CaseData, State> detailsBefore
+    ) {
+        CaseData caseData = details.getData();
+        DecisionTemplate decision = caseData.getCaseIssueDecision().getIssueDecisionTemplate();
+        caseData.setDecisionMainContent(EventUtil.getMainContent(decision));
+
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .data(caseData)
+            .build();
+    }
+
 }
