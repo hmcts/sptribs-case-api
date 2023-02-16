02c589d03e296b0dcef04a577a69c6486d93d20b
sunil-vooturi <Sunil.Vooturi1@hmcts.net>
2023-02-16 14:14:12 +0000

    ST-1080 staycase edit and remove + close case + case flags tests added

---
+++ src/e2eTests/java/uk/gov/hmcts/sptribs/e2e/CloseCase.java
@@ -0,0 +1,61 @@
+package uk.gov.hmcts.sptribs.e2e;
+
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.AriaRole;
+import com.microsoft.playwright.options.SelectOption;
+import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.sptribs.testutils.DateHelpers;
+import uk.gov.hmcts.sptribs.testutils.PageHelpers;
+
+import java.util.Calendar;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+import static uk.gov.hmcts.sptribs.testutils.AssertionHelpers.enabledOptionsWithTimeout;
+import static uk.gov.hmcts.sptribs.testutils.AssertionHelpers.textOptionsWithTimeout;
+import static uk.gov.hmcts.sptribs.testutils.PageHelpers.*;
+
+public class CloseCase extends Base {
+    @Test
+    public void CaseworkerShouldAbleToClosetheCase(){
+        Login login = new Login();
+        login.loginAsStTest1User();
+        Case newCase = new Case();
+        newCase.createCase();
+        newCase.buildCase();
+        page.selectOption("#next-step", new SelectOption().setLabel("Case: Close case"));
+        clickButton("Go");
+        assertThat(page.locator("h1")).hasText("Are you sure you want to close this case?" ,  textOptionsWithTimeout(30000));
+        clickButton("Continue");
+        page.getByLabel("Case Withdrawn").check();
+        clickButton("Continue");
+        assertThat(page.locator("h1")).hasText("Withdrawal details" ,  textOptionsWithTimeout(30000));
+        page.getByLabel("Who withdrew from the case?").fill("case worker");
+        Calendar date = DateHelpers.getYesterdaysDate();
+        getTextBoxByLabel("Day").fill(String.valueOf(date.get(Calendar.DAY_OF_MONTH)));
+        getTextBoxByLabel("Month").fill(String.valueOf(date.get(Calendar.MONTH) + 1));
+        getTextBoxByLabel("Year").type(String.valueOf(date.get(Calendar.YEAR)));
+        page.locator("h1").click();
+        clickButton("Continue");
+
+        assertThat(page.locator("h1")).hasText("Upload case documents" ,  textOptionsWithTimeout(30000));
+        clickButton("Continue");
+        assertThat(page.locator("h1")).hasText("Select recipients" ,  textOptionsWithTimeout(30000));
+        page.getByLabel("Subject").check();
+        page.getByLabel("Respondent").check();
+
+        clickButton("Continue");
+
+        assertThat(page.locator("h2")).hasText("Check your answers" ,  textOptionsWithTimeout(30000));
+
+        clickButton("Save and continue");
+
+        assertThat(page.locator("h1:has-text('Case closed') ")).isVisible();
+        clickButton("Close and Return to case details");
+        page.getByRole(AriaRole.TAB, new Page.GetByRoleOptions().setName("State")).getByText("State").click();
+        page.waitForSelector("h4", PageHelpers.selectorOptionsWithTimeout(60000));
+        assertThat(page.locator("h4")).containsText("Case Status:  Case closed");
+    }
+}
+
+
+
---
+++ src/e2eTests/java/uk/gov/hmcts/sptribs/e2e/CreateFlag.java
@@ -0,0 +1,81 @@
+package uk.gov.hmcts.sptribs.e2e;
+
+
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.AriaRole;
+import com.microsoft.playwright.options.SelectOption;
+import com.netflix.hystrix.contrib.javanica.utils.FutureDecorator;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.sptribs.testutils.DateHelpers;
+import uk.gov.hmcts.sptribs.testutils.PageHelpers;
+
+import java.util.Calendar;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+import static uk.gov.hmcts.sptribs.testutils.AssertionHelpers.textOptionsWithTimeout;
+import static uk.gov.hmcts.sptribs.testutils.PageHelpers.clickButton;
+import static uk.gov.hmcts.sptribs.testutils.PageHelpers.getTextBoxByLabel;
+
+public class CreateFlag extends Base {
+
+    @Test
+    public void caseWorkerShouldBeAbleToCreateACaseFlag(){
+        Login login = new Login();
+        login.loginAsStTest1User();
+        Case newCase = new Case();
+        newCase.createCase();
+        newCase.buildCase();
+        newCase.createCaseFlag();
+
+
+    }
+
+     @Test
+     public void caseWorkerShouldAbleToDoManageFlags(){ Login login = new Login();
+        login.loginAsStTest1User();
+        Case newCase = new Case();
+        newCase.createCase();
+        newCase.buildCase();
+        newCase.createCaseFlag();
+
+         page.selectOption("#next-step", new SelectOption().setLabel("Flags: Manage flags"));
+         page.waitForFunction("selector => document.querySelector(selector).disabled === false",
+             "ccd-event-trigger button[type='submit']", PageHelpers.functionOptionsWithTimeout(6000));
+         PageHelpers.clickButton("Go");
+         assertThat(page.locator("h1")).hasText("Flags: Manage flags" ,  textOptionsWithTimeout(30000));
+         page.getByLabel("Flag Type (Optional)").click();
+         page.getByLabel("Flag Type (Optional)").fill("test flag");
+         page.getByLabel("Party Name (Optional)").click();
+         page.getByLabel("Party Name (Optional)").fill("case party");
+         page.getByLabel("Value (Optional)").click();
+         page.getByLabel("Value (Optional)").fill("test 6");
+         page.getByLabel("Key (Optional)").click();
+         page.getByLabel("Key (Optional)").fill("test123");
+         page.getByRole(AriaRole.GROUP, new Page.GetByRoleOptions().setName("Modified Date (Optional)")).getByLabel("Day").click();
+         page.getByRole(AriaRole.GROUP, new Page.GetByRoleOptions().setName("Modified Date (Optional)")).getByLabel("Day").fill("8");
+
+         page.getByRole(AriaRole.GROUP, new Page.GetByRoleOptions().setName("Modified Date (Optional)")).getByLabel("Month").fill("6");
+
+         page.getByRole(AriaRole.GROUP, new Page.GetByRoleOptions().setName("Modified Date (Optional)")).getByLabel("Year").fill("2021");
+
+         page.getByRole(AriaRole.GROUP, new Page.GetByRoleOptions().setName("Modified Date (Optional)")).getByLabel("Hour").fill("7");
+
+         page.getByRole(AriaRole.GROUP, new Page.GetByRoleOptions().setName("Modified Date (Optional)")).getByLabel("Minute").fill("5");
+
+         page.getByRole(AriaRole.GROUP, new Page.GetByRoleOptions().setName("Modified Date (Optional)")).getByLabel("Second").fill("22");
+         page.getByLabel("No").check();
+         clickButton("Continue");
+         assertThat(page.locator("h1")).hasText("Flags: Manage flags" ,  textOptionsWithTimeout(30000));
+         clickButton("Save and continue");
+     }
+}
+
+
+
+
+
+
+
+
+
