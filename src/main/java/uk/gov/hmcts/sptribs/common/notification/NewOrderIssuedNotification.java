edcd4bf82729dc0cebccb5f3b15afa3eedb200e3
sjamihmcts <110464983+sjamihmcts@users.noreply.github.com>
2023-02-17 09:49:32 +0000

    Feat/st 846: Send Order event Notification changes (#547)
    * ST-846: Initial Commit
    
    * ST-846: Changes to notification logic for New order issued
    
    * ST-846: Changes post Elif's changes - In progress
    
    * ST-846: Email condition changed
    
    * ST-846: Working changes for Notification

--- src/main/java/uk/gov/hmcts/sptribs/common/notification/NewOrderIssuedNotification.java
+++ src/main/java/uk/gov/hmcts/sptribs/common/notification/NewOrderIssuedNotification.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.sptribs.common.notification;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
@@ -13,11 +14,13 @@ import uk.gov.hmcts.sptribs.notification.PartiesNotification;
 import uk.gov.hmcts.sptribs.notification.TemplateName;
 import uk.gov.hmcts.sptribs.notification.model.NotificationRequest;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @Component
 @Slf4j
-public class HearingPostponedNotification implements PartiesNotification {
+public class NewOrderIssuedNotification implements PartiesNotification {
 
     @Autowired
     private NotificationServiceCIC notificationService;
@@ -29,37 +32,39 @@ public class HearingPostponedNotification implements PartiesNotification {
     public void sendToSubject(final CaseData caseData, final String caseNumber) {
         CicCase cicCase = caseData.getCicCase();
         Map<String, Object> templateVars = notificationHelper.getSubjectCommonVars(caseNumber, cicCase);
-        notificationHelper.addHearingPostponedTemplateVars(cicCase, templateVars);
 
-        NotificationResponse hearingNotifyResponse;
+        NotificationResponse notificationResponse;
         if (cicCase.getContactPreferenceType() == ContactPreferenceType.EMAIL) {
-            hearingNotifyResponse = sendEmailNotification(cicCase.getEmail(), templateVars);
-            cicCase.setSubjectNotifyList(hearingNotifyResponse);
+            List<String> uploadedDocumentIds = getUploadedDocumentIds(caseData);
+
+            notificationResponse = sendEmailNotificationWithAttachment(cicCase.getEmail(),
+                uploadedDocumentIds, templateVars);
+            cicCase.setSubjectNotifyList(notificationResponse);
         } else {
             notificationHelper.addAddressTemplateVars(cicCase.getAddress(), templateVars);
-            hearingNotifyResponse = sendLetterNotification(templateVars);
+            notificationResponse = sendLetterNotification(templateVars);
         }
 
-        cicCase.setSubjectNotifyList(hearingNotifyResponse);
+        cicCase.setSubjectNotifyList(notificationResponse);
     }
 
     @Override
     public void sendToRepresentative(final CaseData caseData, final String caseNumber) {
         CicCase cicCase = caseData.getCicCase();
         Map<String, Object> templateVars = notificationHelper.getRepresentativeCommonVars(caseNumber, cicCase);
-        notificationHelper.addHearingPostponedTemplateVars(cicCase, templateVars);
 
-        NotificationResponse hearingNotifyResponse;
+        NotificationResponse notificationResponse;
         if (cicCase.getRepresentativeContactDetailsPreference() == ContactPreferenceType.EMAIL) {
-            hearingNotifyResponse =
-                sendEmailNotification(cicCase.getRepresentativeEmailAddress(), templateVars);
-            cicCase.setRepNotificationResponse(hearingNotifyResponse);
+            List<String> uploadedDocumentIds = getUploadedDocumentIds(caseData);
+            notificationResponse = sendEmailNotificationWithAttachment(cicCase.getRepresentativeEmailAddress(),
+                uploadedDocumentIds, templateVars);
+            cicCase.setRepNotificationResponse(notificationResponse);
         } else {
             notificationHelper.addAddressTemplateVars(cicCase.getRepresentativeAddress(), templateVars);
-            hearingNotifyResponse = sendLetterNotification(templateVars);
+            notificationResponse = sendLetterNotification(templateVars);
         }
 
-        cicCase.setRepNotificationResponse(hearingNotifyResponse);
+        cicCase.setRepNotificationResponse(notificationResponse);
     }
 
     @Override
@@ -67,27 +72,41 @@ public class HearingPostponedNotification implements PartiesNotification {
         CicCase cicCase = caseData.getCicCase();
 
         Map<String, Object> respondentTemplateVars = notificationHelper.getRespondentCommonVars(caseNumber, cicCase);
-        notificationHelper.addHearingPostponedTemplateVars(cicCase, respondentTemplateVars);
+        List<String> uploadedDocumentIds = getUploadedDocumentIds(caseData);
 
-        NotificationResponse hearingNotifyResponse = sendEmailNotification(cicCase.getRespondentEmail(), respondentTemplateVars);
-        cicCase.setResNotificationResponse(hearingNotifyResponse);
+        NotificationResponse notificationResponse = sendEmailNotificationWithAttachment(cicCase.getRespondentEmail(),
+            uploadedDocumentIds, respondentTemplateVars);
+        cicCase.setResNotificationResponse(notificationResponse);
     }
 
-    private NotificationResponse sendEmailNotification(final String destinationAddress, final Map<String, Object> templateVars) {
-        NotificationRequest request = notificationHelper.buildEmailNotificationRequest(
+    private NotificationResponse sendEmailNotificationWithAttachment(final String destinationAddress,
+                                                                     List<String> uploadedDocumentIds,
+                                                                     final Map<String, Object> templateVars) {
+        NotificationRequest emailRequest = notificationHelper.buildEmailNotificationRequest(
             destinationAddress,
+            true,
+            uploadedDocumentIds,
             templateVars,
-            TemplateName.HEARING_POSTPONED_EMAIL);
-        notificationService.setNotificationRequest(request);
+            TemplateName.NEW_ORDER_ISSUED_EMAIL);
+        notificationService.setNotificationRequest(emailRequest);
         return notificationService.sendEmail();
     }
 
     private NotificationResponse sendLetterNotification(Map<String, Object> templateVarsLetter) {
         NotificationRequest letterRequest = notificationHelper.buildLetterNotificationRequest(
             templateVarsLetter,
-            TemplateName.HEARING_POSTPONED_POST);
+            TemplateName.NEW_ORDER_ISSUED_POST);
         notificationService.setNotificationRequest(letterRequest);
         return notificationService.sendLetter();
     }
 
+    private List<String> getUploadedDocumentIds(CaseData caseData) {
+        CicCase cicCase = caseData.getCicCase();
+        List<String> uploadedDocumentIds = new ArrayList<>();
+        if (null != cicCase.getLastSelectedOrder()) {
+            uploadedDocumentIds.add(StringUtils.substringAfterLast(cicCase.getLastSelectedOrder().getUrl(), "/"));
+        }
+
+        return uploadedDocumentIds;
+    }
 }
