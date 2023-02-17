7c69c025e7e488664ef7c10311d2402fe4783394
hmctscivil <version1rhel@hmctscivil.mshome.net>
2023-02-17 11:56:04 +0000

    ST-1025 Post address is mandatory for subject

---
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/model/CaseLinksElement.java
@@ -0,0 +1,24 @@
+package uk.gov.hmcts.sptribs.caseworker.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+@Data
+@Builder
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CaseLinksElement<T>  {
+    @JsonProperty("id")
+    private final String id;
+    @NotNull
+    @Valid
+    @JsonProperty("value")
+    private final T value;
+}
