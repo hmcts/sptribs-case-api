dc757559c8e8e509c47442a6140ca3d8a369bf11
Paul Pearson <46532983+paul-pearson@users.noreply.github.com>
2023-02-23 09:56:35 +0000

    ST-974 Save edited content to order (#596)

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/model/DraftOrderCIC.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/model/DraftOrderCIC.java
@@ -7,25 +7,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import uk.gov.hmcts.ccd.sdk.api.CCD;
 import uk.gov.hmcts.ccd.sdk.type.Document;
-import uk.gov.hmcts.sptribs.ciccase.model.OrderTemplate;
 import uk.gov.hmcts.sptribs.ciccase.model.access.CaseworkerWithCAAAccess;
 import uk.gov.hmcts.sptribs.ciccase.model.access.DefaultAccess;
-
-import static uk.gov.hmcts.ccd.sdk.type.FieldType.FixedList;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class DraftOrderCIC {
-
-    @CCD(
-        label = "Template",
-        access = {DefaultAccess.class, CaseworkerWithCAAAccess.class},
-        typeOverride = FixedList,
-        typeParameterOverride = "OrderTemplate"
-    )
-    private OrderTemplate template;
 
     @CCD(
         label = "Order File",
