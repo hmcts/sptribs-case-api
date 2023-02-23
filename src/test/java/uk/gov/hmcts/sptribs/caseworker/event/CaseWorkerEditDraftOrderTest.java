a40691101556d389b7f4fbe2517aad752a78a3ae
Paul Pearson <46532983+paul-pearson@users.noreply.github.com>
2023-02-23 09:56:35 +0000

    ST-974 Save edited content to order (#596)

--- src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerEditDraftOrderTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerEditDraftOrderTest.java
@@ -9,14 +9,22 @@ import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.Event;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.ccd.sdk.type.Document;
 import uk.gov.hmcts.ccd.sdk.type.DynamicList;
+import uk.gov.hmcts.ccd.sdk.type.DynamicListElement;
+import uk.gov.hmcts.ccd.sdk.type.ListValue;
 import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
+import uk.gov.hmcts.sptribs.caseworker.model.DraftOrderCIC;
 import uk.gov.hmcts.sptribs.caseworker.model.DraftOrderContentCIC;
 import uk.gov.hmcts.sptribs.caseworker.service.OrderService;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.OrderTemplate;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.hmcts.sptribs.testutil.ConfigTestUtil.createCaseDataConfigBuilder;
@@ -61,17 +69,29 @@ class CaseWorkerEditDraftOrderTest {
         updatedCaseDetails.setId(TEST_CASE_ID);
         updatedCaseDetails.setCreatedDate(LOCAL_DATE_TIME);
         caseData.setDraftOrderContentCIC(DraftOrderContentCIC.builder().orderTemplate(OrderTemplate.CIC6_GENERAL_DIRECTIONS).build());
-        caseData.getCicCase().setDraftOrderDynamicList(DynamicList.builder().build());
+        DynamicListElement element = DynamicListElement.builder().code(UUID.randomUUID()).label("label").build();
+        List<DynamicListElement> elements = new ArrayList<>();
+        elements.add(element);
+        caseData.getCicCase().setDraftOrderDynamicList(DynamicList.builder().value(element).listItems(elements).build());
+        List<ListValue<DraftOrderCIC>> cicList = new ArrayList<>();
+        cicList.add(ListValue.<DraftOrderCIC>builder().value(
+            DraftOrderCIC.builder().build()
+        ).build());
+        caseData.getCicCase().setDraftOrderCICList(cicList);
+        caseData.getCicCase().setOrderTemplateIssued(new Document());
 
         //When
         AboutToStartOrSubmitResponse<CaseData, State> response =
             caseWorkerEditDraftOrder.aboutToSubmit(updatedCaseDetails, beforeDetails);
         assertThat(response).isNotNull();
+        DraftOrderCIC updatedDraftOrder = response.getData()
+            .getCicCase().getDraftOrderCICList().get(0).getValue();
+        assertThat(updatedDraftOrder.getDraftOrderContentCIC().getOrderTemplate()).isEqualTo(OrderTemplate.CIC6_GENERAL_DIRECTIONS);
+        assertThat(updatedDraftOrder.getTemplateGeneratedDocument()).isNotNull();
 
         SubmittedCallbackResponse draftCreatedResponse = caseWorkerEditDraftOrder.draftUpdated(updatedCaseDetails, beforeDetails);
         //  Then
         assertThat(draftCreatedResponse).isNotNull();
-
     }
 
 }
