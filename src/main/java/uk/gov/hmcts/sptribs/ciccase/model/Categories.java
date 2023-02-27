743e32d39b12bfc5658d369323880a5c3397d72d
sjamihmcts <santoshini.jami@hmcts.net>
2023-02-27 13:32:13 +0000

    ST-1077 - Removed unused files related to divorce

---
+++ src/main/java/uk/gov/hmcts/sptribs/ciccase/model/Categories.java
@@ -0,0 +1,13 @@
+package uk.gov.hmcts.sptribs.ciccase.model;
+
+import java.time.LocalDate;
+
+public class Categories {
+    private LocalDate liveFrom;
+    private LocalDate liveTo;
+    private String caseTypeID;
+    private String categoryID;
+    private String categoryLabel;
+    private String displayOrder;
+    private String parentCategoryID;
+}
