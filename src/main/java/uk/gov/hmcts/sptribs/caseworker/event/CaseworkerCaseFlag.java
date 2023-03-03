5b924a3f49caed46a609337b30bca9742cc192af
Paul Pearson <paul.pearson@version1.com>
2023-03-03 14:23:11 +0000

    ST-1112 Feature flag for case flag

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerCaseFlag.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerCaseFlag.java
@@ -1,6 +1,8 @@
 package uk.gov.hmcts.sptribs.caseworker.event;
 
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
@@ -41,8 +43,11 @@ import static uk.gov.hmcts.sptribs.ciccase.model.access.Permissions.CREATE_READ_
 
 @Component
 @Slf4j
+@Setter
 public class CaseworkerCaseFlag implements CCDConfig<CaseData, State, UserRole> {
 
+    @Value("${feature.case-flags.enabled}")
+    private boolean caseFlagsEnabled;
 
     private static final CcdPageConfiguration flagLevel = new FlagLevel();
     private static final CcdPageConfiguration flagParties = new FlagParties();
@@ -51,6 +56,12 @@ public class CaseworkerCaseFlag implements CCDConfig<CaseData, State, UserRole> 
 
     @Override
     public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
+        if (caseFlagsEnabled) {
+            doConfigure(configBuilder);
+        }
+    }
+
+    public void doConfigure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
         var pageBuilder = caseFlag(configBuilder);
         flagLevel.addTo(pageBuilder);
         flagParties.addTo(pageBuilder);
