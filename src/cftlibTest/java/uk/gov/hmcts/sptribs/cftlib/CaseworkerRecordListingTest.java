bd6d3ce97589b145a16640e4539664d22574fc90
hmctscivil <version1rhel@hmctscivil.mshome.net>
2023-03-09 17:03:39 +0000

    ST-1073 Hearing class created

--- src/cftlibTest/java/uk/gov/hmcts/sptribs/cftlib/CaseworkerRecordListingTest.java
+++ src/cftlibTest/java/uk/gov/hmcts/sptribs/cftlib/CaseworkerRecordListingTest.java
@@ -3,14 +3,10 @@ package uk.gov.hmcts.sptribs.cftlib;
 import com.microsoft.playwright.Page;
 import org.junit.jupiter.api.Assertions;
 import org.junitpioneer.jupiter.RetryingTest;
-import uk.gov.hmcts.sptribs.cftlib.action.Actions;
 import uk.gov.hmcts.sptribs.cftlib.action.Case;
+import uk.gov.hmcts.sptribs.cftlib.action.Hearing;
 import uk.gov.hmcts.sptribs.cftlib.util.Login;
-import uk.gov.hmcts.sptribs.cftlib.util.PageHelpers;
 import uk.gov.hmcts.sptribs.cftlib.util.PlaywrightHelpers;
-
-import static uk.gov.hmcts.sptribs.cftlib.util.PageHelpers.clickButton;
-import static uk.gov.hmcts.sptribs.cftlib.util.PageHelpers.getTextBoxByLabel;
 
 public class CaseworkerRecordListingTest extends XuiTest {
 
@@ -23,29 +19,9 @@ public class CaseworkerRecordListingTest extends XuiTest {
         Case newCase = new Case(page);
         newCase.createCase();
         newCase.buildCase();
-        newCase.startNextStepAction(Actions.CreateListing);
 
-        page.getByLabel("Final").check();
-        page.getByLabel("Face to Face").check();
-        PageHelpers.clickButton(page, "Continue");
-        page.getByLabel("1-London").check();
-        PageHelpers.clickButton(page, "Continue");
-
-        page.getByLabel("BARNET CIVIL AND FAMILY COURTS CENTRE-ST MARY'S COURT, REGENTS PARK ROAD").check();
-        getTextBoxByLabel(page, "Day").fill("1");
-        getTextBoxByLabel(page, "Month").fill("1");
-        getTextBoxByLabel(page, "Year").fill("1990");
-        page.getByLabel("Morning").check();
-        page.getByLabel("Start time (24hr format)").click();
-        page.getByLabel("Start time (24hr format)").fill("09:00");
-        page.getByLabel("No").check();
-        PageHelpers.clickButton(page, "Continue");
-        PageHelpers.clickButton(page, "Continue");
-        PageHelpers.clickButton(page, "Continue");
-        page.getByLabel("Respondent").check();
-        PageHelpers.clickButton(page, "Continue");
-        clickButton(page, "Save and continue");
-        clickButton(page, "Close and Return to case details");
+        Hearing hearing = new Hearing(page);
+        hearing.createListing();
         Assertions.assertEquals("Awaiting hearing", newCase.getCaseStatus());
 
     }
