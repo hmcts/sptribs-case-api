635525e1d300cc6232afc3604f83911a2b339c48
ElifDenizEsea <61947470+ElifDenizEsea@users.noreply.github.com>
2023-03-09 15:42:19 +0000

    ST-1072 Close case cftlib (#655)
    * ST-1072 Close case cftlib
    
    * ST-1072 Xui fix
    
    * ST-1072 Xui fix
    
    ---------
    
    Co-authored-by: hmctscivil <version1rhel@hmctscivil.mshome.net>

---
+++ src/cftlibTest/java/uk/gov/hmcts/sptribs/cftlib/CaseworkerCloseCaseTest.java
@@ -0,0 +1,58 @@
+package uk.gov.hmcts.sptribs.cftlib;
+
+import com.microsoft.playwright.Page;
+import org.junit.jupiter.api.Assertions;
+import org.junitpioneer.jupiter.RetryingTest;
+import uk.gov.hmcts.sptribs.cftlib.action.Case;
+import uk.gov.hmcts.sptribs.cftlib.util.DateHelpers;
+import uk.gov.hmcts.sptribs.cftlib.util.Login;
+import uk.gov.hmcts.sptribs.cftlib.util.PlaywrightHelpers;
+
+import java.util.Calendar;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+import static uk.gov.hmcts.sptribs.cftlib.util.AssertionHelpers.textOptionsWithTimeout;
+import static uk.gov.hmcts.sptribs.cftlib.util.PageHelpers.clickButton;
+import static uk.gov.hmcts.sptribs.cftlib.util.PageHelpers.getTextBoxByLabel;
+
+public class CaseworkerCloseCaseTest extends XuiTest {
+
+    @RetryingTest(maxAttempts = PlaywrightHelpers.RETRIES)
+    public void caseworkerShouldAbleToCloseTheCase() {
+        Page page = getPage();
+        Login login = new Login(page);
+        login.signInWithCaseworker();
+        Case newCase = new Case(page);
+        newCase.createCase();
+        newCase.buildCase();
+        newCase.startNextStepAction("Case: Close case");
+        assertThat(page.locator("h1")).hasText("Are you sure you want to close this case?", textOptionsWithTimeout(60000));
+        clickButton(page, "Continue");
+        page.getByLabel("Case Withdrawn").check();
+        clickButton(page, "Continue");
+        assertThat(page.locator("h1")).hasText("Withdrawal details", textOptionsWithTimeout(30000));
+        page.getByLabel("Who withdrew from the case?").fill("case worker");
+        Calendar date = DateHelpers.getYesterdaysDate();
+        getTextBoxByLabel(page, "Day").fill(String.valueOf(date.get(Calendar.DAY_OF_MONTH)));
+        getTextBoxByLabel(page, "Month").fill(String.valueOf(date.get(Calendar.MONTH) + 1));
+        getTextBoxByLabel(page, "Year").type(String.valueOf(date.get(Calendar.YEAR)));
+        page.locator("h1").click();
+        clickButton(page, "Continue");
+        assertThat(page.locator("h1")).hasText("Upload case documents", textOptionsWithTimeout(30000));
+        clickButton(page, "Continue");
+        assertThat(page.locator("h1")).hasText("Select recipients", textOptionsWithTimeout(30000));
+        page.getByLabel("Subject").check();
+        page.getByLabel("Respondent").check();
+        clickButton(page, "Continue");
+        assertThat(page.locator("h2")).hasText("Check your answers", textOptionsWithTimeout(30000));
+        clickButton(page, "Save and continue");
+        assertThat(page.locator("ccd-markdown markdown h1"))
+            .hasText("Case closed", textOptionsWithTimeout(60000));
+        clickButton(page, "Close and Return to case details");
+        assertThat(page.locator("h2.heading-h2").first())
+            .hasText("History", textOptionsWithTimeout(60000));
+        Assertions.assertEquals("Case closed", newCase.getCaseStatus());
+    }
+
+
+}
---
+++ src/cftlibTest/java/uk/gov/hmcts/sptribs/cftlib/util/PageHelpers.java
@@ -0,0 +1,74 @@
+package uk.gov.hmcts.sptribs.cftlib.util;
+
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.AriaRole;
+
+public class PageHelpers {
+    private PageHelpers() {
+    }
+
+    public static Page.WaitForSelectorOptions selectorOptionsWithTimeout(int timeout) {
+        return new Page.WaitForSelectorOptions().setTimeout(timeout);
+    }
+
+    public static Page.WaitForLoadStateOptions loadStateOptionsWithTimeout(int timeout) {
+        return new Page.WaitForLoadStateOptions().setTimeout(timeout);
+    }
+
+    public static Page.SelectOptionOptions selectOptionWithTimeout(int timeout) {
+        return new Page.SelectOptionOptions().setTimeout(timeout);
+    }
+
+    public static Page.WaitForURLOptions urlOptionsWithTimeout(int timeout) {
+        return new Page.WaitForURLOptions().setTimeout(timeout);
+    }
+
+    public static Page.WaitForFunctionOptions functionOptionsWithTimeout(int timeout) {
+        return new Page.WaitForFunctionOptions().setTimeout(timeout);
+    }
+
+    public static Locator.ClickOptions clickOptionsWithTimeout(int timeout) {
+        return new Locator.ClickOptions().setTimeout(timeout);
+    }
+
+    public static Page.GetByRoleOptions getRoleOptions(String buttonText) {
+        return new Page.GetByRoleOptions().setName(buttonText);
+    }
+
+    public static void clickButton(Page page, String buttonText) {
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName(buttonText)).click();
+    }
+
+    public static Locator getButtonByText(Page page, String buttonText) {
+        return page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName(buttonText));
+    }
+
+    public static Locator getRadioButtonByLabel(Page page, String label) {
+        return page.getByRole(AriaRole.RADIO, new Page.GetByRoleOptions().setName(label));
+    }
+
+    public static Locator getListBoxByLabel(Page page, String label) {
+        return page.getByRole(AriaRole.LISTBOX, new Page.GetByRoleOptions().setName(label));
+    }
+
+    public static Locator getCheckBoxByLabel(Page page, String label) {
+        return page.getByRole(AriaRole.CHECKBOX, new Page.GetByRoleOptions().setName(label));
+    }
+
+    public static Locator getTabByText(Page page, String tabName) {
+        return page.getByRole(AriaRole.TAB, new Page.GetByRoleOptions().setName(tabName));
+    }
+
+    public static Locator getTextBoxByLabel(Page page, String label) {
+        return page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName(label));
+    }
+
+    public static String getValueFromTableFor(Page page, String rowHeader) {
+        return page.locator("th:has-text(\"" + rowHeader + "\") + td").textContent();
+    }
+
+    public static void clickLink(Page page, String linkText) {
+        page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName(linkText)).click();
+    }
+}
