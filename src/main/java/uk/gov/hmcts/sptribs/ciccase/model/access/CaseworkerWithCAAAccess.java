0e2a97afe5e563ceb7fe4dc8492e8eff97b6a028
Sabir <123706441+Sabir-v1@users.noreply.github.com>
2023-03-16 11:24:26 +0000

    Update CaseworkerWithCAAAccess.java

--- src/main/java/uk/gov/hmcts/sptribs/ciccase/model/access/CaseworkerWithCAAAccess.java
+++ src/main/java/uk/gov/hmcts/sptribs/ciccase/model/access/CaseworkerWithCAAAccess.java
@@ -9,8 +9,8 @@ import uk.gov.hmcts.ccd.sdk.api.Permission;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.CITIZEN_CIC;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.COURT_ADMIN_CIC;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.CREATOR;
-import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.SOLICITOR;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.DISTRICT_JUDGE_CIC;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.SOLICITOR;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.SUPER_USER;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.SYSTEMUPDATE;
     
