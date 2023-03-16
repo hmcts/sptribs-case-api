373be4dde376662875fc4d5105b771ca4ebc558d
Paul Pearson <paul.pearson@version1.com>
2023-03-16 15:24:58 +0000

    ST-1139 Implement refer case reason page

---
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/model/ReferToJudge.java
@@ -0,0 +1,33 @@
+package uk.gov.hmcts.sptribs.caseworker.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uk.gov.hmcts.ccd.sdk.api.CCD;
+import uk.gov.hmcts.sptribs.ciccase.model.access.CaseworkerWithCAAAccess;
+import uk.gov.hmcts.sptribs.ciccase.model.access.DefaultAccess;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonNaming(PropertyNamingStrategies.UpperCamelCaseStrategy.class)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ReferToJudge {
+
+    @CCD(
+        label = "Why are you referring this case?",
+        access = {DefaultAccess.class, CaseworkerWithCAAAccess.class}
+    )
+    private ReferralReason referralReason;
+
+    @CCD(
+        label = "Reason for referral",
+        access = {DefaultAccess.class, CaseworkerWithCAAAccess.class}
+    )
+    private String reasonForReferral;
+}
