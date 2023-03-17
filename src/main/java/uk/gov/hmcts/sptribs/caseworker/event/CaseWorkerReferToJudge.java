b7d70f92eb146b9766b841a9401fddcb39fc7765
Paul Pearson <46532983+paul-pearson@users.noreply.github.com>
2023-03-17 13:04:24 +0000

    ST-1140 Add additional info page (#681)
    * ST-1140 Add additional info page
    
    * ST-1140 Fix test
    
    * ST-1140 Allow test coverage for dependencies

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerReferToJudge.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerReferToJudge.java
@@ -1,10 +1,10 @@
 package uk.gov.hmcts.sptribs.caseworker.event;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.sptribs.caseworker.event.page.ReferToJudgeAdditionalInfo;
 import uk.gov.hmcts.sptribs.caseworker.event.page.ReferToJudgeReason;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
@@ -26,8 +26,9 @@ import static uk.gov.hmcts.sptribs.ciccase.model.access.Permissions.CREATE_READ_
 @Slf4j
 public class CaseWorkerReferToJudge implements CCDConfig<CaseData, State, UserRole> {
 
-    @Autowired
-    private ReferToJudgeReason referToJudgeReason;
+    private ReferToJudgeReason referToJudgeReason = new ReferToJudgeReason();
+
+    private ReferToJudgeAdditionalInfo referToJudgeAdditionalInfo = new ReferToJudgeAdditionalInfo();
 
     @Override
     public void configure(ConfigBuilder<CaseData, State, UserRole> configBuilder) {
@@ -43,5 +44,6 @@ public class CaseWorkerReferToJudge implements CCDConfig<CaseData, State, UserRo
             .grant(CREATE_READ_UPDATE_DELETE, COURT_ADMIN_CIC, SUPER_USER)
             .grantHistoryOnly(SOLICITOR));
         referToJudgeReason.addTo(pageBuilder);
+        referToJudgeAdditionalInfo.addTo(pageBuilder);
     }
 }
--- src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerReferToJudgeTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerReferToJudgeTest.java
@@ -3,11 +3,9 @@ package uk.gov.hmcts.sptribs.caseworker.event;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
 import uk.gov.hmcts.ccd.sdk.api.Event;
-import uk.gov.hmcts.sptribs.caseworker.event.page.ReferToJudgeReason;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
@@ -19,9 +17,6 @@ import static uk.gov.hmcts.sptribs.testutil.ConfigTestUtil.getEventsFrom;
 
 @ExtendWith(MockitoExtension.class)
 public class CaseWorkerReferToJudgeTest {
-
-    @Mock
-    private ReferToJudgeReason referToJudgeReason;
 
     @InjectMocks
     private CaseWorkerReferToJudge caseWorkerReferToJudge;
