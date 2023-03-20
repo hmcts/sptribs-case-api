92a28aa1393c0ab871a49067408d9d97cbe1c280
ElifDenizEsea <61947470+ElifDenizEsea@users.noreply.github.com>
2023-03-20 14:37:13 +0000

    ST-1142 Conformation Page (#690)
    Co-authored-by: hmctscivil <version1rhel@hmctscivil.mshome.net>

--- src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerReferToJudgeTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerReferToJudgeTest.java
@@ -5,7 +5,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.Event;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
@@ -14,6 +17,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.hmcts.sptribs.caseworker.util.EventConstants.CASEWORKER_REFER_TO_JUDGE;
 import static uk.gov.hmcts.sptribs.testutil.ConfigTestUtil.createCaseDataConfigBuilder;
 import static uk.gov.hmcts.sptribs.testutil.ConfigTestUtil.getEventsFrom;
+import static uk.gov.hmcts.sptribs.testutil.TestConstants.TEST_CASE_ID;
+import static uk.gov.hmcts.sptribs.testutil.TestDataHelper.LOCAL_DATE_TIME;
+import static uk.gov.hmcts.sptribs.testutil.TestDataHelper.caseData;
 
 @ExtendWith(MockitoExtension.class)
 public class CaseWorkerReferToJudgeTest {
@@ -35,4 +41,26 @@ public class CaseWorkerReferToJudgeTest {
             .contains(CASEWORKER_REFER_TO_JUDGE);
     }
 
+
+    @Test
+    void shouldReferToJudge() {
+        //Given
+
+        CaseData caseData = caseData();
+        final CaseDetails<CaseData, State> updatedCaseDetails = new CaseDetails<>();
+        final CaseDetails<CaseData, State> beforeDetails = new CaseDetails<>();
+        updatedCaseDetails.setData(caseData);
+        updatedCaseDetails.setId(TEST_CASE_ID);
+        updatedCaseDetails.setCreatedDate(LOCAL_DATE_TIME);
+
+        //When
+        AboutToStartOrSubmitResponse<CaseData, State> response1 =
+            caseWorkerReferToJudge.aboutToSubmit(updatedCaseDetails, beforeDetails);
+        SubmittedCallbackResponse response2 =
+            caseWorkerReferToJudge.referred(updatedCaseDetails, beforeDetails);
+
+        //Then
+        assertThat(response1).isNotNull();
+        assertThat(response2).isNotNull();
+    }
 }
