8ec76b4a006ecd2e664e53e1db79f7a0aecde7a5
ElifDenizEsea <61947470+ElifDenizEsea@users.noreply.github.com>
2023-03-21 11:14:29 +0000

    ST-1147 Conformation Page (#698)
    ST-1142 Conformation Page
    
    Co-authored-by: hmctscivil <version1rhel@hmctscivil.mshome.net>

--- src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerReferToLegalOfficerTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerReferToLegalOfficerTest.java
@@ -3,11 +3,12 @@ package uk.gov.hmcts.sptribs.caseworker.event;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.Event;
-import uk.gov.hmcts.sptribs.caseworker.event.page.ReferToLegalOfficerReason;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
@@ -16,12 +17,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.hmcts.sptribs.caseworker.util.EventConstants.CASEWORKER_REFER_TO_LEGAL_OFFICER;
 import static uk.gov.hmcts.sptribs.testutil.ConfigTestUtil.createCaseDataConfigBuilder;
 import static uk.gov.hmcts.sptribs.testutil.ConfigTestUtil.getEventsFrom;
+import static uk.gov.hmcts.sptribs.testutil.TestConstants.TEST_CASE_ID;
+import static uk.gov.hmcts.sptribs.testutil.TestDataHelper.LOCAL_DATE_TIME;
+import static uk.gov.hmcts.sptribs.testutil.TestDataHelper.caseData;
 
 @ExtendWith(MockitoExtension.class)
 class CaseWorkerReferToLegalOfficerTest {
 
-    @Mock
-    private ReferToLegalOfficerReason referToLegalOfficerReason;
     @InjectMocks
     private CaseWorkerReferToLegalOfficer caseWorkerReferToLegalOfficer;
 
@@ -40,4 +42,24 @@ class CaseWorkerReferToLegalOfficerTest {
             .contains(CASEWORKER_REFER_TO_LEGAL_OFFICER);
     }
 
+    @Test
+    void shouldReferToLegalOfficer() {
+        //Given
+        CaseData caseData = caseData();
+        final CaseDetails<CaseData, State> updatedCaseDetails = new CaseDetails<>();
+        final CaseDetails<CaseData, State> beforeDetails = new CaseDetails<>();
+        updatedCaseDetails.setData(caseData);
+        updatedCaseDetails.setId(TEST_CASE_ID);
+        updatedCaseDetails.setCreatedDate(LOCAL_DATE_TIME);
+
+        //When
+        AboutToStartOrSubmitResponse<CaseData, State> response1 =
+            caseWorkerReferToLegalOfficer.aboutToSubmit(updatedCaseDetails, beforeDetails);
+        SubmittedCallbackResponse response2 =
+            caseWorkerReferToLegalOfficer.referred(updatedCaseDetails, beforeDetails);
+
+        //Then
+        assertThat(response1).isNotNull();
+        assertThat(response2.getConfirmationHeader()).contains("Referral completed");
+    }
 }
