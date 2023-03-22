7c5333ed285db3982369eac19719910e3aa6a509
ElifDenizEsea <61947470+ElifDenizEsea@users.noreply.github.com>
2023-03-22 12:02:36 +0000

    ST-1156 Orders and decisions tab (#710)
    Co-authored-by: hmctscivil <version1rhel@hmctscivil.mshome.net>

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/model/CaseIssueFinalDecision.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/model/CaseIssueFinalDecision.java
@@ -8,15 +8,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import uk.gov.hmcts.ccd.sdk.api.CCD;
 import uk.gov.hmcts.ccd.sdk.type.Document;
-import uk.gov.hmcts.ccd.sdk.type.ListValue;
 import uk.gov.hmcts.sptribs.ciccase.model.DecisionTemplate;
 import uk.gov.hmcts.sptribs.ciccase.model.access.CaseworkerWithCAAAccess;
 import uk.gov.hmcts.sptribs.ciccase.model.access.DefaultAccess;
 import uk.gov.hmcts.sptribs.document.model.CICDocument;
 
-import java.util.List;
-
-import static uk.gov.hmcts.ccd.sdk.type.FieldType.Collection;
 import static uk.gov.hmcts.ccd.sdk.type.FieldType.FixedList;
 
 @Data
@@ -39,7 +35,7 @@ public class CaseIssueFinalDecision {
     private DecisionTemplate decisionTemplate;
 
     @CCD(
-        label = "Final decision notice preview",
+        label = "Final Decision Document",
         access = {DefaultAccess.class, CaseworkerWithCAAAccess.class}
     )
     private Document finalDecisionDraft;
@@ -51,10 +47,9 @@ public class CaseIssueFinalDecision {
     private Document finalDecisionGuidance;
 
     @CCD(
-        label = "Decision notice documents",
-        typeOverride = Collection,
+        label = "Final Decision Document",
         typeParameterOverride = "CICDocument",
         access = {DefaultAccess.class}
     )
-    private List<ListValue<CICDocument>> documents;
+    private CICDocument document;
 }
