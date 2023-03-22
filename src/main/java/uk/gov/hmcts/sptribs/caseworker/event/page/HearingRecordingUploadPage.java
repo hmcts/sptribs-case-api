b6a0e94e1e48b0773bf00c71defc71cc96c40ed3
Jeff Nayler <jeff.nayler@hmcts.net>
2023-03-22 22:43:51 +0000

    ST-1155 . fix missing headings

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/page/HearingRecordingUploadPage.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/page/HearingRecordingUploadPage.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.sptribs.caseworker.model.HearingSummary;
 import uk.gov.hmcts.sptribs.caseworker.model.Listing;
-import uk.gov.hmcts.sptribs.caseworker.util.PageShowConditionsUtil;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.common.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
