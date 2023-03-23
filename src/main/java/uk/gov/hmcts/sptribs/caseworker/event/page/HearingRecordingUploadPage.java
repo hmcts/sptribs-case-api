de9e522cf63df9632fee7fd524d37ea383972f5d
Jeff Nayler <jeff.nayler@hmcts.net>
2023-03-23 13:19:18 +0000

    ST-1155 , restore pageshowcondition status

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/page/HearingRecordingUploadPage.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/page/HearingRecordingUploadPage.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.sptribs.caseworker.model.HearingSummary;
 import uk.gov.hmcts.sptribs.caseworker.model.Listing;
+import uk.gov.hmcts.sptribs.caseworker.util.PageShowConditionsUtil;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.common.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
@@ -17,6 +18,7 @@ public class HearingRecordingUploadPage implements CcdPageConfiguration {
         pageBuilder.page("hearingRecordingUploadPage")
             .pageLabel("Upload hearing recording")
             .label("LabelHearingRecordingUploadPage","")
+            .pageShowConditions(PageShowConditionsUtil.editSummaryShowConditions())
             .label("theHearingRecordingUpload",
                 "\n<h2>Upload the recording of the hearing (Optional)</h2>\n"
                     + "\n\nAdvice on uploads\n\n"
