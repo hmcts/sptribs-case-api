8b1f596fd65cb966125925ab25fe69457cb9e066
Paul Pearson <paul.pearson@version1.com>
2023-03-28 10:09:15 +0100

    ST-1087 Remove judicial roles from test user
    This allows the test user to create cases.

--- src/cftlib/java/uk/gov/hmcts/sptribs/cftlib/CftLibConfig.java
+++ src/cftlib/java/uk/gov/hmcts/sptribs/cftlib/CftLibConfig.java
@@ -53,6 +53,7 @@ public class CftLibConfig implements CFTLibConfigurer {
             "caseworker-st_cic-senior-caseworker",
             "caseworker-st_cic-hearing-centre-admin",
             "caseworker-st_cic-hearing-centre-team-leader",
+            "caseworker-st_cic-senior-judge",
             "caseworker-st_cic-respondent");
 
         var users = Map.of(
