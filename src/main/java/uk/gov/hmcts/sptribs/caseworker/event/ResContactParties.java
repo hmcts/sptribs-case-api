6196810ec83105321d5ceb42c6b80c55dc583ee7
ATedy <weditedy@gmail.com>
2023-03-31 12:07:38 +0100

    working on event error

---
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/ResContactParties.java
@@ -0,0 +1,102 @@
+package uk.gov.hmcts.sptribs.caseworker.event;
+
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
+import uk.gov.hmcts.sptribs.caseworker.util.MessageUtil;
+import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
+import uk.gov.hmcts.sptribs.ciccase.model.State;
+import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
+import uk.gov.hmcts.sptribs.common.ccd.CcdPageConfiguration;
+import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
+import uk.gov.hmcts.sptribs.common.event.page.ResPartiesToContact;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.String.format;
+import static uk.gov.hmcts.sptribs.caseworker.util.EventConstants.RESPONDENT_CONTACT_PARTIES;
+import static uk.gov.hmcts.sptribs.ciccase.model.State.AwaitingHearing;
+import static uk.gov.hmcts.sptribs.ciccase.model.State.AwaitingOutcome;
+import static uk.gov.hmcts.sptribs.ciccase.model.State.CaseClosed;
+import static uk.gov.hmcts.sptribs.ciccase.model.State.CaseManagement;
+import static uk.gov.hmcts.sptribs.ciccase.model.State.CaseStayed;
+import static uk.gov.hmcts.sptribs.ciccase.model.State.Draft;
+import static uk.gov.hmcts.sptribs.ciccase.model.State.NewCaseReceived;
+import static uk.gov.hmcts.sptribs.ciccase.model.State.Rejected;
+import static uk.gov.hmcts.sptribs.ciccase.model.State.Submitted;
+import static uk.gov.hmcts.sptribs.ciccase.model.State.Withdrawn;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.SOLICITOR;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_RESPONDENT;
+import static uk.gov.hmcts.sptribs.ciccase.model.access.Permissions.CREATE_READ_UPDATE_DELETE;
+
+
+@Component
+@Slf4j
+@Setter
+public class ResContactParties implements CCDConfig<CaseData, State, UserRole> {
+
+    private static final CcdPageConfiguration resPartiesToContact= new ResPartiesToContact();
+//    @Value("${feature.contact-parties.enabled}")
+//    private boolean contactPartiesEnabled;
+//
+//
+//    @Override
+//    public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
+//        if (contactPartiesEnabled) {
+//            doConfigure(configBuilder);
+//        }
+//    }
+
+    public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
+        PageBuilder pageBuilder = new PageBuilder(
+            configBuilder
+                .event(RESPONDENT_CONTACT_PARTIES)
+                .forStates(Draft,
+                    Withdrawn,
+                    Rejected,
+                    Submitted,
+                    NewCaseReceived,
+                    CaseManagement,
+                    AwaitingHearing,
+                    AwaitingOutcome,
+                    CaseClosed,
+                    CaseStayed)
+                .name("Case: Respondent Contact parties")
+                .showSummary()
+                .aboutToSubmitCallback(this::aboutToSubmit)
+                .submittedCallback(this::partiesContacted)
+                .grant(CREATE_READ_UPDATE_DELETE, ST_CIC_RESPONDENT)
+                .grantHistoryOnly(SOLICITOR));
+        resPartiesToContact.addTo(pageBuilder);
+    }
+
+    public AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmit(CaseDetails<CaseData, State> details,
+                                                                      CaseDetails<CaseData, State> detailsBefore) {
+        final CaseData data = details.getData();
+        final List<String> errors = new ArrayList<>();
+
+
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .data(data)
+            .errors(errors)
+            .build();
+    }
+
+    public SubmittedCallbackResponse partiesContacted(CaseDetails<CaseData, State> details,
+                                                      CaseDetails<CaseData, State> beforeDetails) {
+
+        return SubmittedCallbackResponse.builder()
+            .confirmationHeader(format("# Message sent %n## %s",
+                MessageUtil.generateSimpleMessage(details.getData().getContactParties())
+            ))
+            .build();
+
+    }
+
+}
---
+++ src/main/java/uk/gov/hmcts/sptribs/common/event/page/ResPartiesToContact.java
@@ -0,0 +1,56 @@
+package uk.gov.hmcts.sptribs.common.event.page;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.sptribs.caseworker.model.ContactParties;
+import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
+import uk.gov.hmcts.sptribs.ciccase.model.State;
+import uk.gov.hmcts.sptribs.common.ccd.CcdPageConfiguration;
+import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Component
+public class ResPartiesToContact implements CcdPageConfiguration {
+
+
+    @Override
+    public void addTo(PageBuilder pageBuilder) {
+        pageBuilder
+            .page("partiesToContact", this::midEvent)
+            .pageLabel("Which parties do you want to contact?")
+            .label("LabelPartiesToContact", "")
+            .complex(CaseData::getContactParties)
+            .optional(ContactParties::getSubjectContactParties)
+            .optional(ContactParties::getRepresentativeContactParties, "cicCaseRepresentativeFullName!=\"\" ")
+            .optional(ContactParties::getTribunal)
+            .mandatory(ContactParties::getMessage)
+            .done();
+    }
+
+    public AboutToStartOrSubmitResponse<CaseData, State> midEvent(CaseDetails<CaseData, State> details,
+                                                                  CaseDetails<CaseData, State> detailsBefore) {
+        final CaseData data = details.getData();
+        final List<String> errors = new ArrayList<>();
+
+
+        if (null != data.getContactParties() && CollectionUtils.isEmpty(data.getContactParties().getRepresentativeContactParties())
+            && CollectionUtils.isEmpty(data.getContactParties().getSubjectContactParties())
+            && CollectionUtils.isEmpty(data.getContactParties().getTribunal())) {
+
+            errors.add("Which parties do you want to contact?. is required.");
+        }
+
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .data(data)
+            .errors(errors)
+            .build();
+    }
+
+}
+
