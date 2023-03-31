d1b606c543bd8a3f1faefe36eb719254bb543ecb
muradalamv1 <107861321+muradalamv1@users.noreply.github.com>
2023-03-31 14:57:43 +0100

    ST-1077_Redundant_Code_PR2 Removed some redundant code (#753)
    * Removed some redundant code.
    
    * Removed some redundant code.
    
    * Removed some redundant code.
    
    * Removed some redundant code.
    
    * Removed some redundant code.
    
    * Removed some redundant code.
    
    * Removed some redundant code.
    
    * Removed some redundant code.
    
    * Removed some redundant code.

--- bin/add-roles.sh
+++ bin/add-roles.sh
@@ -3,11 +3,7 @@
 scriptPath=$(dirname $(realpath $0))
 
 # Roles used during the CCD import
-#${scriptPath}/add-ccd-role.sh "caseworker-divorce-superuser"
 #${scriptPath}/add-ccd-role.sh "caseworker-sptribs-superuser"
-#${scriptPath}/add-ccd-role.sh "caseworker-divorce-solicitor"
-#${scriptPath}/add-ccd-role.sh "caseworker-divorce-systemupdate"
-#${scriptPath}/add-ccd-role.sh "payments"
 #${scriptPath}/add-ccd-role.sh "caseworker-st_cic"
 #${scriptPath}/add-ccd-role.sh "caseworker-sptribs-cic-courtadmin"
 #${scriptPath}/add-ccd-role.sh "caseworker-sptribs-cic-caseofficer"
