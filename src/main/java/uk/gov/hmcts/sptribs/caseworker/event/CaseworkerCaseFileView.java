af7be14dc70a4636d618fb09b110020a5842a8a1
Paul Pearson <46532983+paul-pearson@users.noreply.github.com>
2023-04-04 13:58:06 +0100

    ST-1113 Add feature flag (#769)
    * ST-1113 Add feature flag
    
    Adds feature flag for case file view
    and document management.
    
    * ST-1113 suppress cve

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerCaseFileView.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerCaseFileView.java
@@ -1,7 +1,9 @@
 package uk.gov.hmcts.sptribs.caseworker.event;
 
+import lombok.Setter;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
@@ -20,10 +22,20 @@ import static uk.gov.hmcts.sptribs.ciccase.model.access.Permissions.CREATE_READ_
 
 @Component
 @Slf4j
+@Setter
 public class CaseworkerCaseFileView implements CCDConfig<CaseData, State, UserRole> {
+
+    @Value("${feature.case-file-view-and-document-management.enabled}")
+    private boolean caseFileViewAndDocumentManagementEnabled;
 
     @Override
     public void configure(ConfigBuilder<CaseData, State, UserRole> configBuilder) {
+        if (caseFileViewAndDocumentManagementEnabled) {
+            doConfigure(configBuilder);
+        }
+    }
+
+    private void doConfigure(ConfigBuilder<CaseData, State, UserRole> configBuilder) {
         PageBuilder pageBuilder = new PageBuilder(
             configBuilder
             .event(CASEWORKER_CASE_FILE_VIEW)
