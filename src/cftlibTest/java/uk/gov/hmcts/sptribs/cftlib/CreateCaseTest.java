c40b2add3925a9efc8c0b549afeaa7e28ef153b1
shailesh-walke <shailesh.walke@version1.com>
2023-04-06 11:21:16 +0100

    testing failure of cftlibtests

--- src/cftlibTest/java/uk/gov/hmcts/sptribs/cftlib/CreateCaseTest.java
+++ src/cftlibTest/java/uk/gov/hmcts/sptribs/cftlib/CreateCaseTest.java
@@ -1,14 +1,14 @@
 package uk.gov.hmcts.sptribs.cftlib;
 
 import com.microsoft.playwright.Page;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import uk.gov.hmcts.sptribs.cftlib.action.Case;
 import uk.gov.hmcts.sptribs.cftlib.util.Login;
 
 public class CreateCaseTest extends XuiTest {
 
-    @Disabled
+    @Test
     public void createCase() {
         Mockito.doNothing().when(applicationReceivedNotification).sendToSubject(Mockito.any(), Mockito.any());
         Mockito.doNothing().when(applicationReceivedNotification).sendToApplicant(Mockito.any(), Mockito.any());
