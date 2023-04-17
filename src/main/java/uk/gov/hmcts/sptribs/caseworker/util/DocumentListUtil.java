1d1af9c33573aa0134b3d4bc931ee5e68e1bd063
ElifDenizEsea <61947470+ElifDenizEsea@users.noreply.github.com>
2023-04-17 13:22:55 +0100

    ST-645 Select additional documentation (#819)
    ST-645 Select additonal documentation
    
    Co-authored-by: hmctscivil <version1rhel@hmctscivil.mshome.net>

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/util/DocumentListUtil.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/util/DocumentListUtil.java
@@ -1,12 +1,17 @@
 package uk.gov.hmcts.sptribs.caseworker.util;
 
 import org.springframework.util.CollectionUtils;
-import uk.gov.hmcts.ccd.sdk.type.DynamicList;
+import org.springframework.util.ObjectUtils;
 import uk.gov.hmcts.ccd.sdk.type.DynamicListElement;
+import uk.gov.hmcts.ccd.sdk.type.DynamicMultiSelectList;
 import uk.gov.hmcts.ccd.sdk.type.ListValue;
+import uk.gov.hmcts.sptribs.caseworker.model.ApplicationEvidence;
+import uk.gov.hmcts.sptribs.caseworker.model.CaseIssue;
+import uk.gov.hmcts.sptribs.caseworker.model.TribunalDocuments;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.CicCase;
 import uk.gov.hmcts.sptribs.document.model.CaseworkerCICDocument;
+import uk.gov.hmcts.sptribs.document.model.DocumentType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -42,20 +47,44 @@ public final class DocumentListUtil {
         return docList;
     }
 
-
-    public static DynamicList prepareDocumentList(final CaseData data) {
+    public static DynamicMultiSelectList prepareDocumentList(final CaseData data, boolean withCaseIssueFilters) {
         List<CaseworkerCICDocument> docList = prepareList(data);
-
+        if (withCaseIssueFilters) {
+            docList = filterCaseIssue(docList, data.getCaseIssue());
+        }
         List<DynamicListElement> dynamicListElements = docList
             .stream()
-            .sorted()
-            .map(doc -> DynamicListElement.builder().label(doc.getDocumentLink().getFilename()).code(UUID.randomUUID()).build())
+            .map(doc -> DynamicListElement.builder().label(doc.getDocumentLink().getFilename()
+                + "--" + doc.getDocumentLink().getUrl()).code(UUID.randomUUID()).build())
             .collect(Collectors.toList());
 
-        return DynamicList
+        return DynamicMultiSelectList
             .builder()
             .listItems(dynamicListElements)
+            .value(new ArrayList<>())
             .build();
+    }
+
+    private static List<CaseworkerCICDocument> filterCaseIssue(List<CaseworkerCICDocument> docList, CaseIssue caseIssue) {
+        List<CaseworkerCICDocument> filteredList = new ArrayList<>();
+        List<DocumentType> documentTypes = new ArrayList<>();
+        if (!CollectionUtils.isEmpty(caseIssue.getTribunalDocuments())) {
+            for (TribunalDocuments tribunalDocuments : caseIssue.getTribunalDocuments()) {
+                documentTypes.add(tribunalDocuments.getDocumentType());
+            }
+        }
+        if (!CollectionUtils.isEmpty(caseIssue.getApplicationEvidences())) {
+            for (ApplicationEvidence applicationEvidence : caseIssue.getApplicationEvidences()) {
+                documentTypes.add(applicationEvidence.getDocumentType());
+            }
+        }
+        for (CaseworkerCICDocument caseworkerCICDocument : docList) {
+            if (!ObjectUtils.isEmpty(caseworkerCICDocument.getDocumentCategory())
+                && documentTypes.contains(caseworkerCICDocument.getDocumentCategory())) {
+                filteredList.add(caseworkerCICDocument);
+            }
+        }
+        return filteredList;
     }
 
     private static List<CaseworkerCICDocument> getReinstateDocuments(CicCase cicCase) {
@@ -68,7 +97,7 @@ public final class DocumentListUtil {
         return reinstateDocList;
     }
 
-    private  static List<CaseworkerCICDocument> getCaseDocs(CicCase cicCase) {
+    private static List<CaseworkerCICDocument> getCaseDocs(CicCase cicCase) {
         List<CaseworkerCICDocument> caseDocs = new ArrayList<>();
         if (!CollectionUtils.isEmpty(cicCase.getApplicantDocumentsUploaded())) {
             for (ListValue<CaseworkerCICDocument> document : cicCase.getApplicantDocumentsUploaded()) {
@@ -113,7 +142,7 @@ public final class DocumentListUtil {
         return buildListValues(getDecisionDocs(caseData));
     }
 
-    public  static List<ListValue<CaseworkerCICDocument>> getAllFinalDecisionDocuments(CaseData caseData) {
+    public static List<ListValue<CaseworkerCICDocument>> getAllFinalDecisionDocuments(CaseData caseData) {
         return buildListValues(getFinalDecisionDocs(caseData));
     }
 
