797206d37ff8bb3104f5451e61d2f2a616796444
ElifDenizEsea <61947470+ElifDenizEsea@users.noreply.github.com>
2023-04-18 16:27:26 +0100

    ST-856 Adding bundling events and fields to CCD config (#824)
    Co-authored-by: hmctscivil <version1rhel@hmctscivil.mshome.net>

---
+++ src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerEditBundleTest.java
@@ -0,0 +1,62 @@
+package uk.gov.hmcts.sptribs.caseworker.event;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.ccd.sdk.api.Event;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
+import uk.gov.hmcts.sptribs.ciccase.model.State;
+import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.hmcts.sptribs.testutil.ConfigTestUtil.createCaseDataConfigBuilder;
+import static uk.gov.hmcts.sptribs.testutil.ConfigTestUtil.getEventsFrom;
+import static uk.gov.hmcts.sptribs.testutil.TestConstants.TEST_CASE_ID;
+import static uk.gov.hmcts.sptribs.testutil.TestDataHelper.LOCAL_DATE_TIME;
+import static uk.gov.hmcts.sptribs.testutil.TestDataHelper.caseData;
+import static uk.gov.hmcts.sptribs.testutil.TestEventConstants.EDIT_BUNDLE;
+
+@ExtendWith(MockitoExtension.class)
+class CaseworkerEditBundleTest {
+
+    @InjectMocks
+    private CaseworkerEditBundle caseworkerEditBundle;
+
+    @Test
+    void shouldAddConfigurationToConfigBuilder() throws Exception {
+        //Given
+        final ConfigBuilderImpl<CaseData, State, UserRole> configBuilder = createCaseDataConfigBuilder();
+
+        //When
+        caseworkerEditBundle.configure(configBuilder);
+
+        //Then
+        assertThat(getEventsFrom(configBuilder).values())
+            .extracting(Event::getId)
+            .contains(EDIT_BUNDLE);
+    }
+
+    @Test
+    public void shouldSuccessfullyCreateBundle() {
+        //Given
+        final CaseData caseData = caseData();
+
+        final CaseDetails<CaseData, State> updatedCaseDetails = new CaseDetails<>();
+        updatedCaseDetails.setData(caseData);
+        updatedCaseDetails.setId(TEST_CASE_ID);
+        updatedCaseDetails.setCreatedDate(LOCAL_DATE_TIME);
+
+        //When
+        AboutToStartOrSubmitResponse<CaseData, State> response =
+            caseworkerEditBundle.aboutToSubmit(updatedCaseDetails, CaseDetails.<CaseData, State>builder().build());
+
+        //Then
+        assertThat(response.getData()).isNotNull();
+    }
+
+
+}
