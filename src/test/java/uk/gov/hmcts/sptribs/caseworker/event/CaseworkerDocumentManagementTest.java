ff70a5768f40d044b290a3ca666930e9f77f9e31
sjamihmcts <110464983+sjamihmcts@users.noreply.github.com>
2023-04-19 16:40:46 +0100

    Feat/st 1030 (#831)
    * ST-1030: In progress changes
    
    * ST-1030 - Initial commit with working Case File View
    
    * ST-1030: Added categoryIds to few documents
    
    * ST-1068: Moved common logic to Helper class
    
    * ST-1030: Add category_id to Document class
    
    * ST-1030: Successfully categorise the Document in CICDocument class
    
    * ST-1030: Removed unnecessary check
    
    * ST-1030: More changes in progress
    
    * ST-1030: Regex validation and category updation for documents uploaded from computer
    
    * ST-1030: Fixed build and test errors
    
    * ST-1030: Pointed to updated version of ccd-config-generator
    
    * ST-1030: Added tests to improve code coverage
    
    * ST-1030: Fixed failing test
    
    * ST-1030: Organised imports
    
    * ST-1030: Fixed checkstyle errors
    
    * ST-1030: Fix for Built Case issue - Mistakenly deleted as part of code merge.
    
    * ST-1030: Checkstyle issue fixed
    
    * ST-1030: Commented the midEvent call to check if this is causing the issue
    
    * ST-1030: Commented the midEvent call to check if this is causing the issue 2
    
    * ST-1030: Added log statements to capture error if any
    
    * ST-1030: Fix for de-serialisation error
    
    * ST-1030: Moved upload documents to new page class
    
    * ST-1030: Added more tests for coverage
    
    * ST-1030: Commented the code related to Case File View feature. To find out if the issue in AAT is due to the change with feature or midEvent itself.
    
    * ST-1030: corrected the name to see if it fixes
    
    * ST-1030: Removed unused imports
    
    * ST-1030: Added package level logging
    
    * ST-1030: Reverted the commented code related to File upload midEvent
    
    * ST-1030: Reverting the logging level to DEBUG change
    
    * ST-1030: Reverted the changes commented
    
    * ST-1030: Updated tests
    
    * ST-1030: Added category to files uploaded as part of Document Management upload
    
    * ST-1030: Removed unused imports
    
    * ST-1030: Removed unused imports

--- src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerDocumentManagementTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerDocumentManagementTest.java
@@ -7,7 +7,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.Event;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
+import uk.gov.hmcts.sptribs.caseworker.event.page.UploadCaseDocuments;
+import uk.gov.hmcts.sptribs.caseworker.model.DocumentManagement;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
@@ -18,6 +21,7 @@ import static uk.gov.hmcts.sptribs.testutil.ConfigTestUtil.getEventsFrom;
 import static uk.gov.hmcts.sptribs.testutil.TestConstants.TEST_CASE_ID;
 import static uk.gov.hmcts.sptribs.testutil.TestDataHelper.LOCAL_DATE_TIME;
 import static uk.gov.hmcts.sptribs.testutil.TestDataHelper.caseData;
+import static uk.gov.hmcts.sptribs.testutil.TestDataHelper.getCaseworkerCICDocumentList;
 import static uk.gov.hmcts.sptribs.testutil.TestEventConstants.CASEWORKER_DOCUMENT_MANAGEMENT;
 
 @ExtendWith(MockitoExtension.class)
@@ -25,6 +29,9 @@ public class CaseworkerDocumentManagementTest {
 
     @InjectMocks
     private CaseworkerDocumentManagement caseworkerDocumentManagement;
+
+    @InjectMocks
+    private UploadCaseDocuments uploadCaseDocuments;
 
     @Test
     void shouldAddConfigurationToConfigBuilder() {
@@ -61,6 +68,10 @@ public class CaseworkerDocumentManagementTest {
         final CaseData caseData = caseData();
         final CaseDetails<CaseData, State> updatedCaseDetails = new CaseDetails<>();
         final CaseDetails<CaseData, State> beforeDetails = new CaseDetails<>();
+        DocumentManagement documentManagement = DocumentManagement.builder()
+            .caseworkerCICDocument(getCaseworkerCICDocumentList())
+            .build();
+        caseData.setDocManagement(documentManagement);
         beforeDetails.setData(caseData);
         updatedCaseDetails.setData(caseData);
         updatedCaseDetails.setState(State.CaseManagement);
@@ -68,9 +79,16 @@ public class CaseworkerDocumentManagementTest {
         updatedCaseDetails.setCreatedDate(LOCAL_DATE_TIME);
 
         //When
+
+        AboutToStartOrSubmitResponse<CaseData, State> midResponse =
+            uploadCaseDocuments.midEvent(updatedCaseDetails, beforeDetails);
+        AboutToStartOrSubmitResponse<CaseData, State> response =
+            caseworkerDocumentManagement.aboutToSubmit(updatedCaseDetails, beforeDetails);
         SubmittedCallbackResponse documentMgmtResponse = caseworkerDocumentManagement.submitted(updatedCaseDetails, beforeDetails);
 
         //Then
+        assertThat(midResponse).isNotNull();
+        assertThat(response).isNotNull();
         assertThat(documentMgmtResponse).isNotNull();
     }
 }
