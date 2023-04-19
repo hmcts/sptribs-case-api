726d075775d3f471d52b8af3116be42f94e26e40
sjamihmcts <santoshini.jami@hmcts.net>
2023-04-19 16:09:29 +0100

    ST-1030: Added category to files uploaded as part of Document Management upload

--- src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerDocumentManagementTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerDocumentManagementTest.java
@@ -7,10 +7,16 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.Event;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
+import uk.gov.hmcts.sptribs.caseworker.event.page.ShowCaseDocuments;
+import uk.gov.hmcts.sptribs.caseworker.event.page.UploadCaseDocuments;
+import uk.gov.hmcts.sptribs.caseworker.model.DocumentManagement;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
+
+import javax.print.Doc;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.hmcts.sptribs.testutil.ConfigTestUtil.createCaseDataConfigBuilder;
@@ -18,6 +24,7 @@ import static uk.gov.hmcts.sptribs.testutil.ConfigTestUtil.getEventsFrom;
 import static uk.gov.hmcts.sptribs.testutil.TestConstants.TEST_CASE_ID;
 import static uk.gov.hmcts.sptribs.testutil.TestDataHelper.LOCAL_DATE_TIME;
 import static uk.gov.hmcts.sptribs.testutil.TestDataHelper.caseData;
+import static uk.gov.hmcts.sptribs.testutil.TestDataHelper.getCaseworkerCICDocumentList;
 import static uk.gov.hmcts.sptribs.testutil.TestEventConstants.CASEWORKER_DOCUMENT_MANAGEMENT;
 
 @ExtendWith(MockitoExtension.class)
@@ -25,6 +32,9 @@ public class CaseworkerDocumentManagementTest {
 
     @InjectMocks
     private CaseworkerDocumentManagement caseworkerDocumentManagement;
+
+    @InjectMocks
+    private UploadCaseDocuments uploadCaseDocuments;
 
     @Test
     void shouldAddConfigurationToConfigBuilder() {
@@ -61,6 +71,10 @@ public class CaseworkerDocumentManagementTest {
         final CaseData caseData = caseData();
         final CaseDetails<CaseData, State> updatedCaseDetails = new CaseDetails<>();
         final CaseDetails<CaseData, State> beforeDetails = new CaseDetails<>();
+        DocumentManagement documentManagement = DocumentManagement.builder()
+            .caseworkerCICDocument(getCaseworkerCICDocumentList())
+            .build();
+        caseData.setDocManagement(documentManagement);
         beforeDetails.setData(caseData);
         updatedCaseDetails.setData(caseData);
         updatedCaseDetails.setState(State.CaseManagement);
@@ -68,9 +82,16 @@ public class CaseworkerDocumentManagementTest {
         updatedCaseDetails.setCreatedDate(LOCAL_DATE_TIME);
 
         //When
+
+        AboutToStartOrSubmitResponse<CaseData, State> midResponse =
+            uploadCaseDocuments.midEvent(updatedCaseDetails, beforeDetails);
+        AboutToStartOrSubmitResponse<CaseData, State> response =
+            caseworkerDocumentManagement.aboutToSubmit(updatedCaseDetails, beforeDetails);
         SubmittedCallbackResponse documentMgmtResponse = caseworkerDocumentManagement.submitted(updatedCaseDetails, beforeDetails);
 
         //Then
+        assertThat(midResponse).isNotNull();
+        assertThat(response).isNotNull();
         assertThat(documentMgmtResponse).isNotNull();
     }
 }
