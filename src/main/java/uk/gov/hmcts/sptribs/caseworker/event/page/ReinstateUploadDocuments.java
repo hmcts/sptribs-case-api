7b8d762bb452c0845d4b0e07b0aeea8a993fa32d
sjamihmcts <santoshini.jami@hmcts.net>
2023-04-25 16:39:14 +0100

    ST-1030: Fix build error

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/page/ReinstateUploadDocuments.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/page/ReinstateUploadDocuments.java
@@ -14,7 +14,6 @@ import uk.gov.hmcts.sptribs.document.model.CaseworkerCICDocument;
 import java.util.ArrayList;
 import java.util.List;
 
-import static uk.gov.hmcts.sptribs.document.DocumentUtil.updateCategoryToCaseworkerDocument;
 import static uk.gov.hmcts.sptribs.document.DocumentUtil.validateCaseworkerCICDocumentFormat;
 
 public class ReinstateUploadDocuments implements CcdPageConfiguration {
