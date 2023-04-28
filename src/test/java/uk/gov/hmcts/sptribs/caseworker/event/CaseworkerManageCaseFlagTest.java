5f14e06cc0aa0877bb4c8b668247169fe3b471b0
hmctscivil <version1rhel@hmctscivil.mshome.net>
2023-04-28 13:45:14 +0100

    ST-447 Manage flag- flag list

--- src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerManageCaseFlagTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerManageCaseFlagTest.java
@@ -3,16 +3,24 @@ package uk.gov.hmcts.sptribs.caseworker.event;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.Event;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.sptribs.caseworker.service.FlagService;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
+import uk.gov.hmcts.sptribs.ciccase.model.CicCase;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.hmcts.sptribs.testutil.ConfigTestUtil.createCaseDataConfigBuilder;
 import static uk.gov.hmcts.sptribs.testutil.ConfigTestUtil.getEventsFrom;
+import static uk.gov.hmcts.sptribs.testutil.TestDataHelper.getAppellantFlags;
+import static uk.gov.hmcts.sptribs.testutil.TestDataHelper.getCaseFlags;
+import static uk.gov.hmcts.sptribs.testutil.TestDataHelper.getRespondentFlags;
 import static uk.gov.hmcts.sptribs.testutil.TestEventConstants.CASEWORKER_MANAGE_CASE_FLAG;
 
 @ExtendWith(MockitoExtension.class)
@@ -20,6 +28,9 @@ class CaseworkerManageCaseFlagTest {
 
     @InjectMocks
     private CaseworkerManageCaseFlag caseworkerManageCaseFlag;
+
+    @Mock
+    FlagService flagService;
 
     @Test
     void shouldAddConfigurationToConfigBuilder() {
@@ -50,5 +61,25 @@ class CaseworkerManageCaseFlagTest {
             .doesNotContain(CASEWORKER_MANAGE_CASE_FLAG);
     }
 
+    @Test
+    void shouldRunAboutToStart() {
+        //Given
+        final CaseDetails<CaseData, State> updatedCaseDetails = new CaseDetails<>();
+
+        CicCase cicCase = CicCase.builder()
+            .appellantFlags(getAppellantFlags())
+            .respondentFlags(getRespondentFlags())
+            .caseFlags(getCaseFlags())
+            .build();
+        final CaseData caseData = CaseData.builder()
+            .cicCase(cicCase)
+            .build();
+        updatedCaseDetails.setData(caseData);
+        //When
+        AboutToStartOrSubmitResponse<CaseData, State> response = caseworkerManageCaseFlag.aboutToStart(updatedCaseDetails);
+
+        //Then
+        assertThat(response).isNotNull();
+    }
 
 }
