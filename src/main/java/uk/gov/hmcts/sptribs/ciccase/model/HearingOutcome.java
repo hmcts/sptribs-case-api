619904b008a2655fe30888b3726964316904dcbe
muradalamv1 <107861321+muradalamv1@users.noreply.github.com>
2023-05-03 11:01:28 +0100

    Rolled back all the 1196 changes (#898)
    Rolled back adjournment reasons

--- src/main/java/uk/gov/hmcts/sptribs/ciccase/model/HearingOutcome.java
+++ src/main/java/uk/gov/hmcts/sptribs/ciccase/model/HearingOutcome.java
@@ -3,11 +3,10 @@ package uk.gov.hmcts.sptribs.ciccase.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import uk.gov.hmcts.ccd.sdk.api.HasLabel;
 
 @Getter
 @AllArgsConstructor
-public enum HearingOutcome implements HasLabel {
+public enum HearingOutcome {
 
     @JsonProperty("Adjourned")
     ADJOURNED("Adjourned"),
