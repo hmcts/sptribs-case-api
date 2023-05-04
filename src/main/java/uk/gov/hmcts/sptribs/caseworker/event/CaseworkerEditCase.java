73fae78b428fd04127a906014de2bd2fe87b6521
hmctscivil <version1rhel@hmctscivil.mshome.net>
2023-05-04 14:05:14 +0100

    ST-1345 History Tab

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerEditCase.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerEditCase.java
@@ -28,11 +28,7 @@ import static uk.gov.hmcts.sptribs.ciccase.model.State.AwaitingHearing;
 import static uk.gov.hmcts.sptribs.ciccase.model.State.AwaitingOutcome;
 import static uk.gov.hmcts.sptribs.ciccase.model.State.CaseManagement;
 import static uk.gov.hmcts.sptribs.ciccase.model.State.Submitted;
-import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_CASEWORKER;
-import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_HEARING_CENTRE_ADMIN;
-import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_HEARING_CENTRE_TEAM_LEADER;
-import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_SENIOR_CASEWORKER;
-import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_SENIOR_JUDGE;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.*;
 import static uk.gov.hmcts.sptribs.ciccase.model.access.Permissions.CREATE_READ_UPDATE;
 
 @Component
@@ -77,6 +73,22 @@ public class CaseworkerEditCase implements CCDConfig<CaseData, State, UserRole> 
             .grant(CREATE_READ_UPDATE,
                 ST_CIC_CASEWORKER, ST_CIC_SENIOR_CASEWORKER, ST_CIC_HEARING_CENTRE_ADMIN,
                 ST_CIC_HEARING_CENTRE_TEAM_LEADER, ST_CIC_SENIOR_JUDGE)
+            .grantHistoryOnly(SUPER_USER,
+                SYSTEMUPDATE,
+                SOLICITOR,
+                CREATOR,
+                COURT_ADMIN_CIC,
+                CASE_OFFICER_CIC,
+                DISTRICT_REGISTRAR_CIC,
+                DISTRICT_JUDGE_CIC,
+                RESPONDENT_CIC,
+                ST_CIC_CASEWORKER,
+                ST_CIC_SENIOR_CASEWORKER,
+                ST_CIC_HEARING_CENTRE_ADMIN,
+                ST_CIC_HEARING_CENTRE_TEAM_LEADER,
+                ST_CIC_SENIOR_JUDGE,
+                ST_CIC_JUDGE,
+                ST_CIC_RESPONDENT)
             .aboutToSubmitCallback(this::aboutToSubmit)
             .submittedCallback(this::submitted));
     }
