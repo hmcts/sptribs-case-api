28a89422045c2489bbd42d452a44f23397d3352f
ElifDenizEsea <61947470+ElifDenizEsea@users.noreply.github.com>
2023-05-05 13:36:52 +0100

    ST-1120 Document type validation changed (#915)
    Co-authored-by: hmctscivil <version1rhel@hmctscivil.mshome.net>

--- src/main/java/uk/gov/hmcts/sptribs/document/model/CaseworkerCICDocument.java
+++ src/main/java/uk/gov/hmcts/sptribs/document/model/CaseworkerCICDocument.java
@@ -10,6 +10,8 @@ import org.apache.commons.lang3.StringUtils;
 import uk.gov.hmcts.ccd.sdk.api.CCD;
 import uk.gov.hmcts.ccd.sdk.type.Document;
 
+import java.util.Arrays;
+
 import static uk.gov.hmcts.ccd.sdk.type.FieldType.FixedList;
 import static uk.gov.hmcts.ccd.sdk.type.FieldType.TextArea;
 
@@ -49,19 +51,12 @@ public class CaseworkerCICDocument {
 
     @JsonIgnore
     public boolean isDocumentValid() {
-        String regex = ".pdf,.tif,.tiff,.jpg,.jpeg,.png,.mp3,.mp4";
-        String fileName = this.documentLink.getFilename();
-        String fileExtension = StringUtils.substringAfter(fileName, ".");
-
-        return regex.contains(fileExtension);
+        return isDocumentValid("pdf,csv,txt,rtf,xlsx,docx,doc,xls");
     }
 
-    @JsonIgnore
-    public boolean isDocumentValidForEmail() {
-        String regex = ".pdf,.csv,.odt,.txt,.rtf,.xlsx,.docx";
+    private boolean isDocumentValid(String validExtensions) {
         String fileName = this.documentLink.getFilename();
-        String fileExtension = StringUtils.substringAfter(fileName, ".");
-
-        return regex.contains(fileExtension);
+        String fileExtension = StringUtils.substringAfterLast(fileName, ".");
+        return Arrays.asList(validExtensions.split(",")).contains(fileExtension);
     }
 }
