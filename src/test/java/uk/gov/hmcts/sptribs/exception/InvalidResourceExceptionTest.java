83670643c4a948d433d6f95392b83fbbf999879a
Sabir-v1 <sabir.obul@hmcts.net>
2023-05-06 05:01:39 +0100

    add test coverage for Exceptions

---
+++ src/test/java/uk/gov/hmcts/sptribs/exception/InvalidResourceExceptionTest.java
@@ -0,0 +1,24 @@
+package uk.gov.hmcts.sptribs.exception;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static uk.gov.hmcts.sptribs.testutil.TestConstants.TEST_RESOURCE_NOT_FOUND;
+import static uk.gov.hmcts.sptribs.testutil.TestFileUtil.loadResource;
+
+class InvalidResourceExceptionTest {
+    @Test
+    void testInvalidResourceExceptionThrownForNonExistentFile() throws Exception {
+        String createCaseDataFileNotExist = "CICCaseDataNotExist.json";
+
+        Exception exception = assertThrows(Exception.class, () -> {
+            byte [] caseDataJson = loadResource(createCaseDataFileNotExist);
+            assertNull(caseDataJson);
+        });
+
+        assertTrue(exception.getMessage().contains(TEST_RESOURCE_NOT_FOUND), String.valueOf(true));
+
+    }
+}
