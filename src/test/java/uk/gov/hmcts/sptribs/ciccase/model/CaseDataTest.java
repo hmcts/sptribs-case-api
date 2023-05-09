29a7008cff974d553c66b080c8e2c39d7bd27ce4
ElifDenizEsea <61947470+ElifDenizEsea@users.noreply.github.com>
2023-05-09 17:03:15 +0100

    ST-1343 Search results (#923)
    Co-authored-by: hmctscivil <version1rhel@hmctscivil.mshome.net>

--- src/test/java/uk/gov/hmcts/sptribs/ciccase/model/CaseDataTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/ciccase/model/CaseDataTest.java
@@ -1,7 +1,13 @@
 package uk.gov.hmcts.sptribs.ciccase.model;
 
 import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.sptribs.caseworker.model.Listing;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static java.time.format.DateTimeFormatter.ofPattern;
+import static java.util.Locale.UK;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CaseDataTest {
@@ -13,5 +19,40 @@ class CaseDataTest {
             .build();
         //Then
         assertThat(caseData.getCicCase().getApplicantFullName()).isNull();
+    }
+
+    @Test
+    void shouldGetHearingDate() {
+        //When
+        DateTimeFormatter dateFormatter = ofPattern("dd MMM yyyy", UK);
+        LocalDate now = LocalDate.now();
+        Listing listing = Listing.builder().date(now).build();
+
+        final CaseData caseData = CaseData.builder()
+            .listing(listing)
+            .build();
+
+        //When
+        String result = caseData.getFirstHearingDate();
+
+        //Then
+        assertThat(result).isEqualTo(dateFormatter.format(now));
+    }
+
+    @Test
+    void shouldGetHearingLocation() {
+        //When
+        String someAddress = "WC1";
+        Listing listing = Listing.builder().hearingVenueNameAndAddress(someAddress).build();
+
+        final CaseData caseData = CaseData.builder()
+            .listing(listing)
+            .build();
+
+        //When
+        String result = caseData.getHearingVenueName();
+
+        //Then
+        assertThat(result).isEqualTo(someAddress);
     }
 }
