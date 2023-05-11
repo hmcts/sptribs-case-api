cc7d0105341f580c91ee361d4f9aec98e0e29a4d
Sabir <123706441+Sabir-v1@users.noreply.github.com>
2023-05-11 03:28:18 +0100

    Update InvalidResourceException.java (#928)
    remove unused code

--- src/main/java/uk/gov/hmcts/sptribs/exception/InvalidResourceException.java
+++ src/main/java/uk/gov/hmcts/sptribs/exception/InvalidResourceException.java
@@ -4,10 +4,6 @@ public class InvalidResourceException extends RuntimeException {
 
     private static final long serialVersionUID = 7442994120484411077L;
 
-    public InvalidResourceException(String message) {
-        super(message);
-    }
-
     public InvalidResourceException(String message, Exception cause) {
         super(message, cause);
     }
