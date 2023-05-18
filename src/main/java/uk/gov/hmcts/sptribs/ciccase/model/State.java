116661037b2562d64b256dde7854fe93d93557b8
Shailesh Walke <117093807+Shailesh-walke@users.noreply.github.com>
2023-05-18 16:35:02 +0100

    fix for bundling events not available for expected states (#948)

--- src/main/java/uk/gov/hmcts/sptribs/ciccase/model/State.java
+++ src/main/java/uk/gov/hmcts/sptribs/ciccase/model/State.java
@@ -153,10 +153,10 @@ public enum State {
     ));
 
 
-    public static final EnumSet<State> BUNDLE_STATES = EnumSet.complementOf(EnumSet.of(
+    public static final EnumSet<State> BUNDLE_STATES = EnumSet.of(
         CaseManagement,
         AwaitingHearing
-    ));
+    );
     private final String name;
 }
 
