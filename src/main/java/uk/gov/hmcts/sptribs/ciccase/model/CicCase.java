838287491239cd3d72c958bab28f37bc2b11e984
muradalamv1 <murad.alam@version1.com>
2023-05-22 16:17:58 +0100

    bug fixes for notification, Judge panel etc

--- src/main/java/uk/gov/hmcts/sptribs/ciccase/model/CicCase.java
+++ src/main/java/uk/gov/hmcts/sptribs/ciccase/model/CicCase.java
@@ -263,7 +263,6 @@ public class CicCase {
     @CCD(
         access = {DefaultAccess.class, CaseworkerWithCAAAccess.class}
     )
-    @JsonIgnore
     private Document lastSelectedOrder;
 
     @CCD(
