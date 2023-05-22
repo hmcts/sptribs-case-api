afbd1456aa3cc4a651b4616b287ca0abb5c24794
pavihmcts <110388263+pavihmcts@users.noreply.github.com>
2023-05-22 10:09:33 +0100

    Add DSS front end E2E tests (#953)
    Add dss tests
    
    Co-authored-by: Pavi <pva08@Pavithrans-MacBook-Pro.local>

--- src/e2eTests/java/uk/gov/hmcts/sptribs/e2e/Login.java
+++ src/e2eTests/java/uk/gov/hmcts/sptribs/e2e/Login.java
@@ -4,7 +4,8 @@ import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.LoadState;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
-import static uk.gov.hmcts.sptribs.e2e.Base.BASE_URL;
+import static uk.gov.hmcts.sptribs.e2e.Base.CASE_API_BASE_URL;
+import static uk.gov.hmcts.sptribs.e2e.Base.DSS_BASE_URL;
 import static uk.gov.hmcts.sptribs.testutils.AssertionHelpers.textOptionsWithTimeout;
 import static uk.gov.hmcts.sptribs.testutils.PageHelpers.clickButton;
 import static uk.gov.hmcts.sptribs.testutils.PageHelpers.clickOptionsWithTimeout;
@@ -17,6 +18,9 @@ import static uk.gov.hmcts.sptribs.testutils.PageHelpers.urlOptionsWithTimeout;
 public class Login {
     private final Page page;
 
+    private static final String DSS_USER_PASSWORD = "Password123";
+    private static final String CASE_API_USER_PASSWORD = "Pa55w0rd11";
+
     public Login(Page page) {
         this.page = page;
     }
@@ -28,13 +32,13 @@ public class Login {
             page.locator("button[name=\"hide-accepted\"]").click();
         }
         enterCredentialsAndClickSignIn(user);
-        page.waitForURL(BASE_URL + "/cases", urlOptionsWithTimeout(120000));
+        page.waitForURL(CASE_API_BASE_URL + "/cases", urlOptionsWithTimeout(120000));
         assertThat(page.locator("h1"))
             .hasText("Case list", textOptionsWithTimeout(90000));
         if (page.isVisible("button[value=\"accept\"][name=\"cookies\"]")) {
             clickButton(page, "Accept analytics cookies");
             page.waitForLoadState(LoadState.DOMCONTENTLOADED,loadStateOptionsWithTimeout(120000));
-            page.waitForURL(BASE_URL + "/cases", urlOptionsWithTimeout(90000));
+            page.waitForURL(CASE_API_BASE_URL + "/cases", urlOptionsWithTimeout(90000));
             page.waitForSelector("h1", selectorOptionsWithTimeout(90000));
             assertThat(page.locator("h1"))
                 .hasText("Case list", textOptionsWithTimeout(90000));
@@ -48,12 +52,33 @@ public class Login {
         int i = 0;
         while (page.locator("h1:has-text(\"Sign in\")").count() > 0 && i < 5) {
             getTextBoxByLabel(page, "Email address").fill(user);
-            getTextBoxByLabel(page, "Password").fill("Pa55w0rd11");
+            getTextBoxByLabel(page, "Password").fill(CASE_API_USER_PASSWORD);
             page.locator("input:has-text(\"Sign in\")").click(clickOptionsWithTimeout(120000));
             page.waitForLoadState(LoadState.DOMCONTENTLOADED, loadStateOptionsWithTimeout(120000));
             page.waitForSelector("h1", selectorOptionsWithTimeout(120000));
             i++;
         }
+    }
+
+    private void loginAsDssUser(String user) {
+        page.waitForSelector("h1:has-text(\"Sign in or create an account\")", selectorOptionsWithTimeout(120000));
+        if (page.isVisible("#cookie-accept-submit")) {
+            clickButton(page, "Accept additional cookies");
+            page.locator("button[name=\"hide-accepted\"]").click();
+        }
+        getTextBoxByLabel(page, "Email address").fill(user);
+        getTextBoxByLabel(page, "Password").fill(DSS_USER_PASSWORD);
+        page.locator("input:has-text(\"Sign in\")").click(clickOptionsWithTimeout(120000));
+        page.waitForLoadState(LoadState.DOMCONTENTLOADED, loadStateOptionsWithTimeout(120000));
+        page.waitForURL(DSS_BASE_URL + "/subject-details", urlOptionsWithTimeout(60000));
+        page.waitForSelector("h1", selectorOptionsWithTimeout(60000));
+        assertThat(page.locator("h1"))
+            .hasText("Who is the subject of this case?", textOptionsWithTimeout(60000));
+        if (page.isVisible("button[data-cm-action=\"accept\"][data-module=\"govuk-button\"]")) {
+            clickButton(page, "Accept analytics cookies");
+            clickButton(page, "Hide this message");
+        }
+
     }
 
     public void loginAsStTest1User() {
@@ -70,5 +95,9 @@ public class Login {
 
     public void loginAsStSuperUser() {
         loginAs("st-super@mailinator.com");
+    }
+
+    public void loginAsStCitizen1User() {
+        loginAsDssUser("st-citizen1@mailinator.com");
     }
 }
