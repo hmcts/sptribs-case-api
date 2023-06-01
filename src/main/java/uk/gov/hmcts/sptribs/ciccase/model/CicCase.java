3aacb94771c1585f0bc88b3b31ace3e7bff5b9aa
ElifDenizEsea <61947470+ElifDenizEsea@users.noreply.github.com>
2023-06-01 16:45:21 +0100

    ST-1420 DSS integration bugs (#988)
    Co-authored-by: hmctscivil <version1rhel@hmctscivil.mshome.net>

--- src/main/java/uk/gov/hmcts/sptribs/ciccase/model/CicCase.java
+++ src/main/java/uk/gov/hmcts/sptribs/ciccase/model/CicCase.java
@@ -262,6 +262,7 @@ public class CicCase {
     @CCD(
         access = {DefaultAccess.class, CaseworkerWithCAAAccess.class}
     )
+    @JsonIgnore
     private Document lastSelectedOrder;
 
     @CCD(
