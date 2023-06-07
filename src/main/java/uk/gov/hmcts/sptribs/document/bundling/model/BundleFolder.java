7a1e7903bfb3bd76a57a18b928b3cccbbd2cd7f3
hmctscivil <version1rhel@hmctscivil.mshome.net>
2023-06-07 11:17:39 +0100

    ST-1388 bundling

--- src/main/java/uk/gov/hmcts/sptribs/document/bundling/model/BundleFolder.java
+++ src/main/java/uk/gov/hmcts/sptribs/document/bundling/model/BundleFolder.java
@@ -6,14 +6,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import uk.gov.hmcts.ccd.sdk.api.CCD;
 import uk.gov.hmcts.ccd.sdk.type.ListValue;
-import uk.gov.hmcts.sptribs.ciccase.model.access.CaseworkerWithCAAAccess;
-import uk.gov.hmcts.sptribs.ciccase.model.access.DefaultAccess;
 
 import java.util.List;
-
-import static uk.gov.hmcts.ccd.sdk.type.FieldType.Collection;
 
 @Getter
 @Setter
@@ -25,14 +20,8 @@ public class BundleFolder {
 
     private String name;
 
-    @CCD(access = {DefaultAccess.class, CaseworkerWithCAAAccess.class},
-        typeOverride = Collection,
-        typeParameterOverride = "BundleDocument")
     private List<ListValue<BundleDocument>> documents;
 
-    @CCD(access = {DefaultAccess.class, CaseworkerWithCAAAccess.class},
-        typeOverride = Collection,
-        typeParameterOverride = "BundleSubFolder")
     private List<ListValue<BundleSubFolder>> folders;
 
     private int sortIndex;
--- src/main/java/uk/gov/hmcts/sptribs/document/bundling/model/BundleSubFolder.java
+++ src/main/java/uk/gov/hmcts/sptribs/document/bundling/model/BundleSubFolder.java
@@ -6,14 +6,10 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import uk.gov.hmcts.ccd.sdk.api.CCD;
 import uk.gov.hmcts.ccd.sdk.type.ListValue;
-import uk.gov.hmcts.sptribs.ciccase.model.access.CaseworkerWithCAAAccess;
-import uk.gov.hmcts.sptribs.ciccase.model.access.DefaultAccess;
 
 import java.util.List;
 
-import static uk.gov.hmcts.ccd.sdk.type.FieldType.Collection;
 
 @Getter
 @Setter
@@ -25,14 +21,8 @@ public class BundleSubFolder {
 
     private String name;
 
-    @CCD(access = {DefaultAccess.class, CaseworkerWithCAAAccess.class},
-        typeOverride = Collection,
-        typeParameterOverride = "BundleDocument")
     private List<ListValue<BundleDocument>> documents;
 
-    @CCD(access = {DefaultAccess.class, CaseworkerWithCAAAccess.class},
-        typeOverride = Collection,
-        typeParameterOverride = "BundleSubFolder2")
     private List<ListValue<BundleSubFolder2>> folders;
 
     private int sortIndex;
