c3356b69589f729556a6b9887ce6f3c6030dbe05
pavihmcts <110388263+pavihmcts@users.noreply.github.com>
2023-06-07 10:10:51 +0100

    [ST-1081]: enable DSS E2E Tests (#994)
    enable dss E2E tests
    
    Co-authored-by: Pavi <pva08@Pavithrans-MacBook-Pro.local>

--- src/e2eTests/java/uk/gov/hmcts/sptribs/e2e/DssCaseTests.java
+++ src/e2eTests/java/uk/gov/hmcts/sptribs/e2e/DssCaseTests.java
@@ -1,10 +1,12 @@
 package uk.gov.hmcts.sptribs.e2e;
 
 import com.microsoft.playwright.Page;
-import org.junit.jupiter.api.Disabled;
+import io.github.artsok.RepeatedIfExceptionsTest;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Order;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+import static uk.gov.hmcts.sptribs.e2e.enums.CaseState.DssSubmitted;
 import static uk.gov.hmcts.sptribs.testutils.AssertionHelpers.textOptionsWithTimeout;
 import static uk.gov.hmcts.sptribs.testutils.AssertionHelpers.visibleOptionsWithTimeout;
 import static uk.gov.hmcts.sptribs.testutils.PageHelpers.clickLink;
@@ -13,20 +15,41 @@ import static uk.gov.hmcts.sptribs.testutils.PageHelpers.getCaseUrl;
 public class DssCaseTests extends Base {
 
     @Order(1)
-    @Disabled
+    @RepeatedIfExceptionsTest
     public void caseWorkerShouldBeAbleToSeeTheDssCase() {
         Page page = getPage();
-        DssCase newCase = new DssCase(page);
-        newCase.navigateToDssLoginPage();
+        DssCase newDssCase = new DssCase(page);
+        newDssCase.navigateToDssLoginPage();
         Login login = new Login(page);
         login.loginAsStCitizen1User();
-        final String caseNumber = newCase.createCase("representative");
+        final String caseNumber = newDssCase.createCase("representative");
         clickLink(page, "Sign out");
         assertH1Heading(page, "Submit a First-tier Tribunal form");
         page.navigate(CASE_API_BASE_URL, new Page.NavigateOptions().setTimeout(90000));
         login.loginAsStTest1User();
         page.navigate(getCaseUrl(caseNumber));
+        Case dssCase = new Case(page);
         assertThat(page.locator(".mat-tab-list")).isVisible(visibleOptionsWithTimeout(60000));
+        Assertions.assertEquals(DssSubmitted.label, dssCase.getCaseStatus());
+    }
+
+    @Order(2)
+    @RepeatedIfExceptionsTest
+    public void citizenShouldBeAbleToCreateCaseAfterAnsweringPcqQuestionnaire() {
+        Page page = getPage();
+        DssCase newDssCase = new DssCase(page);
+        newDssCase.navigateToDssLoginPage();
+        Login login = new Login(page);
+        login.loginAsStCitizen1User();
+        final String caseNumber = newDssCase.createCase("representative", "pcq");
+        clickLink(page, "Sign out");
+        assertH1Heading(page, "Submit a First-tier Tribunal form");
+        page.navigate(CASE_API_BASE_URL, new Page.NavigateOptions().setTimeout(90000));
+        login.loginAsStTest1User();
+        page.navigate(getCaseUrl(caseNumber));
+        Case dssCase = new Case(page);
+        assertThat(page.locator(".mat-tab-list")).isVisible(visibleOptionsWithTimeout(60000));
+        Assertions.assertEquals(DssSubmitted.label, dssCase.getCaseStatus());
     }
 
     private void assertH1Heading(Page page, String heading) {
