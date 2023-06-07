20ce663a923560f531acde693023e88a169d0621
sjamihmcts <110464983+sjamihmcts@users.noreply.github.com>
2023-06-07 11:13:24 +0100

    Feat/prod issue label change: Removed check for Launch Darkly flag (#997)
    * SNI-3910: Label change in SelectPartiesssss class
    
    * SNI-3910: Removed check for Launch Darkly
    
    * SNI-3910: Removed Launch Darkly file

--- src/test/java/uk/gov/hmcts/sptribs/common/event/CreateTestCaseTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/common/event/CreateTestCaseTest.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.sptribs.common.event;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
@@ -16,20 +15,15 @@ import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
 import uk.gov.hmcts.sptribs.common.event.page.ContactPreferenceDetails;
 import uk.gov.hmcts.sptribs.common.event.page.SelectParties;
-import uk.gov.hmcts.sptribs.launchdarkly.FeatureToggleService;
 
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.sptribs.testutil.ConfigTestUtil.createCaseDataConfigBuilder;
 import static uk.gov.hmcts.sptribs.testutil.ConfigTestUtil.getEventsFrom;
 
 @ExtendWith(MockitoExtension.class)
 class CreateTestCaseTest {
-
-    @Mock
-    private FeatureToggleService featureToggleService;
 
     @InjectMocks
     private CreateTestCase createTestCase;
@@ -46,7 +40,6 @@ class CreateTestCaseTest {
     void shouldAddConfigurationToConfigBuilder() {
         final ConfigBuilderImpl<CaseData, State, UserRole> configBuilder = createCaseDataConfigBuilder();
 
-        when(featureToggleService.isCicCreateCaseFeatureEnabled()).thenReturn(true);
         createTestCase.configure(configBuilder);
 
         assertThat(getEventsFrom(configBuilder).values())
