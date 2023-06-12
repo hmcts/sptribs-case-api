2a386decc1101fb77966f5adc45a09dedd20234e
Vijay-knr <vijay.kayyam@gmail.com>
2023-06-12 17:24:33 +0100

    Dss tests (#1024)
    * ST-1079 DSS case api end to end journey for creating hearing summary
    
    * checkstyle issues
    
    ---------
    
    Co-authored-by: Vijay-knr <vijay.kayyam@hmcts.net>

--- src/e2eTests/java/uk/gov/hmcts/sptribs/e2e/HearingJourneyTests.java
+++ src/e2eTests/java/uk/gov/hmcts/sptribs/e2e/HearingJourneyTests.java
@@ -6,6 +6,10 @@ import org.junit.jupiter.api.Assertions;
 import uk.gov.hmcts.sptribs.testutils.PageHelpers;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+import static uk.gov.hmcts.sptribs.e2e.enums.CaseState.DssSubmitted;
+import static uk.gov.hmcts.sptribs.testutils.AssertionHelpers.visibleOptionsWithTimeout;
+import static uk.gov.hmcts.sptribs.testutils.PageHelpers.clickLink;
+import static uk.gov.hmcts.sptribs.testutils.PageHelpers.getCaseUrl;
 import static uk.gov.hmcts.sptribs.testutils.PageHelpers.getTabByText;
 
 public class HearingJourneyTests extends Base {
@@ -31,6 +35,29 @@ public class HearingJourneyTests extends Base {
     public void caseWorkerShouldBeAbleToCreateHearingSummaryAndViewDetailsInHearingTab() {
         Page page = getPage();
         createAndBuildCase(page);
+
+        Hearing hearing = createListing(page);
+        hearing.createHearingSummary();
+        getTabByText(page, "Hearings").click();
+        assertThat(page.locator("h4").first()).hasText("Listing details");
+        String hearingStatus = PageHelpers.getValueFromTableFor(page, "Hearing Status");
+        Assertions.assertEquals("Completed", hearingStatus);
+        String hearingType = PageHelpers.getValueFromTableFor(page, "Hearing type");
+        Assertions.assertEquals("Case management", hearingType);
+        String hearingFormat = PageHelpers.getValueFromTableFor(page, "Hearing format");
+        Assertions.assertEquals("Face to Face", hearingFormat);
+        String judge = PageHelpers.getValueFromTableFor(page, "Which judge heard the case?");
+        Assertions.assertEquals("Chetan Lad", judge);
+        String panelMember = PageHelpers.getValueFromTableFor(page, "Name of the panel member");
+        Assertions.assertEquals("Ivy-Rose Rayner", panelMember);
+        String otherAttendee = PageHelpers.getValueFromTableFor(page, "Who was this other attendee?");
+        Assertions.assertEquals("Special officer", otherAttendee);
+    }
+
+    @RepeatedIfExceptionsTest
+    public void caseWorkerShouldBeAbleToCreateHearingSummaryAndViewDetailsInHearingTabForDSSCase() {
+        Page page = getPage();
+        createEditAndBuildDssCase(page);
 
         Hearing hearing = createListing(page);
         hearing.createHearingSummary();
@@ -114,6 +141,23 @@ public class HearingJourneyTests extends Base {
         Case newCase = new Case(page);
         newCase.createCase("representative");
         newCase.buildCase();
+    }
+
+    private void createEditAndBuildDssCase(Page page) {
+        DssCase newDssCase = new DssCase(page);
+        newDssCase.navigateToDssLoginPage();
+        Login login = new Login(page);
+        login.loginAsStCitizen1User();
+        final String caseNumber = newDssCase.createCase("representative");
+        clickLink(page, "Sign out");
+        page.navigate(CASE_API_BASE_URL, new Page.NavigateOptions().setTimeout(90000));
+        login.loginAsStTest1User();
+        page.navigate(getCaseUrl(caseNumber));
+        Case dssCase = new Case(page);
+        assertThat(page.locator(".mat-tab-list")).isVisible(visibleOptionsWithTimeout(60000));
+        Assertions.assertEquals(DssSubmitted.label, dssCase.getCaseStatus());
+        dssCase.editDssCase("representative","applicant");
+        dssCase.buildCase();
     }
 
     private Hearing createListing(Page page) {
