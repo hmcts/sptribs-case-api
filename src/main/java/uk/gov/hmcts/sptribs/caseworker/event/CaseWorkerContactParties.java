c6ee1613a69ef86b1a72b0145a3eec7abeca144f
Peter Moores <82939391+petermoores-version1@users.noreply.github.com>
2023-07-04 13:55:39 +0100

    Revert "ST-1380 XUI test remove Contact Parties event (claimant only)"
    This reverts commit c08c48d7035ae30353ebb2e015fa19f5271f8610.

---
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerContactParties.java
@@ -0,0 +1,167 @@
+package uk.gov.hmcts.sptribs.caseworker.event;
+
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.ccd.sdk.type.DynamicMultiSelectList;
+import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
+import uk.gov.hmcts.sptribs.caseworker.event.page.ContactPartiesSelectDocument;
+import uk.gov.hmcts.sptribs.caseworker.util.DocumentListUtil;
+import uk.gov.hmcts.sptribs.caseworker.util.MessageUtil;
+import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
+import uk.gov.hmcts.sptribs.ciccase.model.CicCase;
+import uk.gov.hmcts.sptribs.ciccase.model.State;
+import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
+import uk.gov.hmcts.sptribs.common.ccd.CcdPageConfiguration;
+import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
+import uk.gov.hmcts.sptribs.common.event.page.PartiesToContact;
+import uk.gov.hmcts.sptribs.common.notification.ContactPartiesNotification;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.String.format;
+import static uk.gov.hmcts.sptribs.caseworker.util.EventConstants.CASEWORKER_CONTACT_PARTIES;
+import static uk.gov.hmcts.sptribs.ciccase.model.State.AwaitingHearing;
+import static uk.gov.hmcts.sptribs.ciccase.model.State.AwaitingOutcome;
+import static uk.gov.hmcts.sptribs.ciccase.model.State.CaseClosed;
+import static uk.gov.hmcts.sptribs.ciccase.model.State.CaseManagement;
+import static uk.gov.hmcts.sptribs.ciccase.model.State.CaseStayed;
+import static uk.gov.hmcts.sptribs.ciccase.model.State.Draft;
+import static uk.gov.hmcts.sptribs.ciccase.model.State.NewCaseReceived;
+import static uk.gov.hmcts.sptribs.ciccase.model.State.Rejected;
+import static uk.gov.hmcts.sptribs.ciccase.model.State.Submitted;
+import static uk.gov.hmcts.sptribs.ciccase.model.State.Withdrawn;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_CASEWORKER;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_HEARING_CENTRE_ADMIN;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_HEARING_CENTRE_TEAM_LEADER;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_JUDGE;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_SENIOR_CASEWORKER;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_SENIOR_JUDGE;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.SUPER_USER;
+import static uk.gov.hmcts.sptribs.ciccase.model.access.Permissions.CREATE_READ_UPDATE;
+
+
+@Component
+@Slf4j
+@Setter
+public class CaseWorkerContactParties implements CCDConfig<CaseData, State, UserRole> {
+
+    @Value("${feature.contact-parties.enabled}")
+    private boolean contactPartiesEnabled;
+
+    private static final CcdPageConfiguration partiesToContact = new PartiesToContact();
+    private static final CcdPageConfiguration contactPartiesSelectDocument = new ContactPartiesSelectDocument();
+
+    @Autowired
+    private ContactPartiesNotification contactPartiesNotification;
+
+    @Override
+    public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
+        if (contactPartiesEnabled) {
+            doConfigure(configBuilder);
+        }
+    }
+
+    private void doConfigure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
+        PageBuilder pageBuilder = new PageBuilder(
+            configBuilder
+                .event(CASEWORKER_CONTACT_PARTIES)
+                .forStates(Draft,
+                    Withdrawn,
+                    Rejected,
+                    Submitted,
+                    NewCaseReceived,
+                    CaseManagement,
+                    AwaitingHearing,
+                    AwaitingOutcome,
+                    CaseClosed,
+                    CaseStayed)
+                .name("Case: Contact parties")
+                .showSummary()
+                .aboutToSubmitCallback(this::aboutToSubmit)
+                .aboutToStartCallback(this::aboutToStart)
+                .submittedCallback(this::partiesContacted)
+                .grant(CREATE_READ_UPDATE, SUPER_USER,
+                    ST_CIC_CASEWORKER, ST_CIC_SENIOR_CASEWORKER, ST_CIC_HEARING_CENTRE_ADMIN,
+                    ST_CIC_HEARING_CENTRE_TEAM_LEADER, ST_CIC_SENIOR_JUDGE)
+                .grantHistoryOnly(
+                    ST_CIC_CASEWORKER,
+                    ST_CIC_SENIOR_CASEWORKER,
+                    ST_CIC_HEARING_CENTRE_ADMIN,
+                    ST_CIC_HEARING_CENTRE_TEAM_LEADER,
+                    ST_CIC_SENIOR_JUDGE,
+                    SUPER_USER,
+                    ST_CIC_JUDGE));
+        contactPartiesSelectDocument.addTo(pageBuilder);
+        partiesToContact.addTo(pageBuilder);
+    }
+
+    public AboutToStartOrSubmitResponse<CaseData, State> aboutToStart(CaseDetails<CaseData, State> details) {
+        final CaseData caseData = details.getData();
+
+        DynamicMultiSelectList documentList = DocumentListUtil.prepareDocumentList(caseData);
+        caseData.getContactPartiesDocuments().setDocumentList(documentList);
+
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .data(caseData)
+            .build();
+    }
+
+    public AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmit(CaseDetails<CaseData, State> details,
+                                                                       CaseDetails<CaseData, State> detailsBefore) {
+        final CaseData caseData = details.getData();
+        final List<String> errors = new ArrayList<>();
+
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .data(caseData)
+            .errors(errors)
+            .build();
+    }
+
+    public SubmittedCallbackResponse partiesContacted(CaseDetails<CaseData, State> details,
+                                                      CaseDetails<CaseData, State> beforeDetails) {
+
+        var data = details.getData();
+        var cicCase = data.getCicCase();
+        String caseNumber = data.getHyphenatedCaseRef();
+
+        try {
+            sendContactPartiesNotification(details, cicCase, caseNumber);
+        } catch (Exception notificationException) {
+            log.error("Contact Parties notification failed with exception : {}", notificationException.getMessage());
+            return SubmittedCallbackResponse.builder()
+                .confirmationHeader(format("# Contact Parties notification failed %n## Please resend the order"))
+                .build();
+        }
+
+        return SubmittedCallbackResponse.builder()
+            .confirmationHeader(format("# Message sent %n## %s",
+                MessageUtil.generateSimpleMessage(data.getCicCase())
+            ))
+            .build();
+    }
+
+    private void sendContactPartiesNotification(CaseDetails<CaseData, State> details, CicCase cicCase, String caseNumber) {
+        if (!CollectionUtils.isEmpty(cicCase.getNotifyPartySubject())) {
+            contactPartiesNotification.sendToSubject(details.getData(), caseNumber);
+        }
+        if (!CollectionUtils.isEmpty(cicCase.getNotifyPartyRepresentative())) {
+            contactPartiesNotification.sendToRepresentative(details.getData(), caseNumber);
+        }
+        if (!CollectionUtils.isEmpty(cicCase.getNotifyPartyApplicant())) {
+            contactPartiesNotification.sendToApplicant(details.getData(), caseNumber);
+        }
+        if (!CollectionUtils.isEmpty(cicCase.getNotifyPartyRespondent())) {
+            contactPartiesNotification.sendToRespondent(details.getData(), caseNumber);
+        }
+    }
+
+}
