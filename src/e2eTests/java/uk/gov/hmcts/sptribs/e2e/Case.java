bcf7b39294cdbbab6f72c0cce7ffb596eb0aeb0a
Vijay-knr <vijay.kayyam@gmail.com>
2023-07-10 13:13:16 +0100

    fix e2e tests (#1100)
    * fix e2e tests
    
    * Update CloseCaseTests.java
    
    ---------
    
    Co-authored-by: Vijay-knr <vijay.kayyam@hmcts.net>

--- src/e2eTests/java/uk/gov/hmcts/sptribs/e2e/Case.java
+++ src/e2eTests/java/uk/gov/hmcts/sptribs/e2e/Case.java
@@ -457,7 +457,7 @@ public class Case {
         assertThat(page.locator("h1")).hasText("Upload case documents", textOptionsWithTimeout(30000));
         clickButton(page, "Continue");
         assertThat(page.locator("h1")).hasText("Select recipients", textOptionsWithTimeout(30000));
-        page.getByLabel("Subject").check();
+        page.getByLabel("Subject", new Page.GetByLabelOptions().setExact(true)).check();
         page.getByLabel("Respondent").check();
         clickButton(page, "Continue");
         assertThat(page.locator("h2.heading-h2")).hasText("Check your answers", textOptionsWithTimeout(30000));
