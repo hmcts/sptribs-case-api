c35f62da8d5cb34af097849e3aa28665714bd7c3
Vijay-knr <vijay.kayyam@hmcts.net>
2023-07-10 10:55:45 +0100

    fix e2e tests

--- src/e2eTests/java/uk/gov/hmcts/sptribs/e2e/CloseCaseTests.java
+++ src/e2eTests/java/uk/gov/hmcts/sptribs/e2e/CloseCaseTests.java
@@ -4,18 +4,12 @@ import com.microsoft.playwright.Page;
 import io.github.artsok.RepeatedIfExceptionsTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Order;
-import uk.gov.hmcts.sptribs.e2e.enums.Actions;
-import uk.gov.hmcts.sptribs.testutils.DateHelpers;
-
-import java.util.Calendar;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 import static uk.gov.hmcts.sptribs.e2e.enums.Actions.ReinstateCase;
-import static uk.gov.hmcts.sptribs.e2e.enums.CaseState.CaseClosed;
 import static uk.gov.hmcts.sptribs.e2e.enums.CaseState.CaseManagement;
 import static uk.gov.hmcts.sptribs.testutils.AssertionHelpers.textOptionsWithTimeout;
 import static uk.gov.hmcts.sptribs.testutils.PageHelpers.clickButton;
-import static uk.gov.hmcts.sptribs.testutils.PageHelpers.getTextBoxByLabel;
 
 public class CloseCaseTests extends Base {
 
@@ -25,37 +19,11 @@ public class CloseCaseTests extends Base {
     public void caseworkerShouldAbleToCloseTheCase() {
         Page page = getPage();
         Login login = new Login(page);
-        login.loginAsStTest1User();
+        login.demoLoginAsSeniorCaseworker();
         Case newCase = new Case(page);
         newCase.createCase();
         newCase.buildCase();
-        newCase.startNextStepAction(Actions.CloseCase);
-        assertThat(page.locator("h1")).hasText("Are you sure you want to close this case?", textOptionsWithTimeout(60000));
-        clickButton(page, "Continue");
-        page.getByLabel("Case Withdrawn").check();
-        clickButton(page, "Continue");
-        assertThat(page.locator("h1")).hasText("Withdrawal details", textOptionsWithTimeout(30000));
-        page.getByLabel("Who withdrew from the case?").fill("case worker");
-        Calendar date = DateHelpers.getYesterdaysDate();
-        getTextBoxByLabel(page, "Day").fill(String.valueOf(date.get(Calendar.DAY_OF_MONTH)));
-        getTextBoxByLabel(page, "Month").fill(String.valueOf(date.get(Calendar.MONTH) + 1));
-        getTextBoxByLabel(page, "Year").type(String.valueOf(date.get(Calendar.YEAR)));
-        page.locator("h1").click();
-        clickButton(page, "Continue");
-        assertThat(page.locator("h1")).hasText("Upload case documents", textOptionsWithTimeout(30000));
-        clickButton(page, "Continue");
-        assertThat(page.locator("h1")).hasText("Select recipients", textOptionsWithTimeout(30000));
-        page.getByLabel("Subject").check();
-        page.getByLabel("Respondent").check();
-        clickButton(page, "Continue");
-        assertThat(page.locator("h2.heading-h2")).hasText("Check your answers", textOptionsWithTimeout(30000));
-        clickButton(page, "Save and continue");
-        assertThat(page.locator("ccd-markdown markdown h1"))
-            .hasText("Case closed", textOptionsWithTimeout(60000));
-        clickButton(page, "Close and Return to case details");
-        assertThat(page.locator("h2.heading-h2").first())
-            .hasText("History", textOptionsWithTimeout(60000));
-        Assertions.assertEquals(CaseClosed.label, newCase.getCaseStatus());
+        newCase.caseworkerShouldAbleToCloseTheCase();
     }
 
     @RepeatedIfExceptionsTest
