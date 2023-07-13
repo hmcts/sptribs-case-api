94ceb96ed7f5f700902f55577f30f782fc0512a8
ShwetaTandel-hmcts <130586258+ShwetaTandel-hmcts@users.noreply.github.com>
2023-07-13 09:26:43 +0100

    Shweta local config (#1074)
    * WA Config Changes - (All docker services running)
    
    * ST-1279
    WA changes for adding Referral Type in create-draft-order-event
    
    * Update build.gradle
    
    * Update CreateDraftOrder.java

--- src/main/java/uk/gov/hmcts/sptribs/common/event/page/CreateDraftOrder.java
+++ src/main/java/uk/gov/hmcts/sptribs/common/event/page/CreateDraftOrder.java
@@ -5,13 +5,14 @@ import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.sptribs.caseworker.model.DraftOrderContentCIC;
 import uk.gov.hmcts.sptribs.caseworker.util.EventUtil;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
+import uk.gov.hmcts.sptribs.ciccase.model.CicCase;
 import uk.gov.hmcts.sptribs.ciccase.model.OrderTemplate;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.common.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
 
 public class CreateDraftOrder implements CcdPageConfiguration {
-
+    
     @Override
     public void addTo(PageBuilder pageBuilder) {
         pageBuilder
@@ -21,6 +22,9 @@ public class CreateDraftOrder implements CcdPageConfiguration {
             .label("createDraftOrder", "Draft to be created")
             .complex(CaseData::getDraftOrderContentCIC)
             .mandatory(DraftOrderContentCIC::getOrderTemplate)
+            .done()
+            .complex(CaseData::getCicCase)
+            .readonly(CicCase::getReferralTypeForWA)
             .done();
     }
 
