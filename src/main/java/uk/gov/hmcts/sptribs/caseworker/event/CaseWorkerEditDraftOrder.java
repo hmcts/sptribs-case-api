fb73ad67a6d188afb8f748f66642e5c1939a8225
sjamihmcts <santoshini.jami@hmcts.net>
2023-07-24 10:44:48 +0100

    SNI-4321: Changed Calendar to LocalDateTime

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerEditDraftOrder.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerEditDraftOrder.java
@@ -21,8 +21,8 @@ import uk.gov.hmcts.sptribs.common.event.page.DraftOrderMainContentPage;
 import uk.gov.hmcts.sptribs.common.event.page.EditDraftOrder;
 import uk.gov.hmcts.sptribs.common.event.page.PreviewDraftOrder;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.UUID;
 import java.util.stream.IntStream;
@@ -52,7 +52,7 @@ public class CaseWorkerEditDraftOrder implements CCDConfig<CaseData, State, User
 
     private static final CcdPageConfiguration draftOrderEditMainContentPage = new DraftOrderMainContentPage();
 
-    private final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss", Locale.ENGLISH);
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss", Locale.ENGLISH);
 
     @Autowired
     private OrderService orderService;
@@ -95,8 +95,8 @@ public class CaseWorkerEditDraftOrder implements CCDConfig<CaseData, State, User
         CaseDetails<CaseData, State> detailsBefore
     ) {
 
-        Calendar cal = Calendar.getInstance();
-        String date = simpleDateFormat.format(cal.getTime());
+        LocalDateTime localTime = LocalDateTime.now();
+        String date = formatter.format(localTime);
         var caseData = orderService.generateOrderFile(details.getData(), details.getId(), date);
 
         return AboutToStartOrSubmitResponse.<CaseData, State>builder()
