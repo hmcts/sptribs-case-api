2c1a51c95a58479fc7300e13e0a7c4e7faa2d464
sjamihmcts <santoshini.jami@hmcts.net>
2023-08-08 12:12:05 +0100

    change to create PR.

--- src/main/java/uk/gov/hmcts/sptribs/ciccase/model/accessprofile/CICAccessProfile.java
+++ src/main/java/uk/gov/hmcts/sptribs/ciccase/model/accessprofile/CICAccessProfile.java
@@ -17,6 +17,7 @@ public class CICAccessProfile extends CriminalInjuriesCompensation {
     public void configure(final ConfigBuilder<CriminalInjuriesCompensationData, State, UserRole> configBuilder) {
         ConfigBuilderHelper.configureWithMandatoryConfig(configBuilder);
 
+
         /*
         // This block works as intended and to our understanding of setting up AM/IDAM for legacy operation
         // However if we uncomment this although be case create cases as expected we cannot see them on the case list
