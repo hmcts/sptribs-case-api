df36364ae458ae6adc970cf255f7176c97013710
hmctscivil <version1rhel@hmctscivil.mshome.net>
2023-08-09 15:15:59 +0100

    ST-1048

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerMaintainLinkCase.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerMaintainLinkCase.java
@@ -11,22 +11,16 @@ import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
-import uk.gov.hmcts.sptribs.caseworker.event.page.MaintainLinkCaseSelectCase;
 import uk.gov.hmcts.sptribs.caseworker.util.MessageUtil;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
-import uk.gov.hmcts.sptribs.common.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
 import uk.gov.hmcts.sptribs.common.links.LinkService;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static uk.gov.hmcts.sptribs.caseworker.util.EventConstants.CASEWORKER_MAINTAIN_LINK_CASE;
 import static uk.gov.hmcts.sptribs.caseworker.util.EventConstants.NO_LINKS;
 import static uk.gov.hmcts.sptribs.caseworker.util.EventConstants.YES;
-import static uk.gov.hmcts.sptribs.caseworker.util.PageShowConditionsUtil.maintainCaseLinks;
 import static uk.gov.hmcts.sptribs.ciccase.model.State.AwaitingHearing;
 import static uk.gov.hmcts.sptribs.ciccase.model.State.AwaitingOutcome;
 import static uk.gov.hmcts.sptribs.ciccase.model.State.CaseManagement;
@@ -52,40 +46,33 @@ public class CaseWorkerMaintainLinkCase implements CCDConfig<CaseData, State, Us
     @Autowired
     private LinkService linkService;
 
-    private static final String SHOW = "caseLinkExists != \"YES\"";
-    private static final CcdPageConfiguration maintainLinkCaseSelectCase = new MaintainLinkCaseSelectCase();
-
     @Override
     public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
         if (linkCaseEnabled) {
-            doConfigure(configBuilder);
+            new PageBuilder(configBuilder
+                .event(CASEWORKER_MAINTAIN_LINK_CASE)
+                .forStates(Submitted, CaseManagement, AwaitingHearing, AwaitingOutcome)
+                .name("Maintain Case Links")
+                .showSummary()
+                .description("Maintain Case Links")
+                .aboutToStartCallback(this::aboutToStart)
+                .aboutToSubmitCallback(this::aboutToSubmit)
+                .submittedCallback(this::submitted)
+                .grant(CREATE_READ_UPDATE, SUPER_USER,
+                    ST_CIC_CASEWORKER, ST_CIC_SENIOR_CASEWORKER, ST_CIC_HEARING_CENTRE_ADMIN,
+                    ST_CIC_HEARING_CENTRE_TEAM_LEADER, CASEWORKER_ADMIN_PROFILE)
+                .grantHistoryOnly(
+                    ST_CIC_CASEWORKER,
+                    ST_CIC_SENIOR_CASEWORKER,
+                    ST_CIC_HEARING_CENTRE_ADMIN,
+                    ST_CIC_HEARING_CENTRE_TEAM_LEADER,
+                    ST_CIC_SENIOR_JUDGE,
+                    SUPER_USER,
+                    ST_CIC_JUDGE))
+                .page("caseworkerMaintainCaseLinkInitial")
+                .pageLabel("Maintain Case Link")
+                .mandatory(CaseData::getCaseLinks);
         }
-    }
-
-    private void doConfigure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
-        PageBuilder pageBuilder = new PageBuilder(configBuilder
-            .event(CASEWORKER_MAINTAIN_LINK_CASE)
-            .forStates(Submitted, CaseManagement, AwaitingHearing, AwaitingOutcome)
-            .name("Maintain Case Links")
-            .showSummary()
-            .description("Maintain Case Links")
-            .aboutToStartCallback(this::aboutToStart)
-            .aboutToSubmitCallback(this::aboutToSubmit)
-            .submittedCallback(this::submitted)
-            .grant(CREATE_READ_UPDATE, SUPER_USER,
-                ST_CIC_CASEWORKER, ST_CIC_SENIOR_CASEWORKER, ST_CIC_HEARING_CENTRE_ADMIN,
-                ST_CIC_HEARING_CENTRE_TEAM_LEADER,CASEWORKER_ADMIN_PROFILE)
-            .grantHistoryOnly(
-                ST_CIC_CASEWORKER,
-                ST_CIC_SENIOR_CASEWORKER,
-                ST_CIC_HEARING_CENTRE_ADMIN,
-                ST_CIC_HEARING_CENTRE_TEAM_LEADER,
-                ST_CIC_SENIOR_JUDGE,
-                SUPER_USER,
-                ST_CIC_JUDGE));
-        addNoLinks(pageBuilder);
-        addWarning(pageBuilder);
-        maintainLinkCaseSelectCase.addTo(pageBuilder);
     }
 
 
@@ -124,37 +111,5 @@ public class CaseWorkerMaintainLinkCase implements CCDConfig<CaseData, State, Us
                 "Link removed from case", ""))
             .build();
     }
-
-    private void addWarning(PageBuilder pageBuilder) {
-        pageBuilder.page("caseworkerMaintainLinkBeforeYouStart")
-            .pageLabel("Before you start")
-            .pageShowConditions(maintainCaseLinks())
-            .label("beforeYouStartLabelForMaintain",
-                "If there are linked hearings for the case you need to un-link then you must unlink the hearing first");
-    }
-
-    private void addNoLinks(PageBuilder pageBuilder) {
-
-        pageBuilder.page("caseworkerCaseLinkNoLinks", this::midEvent)
-            .pageLabel("Warning")
-            .pageShowConditions(maintainCaseLinks())
-            .mandatory(CaseData::getCaseLinkExists);
-    }
-
-    public AboutToStartOrSubmitResponse<CaseData, State> midEvent(
-        CaseDetails<CaseData, State> details,
-        CaseDetails<CaseData, State> detailsBefore
-    ) {
-
-        CaseData caseData = details.getData();
-        final List<String> errors = new ArrayList<>();
-        errors.add("You can not proceed, press cancel to exit");
-
-        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
-            .data(caseData)
-            .errors(errors)
-            .build();
-    }
-
 
 }
