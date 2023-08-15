d800c8c1a2fcbf0f0fc51a2665fe8317ef9b67da
Sabir-v1 <sabir.obul@hmcts.net>
2023-08-15 10:17:50 +0100

    add role assignment service for prev

--- src/main/java/uk/gov/hmcts/sptribs/ciccase/model/UserRolesForAccessProfiles.java
+++ src/main/java/uk/gov/hmcts/sptribs/ciccase/model/UserRolesForAccessProfiles.java
@@ -22,7 +22,7 @@ public enum UserRolesForAccessProfiles implements HasRole {
     ST_CIC_JUDGE("idam:caseworker-st_cic-judge", "CRU"),
     ST_CIC_RESPONDENT("idam:caseworker-st_cic-respondent", "CRU"),
     CITIZEN_CIC("idam:citizen", "CRU"),
-    ST_CIC_WA_CONFIG_USER("idam:caseworker-wa-task-configuration", "CRU"),
+    ST_CIC_WA_CONFIG_USER("caseworker-wa-task-configuration", "CRU"),
     // Below are the Access Profiles for the Idam Roles
     CIC_SUPER_USER("caseworker-sptribs-superuser", "CRU"),
     CIC_CASEWORKER("caseworker-st_cic-caseworker", "CRU"),
