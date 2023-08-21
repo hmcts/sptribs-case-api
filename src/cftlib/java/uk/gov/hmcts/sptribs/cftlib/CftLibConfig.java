8e94ea66e14137e3bfde2ad71a934e1e1306539c
Fergal O'Shea <94917860+osheaf01@users.noreply.github.com>
2023-08-21 16:04:51 +0100

    ST-1048 Further tweaks to userroles and access profiles (#1223)
    * ST-1048 Further tweaks to userroles and access profiles
    
    * ST-1048 Further tweaks to userroles and access profiles
    
    ---------
    
    Co-authored-by: osheaf01 <fergal.oshea@version1.com>

--- src/cftlib/java/uk/gov/hmcts/sptribs/cftlib/CftLibConfig.java
+++ src/cftlib/java/uk/gov/hmcts/sptribs/cftlib/CftLibConfig.java
@@ -83,6 +83,7 @@ public class CftLibConfig implements CFTLibConfigurer {
             "caseworker-st_cic-senior-judge",
             "caseworker-st_cic-judge",
             "caseworker-st_cic-respondent",
+            "caseworker-admin",
             "hmcts-admin",
             "citizen"
         );
