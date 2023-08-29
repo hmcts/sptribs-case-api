73bbdd2eb9ec8592e479b20b63f5cd91db6ffc46
sjamihmcts <santoshini.jami@hmcts.net>
2023-08-29 11:29:59 +0100

    ST-1388: Removed unwanted imports

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerCreateBundle.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerCreateBundle.java
@@ -18,8 +18,6 @@ import uk.gov.hmcts.sptribs.document.bundling.client.BundlingService;
 import uk.gov.hmcts.sptribs.document.bundling.model.BundleCallback;
 import uk.gov.hmcts.sptribs.document.bundling.model.Callback;
 
-import java.util.ArrayList;
-
 import static uk.gov.hmcts.sptribs.caseworker.util.EventConstants.CREATE_BUNDLE;
 import static uk.gov.hmcts.sptribs.ciccase.model.State.BUNDLE_STATES;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_CASEWORKER;
