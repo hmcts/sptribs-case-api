e29ff1e12df52e97e89f0ffd332dea94f6506950
Amanuel Tedros <73557032+ATedy@users.noreply.github.com>
2023-09-01 14:24:10 +0100

    to do comment added

--- src/main/java/uk/gov/hmcts/sptribs/common/event/CreateTestCase.java
+++ src/main/java/uk/gov/hmcts/sptribs/common/event/CreateTestCase.java
@@ -135,6 +135,7 @@ public class CreateTestCase implements CCDConfig<CaseData, State, UserRole> {
         String claimNumber = data.getHyphenatedCaseRef();
         try {
             sendApplicationReceivedNotification(claimNumber, data);
+         // TO-DO: temporary fix, it needs to be looked at
         //            coreCaseApiService.submitSupplementaryDataRequestToCcd(claimNumber);
         } catch (Exception notificationException) {
             log.error("Create case notification failed with exception : {}", notificationException.getMessage());
