996a7d5f349a5e044f59541063e66701008d4d03
sjamihmcts <santoshini.jami@hmcts.net>
2023-09-07 11:07:59 +0100

    ST-1048: Enabled setting supplementary data by using system user to make call

--- src/main/java/uk/gov/hmcts/sptribs/common/service/CcdSupplementaryDataService.java
+++ src/main/java/uk/gov/hmcts/sptribs/common/service/CcdSupplementaryDataService.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
+import uk.gov.hmcts.reform.idam.client.models.User;
 import uk.gov.hmcts.sptribs.config.CaseFlagsConfiguration;
+import uk.gov.hmcts.sptribs.idam.IdamService;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,6 +15,9 @@ import static java.util.Collections.singletonMap;
 
 @Service
 public class CcdSupplementaryDataService {
+
+    @Autowired
+    private IdamService idamService;
 
     @Autowired
     private AuthTokenGenerator authTokenGenerator;
@@ -46,7 +51,8 @@ public class CcdSupplementaryDataService {
             singletonMap("$set", singletonMap("HMCTSServiceId",
                 caseFlagsConfiguration.getHmctsId())));
 
-        coreCaseDataApi.submitSupplementaryData(authorisationService.getAuthorisation(),
+        final User caseworkerUser = idamService.retrieveSystemUpdateUserDetails();
+        coreCaseDataApi.submitSupplementaryData(caseworkerUser.getAuthToken(),
             authTokenGenerator.generate(),
             caseId,
             supplementaryDataRequest);
