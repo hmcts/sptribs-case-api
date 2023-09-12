bd0931990b8b8f24127b03042fa76926157eab14
Shailesh Walke <117093807+Shailesh-walke@users.noreply.github.com>
2023-09-12 11:01:00 +0100

    Sni 4576 (#1254)
    * fix for bug SNI-4576
    
    * removed enableCleanupOfHelmReleaseOnSuccess from Jenkinsfile_CNP

---
+++ src/main/java/uk/gov/hmcts/sptribs/document/DocumentClient.java
@@ -0,0 +1,62 @@
+package uk.gov.hmcts.sptribs.document;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+import uk.gov.hmcts.sptribs.common.config.ControllerConstants;
+
+import java.util.UUID;
+
+@Component
+@Slf4j
+public class DocumentClient {
+    private final RestTemplate restTemplate;
+
+    @Value("${case_document_am.url}")
+    private String baseUrl;
+
+    @Autowired
+    public DocumentClient(final RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    // @GetMapping(value = "/{documentId}/binary")
+    public ResponseEntity<byte[]> getDocumentBinary(String authorisation,
+                                                    String serviceAuth,
+                                                    UUID documentId) {
+        try {
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.set(HttpHeaders.AUTHORIZATION, authorisation);
+            headers.set(ControllerConstants.SERVICE_AUTHORIZATION, serviceAuth);
+            MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
+            HttpEntity<?> requestEntity = new HttpEntity<>(requestBody, headers);
+            String apiUrl = baseUrl + "/cases/documents/%s/binary";
+            String url = String.format(apiUrl, documentId);
+            log.info(url);
+
+            final ResponseEntity<byte[]> document = restTemplate.exchange(
+                url,
+                HttpMethod.GET, requestEntity,
+                byte[].class
+            );
+
+            log.debug("Result of {} metadata call: {}", documentId, document);
+
+            return document;
+        } catch (HttpClientErrorException exception) {
+            log.error("Exception: {}", exception.getMessage());
+            throw exception;
+        }
+    }
+}
