9e90b1aa3ca75080c0d27711b507c16a027b77be
Hellen <hellen.emerem@hmcts.net>
2023-09-13 09:32:14 +0100

    Smoke test added- yaml files

--- src/functionalTest/java/uk/gov/hmcts/sptribs/smoke/SmokeFT.java
+++ src/functionalTest/java/uk/gov/hmcts/sptribs/smoke/SmokeFT.java
@@ -5,11 +5,51 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static org.junit.Assert.assertEquals;
+
 @SpringBootTest()
-public class SmokeFT {
+
+public class SmokeFT{
 
     @Value("${test.url:http://localhost:4013}")
     private String testUrl;
+
+
+    @Test
+    public void testLandingPageAccessibility() {
+
+        try {
+            URL url = new URL(testUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+
+            int responseCode = connection.getResponseCode();
+
+
+            if (responseCode == 200) {
+                System.out.println("Landing page is accessible. Smoke test passed.");
+            } else {
+                System.err.println("Landing page is not accessible. Smoke test failed. Status code: " + responseCode);
+            }
+
+            connection.disconnect();
+
+            assertEquals(200, responseCode);
+
+        } catch (IOException e) {
+
+            System.err.println("Failed to make the GET request: " + e.getMessage());
+
+        }
+
+    }
+
+
 
     @Test
     public void testHealthEndpoint() {
@@ -23,5 +63,7 @@ public class SmokeFT {
             .extract()
             .body()
             .asString();
+
     }
+
 }
