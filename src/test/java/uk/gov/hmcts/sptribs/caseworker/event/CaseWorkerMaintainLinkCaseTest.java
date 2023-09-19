0a6614052cf5ba77fff8646b2b334e5709823205
sjamihmcts <santoshini.jami@hmcts.net>
2023-09-19 12:09:30 +0100

    ST-1051: Remove unused imports

--- src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerMaintainLinkCaseTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerMaintainLinkCaseTest.java
@@ -3,14 +3,12 @@ package uk.gov.hmcts.sptribs.caseworker.event;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
 import uk.gov.hmcts.ccd.sdk.api.Event;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
-import uk.gov.hmcts.sptribs.common.notification.CaseUnlinkedNotification;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.hmcts.sptribs.testutil.ConfigTestUtil.createCaseDataConfigBuilder;
