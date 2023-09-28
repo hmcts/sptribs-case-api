34f1ae0ab09861f7c4d02f6f93ec43275781def8
Amanuel Tedros <73557032+ATedy@users.noreply.github.com>
2023-09-28 16:33:16 +0100

    ST-792 :  Case Linked (#1300)
    * case link notification added
    
    * case link notification added
    
    * comment removed
    
    * unit test added
    
    ---------
    
    Co-authored-by: sjamihmcts <santoshini.jami@hmcts.net>

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerLinkCase.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerLinkCase.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.sptribs.caseworker.event;
 
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
@@ -9,9 +10,11 @@ import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
 import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
+import uk.gov.hmcts.sptribs.ciccase.model.CicCase;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
 import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
+import uk.gov.hmcts.sptribs.common.notification.CaseLinkedNotification;
 
 import static java.lang.String.format;
 import static uk.gov.hmcts.sptribs.caseworker.util.EventConstants.CASEWORKER_LINK_CASE;
@@ -39,6 +42,9 @@ public class CaseWorkerLinkCase implements CCDConfig<CaseData, State, UserRole> 
     @Value("${feature.link-case.enabled}")
     private boolean linkCaseEnabled;
 
+    @Autowired
+    CaseLinkedNotification caseLinkedNotification;
+
     @Override
     public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
         if (linkCaseEnabled) {
@@ -46,8 +52,8 @@ public class CaseWorkerLinkCase implements CCDConfig<CaseData, State, UserRole> 
                 .event(CASEWORKER_LINK_CASE)
                 .forStates(Submitted, CaseManagement, AwaitingHearing, AwaitingOutcome)
                 .name("Link cases")
-                .submittedCallback(this::linkCreated)
                 .description("To link related cases")
+                .submittedCallback(this::submitted)
                 .grant(CREATE_READ_UPDATE, SUPER_USER,
                     ST_CIC_CASEWORKER, ST_CIC_SENIOR_CASEWORKER, ST_CIC_HEARING_CENTRE_ADMIN, CASEWORKER_ADMIN_PROFILE,
                     ST_CIC_HEARING_CENTRE_TEAM_LEADER)
@@ -68,10 +74,40 @@ public class CaseWorkerLinkCase implements CCDConfig<CaseData, State, UserRole> 
         }
     }
 
-    public SubmittedCallbackResponse linkCreated(CaseDetails<CaseData, State> details,
-                                                 CaseDetails<CaseData, State> beforeDetails) {
+    public SubmittedCallbackResponse submitted(CaseDetails<CaseData, State> details,
+                                               CaseDetails<CaseData, State> beforeDetails) {
+        var data = details.getData();
+        String claimNumber = data.getHyphenatedCaseRef();
+        try {
+            linkedCaseNotification(claimNumber, data);
+        } catch (Exception notificationException) {
+            log.error("Case Link notification failed with exception : {}", notificationException.getMessage());
+            return SubmittedCallbackResponse.builder()
+                .confirmationHeader(format("# Case Link created %n"))
+                .build();
+        }
         return SubmittedCallbackResponse.builder()
             .confirmationHeader(format("# Case Link created %n"))
+
             .build();
     }
+
+
+    private void linkedCaseNotification(String caseNumber, CaseData data) {
+        CicCase cicCase = data.getCicCase();
+
+        if (!cicCase.getSubjectCIC().isEmpty()) {
+            caseLinkedNotification.sendToSubject(data, caseNumber);
+        }
+
+        if (!cicCase.getApplicantCIC().isEmpty()) {
+            caseLinkedNotification.sendToApplicant(data, caseNumber);
+        }
+
+        if (!cicCase.getRepresentativeCIC().isEmpty()) {
+            caseLinkedNotification.sendToRepresentative(data, caseNumber);
+        }
+    }
+
+
 }
