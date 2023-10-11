47aab861ad48eecccd1d055278aa8882603babdb
osheaf01 <fergal.oshea@version1.com>
2023-10-11 09:57:38 +0100

    ST-1388 Trying some JSON annotations to ignore properties on both sides of the Java/JSON divide.

--- src/main/java/uk/gov/hmcts/sptribs/document/model/AbstractCaseworkerCICDocument.java
+++ src/main/java/uk/gov/hmcts/sptribs/document/model/AbstractCaseworkerCICDocument.java
@@ -1,6 +1,8 @@
 package uk.gov.hmcts.sptribs.document.model;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
@@ -9,6 +11,8 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class AbstractCaseworkerCICDocument<D extends CaseworkerCICDocument> {
 
     private D value;
