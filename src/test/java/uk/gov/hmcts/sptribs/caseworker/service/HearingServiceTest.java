e10258a7cd5693c61acdf09d61beae8b6d2e8bfd
osheaf01 <fergal.oshea@version1.com>
2023-10-19 23:50:11 +0100

    ST-1382 Adding more test coverage for HearingService

--- src/test/java/uk/gov/hmcts/sptribs/caseworker/service/HearingServiceTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/caseworker/service/HearingServiceTest.java
@@ -1,14 +1,18 @@
 package uk.gov.hmcts.sptribs.caseworker.service;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.type.DynamicList;
+import uk.gov.hmcts.ccd.sdk.type.DynamicListElement;
 import uk.gov.hmcts.ccd.sdk.type.ListValue;
 import uk.gov.hmcts.sptribs.caseworker.model.Listing;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
+import uk.gov.hmcts.sptribs.ciccase.model.CicCase;
 import uk.gov.hmcts.sptribs.ciccase.model.HearingState;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 
@@ -16,10 +20,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.sptribs.testutil.TestDataHelper.getRecordListing;
 
 @ExtendWith(MockitoExtension.class)
 class HearingServiceTest {
+
+    @Mock
+    private CicCase cicCase;
+
+    @Mock
+    private DynamicList cicCaseHearingList;
+
+    @Mock
+    private DynamicListElement cicCaseHearingLabel;
+
+    @Mock
+    private CaseData caseData;
 
     @InjectMocks
     private HearingService hearingService;
@@ -60,6 +77,48 @@ class HearingServiceTest {
 
         //Then
         assertThat(hearingList).isNotNull();
+
+    }
+
+    @Test
+    void shouldAddListingToCaseData() {
+        //Given
+        CaseData caseData = CaseData.builder().build();
+        Listing listing = getRecordListing();
+        assertThat(caseData.getHearingList()).isEmpty();
+
+        //When
+        hearingService.addListing(caseData, listing);
+
+        //Then
+        assertThat(caseData.getHearingList()).isNotEmpty();
+    }
+
+    @Test
+    void shouldUpdateHearingList() {
+        //Given
+        ListValue<Listing> listingListValue = new ListValue<>();
+        // A listed Listing
+        listingListValue.setValue(getRecordListing());
+        List<ListValue<Listing>> listValueList = new ArrayList<>();
+        listValueList.add(listingListValue);
+        // A completed listing
+        Listing completedListing = getRecordListing();
+        completedListing.setHearingStatus(HearingState.Complete);
+
+        when(cicCaseHearingLabel.getLabel()).thenReturn("Final 10:00");
+        when(cicCaseHearingList.getValue()).thenReturn(cicCaseHearingLabel);
+        when(cicCase.getHearingList()).thenReturn(cicCaseHearingList);
+        when(caseData.getCicCase()).thenReturn(cicCase);
+        when(caseData.getListing()).thenReturn(completedListing);
+        when(caseData.getHearingList()).thenReturn(listValueList);
+
+        //When
+        hearingService.updateHearingList(caseData);
+
+        //Then
+        // Have replaced listed Listing with completed Listing
+        Assertions.assertEquals(listValueList.get(0).getValue(), completedListing);
 
     }
 
