3c4284a252e052947da093f31b06a5b4dceb387d
osheaf01 <fergal.oshea@version1.com>
2023-10-24 16:38:37 +0100

    ST-1048 Turning CaseFlags On for AAT.

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerContactParties.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerContactParties.java
@@ -97,7 +97,6 @@ public class CaseWorkerContactParties implements CCDConfig<CaseData, State, User
                 .grant(CREATE_READ_UPDATE, SUPER_USER,
                     ST_CIC_CASEWORKER, ST_CIC_SENIOR_CASEWORKER, ST_CIC_HEARING_CENTRE_ADMIN,
                     ST_CIC_HEARING_CENTRE_TEAM_LEADER, ST_CIC_SENIOR_JUDGE));
-                    ST_CIC_HEARING_CENTRE_TEAM_LEADER, ST_CIC_SENIOR_JUDGE, ST_CIC_RESPONDENT));
         contactPartiesSelectDocument.addTo(pageBuilder);
         partiesToContact.addTo(pageBuilder);
     }
