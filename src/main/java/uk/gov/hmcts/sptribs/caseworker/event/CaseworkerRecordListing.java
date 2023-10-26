b42a7cca114153f67134a2615281bd4201408df8
Tom Elliott <tomelliott@Toms-MacBook-Pro.local>
2023-10-26 17:30:22 +0100

    DTSSTCI-40: Remove unused import

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerRecordListing.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerRecordListing.java
@@ -9,7 +9,6 @@ import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
-import uk.gov.hmcts.ccd.sdk.type.DynamicList;
 import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
 import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import uk.gov.hmcts.sptribs.caseworker.event.page.HearingTypeAndFormat;
