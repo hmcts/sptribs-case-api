927a3c3b30a784e0209d2cf1813ab0763f060381
gabriellemagruder <gabrielle.magruder@solirius.com>
2023-10-31 23:25:51 +0000

    Revert ReinstateUploadDocuments back to master

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/page/ReinstateUploadDocuments.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/page/ReinstateUploadDocuments.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.sptribs.caseworker.event.page;
 
-import org.apache.commons.lang.StringUtils;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.ccd.sdk.type.ListValue;
@@ -11,10 +10,9 @@ import uk.gov.hmcts.sptribs.common.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
 import uk.gov.hmcts.sptribs.document.model.CaseworkerCICDocument;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import static uk.gov.hmcts.sptribs.document.DocumentUtil.validateCaseworkerCICDocumentFormat;
+import static uk.gov.hmcts.sptribs.document.DocumentUtil.validateUploadedDocuments;
 
 public class ReinstateUploadDocuments implements CcdPageConfiguration {
 
@@ -44,18 +42,9 @@ public class ReinstateUploadDocuments implements CcdPageConfiguration {
     public AboutToStartOrSubmitResponse<CaseData, State> midEvent(CaseDetails<CaseData, State> details,
                                                                   CaseDetails<CaseData, State> detailsBefore) {
         final CaseData data = details.getData();
-        final List<String> errors = new ArrayList<>();
         List<ListValue<CaseworkerCICDocument>> documents = data.getCicCase().getReinstateDocuments();
+        List<String> errors = validateUploadedDocuments(documents);
 
-        if (documents != null) {
-            errors.addAll(validateCaseworkerCICDocumentFormat(documents));
-            for (ListValue<CaseworkerCICDocument> documentListValue : data.getCicCase().getReinstateDocuments()) {
-                if (null != documentListValue.getValue().getDocumentLink()
-                    && StringUtils.isEmpty(documentListValue.getValue().getDocumentEmailContent())) {
-                    errors.add("Description is mandatory for each document");
-                }
-            }
-        }
         return AboutToStartOrSubmitResponse.<CaseData, State>builder()
             .data(data)
             .errors(errors)
