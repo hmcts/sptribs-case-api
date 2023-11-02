1b7cf927f47d564124df6c00e0aee8146637df46
rajaram <rajaram.nagarajan@solirius.com>
2023-11-02 14:58:51 +0000

    SNI-4236: Added tests for Docmosis template helper

--- src/main/java/uk/gov/hmcts/sptribs/document/content/DocmosisTemplateHelper.java
+++ src/main/java/uk/gov/hmcts/sptribs/document/content/DocmosisTemplateHelper.java
@@ -27,23 +27,31 @@ public final class DocmosisTemplateHelper {
     public static String getMembers(List<ListValue<PanelMember>> memberList) {
         if (!CollectionUtils.isEmpty(memberList)) {
             StringBuilder members = new StringBuilder(100);
+
             for (ListValue<PanelMember> panelMember : memberList) {
-                members.append(panelMember.getValue().getName().getValue().getLabel()).append(COMMA_SPACE);
+                if (panelMember != null && panelMember.getValue() != null) {
+                    members.append(panelMember.getValue().getName().getValue().getLabel()).append(COMMA_SPACE);
+                }
             }
-            return members.substring(0, members.length() - 2);
+
+            return (members.length() > 0) ? members.substring(0, members.length() - 2) : EMPTY;
         }
+
         return EMPTY;
     }
 
-    public static Map<String, Object> getCommonFields(final CaseData caseData,
-                                                      final Long ccdCaseReference) {
+    public static Map<String, Object> getCommonFields(final CaseData caseData, final Long ccdCaseReference) {
 
         Map<String, Object> templateContent = new HashMap<>();
 
         templateContent.put(DECISION_DATED, LocalDate.now().format(FILE_NAME_DATE_FORMATTER));
         templateContent.put(DIRECTION_DATED, LocalDate.now().format(FILE_NAME_DATE_FORMATTER));
-        templateContent.put(CIC_CASE_SCHEME, caseData.getCicCase().getSchemeCic().getLabel());
         templateContent.put(CASE_NUMBER, ccdCaseReference);
+
+        if (caseData.getCicCase() != null && caseData.getCicCase().getSchemeCic() != null) {
+            templateContent.put(CIC_CASE_SCHEME, caseData.getCicCase().getSchemeCic().getLabel());
+        }
+
         return templateContent;
     }
 }
---
+++ src/test/java/uk/gov/hmcts/sptribs/document/content/DocmosisTemplateHelperTest.java
@@ -0,0 +1,187 @@
+package uk.gov.hmcts.sptribs.document.content;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.ccd.sdk.type.DynamicList;
+import uk.gov.hmcts.ccd.sdk.type.DynamicListElement;
+import uk.gov.hmcts.ccd.sdk.type.ListValue;
+import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
+import uk.gov.hmcts.sptribs.ciccase.model.CicCase;
+import uk.gov.hmcts.sptribs.ciccase.model.PanelMember;
+import uk.gov.hmcts.sptribs.ciccase.model.SchemeCic;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static uk.gov.hmcts.sptribs.document.content.DocmosisTemplateConstants.CASE_NUMBER;
+import static uk.gov.hmcts.sptribs.document.content.DocmosisTemplateConstants.CIC_CASE_SCHEME;
+import static uk.gov.hmcts.sptribs.document.content.DocmosisTemplateConstants.COMMA_SPACE;
+import static uk.gov.hmcts.sptribs.document.content.DocmosisTemplateConstants.DECISION_DATED;
+import static uk.gov.hmcts.sptribs.document.content.DocmosisTemplateConstants.DIRECTION_DATED;
+import static uk.gov.hmcts.sptribs.document.content.DocmosisTemplateHelper.getCommonFields;
+import static uk.gov.hmcts.sptribs.document.content.DocmosisTemplateHelper.getMembers;
+
+@ExtendWith(MockitoExtension.class)
+class DocmosisTemplateHelperTest {
+
+    private final Long ccdCaseReference = 1649246783609781L;
+    private final String PANEL_MEMBER_NAME_1 = "Hubert Blaine Earl Frederick Gerald Irvin John Kenneth Charles David";
+    private final String PANEL_MEMBER_NAME_2 = "Charles David Earl Frederick Gerald Hubert Irvin John Kenneth Hubert Blaine";
+
+    @Test
+    void shouldReturnEmptyStringWhenMembersListIsNull() {
+        //When
+        String members = getMembers(null);
+
+        //Then
+        assertTrue(members.isEmpty());
+    }
+
+    @Test
+    void shouldReturnEmptyStringWhenMembersListIsEmpty() {
+        //When
+        String members = getMembers(new ArrayList<>());
+
+        //Then
+        assertTrue(members.isEmpty());
+    }
+
+    @Test
+    void shouldReturnFullMembersWhenMembersListSupplied() {
+        //Given
+        List<ListValue<PanelMember>> memberList = getPanelMembers();
+
+        //When
+        String members = getMembers(memberList);
+
+        //Then
+        assertEquals( PANEL_MEMBER_NAME_1 + COMMA_SPACE + PANEL_MEMBER_NAME_2, members);
+    }
+
+    @Test
+    void shouldReturnFullMembersWhenMemberNull() {
+        //Given
+        List<ListValue<PanelMember>> memberList = new ArrayList<>();
+        memberList.add(null);
+
+        //When
+        String members = getMembers(memberList);
+
+        //Then
+        assertTrue(members.isEmpty());
+    }
+
+    @Test
+    void shouldReturnFullMembersWhenPanelMembersListNull() {
+        //Given
+        ListValue<PanelMember> member1 = new ListValue<>();
+        List<ListValue<PanelMember>> memberList = new ArrayList<>();
+        member1.setValue(null);
+        memberList.add(member1);
+
+        //When
+        String members = getMembers(memberList);
+
+        //Then
+        assertTrue(members.isEmpty());
+    }
+
+    @Test
+    void shouldReplaceCommonFieldsInTemplate() {
+        //Given
+        CaseData caseData = getCaseDataWithCICScheme(SchemeCic.Year1996);
+
+        //When
+        Map<String, Object> templateContent = getCommonFields(caseData, ccdCaseReference);
+
+        //Then
+        assertNotNull(templateContent.get(DECISION_DATED));
+        assertNotNull(templateContent.get(DIRECTION_DATED));
+        assertEquals(ccdCaseReference, templateContent.get(CASE_NUMBER));
+        assertEquals(SchemeCic.Year1996.getLabel(), templateContent.get(CIC_CASE_SCHEME));
+    }
+
+    @Test
+    void shouldReplaceCommonFieldsInTemplateWithNoCICCase() {
+        //Given
+        CaseData caseData = CaseData
+            .builder()
+            .cicCase(null)
+            .build();
+
+        //When
+        Map<String, Object> templateContent = getCommonFields(caseData, ccdCaseReference);
+
+        //Then
+        assertNotNull(templateContent.get(DECISION_DATED));
+        assertNotNull(templateContent.get(DIRECTION_DATED));
+        assertEquals(ccdCaseReference, templateContent.get(CASE_NUMBER));
+        assertNull(templateContent.get(CIC_CASE_SCHEME));
+    }
+
+    @Test
+    void shouldReplaceCommonFieldsInTemplateWithNoCICScheme() {
+        //Given
+        CaseData caseData = getCaseDataWithCICScheme(null);
+
+        //When
+        Map<String, Object> templateContent = getCommonFields(caseData, ccdCaseReference);
+
+        //Then
+        assertNotNull(templateContent.get(DECISION_DATED));
+        assertNotNull(templateContent.get(DIRECTION_DATED));
+        assertEquals(ccdCaseReference, templateContent.get(CASE_NUMBER));
+        assertNull(templateContent.get(CIC_CASE_SCHEME));
+    }
+
+    private List<ListValue<PanelMember>> getPanelMembers() {
+        List<ListValue<PanelMember>> members = new ArrayList<>();
+        ListValue<PanelMember> member1 = new ListValue<>();
+        ListValue<PanelMember> member2 = new ListValue<>();
+
+        PanelMember panelMember1 = PanelMember.builder()
+            .name(getDynamicList(PANEL_MEMBER_NAME_1))
+            .build();
+        PanelMember panelMember2 = PanelMember.builder()
+            .name(getDynamicList(PANEL_MEMBER_NAME_2))
+            .build();
+
+        member1.setValue(panelMember1);
+        member2.setValue(panelMember2);
+        members.add(member1);
+        members.add(member2);
+        return members;
+    }
+
+    private DynamicList getDynamicList(String panelMemberName) {
+        final DynamicListElement listItem = DynamicListElement
+            .builder()
+            .label(panelMemberName)
+            .code(UUID.randomUUID())
+            .build();
+
+        return DynamicList
+            .builder()
+            .value(listItem)
+            .listItems(List.of(listItem))
+            .build();
+    }
+
+    private CaseData getCaseDataWithCICScheme(SchemeCic schemeCic) {
+        return CaseData
+            .builder()
+            .cicCase(CicCase
+                .builder()
+                .schemeCic(schemeCic)
+                .build())
+            .build();
+    }
+
+}
