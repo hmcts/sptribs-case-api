7aa263dd89ed0aebc1c2b2d1daac41b1eff397c1
osheaf01 <fergal.oshea@version1.com>
2023-11-07 16:47:21 +0000

    ST-1382 Adding fields to SystemMigrateCase

--- src/main/java/uk/gov/hmcts/sptribs/systemupdate/event/SystemMigrateCase.java
+++ src/main/java/uk/gov/hmcts/sptribs/systemupdate/event/SystemMigrateCase.java
@@ -7,11 +7,13 @@ import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
+import uk.gov.hmcts.sptribs.ciccase.model.RetiredFields;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
+import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
 
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.SYSTEMUPDATE;
-import static uk.gov.hmcts.sptribs.ciccase.model.access.Permissions.CREATE_READ_UPDATE;
+import static uk.gov.hmcts.sptribs.ciccase.model.access.Permissions.CREATE_READ_UPDATE_DELETE;
 
 @Component
 @Slf4j
@@ -21,13 +23,29 @@ public class SystemMigrateCase implements CCDConfig<CaseData, State, UserRole> {
 
     @Override
     public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
-        configBuilder
+        PageBuilder pageBuilder = new PageBuilder(configBuilder
                 .event(SYSTEM_MIGRATE_CASE)
                 .forAllStates()
                 .aboutToSubmitCallback(this::aboutToSubmit)
                 .name("Migrate case data")
                 .description("Migrate case data to the latest version")
-                .grant(CREATE_READ_UPDATE, SYSTEMUPDATE);
+                .grant(CREATE_READ_UPDATE_DELETE, SYSTEMUPDATE));
+
+        addFields(pageBuilder);
+
+    }
+
+    private void addFields(PageBuilder pageBuilder) {
+        pageBuilder.page("retiredFields")
+                .pageLabel("Retired Fields")
+                .label("RetiredFields", "")
+                .complex(CaseData::getRetiredFields)
+                .readonly(RetiredFields::getCicBundles)
+                .readonly(RetiredFields::getCicCaseHearingCancellationReason)
+                .readonly(RetiredFields::getCicCaseCancelHearingAdditionalDetail)
+                .readonly(RetiredFields::getCicCasePostponeReason)
+                .readonly(RetiredFields::getCicCasePostponeAdditionalInformation)
+                .done();
     }
 
     public AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmit(final CaseDetails<CaseData, State> details,
