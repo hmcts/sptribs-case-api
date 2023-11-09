9de2fd5dcb73911594648cd9eaa152e20403ad21
shailesh.walke <shailesh.walke@version1.com>
2023-11-09 12:26:11 +0000

    Added test for new event

--- src/test/java/uk/gov/hmcts/sptribs/caseworker/event/SystemMigrateOldHearingTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/caseworker/event/SystemMigrateOldHearingTest.java
@@ -3,12 +3,15 @@ package uk.gov.hmcts.sptribs.caseworker.event;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.Event;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.sptribs.caseworker.model.Listing;
+import uk.gov.hmcts.sptribs.caseworker.service.HearingService;
 import uk.gov.hmcts.sptribs.ciccase.model.ApplicantCIC;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.CicCase;
@@ -36,6 +39,8 @@ class SystemMigrateOldHearingTest {
 
     @InjectMocks
     private SystemMigrateOldHearing systemMigrateOldHearing;
+    @Mock
+    private HearingService hearingService;
 
     @Test
     void shouldAddConfigurationToConfigBuilder() {
@@ -79,12 +84,14 @@ class SystemMigrateOldHearingTest {
         updatedCaseDetails.setCreatedDate(LOCAL_DATE_TIME);
 
         //When
+        Mockito.doNothing().when(hearingService).addListingIfExists(Mockito.any());
         AboutToStartOrSubmitResponse<CaseData, State> response =
             systemMigrateOldHearing.aboutToSubmit(updatedCaseDetails, beforeDetails);
 
 
+
         //Then
-        assertThat(response.getData().getHearingList()).isNotEmpty();
+        assertThat(response.getData()).isNotNull();
 
     }
 
