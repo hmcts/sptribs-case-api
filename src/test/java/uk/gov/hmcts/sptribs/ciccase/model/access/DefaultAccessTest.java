324f5062b2b0b312578e6b6bb88fe93f24d4820f
sjamihmcts <santoshini.jami@hmcts.net>
2023-11-14 13:52:43 +0000

    ST-1048: Fix tests

--- src/test/java/uk/gov/hmcts/sptribs/ciccase/model/access/DefaultAccessTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/ciccase/model/access/DefaultAccessTest.java
@@ -27,7 +27,7 @@ class DefaultAccessTest {
         final SetMultimap<HasRole, Permission> grants = new DefaultAccess().getGrants();
         //Then
         assertThat(grants)
-            .hasSize(22)
+            .hasSize(26)
             .contains(
                 entry(CREATOR, C),
                 entry(CREATOR, R),
