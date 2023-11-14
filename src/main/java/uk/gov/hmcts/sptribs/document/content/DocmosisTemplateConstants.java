ae2656e4ae996df66def61da814e25f6aa63aa07
Rajaram <70951210+rajaram-nagarajan@users.noreply.github.com>
2023-11-14 11:47:04 +0000

    SNI-4236: Added Legal officer template for orders (#1416)

--- src/main/java/uk/gov/hmcts/sptribs/document/content/DocmosisTemplateConstants.java
+++ src/main/java/uk/gov/hmcts/sptribs/document/content/DocmosisTemplateConstants.java
@@ -10,7 +10,8 @@ public final class DocmosisTemplateConstants {
     public static final String CIC_CASE_SCHEME = "cicCaseSchemeCic";
     public static final String SUBJECT_FULL_NAME = "SubjectRepFullName";
     public static final String CASE_NUMBER = "cicCaseCaseNumber";
-    public static final String DATED = "DateDecisionDocCreated";
+    public static final String DECISION_DATED = "DateDecisionDocCreated";
+    public static final String DIRECTION_DATED = "DateDirectionDocCreated";
     public static final String HEARING_TYPE = "venueOrHearingType";
     public static final String TRIBUNAL_MEMBERS = "tribunalMembers";
     public static final String DECISION_SIGNATURE = "decisionSignature";
