7eeb05c377203a337e27e698ed78951804512711
sjamihmcts <santoshini.jami@hmcts.net>
2023-11-14 10:22:00 +0000

    Feat/st 1051: Few more changes and Tests for Case Links

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerMaintainLinkCase.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerMaintainLinkCase.java
@@ -1,31 +1,35 @@
 package uk.gov.hmcts.sptribs.caseworker.event;
 
 import lombok.Setter;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
-import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
-import uk.gov.hmcts.sptribs.caseworker.event.page.LinkCaseSelectCase;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
+import uk.gov.hmcts.sptribs.ciccase.model.CicCase;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
-import uk.gov.hmcts.sptribs.common.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
+import uk.gov.hmcts.sptribs.common.notification.CaseUnlinkedNotification;
 
+import static java.lang.String.format;
 import static uk.gov.hmcts.sptribs.caseworker.util.EventConstants.CASEWORKER_MAINTAIN_LINK_CASE;
 import static uk.gov.hmcts.sptribs.ciccase.model.State.AwaitingHearing;
 import static uk.gov.hmcts.sptribs.ciccase.model.State.AwaitingOutcome;
 import static uk.gov.hmcts.sptribs.ciccase.model.State.CaseManagement;
 import static uk.gov.hmcts.sptribs.ciccase.model.State.Submitted;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.CASEWORKER_ADMIN_PROFILE;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_CASEWORKER;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_HEARING_CENTRE_ADMIN;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_HEARING_CENTRE_TEAM_LEADER;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_JUDGE;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_SENIOR_CASEWORKER;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_SENIOR_JUDGE;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.SUPER_USER;
 import static uk.gov.hmcts.sptribs.ciccase.model.access.Permissions.CREATE_READ_UPDATE;
 
 @Component
@@ -33,51 +37,74 @@ import static uk.gov.hmcts.sptribs.ciccase.model.access.Permissions.CREATE_READ_
 @Setter
 public class CaseWorkerMaintainLinkCase implements CCDConfig<CaseData, State, UserRole> {
 
+    private static final String ALWAYS_HIDE = "LinkedCasesComponentLauncher = \"DONOTSHOW\"";
+
     @Value("${feature.link-case.enabled}")
     private boolean linkCaseEnabled;
 
-    private static final CcdPageConfiguration linkCaseSelectCase = new LinkCaseSelectCase();
+    @Autowired
+    CaseUnlinkedNotification caseUnlinkedNotification;
 
     @Override
     public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
         if (linkCaseEnabled) {
-            doConfigure(configBuilder);
+            new PageBuilder(configBuilder
+                .event(CASEWORKER_MAINTAIN_LINK_CASE)
+                .forStates(Submitted, CaseManagement, AwaitingHearing, AwaitingOutcome)
+                .name("Manage case links")
+                .submittedCallback(this::linkUpdated)
+                .description("To maintain linked cases")
+                .grant(CREATE_READ_UPDATE, SUPER_USER,
+                    ST_CIC_CASEWORKER, ST_CIC_SENIOR_CASEWORKER, ST_CIC_HEARING_CENTRE_ADMIN,
+                    ST_CIC_HEARING_CENTRE_TEAM_LEADER, CASEWORKER_ADMIN_PROFILE)
+                .grantHistoryOnly(
+                    ST_CIC_CASEWORKER,
+                    ST_CIC_SENIOR_CASEWORKER,
+                    ST_CIC_HEARING_CENTRE_ADMIN,
+                    ST_CIC_HEARING_CENTRE_TEAM_LEADER,
+                    ST_CIC_SENIOR_JUDGE,
+                    SUPER_USER,
+                    ST_CIC_JUDGE))
+                .page("maintainCaseLink")
+                .pageLabel("Maintain Case Link")
+                .optional(CaseData::getCaseLinks, ALWAYS_HIDE, null, true)
+                .optional(CaseData::getLinkedCasesComponentLauncher,
+                    null, null, null, null, "#ARGUMENT(UPDATE,LinkedCases)");
         }
     }
 
-    private void doConfigure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
-        PageBuilder pageBuilder = new PageBuilder(configBuilder
-            .event(CASEWORKER_MAINTAIN_LINK_CASE)
-            .forStates(Submitted, CaseManagement, AwaitingHearing, AwaitingOutcome)
-            .name("Links: Maintain Link case")
-            .showSummary()
-            .description("Links: Maintain Link case")
-            .aboutToSubmitCallback(this::aboutToSubmit)
-            .submittedCallback(this::submitted)
-            .grant(CREATE_READ_UPDATE,
-                ST_CIC_CASEWORKER, ST_CIC_SENIOR_CASEWORKER, ST_CIC_HEARING_CENTRE_ADMIN,
-                ST_CIC_HEARING_CENTRE_TEAM_LEADER));
-
-        linkCaseSelectCase.addTo(pageBuilder);
-    }
-
-    @SneakyThrows
-    public AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmit(CaseDetails<CaseData, State> details,
-                                                                       CaseDetails<CaseData, State> beforeDetails) {
-        log.info("Caseworker link the case callback invoked for Case Id: {}", details.getId());
+    public SubmittedCallbackResponse linkUpdated(CaseDetails<CaseData, State> details,
+                                                 CaseDetails<CaseData, State> beforeDetails) {
         var data = details.getData();
-
-        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
-            .data(data)
-            .state(details.getState())
+        String claimNumber = data.getHyphenatedCaseRef();
+        try {
+            unLinkedCaseNotification(claimNumber, data);
+        } catch (Exception notificationException) {
+            log.error("Case Link notification failed with exception : {}", notificationException.getMessage());
+            return SubmittedCallbackResponse.builder()
+                .confirmationHeader(format("# Case Link updated %n"))
+                .build();
+        }
+        return SubmittedCallbackResponse.builder()
+            .confirmationHeader(format("# Case Link updated %n"))
             .build();
     }
 
-    public SubmittedCallbackResponse submitted(CaseDetails<CaseData, State> details,
-                                               CaseDetails<CaseData, State> beforeDetails) {
-        return SubmittedCallbackResponse.builder()
-            .confirmationHeader("Case Updated")
-            .build();
+
+    private void unLinkedCaseNotification(String caseNumber, CaseData data) {
+        CicCase cicCase = data.getCicCase();
+
+        if (!cicCase.getSubjectCIC().isEmpty()) {
+            caseUnlinkedNotification.sendToSubject(data, caseNumber);
+        }
+
+        if (!cicCase.getApplicantCIC().isEmpty()) {
+            caseUnlinkedNotification.sendToApplicant(data, caseNumber);
+        }
+
+        if (!cicCase.getRepresentativeCIC().isEmpty()) {
+            caseUnlinkedNotification.sendToRepresentative(data, caseNumber);
+        }
     }
 
 }
--- src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerMaintainLinkCaseTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerMaintainLinkCaseTest.java
@@ -3,20 +3,27 @@ package uk.gov.hmcts.sptribs.caseworker.event;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.Event;
-import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
-import uk.gov.hmcts.ccd.sdk.type.CaseLink;
 import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
-import uk.gov.hmcts.sptribs.caseworker.model.LinkCaseReason;
+import uk.gov.hmcts.sptribs.ciccase.model.ApplicantCIC;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.CicCase;
+import uk.gov.hmcts.sptribs.ciccase.model.RepresentativeCIC;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
+import uk.gov.hmcts.sptribs.ciccase.model.SubjectCIC;
 import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
+import uk.gov.hmcts.sptribs.common.notification.CaseUnlinkedNotification;
+
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doNothing;
 import static uk.gov.hmcts.sptribs.testutil.ConfigTestUtil.createCaseDataConfigBuilder;
 import static uk.gov.hmcts.sptribs.testutil.ConfigTestUtil.getEventsFrom;
 import static uk.gov.hmcts.sptribs.testutil.TestConstants.TEST_CASE_ID;
@@ -28,6 +35,9 @@ import static uk.gov.hmcts.sptribs.testutil.TestEventConstants.CASEWORKER_MAINTA
 class CaseWorkerMaintainLinkCaseTest {
     @InjectMocks
     private CaseWorkerMaintainLinkCase caseWorkerMaintainLinkCase;
+
+    @Mock
+    private CaseUnlinkedNotification caseUnlinkedNotification;
 
     @Test
     void shouldAddConfigurationToConfigBuilder() {
@@ -59,26 +69,31 @@ class CaseWorkerMaintainLinkCaseTest {
     }
 
     @Test
-    void shouldAdd2LinksToCase() {
+    void shouldSuccessfullyStayTheCase() {
         //Given
-        CicCase cicCase = CicCase.builder()
-            .linkCaseNumber(new CaseLink())
-            .linkCaseReason(LinkCaseReason.CASE_CONSOLIDATED)
-            .build();
-        CaseData caseData = caseData();
+        final CaseData caseData = caseData();
+        CicCase cicCase = new CicCase();
+        cicCase.setSubjectCIC(Set.of(SubjectCIC.SUBJECT));
+        cicCase.setApplicantCIC(Set.of(ApplicantCIC.APPLICANT_CIC));
+        cicCase.setRepresentativeCIC(Set.of(RepresentativeCIC.REPRESENTATIVE));
         caseData.setCicCase(cicCase);
         final CaseDetails<CaseData, State> updatedCaseDetails = new CaseDetails<>();
         final CaseDetails<CaseData, State> beforeDetails = new CaseDetails<>();
         updatedCaseDetails.setData(caseData);
         updatedCaseDetails.setId(TEST_CASE_ID);
         updatedCaseDetails.setCreatedDate(LOCAL_DATE_TIME);
+
         //When
-        AboutToStartOrSubmitResponse<CaseData, State> response1 =
-            caseWorkerMaintainLinkCase.aboutToSubmit(updatedCaseDetails, beforeDetails);
-        SubmittedCallbackResponse submitted = caseWorkerMaintainLinkCase.submitted(updatedCaseDetails, beforeDetails);
+        doNothing().when(caseUnlinkedNotification).sendToSubject(any(CaseData.class), eq(null));
+        doNothing().when(caseUnlinkedNotification).sendToApplicant(any(CaseData.class), eq(null));
+        doNothing().when(caseUnlinkedNotification).sendToRepresentative(any(CaseData.class), eq(null));
+
+        SubmittedCallbackResponse response =
+            caseWorkerMaintainLinkCase.linkUpdated(updatedCaseDetails, beforeDetails);
 
         //Then
-        assertThat(response1).isNotNull();
-        assertThat(submitted).isNotNull();
+        assertThat(response).isNotNull();
+
     }
+
 }
