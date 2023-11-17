e079a5bf298b6c233a30e884f50658a8fc4535d5
gabriellemagruder <gabrielle.magruder@solirius.com>
2023-11-17 09:26:31 +0000

    Remove space.

--- src/main/java/uk/gov/hmcts/sptribs/ciccase/search/CaseFieldsConstants.java
+++ src/main/java/uk/gov/hmcts/sptribs/ciccase/search/CaseFieldsConstants.java
@@ -15,7 +15,6 @@ public final class CaseFieldsConstants {
     public static final String CASE_STATE = "[STATE]";
     public static final String CASE_REGION = "cicCaseRegionCIC";
     public static final String DUE_DATE = "cicCaseFirstDueDate";
-
     public static final String SCHEME = "cicCaseSchemeCic";
     public static final String LAST_MODIFIED_DATE = "[LAST_MODIFIED_DATE]";
     public static final String LAST_STATE_MODIFIED_DATE = "[LAST_STATE_MODIFIED_DATE]";
