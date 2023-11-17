e81ce84bbf8979b4c6f25ba8ca07cc565b71a7b8
osheaf01 <fergal.oshea@version1.com>
2023-11-17 12:15:50 +0000

    ST-1382 Adding HearingServiceTest master changes

--- src/test/java/uk/gov/hmcts/sptribs/caseworker/service/HearingServiceTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/caseworker/service/HearingServiceTest.java
@@ -1,16 +1,20 @@
 package uk.gov.hmcts.sptribs.caseworker.service;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.type.DynamicList;
+import uk.gov.hmcts.ccd.sdk.type.DynamicListElement;
 import uk.gov.hmcts.ccd.sdk.type.ListValue;
 import uk.gov.hmcts.sptribs.caseworker.model.HearingCancellationReason;
 import uk.gov.hmcts.sptribs.caseworker.model.Listing;
 import uk.gov.hmcts.sptribs.caseworker.model.PostponeReason;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
+import uk.gov.hmcts.sptribs.ciccase.model.CicCase;
 import uk.gov.hmcts.sptribs.ciccase.model.HearingState;
 import uk.gov.hmcts.sptribs.ciccase.model.RetiredFields;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
@@ -19,10 +23,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.sptribs.testutil.TestDataHelper.getRecordListing;
 
 @ExtendWith(MockitoExtension.class)
 class HearingServiceTest {
+
+    @Mock
+    private CicCase cicCase;
+
+    @Mock
+    private DynamicList cicCaseHearingList;
+
+    @Mock
+    private DynamicListElement cicCaseHearingLabel;
+
+    @Mock
+    private CaseData caseData;
 
     @InjectMocks
     private HearingService hearingService;
@@ -135,7 +152,6 @@ class HearingServiceTest {
         final CaseData caseData = CaseData.builder().hearingList(listValueList).build();
         details.setData(caseData);
         //When
-
         DynamicList hearingList = hearingService.getCompletedHearingDynamicList(caseData);
 
         //Then
@@ -181,6 +197,7 @@ class HearingServiceTest {
         //Then
         // Have replaced listed Listing with completed Listing
         Assertions.assertEquals(listValueList.get(0).getValue(), completedListing);
+
     }
 
     @Test
