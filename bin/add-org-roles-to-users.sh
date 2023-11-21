2a271f855e16829061eb9988ccd03a7b46ac4608
colmlynch60 <colm.lynch@hmcts.net>
2023-11-21 13:24:24 +0000

    Temp revert to fix build issues

--- bin/add-org-roles-to-users.sh
+++ bin/add-org-roles-to-users.sh
@@ -3,6 +3,6 @@
 # Setup Users
 echo ""
 echo "Setting up WA Users and role assignments..."
-./bin/utils/organisational-role-assignment.sh "${ET_CASEOFFICER_USERNAME}" "${ET_CASEOFFICER_PASSWORD}" "PUBLIC" "case-allocator" '{"jurisdiction":"EMPLOYMENT","primaryLocation":"765324"}'
-./bin/utils/organisational-role-assignment.sh "${ET_CASEOFFICER_USERNAME}" "${ET_CASEOFFICER_PASSWORD}" "PUBLIC" "task-supervisor" '{"jurisdiction":"EMPLOYMENT","primaryLocation":"765324"}'
-./bin/utils/organisational-role-assignment.sh "${ET_CASEOFFICER_USERNAME}" "${ET_CASEOFFICER_PASSWORD}" "PUBLIC" "tribunal-caseworker" '{"jurisdiction":"EMPLOYMENT","primaryLocation":"765324"}'
+./bin/utils/organisational-role-assignment.sh "${IDAM_SOLICITOR_USERNAME}" "${IDAM_SOLICITOR_PASSWORD}" "PUBLIC" "case-allocator" '{"jurisdiction":"ST_CIC","primaryLocation":"765324"}'
+./bin/utils/organisational-role-assignment.sh "${IDAM_SYSTEM_UPDATE_USERNAME}" "${IDAM_SYSTEM_UPDATE_PASSWORD}" "PUBLIC" "task-supervisor" '{"jurisdiction":"ST_CIC","primaryLocation":"765324"}'
+./bin/utils/organisational-role-assignment.sh "${IDAM_SOLICITOR1_USERNAME}" "${IDAM_SOLICITOR1_PASSWORD}" "PUBLIC" "tribunal-caseworker" '{"jurisdiction":"ST_CIC","primaryLocation":"765324"}'
