32d17c74e3afe450fcc7eaaef0ea550d7a5a0259
Tom Elliott <tomelliott@Toms-MacBook-Pro.local>
2023-11-23 12:28:43 +0000

    SNI-4966: Add functional test to ensure last selected order issue is caught in future

---
+++ src/functionalTest/java/uk/gov/hmcts/sptribs/caseworker/CaseworkerSendOrderFT.java
@@ -0,0 +1,37 @@
+package uk.gov.hmcts.sptribs.caseworker;
+
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import uk.gov.hmcts.sptribs.testutil.FunctionalTestSuite;
+
+import java.util.Map;
+
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.json;
+import static net.javacrumbs.jsonunit.core.Option.IGNORING_EXTRA_FIELDS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpStatus.OK;
+import static uk.gov.hmcts.sptribs.caseworker.util.EventConstants.CASEWORKER_SEND_ORDER;
+import static uk.gov.hmcts.sptribs.testutil.CaseDataUtil.caseData;
+import static uk.gov.hmcts.sptribs.testutil.TestConstants.ABOUT_TO_SUBMIT_URL;
+import static uk.gov.hmcts.sptribs.testutil.TestResourceUtil.expectedResponse;
+
+@SpringBootTest
+public class CaseworkerSendOrderFT extends FunctionalTestSuite {
+
+    private static final String REQUEST = "classpath:request/casedata/ccd-callback-casedata-send-order-about-to-start.json";
+    private static final String RESPONSE = "classpath:responses/response-caseworker-send-order-about-to-submit.json";
+
+    @Test
+    public void shouldRetainLastSelectedOrderDataWhenAboutToSubmitCallbackIsTriggered() throws Exception {
+        final Map<String, Object> caseData = caseData(REQUEST);
+
+        final Response response = triggerCallback(caseData, CASEWORKER_SEND_ORDER, ABOUT_TO_SUBMIT_URL);
+        assertThat(response.getStatusCode()).isEqualTo(OK.value());
+
+        assertThatJson(response.asString())
+            .when(IGNORING_EXTRA_FIELDS)
+            .isEqualTo(json(expectedResponse(RESPONSE)));
+    }
+}
