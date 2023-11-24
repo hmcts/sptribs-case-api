a7652fd07573066f03364f98880391a1cdc3bf6c
rajaram <rajaram.nagarajan@solirius.com>
2023-11-24 10:18:28 +0000

    DTSSTCI-67: Handling CCD definition import failure

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/model/Order.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/model/Order.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.sptribs.caseworker.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -74,7 +73,6 @@ public class Order {
     @CCD(
         access = {DefaultAccess.class, CaseworkerWithCAAAccess.class}
     )
-    @JsonIgnore
     private YesOrNo isLastSelectedOrder;
 
 }
