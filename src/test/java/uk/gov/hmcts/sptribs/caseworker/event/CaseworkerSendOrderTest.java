a8854d90e2136250e2df9ed910c879253ad63b12
osheaf01 <fergal.oshea@version1.com>
2023-11-28 14:56:00 +0000

    ST-1688 Fixing R3 build

--- src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerSendOrderTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerSendOrderTest.java
@@ -13,7 +13,6 @@ import uk.gov.hmcts.ccd.sdk.type.Document;
 import uk.gov.hmcts.ccd.sdk.type.DynamicList;
 import uk.gov.hmcts.ccd.sdk.type.DynamicListElement;
 import uk.gov.hmcts.ccd.sdk.type.ListValue;
-import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
 import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import uk.gov.hmcts.sptribs.caseworker.model.DateModel;
 import uk.gov.hmcts.sptribs.caseworker.model.DraftOrderCIC;
@@ -149,7 +148,6 @@ class CaseworkerSendOrderTest {
         Order order = response.getData().getCicCase().getOrderList().get(0).getValue();
         assertThat(order.getDueDateList().get(0).getValue().getDueDate()).isNotNull();
         assertThat(order.getUploadedFile()).isNotNull();
-        assertThat(order.getIsLastSelectedOrder()).isEqualTo(YesOrNo.YES);
     }
 
     @Test
@@ -288,7 +286,6 @@ class CaseworkerSendOrderTest {
         dates.setValue(dateModel);
         final DraftOrderCIC draftOrderCIC = DraftOrderCIC.builder()
             .templateGeneratedDocument(Document.builder().filename("aa--bb--cc").build())
-
             .draftOrderContentCIC(DraftOrderContentCIC.builder().orderTemplate(OrderTemplate.CIC6_GENERAL_DIRECTIONS).build())
             .build();
         final ListValue<DraftOrderCIC> draftOrderCICListValue = new ListValue<>();
@@ -347,7 +344,6 @@ class CaseworkerSendOrderTest {
         final DraftOrderCIC draftOrderCIC = DraftOrderCIC.builder()
             .templateGeneratedDocument(Document.builder().filename("aa--bb--cc").build())
             .draftOrderContentCIC(DraftOrderContentCIC.builder().orderTemplate(OrderTemplate.CIC6_GENERAL_DIRECTIONS).build())
-            .templateGeneratedDocument(Document.builder().filename("aa--bb--cc").build())
             .build();
         final ListValue<DraftOrderCIC> draftOrderCICListValue = new ListValue<>();
         draftOrderCICListValue.setValue(draftOrderCIC);
