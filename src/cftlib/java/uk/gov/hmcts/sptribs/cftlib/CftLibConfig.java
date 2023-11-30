45665d8ed48d3c6e80efad7d26e31bdbd9e09315
osheaf01 <fergal.oshea@version1.com>
2023-11-30 09:56:44 +0000

    ST-1688 Fixing R2/2.1 build - completing removal of caseworker-admin UserRole and all its associated assignments

--- src/cftlib/java/uk/gov/hmcts/sptribs/cftlib/CftLibConfig.java
+++ src/cftlib/java/uk/gov/hmcts/sptribs/cftlib/CftLibConfig.java
@@ -83,7 +83,6 @@ public class CftLibConfig implements CFTLibConfigurer {
             "caseworker-st_cic-senior-judge",
             "caseworker-st_cic-judge",
             "caseworker-st_cic-respondent",
-            "caseworker-admin",
             "caseflags-admin",
             "caseflags-viewer",
             "citizen"
