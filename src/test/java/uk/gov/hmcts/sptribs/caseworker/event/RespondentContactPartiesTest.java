d2ebd3a59a2c67f8c87eed809625d5177fce9ec0
rajaram <rajaram.nagarajan@solirius.com>
2023-12-05 12:07:39 +0000

    ST-1663: Contact parties notification fix

--- src/test/java/uk/gov/hmcts/sptribs/caseworker/event/RespondentContactPartiesTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/caseworker/event/RespondentContactPartiesTest.java
@@ -12,7 +12,15 @@ import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import uk.gov.hmcts.sptribs.caseworker.event.page.RespondentPartiesToContact;
 import uk.gov.hmcts.sptribs.caseworker.model.ContactParties;
-import uk.gov.hmcts.sptribs.ciccase.model.*;
+import uk.gov.hmcts.sptribs.ciccase.model.ApplicantCIC;
+import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
+import uk.gov.hmcts.sptribs.ciccase.model.CicCase;
+import uk.gov.hmcts.sptribs.ciccase.model.ContactPartiesCIC;
+import uk.gov.hmcts.sptribs.ciccase.model.RepresentativeCIC;
+import uk.gov.hmcts.sptribs.ciccase.model.State;
+import uk.gov.hmcts.sptribs.ciccase.model.SubjectCIC;
+import uk.gov.hmcts.sptribs.ciccase.model.TribunalCIC;
+import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
 import uk.gov.hmcts.sptribs.common.notification.ContactPartiesNotification;
 
 import java.util.HashSet;
