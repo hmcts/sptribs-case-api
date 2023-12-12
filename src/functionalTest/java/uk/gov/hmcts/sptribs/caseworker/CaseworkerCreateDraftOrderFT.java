bf2b88008215665e4a7e6cb54c2c667bd1d6ad83
muradalamv1 <murad.alam@version1.com>
2023-12-12 11:11:25 +0000

    Added functional test for Draft order

--- src/functionalTest/java/uk/gov/hmcts/sptribs/caseworker/CaseworkerCreateDraftOrderFT.java
+++ src/functionalTest/java/uk/gov/hmcts/sptribs/caseworker/CaseworkerCreateDraftOrderFT.java
@@ -1,6 +1,13 @@
 package uk.gov.hmcts.sptribs.caseworker;
 
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 import uk.gov.hmcts.sptribs.caseworker.util.EventConstants;
+import uk.gov.hmcts.sptribs.testutil.FunctionalTestSuite;
+
+import java.util.Map;
+
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.json;
 import static net.javacrumbs.jsonunit.core.Option.IGNORING_EXTRA_FIELDS;
@@ -9,21 +16,20 @@ import static org.springframework.http.HttpStatus.OK;
 import static uk.gov.hmcts.sptribs.testutil.CaseDataUtil.caseData;
 import static uk.gov.hmcts.sptribs.testutil.TestConstants.ABOUT_TO_SUBMIT_URL;
 import static uk.gov.hmcts.sptribs.testutil.TestResourceUtil.expectedResponse;
-import   io.restassured.response.Response ;
 
-@org.springframework.boot.test.context.SpringBootTest
-public class CaseworkerCreateDraftOrderFT extends uk.gov.hmcts.sptribs.testutil.FunctionalTestSuite {
+@SpringBootTest
+public class CaseworkerCreateDraftOrderFT extends FunctionalTestSuite {
 
     private static final String REQUEST = "classpath:request/casedata/ccd-callback-casedata.json";
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void shouldSuccessfullyCreateDraftOrder() throws Exception {
-        final java.util.Map<String, Object> caseData = caseData(REQUEST);
+        final Map<String, Object> caseData = caseData(REQUEST);
 
         final Response response = triggerCallback(caseData, EventConstants.CASEWORKER_CREATE_DRAFT_ORDER, ABOUT_TO_SUBMIT_URL);
         assertThat(response.getStatusCode()).isEqualTo(OK.value());
 
-             assertThatJson(response.asString())
+        assertThatJson(response.asString())
             .when(IGNORING_EXTRA_FIELDS)
             .isEqualTo(json(expectedResponse(
                 "classpath:responses/response-caseworker-create-draft-about-to-submit.json"
