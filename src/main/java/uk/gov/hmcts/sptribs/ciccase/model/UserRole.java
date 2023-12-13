80a33e74265347902dafe831fe2a1ee4d0ae0738
Fergal O'Shea <94917860+osheaf01@users.noreply.github.com>
2023-12-13 16:48:06 +0000

    St 1051 - Unshuttering after migration (#1586)
    * ST-1051 Promoting flags to true on PROD for case flags and case linking. Removing CRUD permissions for all user types bar systemupdate
    
    * ST-1051 Upping limit of number of cases to be migrated
    
    * ST-1051 Rolling back UserRole restrictions
    
    * ST-1051 UserRoles back to CRUD bar flag-viewer as R
    
    * ST-1051 UserRoles back to CRU bar flag-viewer as R
    
    * ST-1051 Re-enabling e2e tests
    
    ---------
    
    Co-authored-by: osheaf01 <fergal.oshea@version1.com>

--- src/main/java/uk/gov/hmcts/sptribs/ciccase/model/UserRole.java
+++ src/main/java/uk/gov/hmcts/sptribs/ciccase/model/UserRole.java
@@ -11,29 +11,29 @@ public enum UserRole implements HasRole {
 
     // Common User roles
     //TODO: update : SUPER_USER, SOLICITOR before prod deploy
-    SUPER_USER("caseworker-sptribs-superuser", "D"),
+    SUPER_USER("caseworker-sptribs-superuser", "CRU"),
     SYSTEMUPDATE("caseworker-sptribs-systemupdate", "CRUD"),
-    SOLICITOR("caseworker", "D"),
-    CREATOR("[CREATOR]", "D"),
+    SOLICITOR("caseworker", "CRU"),
+    CREATOR("[CREATOR]", "CRU"),
 
     // CIC User roles
-    COURT_ADMIN_CIC("caseworker-sptribs-cic-courtadmin", "D"),
-    CASE_OFFICER_CIC("caseworker-sptribs-cic-caseofficer", "D"),
-    DISTRICT_REGISTRAR_CIC("caseworker-sptribs-cic-districtregistrar", "D"),
-    DISTRICT_JUDGE_CIC("caseworker-sptribs-cic-districtjudge", "D"),
-    RESPONDENT_CIC("caseworker-sptribs-cic-respondent", "D"),
+    COURT_ADMIN_CIC("caseworker-sptribs-cic-courtadmin", "CRU"),
+    CASE_OFFICER_CIC("caseworker-sptribs-cic-caseofficer", "CRU"),
+    DISTRICT_REGISTRAR_CIC("caseworker-sptribs-cic-districtregistrar", "CRU"),
+    DISTRICT_JUDGE_CIC("caseworker-sptribs-cic-districtjudge", "CRU"),
+    RESPONDENT_CIC("caseworker-sptribs-cic-respondent", "CRU"),
 
-    ST_CIC_CASEWORKER("caseworker-st_cic-caseworker", "D"),
-    ST_CIC_SENIOR_CASEWORKER("caseworker-st_cic-senior-caseworker", "D"),
-    ST_CIC_HEARING_CENTRE_ADMIN("caseworker-st_cic-hearing-centre-admin", "D"),
-    ST_CIC_HEARING_CENTRE_TEAM_LEADER("caseworker-st_cic-hearing-centre-team-leader", "D"),
-    ST_CIC_SENIOR_JUDGE("caseworker-st_cic-senior-judge", "D"),
+    ST_CIC_CASEWORKER("caseworker-st_cic-caseworker", "CRU"),
+    ST_CIC_SENIOR_CASEWORKER("caseworker-st_cic-senior-caseworker", "CRU"),
+    ST_CIC_HEARING_CENTRE_ADMIN("caseworker-st_cic-hearing-centre-admin", "CRU"),
+    ST_CIC_HEARING_CENTRE_TEAM_LEADER("caseworker-st_cic-hearing-centre-team-leader", "CRU"),
+    ST_CIC_SENIOR_JUDGE("caseworker-st_cic-senior-judge", "CRU"),
 
-    ST_CIC_JUDGE("caseworker-st_cic-judge", "D"),
-    ST_CIC_RESPONDENT("caseworker-st_cic-respondent", "D"),
-    AC_CASEFLAGS_ADMIN("caseflags-admin", "D"),
-    AC_CASEFLAGS_VIEWER("caseflags-viewer", "D"),
-    CITIZEN_CIC("citizen", "D");
+    ST_CIC_JUDGE("caseworker-st_cic-judge", "CRU"),
+    ST_CIC_RESPONDENT("caseworker-st_cic-respondent", "CRU"),
+    AC_CASEFLAGS_ADMIN("caseflags-admin", "CRU"),
+    AC_CASEFLAGS_VIEWER("caseflags-viewer", "R"),
+    CITIZEN_CIC("citizen", "CRU");
 
     @JsonValue
     private final String role;
