94963fe10c70b930840eb9b0c33e95357ed1eab8
muradalamv1 <murad.alam@version1.com>
2023-12-19 13:51:38 +0000

    Added some requests and responses in functional tests

---
+++ src/functionalTest/java/uk/gov/hmcts/sptribs/caseworker/CaseworkerReferToLegalOfficerFT.java
@@ -0,0 +1,57 @@
+package uk.gov.hmcts.sptribs.caseworker;
+
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import uk.gov.hmcts.sptribs.caseworker.util.EventConstants;
+
+import java.util.Map;
+
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.json;
+import static net.javacrumbs.jsonunit.core.Option.IGNORING_EXTRA_FIELDS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpStatus.OK;
+import static uk.gov.hmcts.sptribs.testutil.CaseDataUtil.caseData;
+import static uk.gov.hmcts.sptribs.testutil.TestConstants.ABOUT_TO_SUBMIT_URL;
+import static uk.gov.hmcts.sptribs.testutil.TestConstants.SUBMITTED_URL;
+import static uk.gov.hmcts.sptribs.testutil.TestResourceUtil.expectedResponse;
+
+@SpringBootTest
+public class CaseworkerReferToLegalOfficerFT extends uk.gov.hmcts.sptribs.testutil.FunctionalTestSuite {
+
+    private static final String REQUEST = "classpath:request/casedata/ccd-callback-casedata.json";
+    private static final String REQUEST_SUBMITTED =
+        "classpath:request/casedata/ccd-callback-casedata-refer-cases-to-legal-officer-submitted.json";
+
+    @org.junit.jupiter.api.Test
+    public void shouldRaiseErrorIfLegalOfficerDetailsAreNull() throws Exception {
+        final java.util.Map<String, Object> caseData = caseData(REQUEST);
+
+        final io.restassured.response.Response response =
+            triggerCallback(caseData, EventConstants.CASEWORKER_REFER_TO_LEGAL_OFFICER, ABOUT_TO_SUBMIT_URL);
+        assertThat(response.getStatusCode()).isEqualTo(OK.value());
+
+        assertThatJson(response.asString())
+            .when(IGNORING_EXTRA_FIELDS)
+            .isEqualTo(json(expectedResponse(
+                "classpath:responses/response-caseworker-refer-cases-to-legal-officer-about-to-submit.json"
+            )));
+    }
+
+    @Test
+    public void shouldReceiveNoticeWhenSubmittedIsInvoked() throws Exception {
+        final Map<String, Object> caseData = caseData(REQUEST_SUBMITTED);
+
+        final Response response = triggerCallback(caseData, EventConstants.CASEWORKER_REFER_TO_LEGAL_OFFICER, SUBMITTED_URL);
+        assertThat(response.getStatusCode()).isEqualTo(OK.value());
+
+        // notes.date value is compared using ${json-unit.any-string}
+        // assertion will fail if the above value is missing
+        assertThatJson(response.asString())
+            .when(IGNORING_EXTRA_FIELDS)
+            .isEqualTo(json(expectedResponse(
+                "classpath:responses/response-caseworker-refer-cases-to-legal-officer-submitted.json"
+            )));
+    }
+}
