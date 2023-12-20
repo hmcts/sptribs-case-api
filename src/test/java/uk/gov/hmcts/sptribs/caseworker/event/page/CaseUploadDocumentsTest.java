804d2eab758a18e08b5d2fc009468705d64d8db9
oleon23 <oliver.leon@hmcts.net>
2023-12-20 09:48:53 +0000

    [DTSSTCI-117] Update checkstyle

--- src/test/java/uk/gov/hmcts/sptribs/caseworker/event/page/CaseUploadDocumentsTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/caseworker/event/page/CaseUploadDocumentsTest.java
@@ -3,6 +3,8 @@ package uk.gov.hmcts.sptribs.caseworker.event.page;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
@@ -10,8 +12,13 @@ import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.CicCase;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.common.event.page.CaseUploadDocuments;
+import uk.gov.hmcts.sptribs.document.DocumentUtil;
+
+import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.times;
 import static uk.gov.hmcts.sptribs.testutil.TestDataHelper.getCaseworkerCICDocumentList;
 
 @ExtendWith(MockitoExtension.class)
@@ -31,8 +38,15 @@ public class CaseUploadDocumentsTest {
             .build();
         caseDetails.setData(caseData);
 
-        final AboutToStartOrSubmitResponse<CaseData, State> response = caseUploadDocuments.midEvent(caseDetails, caseDetails);
+        try (MockedStatic<DocumentUtil> utilities = Mockito.mockStatic(DocumentUtil.class)) {
+            utilities.when(() -> DocumentUtil.validateUploadedDocuments(anyList()))
+                .thenReturn(Collections.emptyList());
+            
+            final AboutToStartOrSubmitResponse<CaseData, State> response = caseUploadDocuments.midEvent(caseDetails, caseDetails);
+        
+            utilities.verify(() ->  DocumentUtil.validateUploadedDocuments(anyList()), times(1));
+            assertThat(response.getData().getCicCase().getApplicantDocumentsUploaded()).isNotNull();
+        }
 
-        assertThat(response.getData().getCicCase().getApplicantDocumentsUploaded()).isNotNull();
     }
 }
