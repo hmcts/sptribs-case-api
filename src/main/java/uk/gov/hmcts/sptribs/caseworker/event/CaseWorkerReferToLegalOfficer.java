063395e50d1438e435b3ff00a37666b070cc68af
sjamihmcts <110464983+sjamihmcts@users.noreply.github.com>
2024-01-02 09:52:54 +0000

    St 1696 Refer event permissions - R3 branch (#1629)
    * st-1696: Added Judge and Senior Judge permissions to Refer to Legal Officer event
    
    * ST-1696: Added Judge and Senior Judge permissions to Refer to Judge event

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerReferToLegalOfficer.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerReferToLegalOfficer.java
@@ -50,7 +50,7 @@ public class CaseWorkerReferToLegalOfficer implements CCDConfig<CaseData, State,
             .submittedCallback(this::referred)
             .grant(CREATE_READ_UPDATE, SUPER_USER,
                 ST_CIC_HEARING_CENTRE_ADMIN, ST_CIC_HEARING_CENTRE_TEAM_LEADER,
-                ST_CIC_CASEWORKER, ST_CIC_SENIOR_CASEWORKER)
+                ST_CIC_CASEWORKER, ST_CIC_SENIOR_CASEWORKER, ST_CIC_JUDGE, ST_CIC_SENIOR_JUDGE)
             .grantHistoryOnly(
                 ST_CIC_CASEWORKER,
                 ST_CIC_SENIOR_CASEWORKER,
