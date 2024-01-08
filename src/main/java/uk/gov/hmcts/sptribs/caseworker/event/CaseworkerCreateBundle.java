3c651901d63428fe8151846987c4e001528fe3c0
osheaf01 <fergal.oshea@version1.com>
2024-01-08 14:58:18 +0000

    ST-1711 Bundling - fixing build, some code tweaks, mainly exposing DocumentListUtil.getAllCaseDocuments

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerCreateBundle.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerCreateBundle.java
@@ -31,7 +31,10 @@ import static uk.gov.hmcts.sptribs.ciccase.model.State.BUNDLE_STATES;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_CASEWORKER;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_HEARING_CENTRE_ADMIN;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_HEARING_CENTRE_TEAM_LEADER;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_JUDGE;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_SENIOR_CASEWORKER;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_SENIOR_JUDGE;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.SUPER_USER;
 import static uk.gov.hmcts.sptribs.ciccase.model.access.Permissions.CREATE_READ_UPDATE;
 
 @Component
