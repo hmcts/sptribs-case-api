504bcae7234c5a43f7467b1dfec914b7ecfd5ffb
gabriellemagruder <gabrielle.magruder@solirius.com>
2024-01-10 11:42:20 +0000

    Test with removed code

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/page/HearingAttendees.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/page/HearingAttendees.java
@@ -2,17 +2,12 @@ package uk.gov.hmcts.sptribs.caseworker.event.page;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
-import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.sptribs.caseworker.model.HearingSummary;
 import uk.gov.hmcts.sptribs.caseworker.model.Listing;
 import uk.gov.hmcts.sptribs.caseworker.util.PageShowConditionsUtil;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
-import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.common.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
-
-import static java.util.Objects.isNull;
 
 @Slf4j
 @Component
@@ -33,27 +28,5 @@ public class HearingAttendees implements CcdPageConfiguration {
             .mandatory(HearingSummary::getIsFullPanel)
             .mandatory(HearingSummary::getMemberList, "isFullPanel = \"Yes\"")
             .done();
-    }
-
-    public AboutToStartOrSubmitResponse<CaseData, State> midEvent(CaseDetails<CaseData, State> details,
-                                                                  CaseDetails<CaseData, State> detailsBefore) {
-
-        final CaseData caseData = details.getData();
-        final CaseData caseDataBefore = detailsBefore.getData();
-
-        // Retain judge for edit journey
-        if (!isNull(caseDataBefore.getListing().getSummary().getJudge())) {
-            caseData.getListing().getSummary().setJudge(caseDataBefore.getListing().getSummary().getJudge());
-            caseData.getListing().getSummary().getJudge().setValue(caseDataBefore.getListing().getSummary().getJudge().getValue());
-        }
-
-        // Retain members list for edit journey
-        if (!isNull(caseDataBefore.getListing().getSummary().getMemberList())) {
-            caseData.getListing().getSummary().setMemberList(caseDataBefore.getListing().getSummary().getMemberList());
-        }
-
-        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
-            .data(caseData)
-            .build();
     }
 }
--- src/test/java/uk/gov/hmcts/sptribs/caseworker/event/page/HearingAttendeesTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/caseworker/event/page/HearingAttendeesTest.java
@@ -1,105 +1,105 @@
-package uk.gov.hmcts.sptribs.caseworker.event.page;
-
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.junit.jupiter.MockitoExtension;
-import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
-import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
-import uk.gov.hmcts.ccd.sdk.type.DynamicList;
-import uk.gov.hmcts.ccd.sdk.type.DynamicListElement;
-import uk.gov.hmcts.sptribs.caseworker.model.HearingSummary;
-import uk.gov.hmcts.sptribs.caseworker.model.Listing;
-import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
-import uk.gov.hmcts.sptribs.ciccase.model.State;
-
-import java.util.List;
-import java.util.UUID;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static uk.gov.hmcts.sptribs.testutil.TestDataHelper.caseData;
-
-@ExtendWith(MockitoExtension.class)
-public class HearingAttendeesTest {
-    @InjectMocks
-    private HearingAttendees hearingAttendees;
-
-    @Test
-    void shouldPopulateJudgeWithPreviouslySelectedData() {
-        //Given
-        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
-        final CaseData caseData = caseData();
-        final DynamicListElement listItem = DynamicListElement
-            .builder()
-            .label("0")
-            .code(UUID.randomUUID())
-            .build();
-        final DynamicList judgeList = DynamicList
-            .builder()
-            .listItems(List.of(listItem))
-            .build();
-        caseData.setListing(
-            Listing.builder()
-                .summary(HearingSummary.builder()
-                    .judge(judgeList)
-                    .build()
-                )
-                .build()
-        );
-        caseDetails.setData(caseData);
-
-        final CaseDetails<CaseData, State> caseDetailsBefore = new CaseDetails<>();
-        final CaseData caseDataBefore = caseData();
-        final DynamicList judgeListBefore = DynamicList
-            .builder()
-            .value(listItem)
-            .listItems(List.of(listItem))
-            .build();
-        caseDataBefore.setListing(
-            Listing.builder()
-                .summary(HearingSummary.builder()
-                    .judge(judgeListBefore)
-                    .build()
-                )
-                .build()
-        );
-        caseDetailsBefore.setData(caseDataBefore);
-
-        // When
-        final AboutToStartOrSubmitResponse<CaseData, State> response = hearingAttendees.midEvent(caseDetails, caseDetailsBefore);
-
-        // Then
-        assertThat(response.getData().getListing().getSummary().getJudge().getValue()).isEqualTo(listItem);
-    }
-
-    @Test
-    void shouldNotSelectJudgeWithNoPreviouslySelectedData() {
-        //Given
-        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
-        final CaseData caseData = caseData();
-        final DynamicListElement listItem = DynamicListElement
-            .builder()
-            .label("0")
-            .code(UUID.randomUUID())
-            .build();
-        final DynamicList judgeList = DynamicList
-            .builder()
-            .listItems(List.of(listItem))
-            .build();
-        caseData.setListing(
-            Listing.builder()
-                .summary(HearingSummary.builder()
-                    .judge(judgeList)
-                    .build()
-                )
-                .build()
-        );
-        caseDetails.setData(caseData);
-
-        // When
-        final AboutToStartOrSubmitResponse<CaseData, State> response = hearingAttendees.midEvent(caseDetails, caseDetails);
-
-        // Then
-        assertThat(response.getData().getListing().getSummary().getJudge().getValue()).isNull();
-    }
-}
+//package uk.gov.hmcts.sptribs.caseworker.event.page;
+//
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.junit.jupiter.MockitoExtension;
+//import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+//import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+//import uk.gov.hmcts.ccd.sdk.type.DynamicList;
+//import uk.gov.hmcts.ccd.sdk.type.DynamicListElement;
+//import uk.gov.hmcts.sptribs.caseworker.model.HearingSummary;
+//import uk.gov.hmcts.sptribs.caseworker.model.Listing;
+//import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
+//import uk.gov.hmcts.sptribs.ciccase.model.State;
+//
+//import java.util.List;
+//import java.util.UUID;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//import static uk.gov.hmcts.sptribs.testutil.TestDataHelper.caseData;
+//
+//@ExtendWith(MockitoExtension.class)
+//public class HearingAttendeesTest {
+//    @InjectMocks
+//    private HearingAttendees hearingAttendees;
+//
+//    @Test
+//    void shouldPopulateJudgeWithPreviouslySelectedData() {
+//        //Given
+//        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
+//        final CaseData caseData = caseData();
+//        final DynamicListElement listItem = DynamicListElement
+//            .builder()
+//            .label("0")
+//            .code(UUID.randomUUID())
+//            .build();
+//        final DynamicList judgeList = DynamicList
+//            .builder()
+//            .listItems(List.of(listItem))
+//            .build();
+//        caseData.setListing(
+//            Listing.builder()
+//                .summary(HearingSummary.builder()
+//                    .judge(judgeList)
+//                    .build()
+//                )
+//                .build()
+//        );
+//        caseDetails.setData(caseData);
+//
+//        final CaseDetails<CaseData, State> caseDetailsBefore = new CaseDetails<>();
+//        final CaseData caseDataBefore = caseData();
+//        final DynamicList judgeListBefore = DynamicList
+//            .builder()
+//            .value(listItem)
+//            .listItems(List.of(listItem))
+//            .build();
+//        caseDataBefore.setListing(
+//            Listing.builder()
+//                .summary(HearingSummary.builder()
+//                    .judge(judgeListBefore)
+//                    .build()
+//                )
+//                .build()
+//        );
+//        caseDetailsBefore.setData(caseDataBefore);
+//
+//        // When
+//        final AboutToStartOrSubmitResponse<CaseData, State> response = hearingAttendees.midEvent(caseDetails, caseDetailsBefore);
+//
+//        // Then
+//        assertThat(response.getData().getListing().getSummary().getJudge().getValue()).isEqualTo(listItem);
+//    }
+//
+//    @Test
+//    void shouldNotSelectJudgeWithNoPreviouslySelectedData() {
+//        //Given
+//        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
+//        final CaseData caseData = caseData();
+//        final DynamicListElement listItem = DynamicListElement
+//            .builder()
+//            .label("0")
+//            .code(UUID.randomUUID())
+//            .build();
+//        final DynamicList judgeList = DynamicList
+//            .builder()
+//            .listItems(List.of(listItem))
+//            .build();
+//        caseData.setListing(
+//            Listing.builder()
+//                .summary(HearingSummary.builder()
+//                    .judge(judgeList)
+//                    .build()
+//                )
+//                .build()
+//        );
+//        caseDetails.setData(caseData);
+//
+//        // When
+//        final AboutToStartOrSubmitResponse<CaseData, State> response = hearingAttendees.midEvent(caseDetails, caseDetails);
+//
+//        // Then
+//        assertThat(response.getData().getListing().getSummary().getJudge().getValue()).isNull();
+//    }
+//}
