7a89c50d461a5c1df4859484a28499528965972c
gabriellemagruder <gabrielle.magruder@solirius.com>
2024-01-10 17:24:52 +0000

    Update changes to integration tests

--- src/integrationTest/java/uk/gov/hmcts/sptribs/caseworker/CaseworkerEditHearingSummaryIT.java
+++ src/integrationTest/java/uk/gov/hmcts/sptribs/caseworker/CaseworkerEditHearingSummaryIT.java
@@ -13,15 +13,20 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import uk.gov.hmcts.sptribs.caseworker.model.HearingSummary;
+import uk.gov.hmcts.sptribs.caseworker.model.Listing;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.common.config.WebMvcConfig;
 import uk.gov.hmcts.sptribs.testutil.IdamWireMock;
 
+import static java.util.Collections.emptySet;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.YES;
+import static uk.gov.hmcts.sptribs.ciccase.model.HearingFormat.FACE_TO_FACE;
 import static uk.gov.hmcts.sptribs.testutil.IdamWireMock.COURT_ADMIN_CIC;
 import static uk.gov.hmcts.sptribs.testutil.IdamWireMock.stubForIdamDetails;
 import static uk.gov.hmcts.sptribs.testutil.TestConstants.ABOUT_TO_SUBMIT_URL;
@@ -31,6 +36,10 @@ import static uk.gov.hmcts.sptribs.testutil.TestConstants.SERVICE_AUTHORIZATION;
 import static uk.gov.hmcts.sptribs.testutil.TestConstants.TEST_AUTHORIZATION_TOKEN;
 import static uk.gov.hmcts.sptribs.testutil.TestDataHelper.callbackRequest;
 import static uk.gov.hmcts.sptribs.testutil.TestDataHelper.caseData;
+import static uk.gov.hmcts.sptribs.testutil.TestDataHelper.getDynamicList;
+import static uk.gov.hmcts.sptribs.testutil.TestDataHelper.getMembers;
+import static uk.gov.hmcts.sptribs.testutil.TestDataHelper.getMockedHearingVenueData;
+import static uk.gov.hmcts.sptribs.testutil.TestDataHelper.getMockedRegionData;
 import static uk.gov.hmcts.sptribs.testutil.TestEventConstants.CASEWORKER_EDIT_HEARING_SUMMARY;
 import static uk.gov.hmcts.sptribs.testutil.TestResourceUtil.expectedResponse;
 
@@ -63,8 +72,23 @@ public class CaseworkerEditHearingSummaryIT {
     private WebMvcConfig webMvcConfig;
 
     @Test
-    void shouldSuccessfullyCreateHearingSummary() throws Exception {
+    void shouldSuccessfullyEditHearingSummary() throws Exception {
         final CaseData caseData = caseData();
+        final HearingSummary hearingSummary = HearingSummary.builder()
+            .judge(getDynamicList())
+            .memberList(getMembers())
+            .build();
+        final Listing listing = Listing.builder()
+            .regionList(getMockedRegionData())
+            .hearingVenues(getMockedHearingVenueData())
+            .venueNotListedOption(emptySet())
+            .roomAtVenue("G.01")
+            .addlInstr("Ground floor")
+            .hearingFormat(FACE_TO_FACE)
+            .shortNotice(YES)
+            .summary(hearingSummary)
+            .build();
+        caseData.setListing(listing);
 
         stubForIdamDetails(TEST_AUTHORIZATION_TOKEN, CASEWORKER_USER_ID, COURT_ADMIN_CIC);
 
