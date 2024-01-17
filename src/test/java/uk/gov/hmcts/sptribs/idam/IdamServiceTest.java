1c91ded3951196c8026bb0ffe9deb8d09410b3de
Jefex7 <duartepireslopes@gmail.com>
2024-01-17 09:44:09 +0000

    Added test

--- src/test/java/uk/gov/hmcts/sptribs/idam/IdamServiceTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/idam/IdamServiceTest.java
@@ -8,10 +8,13 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 import uk.gov.hmcts.reform.idam.client.IdamClient;
+import uk.gov.hmcts.reform.idam.client.models.User;
 import uk.gov.hmcts.reform.idam.client.models.UserDetails;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
@@ -19,6 +22,7 @@ import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.sptribs.testutil.TestConstants.INVALID_AUTH_TOKEN;
 import static uk.gov.hmcts.sptribs.testutil.TestConstants.SYSTEM_UPDATE_AUTH_TOKEN;
 import static uk.gov.hmcts.sptribs.testutil.TestConstants.SYSTEM_USER_USER_ID;
+import static uk.gov.hmcts.sptribs.testutil.TestConstants.TEST_SERVICE_AUTH_TOKEN;
 import static uk.gov.hmcts.sptribs.testutil.TestConstants.TEST_SYSTEM_UPDATE_USER_EMAIL;
 import static uk.gov.hmcts.sptribs.testutil.TestConstants.TEST_SYSTEM_USER_PASSWORD;
 import static uk.gov.hmcts.sptribs.testutil.TestDataHelper.feignException;
@@ -89,6 +93,21 @@ public class IdamServiceTest {
         assertThatThrownBy(() -> idamService.retrieveSystemUpdateUserDetails())
             .isExactlyInstanceOf(FeignException.Unauthorized.class)
             .hasMessageContaining("Failed to retrieve Idam user");
+    }
+    @Test
+    public void shouldRetrieveUserDetails() {
+        // Given
+        String bearerToken = TEST_SERVICE_AUTH_TOKEN;
+        UserDetails userDetails = userDetails();
+
+        // When
+        when(idamClient.getUserDetails(bearerToken)).thenReturn(userDetails());
+
+        User result = idamService.retrieveUser(bearerToken);
+
+        //Then
+        assertEquals(userDetails, result.getUserDetails());
+        assertEquals(bearerToken, result.getAuthToken());
     }
 
     private void setSystemUserCredentials() {
