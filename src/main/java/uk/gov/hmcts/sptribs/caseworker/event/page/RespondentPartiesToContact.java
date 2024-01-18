40c40c3e690e000e6d260fc68b4a92dc619fd700
sjamihmcts <santoshini.jami@hmcts.net>
2024-01-18 15:28:48 +0000

    Remove unused import

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/page/RespondentPartiesToContact.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/page/RespondentPartiesToContact.java
@@ -10,7 +10,6 @@ import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseSubcategory;
 import uk.gov.hmcts.sptribs.ciccase.model.CicCase;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
-import uk.gov.hmcts.sptribs.common.CommonConstants;
 import uk.gov.hmcts.sptribs.common.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
 
