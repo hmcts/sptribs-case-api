4af50586223c61be27a7b70e5a392bccf77b20be
JakeCG <jakecgowler@gmail.com>
2024-01-25 14:23:00 +0000

    DTSSTCI-361 (#1681)
    * Removing DSS tests
    
    * Jenkinsfile_nightly updates

--- src/e2eTests/java/uk/gov/hmcts/sptribs/e2e/Login.java
+++ src/e2eTests/java/uk/gov/hmcts/sptribs/e2e/Login.java
@@ -7,7 +7,6 @@ import com.microsoft.playwright.options.WaitForSelectorState;
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 import static java.lang.System.getenv;
 import static uk.gov.hmcts.sptribs.e2e.Base.CASE_API_BASE_URL;
-import static uk.gov.hmcts.sptribs.e2e.Base.DSS_BASE_URL;
 import static uk.gov.hmcts.sptribs.testutils.AssertionHelpers.textOptionsWithTimeout;
 import static uk.gov.hmcts.sptribs.testutils.PageHelpers.clickButton;
 import static uk.gov.hmcts.sptribs.testutils.PageHelpers.clickOptionsWithTimeout;
@@ -20,7 +19,6 @@ import static uk.gov.hmcts.sptribs.testutils.PageHelpers.urlOptionsWithTimeout;
 public class Login {
     private final Page page;
 
-    private static final String DSS_USER_PASSWORD = "Password123";
     private static final String CASE_API_USER_PASSWORD = "Pa55w0rd11";
 
     public Login(Page page) {
@@ -69,27 +67,6 @@ public class Login {
         }
     }
 
-    private void loginAsDssUser(String user) {
-        page.waitForSelector("h1:has-text(\"Sign in or create an account\")", selectorOptionsWithTimeout(120000));
-        if (page.isVisible("#cookie-accept-submit")) {
-            clickButton(page, "Accept additional cookies");
-            page.locator("button[name=\"hide-accepted\"]").click();
-        }
-        getTextBoxByLabel(page, "Email address").fill(user);
-        getTextBoxByLabel(page, "Password").fill(DSS_USER_PASSWORD);
-        page.locator("input:has-text(\"Sign in\")").click(clickOptionsWithTimeout(120000));
-        page.waitForLoadState(LoadState.DOMCONTENTLOADED, loadStateOptionsWithTimeout(120000));
-        page.waitForURL(DSS_BASE_URL + "/subject-details", urlOptionsWithTimeout(60000));
-        page.waitForSelector("h1", selectorOptionsWithTimeout(60000));
-        assertThat(page.locator("h1"))
-            .hasText("Who is the subject of this case?", textOptionsWithTimeout(60000));
-        if (page.isVisible("button[data-cm-action=\"accept\"][data-module=\"govuk-button\"]")) {
-            clickButton(page, "Accept analytics cookies");
-            clickButton(page, "Hide this message");
-        }
-
-    }
-
     public void loginAsCaseWorker() {
         loginAs(getenv("LEGAL_OFFICER"));
     }
@@ -116,10 +93,6 @@ public class Login {
 
     public void loginAsStRespondentUser() {
         loginAs(getenv("RESPONDENT"));
-    }
-
-    public void loginAsStCitizen1User() {
-        loginAsDssUser(getenv("DSS_CITIZEN_USER"));
     }
 
     public void refreshPageWhenServerErrorIsDisplayed() {
