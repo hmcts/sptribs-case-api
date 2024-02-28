3038e23e59b1b841d3d9987a40afcafc981b2a96
Tom Elliott <tomelliott@Toms-MacBook-Pro.local>
2024-02-28 14:11:09 +0000

    DTSSTCI-357: Remove public

--- src/test/java/uk/gov/hmcts/sptribs/common/notification/DssUpdateCaseSubmissionNotificationTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/common/notification/DssUpdateCaseSubmissionNotificationTest.java
@@ -23,7 +23,7 @@ import static uk.gov.hmcts.sptribs.testutil.TestConstants.TEST_APPLICANT_EMAIL;
 import static uk.gov.hmcts.sptribs.testutil.TestConstants.TEST_CASE_ID;
 
 @ExtendWith(MockitoExtension.class)
-public class DssUpdateCaseSubmissionNotificationTest {
+class DssUpdateCaseSubmissionNotificationTest {
 
     @Mock
     private NotificationServiceCIC notificationService;
