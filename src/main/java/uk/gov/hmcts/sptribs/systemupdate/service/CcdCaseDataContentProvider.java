b888c0356e239836cf4e8e3b8ded1d76a10ada5d
Tom Elliott <tomelliott@Toms-MacBook-Pro.local>
2024-03-18 16:37:03 +0000

    DTSSTCI-617: Refactor CcdCaseDataContentProvider code to make it more resilient and less prone to concurrency bugs

--- src/main/java/uk/gov/hmcts/sptribs/systemupdate/service/CcdCaseDataContentProvider.java
+++ src/main/java/uk/gov/hmcts/sptribs/systemupdate/service/CcdCaseDataContentProvider.java
@@ -1,27 +1,61 @@
 package uk.gov.hmcts.sptribs.systemupdate.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDataContent;
 import uk.gov.hmcts.reform.ccd.client.model.Event;
 import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
+import uk.gov.hmcts.sptribs.ciccase.task.CaseTask;
 
 @Component
 public class CcdCaseDataContentProvider {
 
+    private final CaseDetailsUpdater caseDetailsUpdater;
+
+    @Autowired
+    public CcdCaseDataContentProvider(CaseDetailsUpdater caseDetailsUpdater) {
+        this.caseDetailsUpdater = caseDetailsUpdater;
+    }
+
     public CaseDataContent createCaseDataContent(final StartEventResponse startEventResponse,
+                                                 final String summary,
+                                                 final String description) {
+
+        return createCaseDataContent(
+            startEventResponse,
+            summary,
+            description,
+            startEventResponse.getCaseDetails().getData()
+        );
+    }
+
+    public CaseDataContent createCaseDataContent(final StartEventResponse startEventResponse,
+                                                 final String summary,
+                                                 final String description,
+                                                 final CaseTask caseTask) {
+
+        return createCaseDataContent(
+            startEventResponse,
+            summary,
+            description,
+            caseDetailsUpdater.updateCaseData(caseTask, startEventResponse).getData()
+        );
+    }
+
+    private CaseDataContent createCaseDataContent(final StartEventResponse startEventResponse,
                                                  final String summary,
                                                  final String description,
                                                  final Object data) {
 
         return CaseDataContent.builder()
-                .eventToken(startEventResponse.getToken())
-                .event(
-                        Event.builder()
-                                .id(startEventResponse.getEventId())
-                                .summary(summary)
-                                .description(description)
-                                .build())
-                .data(data)
-                .build();
+            .eventToken(startEventResponse.getToken())
+            .event(
+                Event.builder()
+                    .id(startEventResponse.getEventId())
+                    .summary(summary)
+                    .description(description)
+                    .build())
+            .data(data)
+            .build();
     }
 }
