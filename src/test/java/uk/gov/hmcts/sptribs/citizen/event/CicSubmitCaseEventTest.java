1a00507c8a859797083a36cee0411d5a360360fe
Rajaram <70951210+rajaram-nagarajan@users.noreply.github.com>
2024-04-15 11:07:18 +0100

    DTSSTCI-204: Added submit case notification logic (#1724)
    * DTSSTCI-204: Added submit case notification logic
    
    * DTSSTCI-204: Remove toggle after testing and clean up
    
    ---------
    
    Co-authored-by: Tom Elliott <tomelliott@Toms-MacBook-Pro.local>

--- src/test/java/uk/gov/hmcts/sptribs/citizen/event/CicSubmitCaseEventTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/citizen/event/CicSubmitCaseEventTest.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.sptribs.citizen.event;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import jakarta.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,6 +21,7 @@ import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.ccd.sdk.type.Document;
 import uk.gov.hmcts.ccd.sdk.type.ListValue;
 import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
+import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.CicCase;
 import uk.gov.hmcts.sptribs.ciccase.model.ContactPreferenceType;
@@ -33,21 +36,27 @@ import uk.gov.hmcts.sptribs.idam.IdamService;
 import uk.gov.hmcts.sptribs.testutil.TestDataHelper;
 import uk.gov.hmcts.sptribs.util.AppsUtil;
 
+import java.io.IOException;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static uk.gov.hmcts.sptribs.testutil.ConfigTestUtil.createCaseDataConfigBuilder;
 import static uk.gov.hmcts.sptribs.testutil.ConfigTestUtil.getEventsFrom;
 import static uk.gov.hmcts.sptribs.testutil.TestConstants.CASE_DATA_CIC_ID;
+import static uk.gov.hmcts.sptribs.testutil.TestConstants.CASE_DATA_FILE_CIC;
 import static uk.gov.hmcts.sptribs.testutil.TestConstants.TEST_AUTHORIZATION_TOKEN;
 import static uk.gov.hmcts.sptribs.testutil.TestConstants.TEST_CASE_ID;
 import static uk.gov.hmcts.sptribs.testutil.TestConstants.TEST_FIRST_NAME;
 import static uk.gov.hmcts.sptribs.testutil.TestConstants.TEST_SOLICITOR_EMAIL;
 import static uk.gov.hmcts.sptribs.testutil.TestConstants.TEST_SOLICITOR_NAME;
+import static uk.gov.hmcts.sptribs.testutil.TestConstants.TEST_UPDATE_CASE_EMAIL_ADDRESS;
 import static uk.gov.hmcts.sptribs.testutil.TestDataHelper.LOCAL_DATE_TIME;
 import static uk.gov.hmcts.sptribs.testutil.TestDataHelper.caseData;
+import static uk.gov.hmcts.sptribs.testutil.TestFileUtil.loadJson;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -58,6 +67,9 @@ class CicSubmitCaseEventTest {
 
     @InjectMocks
     private CicSubmitCaseEvent cicSubmitCaseEvent;
+
+    @Mock
+    private DssApplicationReceivedNotification dssApplicationReceivedNotification;
 
     @Mock
     private AppsConfig appsConfig;
@@ -72,7 +84,6 @@ class CicSubmitCaseEventTest {
     private IdamService idamService;
 
     private AutoCloseable autoCloseableMocks;
-
 
     @BeforeEach
     public void setUp() {
@@ -146,4 +157,99 @@ class CicSubmitCaseEventTest {
         assertThat(response.getData().getCicCase().getApplicantDocumentsUploaded().get(0).getValue().getDocumentCategory())
             .isIn(DocumentType.DSS_OTHER, DocumentType.DSS_SUPPORTING, DocumentType.DSS_TRIBUNAL_FORM);
     }
+
+    @Test
+    void shouldSubmitEventThroughSubmittedCallbackWithNotification() throws IOException {
+        final ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule());
+        String caseDataJson = loadJson(CASE_DATA_FILE_CIC);
+        final CaseData caseBeforeData = mapper.readValue(caseDataJson, CaseData.class);
+
+        final CaseDetails<CaseData, State> beforeCaseDetails = new CaseDetails<>();
+        beforeCaseDetails.setData(caseBeforeData);
+        beforeCaseDetails.setState(State.Submitted);
+        beforeCaseDetails.setId(TEST_CASE_ID);
+        beforeCaseDetails.setCreatedDate(LOCAL_DATE_TIME);
+
+        CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
+        CaseData caseData = mapper.readValue(caseDataJson, CaseData.class);
+        caseDetails.setData(caseData);
+        caseDetails.setState(State.Submitted);
+        caseDetails.setId(TEST_CASE_ID);
+        caseDetails.setCreatedDate(LOCAL_DATE_TIME);
+
+        caseDetails.getData().getDssCaseData().setSubjectFullName(TEST_FIRST_NAME);
+        caseDetails.getData().getDssCaseData().setSubjectEmailAddress(TEST_UPDATE_CASE_EMAIL_ADDRESS);
+        caseDetails.getData().getDssCaseData().setRepresentativeFullName(TEST_FIRST_NAME);
+        caseDetails.getData().getDssCaseData().setRepresentativeEmailAddress(TEST_UPDATE_CASE_EMAIL_ADDRESS);
+
+        when(request.getHeader(AUTHORIZATION)).thenReturn(TEST_AUTHORIZATION_TOKEN);
+        when(idamService.retrieveUser(TEST_AUTHORIZATION_TOKEN)).thenReturn(TestDataHelper.getUser());
+        when(appsConfig.getApps()).thenReturn(List.of(cicAppDetail));
+        cicSubmitCaseEvent.configure(configBuilder);
+
+        AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmitResponse = cicSubmitCaseEvent.aboutToSubmit(
+            caseDetails,
+            beforeCaseDetails
+        );
+
+        assertThat(aboutToSubmitResponse.getState()).isEqualTo(State.DSS_Submitted);
+        assertThat(aboutToSubmitResponse.getData().getDssQuestion1()).isEqualTo("Full Name");
+        assertThat(aboutToSubmitResponse.getData().getDssQuestion2()).isEqualTo("Date of Birth");
+        assertThat(aboutToSubmitResponse.getData().getDssAnswer1()).isEqualTo("case_data.dssCaseDataSubjectFullName");
+        assertThat(aboutToSubmitResponse.getData().getDssAnswer2()).isEqualTo("case_data.dssCaseDataSubjectDateOfBirth");
+        assertThat(aboutToSubmitResponse.getData().getDssHeaderDetails()).isEqualTo("Subject of this case");
+
+        SubmittedCallbackResponse submittedResponse = cicSubmitCaseEvent.submitted(caseDetails, beforeCaseDetails);
+
+        assertThat(submittedResponse.getConfirmationHeader())
+            .isEqualTo("# Application Received \n## A notification has been sent to: Subject, Representative");
+        verify(dssApplicationReceivedNotification).sendToSubject(any(DssCaseData.class), any());
+        verify(dssApplicationReceivedNotification).sendToRepresentative(any(DssCaseData.class), any());
+    }
+
+    @Test
+    void shouldSubmitEventThroughSubmittedCallbackWithoutNotificationParties() throws IOException {
+        final ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule());
+        String caseDataJson = loadJson(CASE_DATA_FILE_CIC);
+        final CaseData caseBeforeData = mapper.readValue(caseDataJson, CaseData.class);
+
+        final CaseDetails<CaseData, State> beforeCaseDetails = new CaseDetails<>();
+        beforeCaseDetails.setData(caseBeforeData);
+        beforeCaseDetails.setState(State.Submitted);
+        beforeCaseDetails.setId(TEST_CASE_ID);
+        beforeCaseDetails.setCreatedDate(LOCAL_DATE_TIME);
+
+        CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
+        CaseData caseData = mapper.readValue(caseDataJson, CaseData.class);
+        caseDetails.setData(caseData);
+        caseDetails.setState(State.Submitted);
+        caseDetails.setId(TEST_CASE_ID);
+        caseDetails.setCreatedDate(LOCAL_DATE_TIME);
+
+        caseDetails.getData().getDssCaseData().setSubjectFullName(TEST_FIRST_NAME);
+        caseDetails.getData().getDssCaseData().setRepresentativeFullName(TEST_FIRST_NAME);
+
+        when(request.getHeader(AUTHORIZATION)).thenReturn(TEST_AUTHORIZATION_TOKEN);
+        when(idamService.retrieveUser(TEST_AUTHORIZATION_TOKEN)).thenReturn(TestDataHelper.getUser());
+        when(appsConfig.getApps()).thenReturn(List.of(cicAppDetail));
+        cicSubmitCaseEvent.configure(configBuilder);
+
+        AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmitResponse = cicSubmitCaseEvent.aboutToSubmit(
+            caseDetails,
+            beforeCaseDetails
+        );
+
+        assertThat(aboutToSubmitResponse.getState()).isEqualTo(State.DSS_Submitted);
+        assertThat(aboutToSubmitResponse.getData().getDssQuestion1()).isEqualTo("Full Name");
+        assertThat(aboutToSubmitResponse.getData().getDssQuestion2()).isEqualTo("Date of Birth");
+        assertThat(aboutToSubmitResponse.getData().getDssAnswer1()).isEqualTo("case_data.dssCaseDataSubjectFullName");
+        assertThat(aboutToSubmitResponse.getData().getDssAnswer2()).isEqualTo("case_data.dssCaseDataSubjectDateOfBirth");
+        assertThat(aboutToSubmitResponse.getData().getDssHeaderDetails()).isEqualTo("Subject of this case");
+
+        SubmittedCallbackResponse submittedResponse = cicSubmitCaseEvent.submitted(caseDetails, beforeCaseDetails);
+
+        assertThat(submittedResponse.getConfirmationHeader())
+            .isEqualTo("# Application Received %n##");
+    }
+
 }
