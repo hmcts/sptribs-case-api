9bcfce1f5959bc9ce42399cfe24999fbb5f3be65
Tom Elliott <tomelliott@Toms-MacBook-Pro.local>
2024-04-17 10:34:11 +0100

    DTSSTCI-692: Resolve functional test checkstyle errors

--- src/functionalTest/java/uk/gov/hmcts/sptribs/caseworker/CaseworkerCreateTestCaseFT.java
+++ src/functionalTest/java/uk/gov/hmcts/sptribs/caseworker/CaseworkerCreateTestCaseFT.java
@@ -7,21 +7,20 @@ import uk.gov.hmcts.sptribs.testutil.FunctionalTestSuite;
 
 import java.util.Map;
 
-
-import static uk.gov.hmcts.sptribs.testutil.CaseDataUtil.caseData;
-import static net.javacrumbs.jsonunit.assertj.JsonAssertions.json;
-import static org.assertj.core.api.Assertions.assertThat;
-import static uk.gov.hmcts.sptribs.testutil.TestResourceUtil.expectedResponse;
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.json;
 import static net.javacrumbs.jsonunit.core.Option.IGNORING_ARRAY_ORDER;
 import static net.javacrumbs.jsonunit.core.Option.IGNORING_EXTRA_FIELDS;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.HttpStatus.OK;
+import static uk.gov.hmcts.sptribs.testutil.CaseDataUtil.caseData;
 import static uk.gov.hmcts.sptribs.testutil.TestConstants.ABOUT_TO_SUBMIT_URL;
+import static uk.gov.hmcts.sptribs.testutil.TestResourceUtil.expectedResponse;
 
 @SpringBootTest
 public class CaseworkerCreateTestCaseFT extends FunctionalTestSuite {
-    private static final String REQUEST = "classpath:request/casedata/ccd-callback-casedata-caseworker-submit-test-case.json";
 
+    private static final String REQUEST = "classpath:request/casedata/ccd-callback-casedata-caseworker-submit-test-case.json";
     private static final String RESPONSE = "classpath:responses/response-caseworker-submit-test-case.json";
 
     private static final String CASEWORKER_CREATE_TEST_CASE_EVENT_ID = "create-test-case";
