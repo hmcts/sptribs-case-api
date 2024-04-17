1a8676eef21f1ceb4acd5dbadd4c19ca46a28142
Jake Gowler <jakecgowler@gmail.com>
2024-04-17 11:38:48 +0100

    DTSSTCI692 - Comments changes

--- src/functionalTest/java/uk/gov/hmcts/sptribs/caseworker/CaseworkerCreateTestCaseFT.java
+++ src/functionalTest/java/uk/gov/hmcts/sptribs/caseworker/CaseworkerCreateTestCaseFT.java
@@ -5,7 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import uk.gov.hmcts.sptribs.testutil.FunctionalTestSuite;
 
+
+import java.util.HashMap;
 import java.util.Map;
+import org.json.JSONObject;
 
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.json;
@@ -13,21 +16,23 @@ import static net.javacrumbs.jsonunit.core.Option.IGNORING_ARRAY_ORDER;
 import static net.javacrumbs.jsonunit.core.Option.IGNORING_EXTRA_FIELDS;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.HttpStatus.OK;
-import static uk.gov.hmcts.sptribs.testutil.CaseDataUtil.caseData;
 import static uk.gov.hmcts.sptribs.testutil.TestConstants.ABOUT_TO_SUBMIT_URL;
 import static uk.gov.hmcts.sptribs.testutil.TestResourceUtil.expectedResponse;
 
 @SpringBootTest
 public class CaseworkerCreateTestCaseFT extends FunctionalTestSuite {
 
-    private static final String REQUEST = "classpath:request/casedata/ccd-callback-casedata-caseworker-submit-test-case.json";
     private static final String RESPONSE = "classpath:responses/response-caseworker-submit-test-case.json";
 
     private static final String CASEWORKER_CREATE_TEST_CASE_EVENT_ID = "create-test-case";
 
     @Test
     public void shouldSuccessfullySubmitWhenAboutToSubmitCallbackIsTriggered() throws Exception {
-        final Map<String, Object> caseData = caseData(REQUEST);
+        JSONObject emptyJsonObject = new JSONObject();
+        Map<String, Object> caseData = new HashMap<>();
+        caseData.put("emptyString", "");
+        caseData.put("emptyJsonObject", emptyJsonObject);
+
         final Response response = triggerCallback(caseData, CASEWORKER_CREATE_TEST_CASE_EVENT_ID, ABOUT_TO_SUBMIT_URL);
 
         assertThat(response.getStatusCode()).isEqualTo(OK.value());
