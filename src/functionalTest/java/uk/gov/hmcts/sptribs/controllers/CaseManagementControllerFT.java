5cc0d166b31fc16ee4b7ba5ba2c3c458b9bdcc6d
Tom Elliott <tomelliott@Toms-MacBook-Pro.local>
2024-04-18 17:31:47 +0100

    DTSSTCI-693: Begin work on functional tests for controllers module

---
+++ src/functionalTest/java/uk/gov/hmcts/sptribs/controllers/CaseManagementControllerFT.java
@@ -0,0 +1,14 @@
+package uk.gov.hmcts.sptribs.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import uk.gov.hmcts.sptribs.testutil.FunctionalTestSuite;
+
+@SpringBootTest
+public class CaseManagementControllerFT extends FunctionalTestSuite {
+
+    @Test
+    public void should() {
+
+    }
+}
---
+++ src/functionalTest/java/uk/gov/hmcts/sptribs/controllers/DocumentManagementControllerFT.java
@@ -0,0 +1,14 @@
+package uk.gov.hmcts.sptribs.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import uk.gov.hmcts.sptribs.testutil.FunctionalTestSuite;
+
+@SpringBootTest
+public class DocumentManagementControllerFT extends FunctionalTestSuite {
+
+    @Test
+    public void should() {
+
+    }
+}
