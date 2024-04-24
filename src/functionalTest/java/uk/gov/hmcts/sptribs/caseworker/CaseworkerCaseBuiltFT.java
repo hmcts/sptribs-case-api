21226698a0b417b63315baf358fe45fd29b799e2
Jake Gowler <jakecgowler@gmail.com>
2024-04-24 15:28:26 +0100

    DTSSTCI698 - CaseworkerCaseBuiltFT

--- src/functionalTest/java/uk/gov/hmcts/sptribs/caseworker/CaseworkerCaseBuiltFT.java
+++ src/functionalTest/java/uk/gov/hmcts/sptribs/caseworker/CaseworkerCaseBuiltFT.java
@@ -9,30 +9,40 @@ import uk.gov.hmcts.sptribs.testutil.FunctionalTestSuite;
 import java.util.Map;
 
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
-import static net.javacrumbs.jsonunit.assertj.JsonAssertions.json;
-import static net.javacrumbs.jsonunit.core.Option.IGNORING_EXTRA_FIELDS;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.HttpStatus.OK;
 import static uk.gov.hmcts.sptribs.testutil.CaseDataUtil.caseData;
 import static uk.gov.hmcts.sptribs.testutil.TestConstants.ABOUT_TO_SUBMIT_URL;
-import static uk.gov.hmcts.sptribs.testutil.TestResourceUtil.expectedResponse;
+import static uk.gov.hmcts.sptribs.testutil.TestConstants.SUBMITTED_URL;
 
 @SpringBootTest
 public class CaseworkerCaseBuiltFT extends FunctionalTestSuite {
 
     private static final String REQUEST = "classpath:request/casedata/ccd-callback-casedata.json";
 
+    private static final String CONFIRMATION_HEADER = "$.confirmation_header";
+    private static final String STATE = "$.state";
+
     @Test
     public void shouldUpdateCaseDataWithNotesWhenAboutToSubmitCallbackIsInvoked() throws Exception {
         final Map<String, Object> caseData = caseData(REQUEST);
 
         final Response response = triggerCallback(caseData, EventConstants.CASEWORKER_CASE_BUILT, ABOUT_TO_SUBMIT_URL);
+
         assertThat(response.getStatusCode()).isEqualTo(OK.value());
+        assertThatJson(response.asString())
+            .inPath(STATE)
+            .isEqualTo("CaseManagement");
+    }
+
+    @Test
+    public void shouldRecieveErrorIfInvalidRequestIsInvoked() throws Exception {
+        final Map<String, Object> caseData = caseData(REQUEST);
+
+        final Response response = triggerCallback(caseData, EventConstants.CASEWORKER_CASE_BUILT, SUBMITTED_URL);
 
         assertThatJson(response.asString())
-            .when(IGNORING_EXTRA_FIELDS)
-            .isEqualTo(json(expectedResponse(
-                "classpath:responses/response-caseworker-build-case-about-to-submit.json"
-            )));
+            .inPath(CONFIRMATION_HEADER)
+            .isEqualTo("# Case built successful");
     }
 }
