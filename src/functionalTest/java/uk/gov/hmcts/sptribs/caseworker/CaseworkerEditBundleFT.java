added8f40a2d658120e6d797d33988aca7761306
Jake Gowler <jakecgowler@gmail.com>
2024-05-08 14:27:02 +0100

    DTSSTCI700 - CaseworkerEditBundleFT

---
+++ src/functionalTest/java/uk/gov/hmcts/sptribs/caseworker/CaseworkerEditBundleFT.java
@@ -0,0 +1,39 @@
+package uk.gov.hmcts.sptribs.caseworker;
+
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import uk.gov.hmcts.sptribs.testutil.FunctionalTestSuite;
+
+import java.util.Map;
+
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.json;
+import static net.javacrumbs.jsonunit.core.Option.IGNORING_ARRAY_ORDER;
+import static net.javacrumbs.jsonunit.core.Option.IGNORING_EXTRA_FIELDS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpStatus.OK;
+import static uk.gov.hmcts.sptribs.caseworker.util.EventConstants.EDIT_BUNDLE;
+import static uk.gov.hmcts.sptribs.testutil.CaseDataUtil.caseData;
+import static uk.gov.hmcts.sptribs.testutil.TestConstants.ABOUT_TO_SUBMIT_URL;
+import static uk.gov.hmcts.sptribs.testutil.TestResourceUtil.expectedResponse;
+
+@SpringBootTest
+public class CaseworkerEditBundleFT extends FunctionalTestSuite {
+    private static final String REQUEST_ABOUT_TO_SUBMIT = "classpath:request/casedata/ccd-callback-casedata-caseworker-edit-bundle.json";
+
+    private static final String RESPONSE_ABOUT_TO_SUBMIT = "classpath:responses/response-caseworker-edit-bundle-about-to-submit.json";
+
+    @Test
+    public void shouldReturnCorrectBundleWhenAboutToSubmitCallbackIsInvoked() throws Exception {
+        final Map<String, Object> caseData = caseData(REQUEST_ABOUT_TO_SUBMIT);
+
+        final Response response = triggerCallback(caseData, EDIT_BUNDLE, ABOUT_TO_SUBMIT_URL);
+
+        assertThat(response.getStatusCode()).isEqualTo(OK.value());
+        assertThatJson(response.asString())
+            .when(IGNORING_ARRAY_ORDER)
+            .when(IGNORING_EXTRA_FIELDS)
+            .isEqualTo(json(expectedResponse(RESPONSE_ABOUT_TO_SUBMIT)));
+    }
+}
