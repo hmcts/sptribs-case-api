c8c9ce9e273eb869857f6172bc5338800ed3de14
JakeCG <jakecgowler@gmail.com>
2024-05-13 10:08:48 +0100

    DTSSTCI-700 (#1765)
    * DTSSTCI700 - CaseworkerDocumentManagementAmendFT.java, CaseworkerDocumentManagementRemoveFT, CaseworkerEditBundleFT, CaseworkerEditCaseFT

---
+++ src/functionalTest/java/uk/gov/hmcts/sptribs/caseworker/CaseworkerDocumentManagementAmendFT.java
@@ -0,0 +1,75 @@
+package uk.gov.hmcts.sptribs.caseworker;
+
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import uk.gov.hmcts.sptribs.testutil.FunctionalTestSuite;
+
+import java.util.Map;
+
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.json;
+import static net.javacrumbs.jsonunit.core.Option.IGNORING_ARRAY_ORDER;
+import static net.javacrumbs.jsonunit.core.Option.IGNORING_EXTRA_FIELDS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpStatus.OK;
+import static uk.gov.hmcts.sptribs.caseworker.util.EventConstants.CASEWORKER_DOCUMENT_MANAGEMENT_AMEND;
+import static uk.gov.hmcts.sptribs.testutil.CaseDataUtil.caseData;
+import static uk.gov.hmcts.sptribs.testutil.TestConstants.ABOUT_TO_START_URL;
+import static uk.gov.hmcts.sptribs.testutil.TestConstants.ABOUT_TO_SUBMIT_URL;
+import static uk.gov.hmcts.sptribs.testutil.TestConstants.SUBMITTED_URL;
+import static uk.gov.hmcts.sptribs.testutil.TestResourceUtil.expectedResponse;
+
+@SpringBootTest
+public class CaseworkerDocumentManagementAmendFT extends FunctionalTestSuite {
+
+    private static final String REQUEST_ABOUT_TO_START = "classpath:request/casedata/ccd-callback-casedata-general.json";
+    private static final String CALLBACK_REQUEST =
+        "classpath:request/casedata/ccd-callback-casedata-caseworker-document-management-amend-request.json";
+
+    private static final String RESPONSE_ABOUT_TO_START =
+        "classpath:responses/response-caseworker-document-management-amend-about-to-start.json";
+    private static final String RESPONSE_ABOUT_TO_SUBMIT =
+        "classpath:responses/response-caseworker-document-management-amend-about-to-submit.json";
+
+    private static final String CONFIRMATION_HEADER = "$.confirmation_header";
+
+    @Test
+    public void shouldInitialiseDocumentListWhenAboutToStartCallbackIsInvoked() throws Exception {
+        final Map<String, Object> caseData = caseData(REQUEST_ABOUT_TO_START);
+
+        final Response response = triggerCallback(caseData,CASEWORKER_DOCUMENT_MANAGEMENT_AMEND, ABOUT_TO_START_URL);
+
+        assertThat(response.getStatusCode()).isEqualTo(OK.value());
+        assertThatJson(response.asString())
+            .when(IGNORING_EXTRA_FIELDS)
+            .when(IGNORING_ARRAY_ORDER)
+            .isEqualTo(json(expectedResponse(RESPONSE_ABOUT_TO_START)));
+    }
+
+    @Test
+    public void shouldReturnCorrectDocumentInformationWhenAboutToSubmitCallbackIsInvoked() throws Exception {
+        final Map<String, Object> caseData = caseData(CALLBACK_REQUEST);
+
+        final Response response = triggerCallback(caseData, CASEWORKER_DOCUMENT_MANAGEMENT_AMEND, ABOUT_TO_SUBMIT_URL);
+
+        assertThat(response.getStatusCode()).isEqualTo(OK.value());
+        assertThatJson(response.asString())
+            .when(IGNORING_EXTRA_FIELDS)
+            .when(IGNORING_ARRAY_ORDER)
+            .isEqualTo(json(expectedResponse(RESPONSE_ABOUT_TO_SUBMIT)));
+    }
+
+    @Test
+    public void shouldReceiveValidResponseWhenSubmittedCallbackIsInvoked() throws Exception {
+        final Map<String, Object> caseData = caseData(CALLBACK_REQUEST);
+
+        final Response response = triggerCallback(caseData, CASEWORKER_DOCUMENT_MANAGEMENT_AMEND, SUBMITTED_URL);
+
+        assertThat(response.getStatusCode()).isEqualTo(OK.value());
+        assertThatJson(response.asString())
+            .inPath(CONFIRMATION_HEADER)
+            .isString()
+            .contains("# Document Updated");
+    }
+}
