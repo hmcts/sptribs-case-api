9cde8460bfd56b72ef5d5f8f4a2d7d385995a077
Gabrielle Magruder <51362727+gmmagruder@users.noreply.github.com>
2024-06-06 15:17:12 +0100

    DTSSTCI 866 (#1787)
    * Add cron jobs for SystemTriggerCompleteHearingOutcome and SystemTriggerStitchCollateHearingBundle events
    
    * Shorten name and description of events
    
    * Add submitted callback to SystemTriggerCompleteHearingOutcome and add functional test
    
    * Remove unnecessary submitted callback
    
    * Remove unnecessary submitted callback
    
    * Add check to SystemTriggerStitchCollateHearingBundleTask to check if it has been run before.
    
    * Add unit tests
    
    * Add functional test for SystemTriggerStitchCollateHearingBundle event.
    
    * Increase java version to 5.2.0 in Chart.yaml
    
    * Bumping chart version/ fixing aliases
    
    * dummy commit
    
    * Revert java upgrade
    
    * Fix code smells
    
    * change range query to use to and from.
    
    * change range query to trigger if hearing date is between now and 14 days
    
    * Update event name to be consistent with other event
    
    ---------
    
    Co-authored-by: hmcts-jenkins-j-to-z <61242337+hmcts-jenkins-j-to-z[bot]@users.noreply.github.com>
    Co-authored-by: Tom Elliott <tomlaelliott@gmail.com>

---
+++ src/main/java/uk/gov/hmcts/sptribs/systemupdate/event/SystemTriggerStitchCollateHearingBundle.java
@@ -0,0 +1,41 @@
+package uk.gov.hmcts.sptribs.systemupdate.event;
+
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
+import uk.gov.hmcts.sptribs.ciccase.model.State;
+import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
+
+import static uk.gov.hmcts.sptribs.caseworker.model.YesNo.YES;
+import static uk.gov.hmcts.sptribs.ciccase.model.State.AwaitingHearing;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.SYSTEM_UPDATE;
+import static uk.gov.hmcts.sptribs.ciccase.model.access.Permissions.CREATE_READ_UPDATE_DELETE;
+
+@Component
+public class SystemTriggerStitchCollateHearingBundle implements CCDConfig<CaseData, State, UserRole> {
+    public static final String SYSTEM_TRIGGER_STITCH_COLLATE_HEARING_BUNDLE = "system-trigger-stitch-collate-hearing-bundle";
+
+    @Override
+    public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
+        configBuilder
+            .event(SYSTEM_TRIGGER_STITCH_COLLATE_HEARING_BUNDLE)
+            .forState(AwaitingHearing)
+            .name("Trigger stitch hearing bundle")
+            .description("Trigger stitch hearing bundle")
+            .aboutToSubmitCallback(this::aboutToSubmit)
+            .grant(CREATE_READ_UPDATE_DELETE, SYSTEM_UPDATE);
+    }
+
+    public AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmit(final CaseDetails<CaseData, State> details,
+                                                                       final CaseDetails<CaseData, State> beforeDetails) {
+        CaseData caseData = details.getData();
+        caseData.setStitchHearingBundleTask(YES);
+
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .data(caseData)
+            .build();
+    }
+}
