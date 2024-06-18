a6c1d56b8a493df6a631a5e31f355fd23bd4de94
Tom Elliott <tomelliott@Toms-MacBook-Pro.local>
2024-06-18 15:13:01 +0100

    DTSSTCI-178: Remove test class annotations messing with mocks

--- src/test/java/uk/gov/hmcts/sptribs/citizen/event/CicCreateCaseEventTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/citizen/event/CicCreateCaseEventTest.java
@@ -1,70 +1,30 @@
 package uk.gov.hmcts.sptribs.citizen.event;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
-import uk.gov.hmcts.sptribs.common.config.AppsConfig;
 import uk.gov.hmcts.sptribs.common.service.CcdSupplementaryDataService;
-import uk.gov.hmcts.sptribs.constants.CommonConstants;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
 import static uk.gov.hmcts.sptribs.ciccase.model.State.DSS_Draft;
-import static uk.gov.hmcts.sptribs.constants.CommonConstants.ST_CIC_CASE_TYPE;
-import static uk.gov.hmcts.sptribs.testutil.TestConstants.CASE_DATA_CIC_ID;
 import static uk.gov.hmcts.sptribs.testutil.TestConstants.TEST_CASE_ID;
 import static uk.gov.hmcts.sptribs.testutil.TestDataHelper.caseData;
 
 @ExtendWith({MockitoExtension.class})
-@SpringBootTest
-@TestPropertySource("classpath:application.yaml")
-@ActiveProfiles("test")
 class CicCreateCaseEventTest {
-
-    @InjectMocks
-    private CicCreateCaseEvent cicCreateCaseEvent;
-
-    @Mock
-    private AppsConfig.AppsDetails cicAppDetail;
 
     @Mock
     private CcdSupplementaryDataService ccdSupplementaryDataService;
 
-    private AutoCloseable autoCloseableMocks;
-
-    @BeforeEach
-    public void setUp() {
-        autoCloseableMocks = MockitoAnnotations.openMocks(this);
-        cicAppDetail = new AppsConfig.AppsDetails();
-        cicAppDetail.setCaseType(ST_CIC_CASE_TYPE);
-        cicAppDetail.setJurisdiction(CommonConstants.ST_CIC_JURISDICTION);
-        cicAppDetail.setCaseTypeOfApplication(List.of(CASE_DATA_CIC_ID));
-
-        AppsConfig.EventsConfig eventsConfig = new AppsConfig.EventsConfig();
-        eventsConfig.setCreateEvent("citizen-cic-create-dss-application");
-
-        cicAppDetail.setEventIds(eventsConfig);
-    }
-
-    @AfterEach
-    public void tearDown() throws Exception {
-        autoCloseableMocks.close();
-    }
+    @InjectMocks
+    private CicCreateCaseEvent cicCreateCaseEvent;
 
     @Test
     void shouldChangeCaseStateWhenAboutToSubmit() {
@@ -83,7 +43,6 @@ class CicCreateCaseEventTest {
         assertThat(response.getState()).isEqualTo(State.DSS_Draft);
     }
 
-    @Disabled
     @Test
     void shouldSubmitSupplementaryDataToCcdWhenSubmittedEventTriggered() {
         final CaseData caseData = caseData();
@@ -97,5 +56,4 @@ class CicCreateCaseEventTest {
 
         verify(ccdSupplementaryDataService).submitSupplementaryDataRequestToCcd(TEST_CASE_ID.toString());
     }
-
 }
