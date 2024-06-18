c42aeb9d89e50d65ded086899fcc9fdc5cea787b
rajaram <rajaram.nagarajan@solirius.com>
2024-06-18 17:58:28 +0100

    DTSSTCI-178: Configure Global search for CIC

--- src/main/java/uk/gov/hmcts/sptribs/ciccase/search/SearchCriteria.java
+++ src/main/java/uk/gov/hmcts/sptribs/ciccase/search/SearchCriteria.java
@@ -3,17 +3,26 @@ package uk.gov.hmcts.sptribs.ciccase.search;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.ccd.sdk.api.SearchCriteriaField;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
 
+import java.util.List;
+
+import static java.util.List.of;
+
 @Component
 public class SearchCriteria implements CCDConfig<CaseData, State, UserRole> {
+
+    private static final List<SearchCriteriaField> SEARCH_CRITERIA_LIST = of(
+        SearchCriteriaField.builder().otherCaseReference("cicCaseCicaReferenceNumber").build()
+    );
 
     @Override
     public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
         configBuilder.searchCriteria()
-            .otherCaseReference("cicCaseCicaReferenceNumber")
+            .fields(SEARCH_CRITERIA_LIST)
             .build();
     }
 
--- src/main/java/uk/gov/hmcts/sptribs/common/event/CreateTestCase.java
+++ src/main/java/uk/gov/hmcts/sptribs/common/event/CreateTestCase.java
@@ -11,10 +11,10 @@ import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
-import uk.gov.hmcts.ccd.sdk.type.CaseLocation;
 import uk.gov.hmcts.ccd.sdk.type.DynamicList;
 import uk.gov.hmcts.ccd.sdk.type.DynamicListElement;
 import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
+import uk.gov.hmcts.sptribs.caseworker.model.CaseManagementLocation;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
@@ -115,13 +115,23 @@ public class CreateTestCase implements CCDConfig<CaseData, State, UserRole> {
     }
 
     private void setDefaultCaseDetails(CaseData data) {
+        CaseManagementLocation caseManagementLocation = new CaseManagementLocation(ST_CIC_WA_CASE_BASE_LOCATION, ST_CIC_WA_CASE_REGION);
+        log.info("Case Management base location {}, region {}",
+            caseManagementLocation.getBaseLocation(), caseManagementLocation.getRegion());
+
+        CaseManagementLocation caseManagementLocation1 = CaseManagementLocation
+            .builder()
+            .baseLocation(ST_CIC_WA_CASE_BASE_LOCATION)
+            .region(ST_CIC_WA_CASE_REGION)
+            .build();
+        log.info("Case Management (builder) base location {}, region {}",
+            caseManagementLocation1.getBaseLocation(), caseManagementLocation1.getRegion());
+
         data.setCaseManagementLocation(
-            CaseLocation
-                .builder()
-                .baseLocation(ST_CIC_WA_CASE_BASE_LOCATION)
-                .region(ST_CIC_WA_CASE_REGION)
-                .build()
+            caseManagementLocation
         );
+        log.info("Case Management (data) base location {}, region {}",
+            data.getCaseManagementLocation().getBaseLocation(), data.getCaseManagementLocation().getRegion());
 
         DynamicListElement caseManagementCategory = new DynamicListElement(
             UUID.randomUUID(), ST_CIC_WA_CASE_MANAGEMENT_CATEGORY);
