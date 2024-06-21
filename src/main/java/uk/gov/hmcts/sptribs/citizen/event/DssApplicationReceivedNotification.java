a33f1335d53b2a09b10f04188b86dee4a9633e79
Tom Elliott <tomelliott@Toms-MacBook-Pro.local>
2024-06-21 18:02:16 +0100

    DTSSTCI-73: Add Welsh template to config and determine whether to send English or Welsh email

--- src/main/java/uk/gov/hmcts/sptribs/citizen/event/DssApplicationReceivedNotification.java
+++ src/main/java/uk/gov/hmcts/sptribs/citizen/event/DssApplicationReceivedNotification.java
@@ -4,6 +4,7 @@ import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.sptribs.ciccase.model.DssCaseData;
+import uk.gov.hmcts.sptribs.ciccase.model.LanguagePreference;
 import uk.gov.hmcts.sptribs.ciccase.model.NotificationResponse;
 import uk.gov.hmcts.sptribs.notification.DssNotificationHelper;
 import uk.gov.hmcts.sptribs.notification.NotificationServiceCIC;
@@ -13,9 +14,12 @@ import uk.gov.hmcts.sptribs.notification.model.NotificationRequest;
 
 import java.util.Map;
 
+import static uk.gov.hmcts.sptribs.ciccase.model.LanguagePreference.ENGLISH;
 import static uk.gov.hmcts.sptribs.common.CommonConstants.CIC_CASE_REPRESENTATIVE_NAME;
 import static uk.gov.hmcts.sptribs.common.CommonConstants.CIC_CASE_SUBJECT_NAME;
 import static uk.gov.hmcts.sptribs.common.CommonConstants.CONTACT_PARTY_INFO;
+import static uk.gov.hmcts.sptribs.notification.TemplateName.APPLICATION_RECEIVED;
+import static uk.gov.hmcts.sptribs.notification.TemplateName.APPLICATION_RECEIVED_CY;
 
 @Component
 @Setter
@@ -33,7 +37,11 @@ public class DssApplicationReceivedNotification implements PartiesNotification {
         templateVarsSubject.put(CIC_CASE_SUBJECT_NAME, dssCaseData.getSubjectFullName());
         templateVarsSubject.put(CONTACT_PARTY_INFO, dssCaseData.getNotifyPartyMessage());
 
-        NotificationResponse notificationResponse = sendEmailNotification(templateVarsSubject, dssCaseData.getSubjectEmailAddress());
+        NotificationResponse notificationResponse = sendEmailNotification(
+            templateVarsSubject,
+            dssCaseData.getSubjectEmailAddress(),
+            dssCaseData.getLanguagePreference()
+        );
         dssCaseData.setSubjectNotificationResponse(notificationResponse);
     }
 
@@ -43,13 +51,20 @@ public class DssApplicationReceivedNotification implements PartiesNotification {
         templateVarsRep.put(CIC_CASE_REPRESENTATIVE_NAME, dssCaseData.getRepresentativeFullName());
         templateVarsRep.put(CONTACT_PARTY_INFO, dssCaseData.getNotifyPartyMessage());
 
-        NotificationResponse notificationResponse = sendEmailNotification(templateVarsRep, dssCaseData.getRepresentativeEmailAddress());
+        NotificationResponse notificationResponse = sendEmailNotification(
+            templateVarsRep,
+            dssCaseData.getRepresentativeEmailAddress(),
+            dssCaseData.getLanguagePreference()
+        );
         dssCaseData.setRepNotificationResponse(notificationResponse);
     }
 
-    private NotificationResponse sendEmailNotification(final Map<String, Object> templateVars, String toEmail) {
-        NotificationRequest request = dssNotificationHelper.buildEmailNotificationRequest(
-            toEmail, templateVars, TemplateName.APPLICATION_RECEIVED);
+    private NotificationResponse sendEmailNotification(final Map<String, Object> templateVars,
+                                                       String toEmail,
+                                                       LanguagePreference languagePreference) {
+        TemplateName templateName = ENGLISH.equals(languagePreference) ? APPLICATION_RECEIVED : APPLICATION_RECEIVED_CY;
+        NotificationRequest request =
+            dssNotificationHelper.buildEmailNotificationRequest(toEmail, templateVars, templateName);
         return notificationService.sendEmail(request);
     }
 
