777723c12822ad0950dc99c5bb9deb1ec2ea6556
rajaram <rajaram.nagarajan@solirius.com>
2024-06-27 15:42:25 +0100

    DTSSTCI-178: Configure Global search for CIC

--- src/main/java/uk/gov/hmcts/sptribs/common/config/jackson/JacksonConfiguration.java
+++ src/main/java/uk/gov/hmcts/sptribs/common/config/jackson/JacksonConfiguration.java
@@ -1,20 +1,19 @@
 package uk.gov.hmcts.sptribs.common.config.jackson;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import uk.gov.hmcts.ccd.sdk.api.HasRole;
 import uk.gov.hmcts.reform.ccd.document.am.healthcheck.InternalHealth;
 
-import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
-import static com.fasterxml.jackson.core.JsonParser.Feature.INCLUDE_SOURCE_IN_LOCATION;
 import static com.fasterxml.jackson.databind.MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS;
 import static com.fasterxml.jackson.databind.MapperFeature.INFER_BUILDER_TYPE_BINDINGS;
-import static com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer.INSTANCE;
 
 @Configuration
 public class JacksonConfiguration {
@@ -25,7 +24,7 @@ public class JacksonConfiguration {
         ObjectMapper mapper = JsonMapper.builder()
             .configure(ACCEPT_CASE_INSENSITIVE_ENUMS, true)
             .enable(INFER_BUILDER_TYPE_BINDINGS)
-            .serializationInclusion(NON_NULL)
+            .serializationInclusion(JsonInclude.Include.NON_NULL)
             .build();
 
         SimpleModule deserialization = new SimpleModule();
@@ -34,9 +33,8 @@ public class JacksonConfiguration {
         mapper.registerModule(deserialization);
 
         JavaTimeModule datetime = new JavaTimeModule();
-        datetime.addSerializer(INSTANCE);
+        datetime.addSerializer(LocalDateSerializer.INSTANCE);
         mapper.registerModule(datetime);
-        mapper.enable(INCLUDE_SOURCE_IN_LOCATION);
 
         return mapper;
     }
