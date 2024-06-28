1f38676daf7d817e3b180742d19f067ab714a910
rajaram <rajaram.nagarajan@solirius.com>
2024-06-28 09:19:42 +0100

    DTSSTCI-178: Configure Global search for CIC

--- src/main/java/uk/gov/hmcts/sptribs/ciccase/model/CaseData.java
+++ src/main/java/uk/gov/hmcts/sptribs/ciccase/model/CaseData.java
@@ -13,6 +13,7 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.util.ObjectUtils;
 import uk.gov.hmcts.ccd.sdk.api.CCD;
 import uk.gov.hmcts.ccd.sdk.type.CaseLink;
+import uk.gov.hmcts.ccd.sdk.type.CaseLocation;
 import uk.gov.hmcts.ccd.sdk.type.ComponentLauncher;
 import uk.gov.hmcts.ccd.sdk.type.DynamicList;
 import uk.gov.hmcts.ccd.sdk.type.FlagLauncher;
@@ -24,7 +25,6 @@ import uk.gov.hmcts.sptribs.caseworker.model.CaseBuilt;
 import uk.gov.hmcts.sptribs.caseworker.model.CaseIssue;
 import uk.gov.hmcts.sptribs.caseworker.model.CaseIssueDecision;
 import uk.gov.hmcts.sptribs.caseworker.model.CaseIssueFinalDecision;
-import uk.gov.hmcts.sptribs.caseworker.model.CaseManagementLocation;
 import uk.gov.hmcts.sptribs.caseworker.model.CaseNote;
 import uk.gov.hmcts.sptribs.caseworker.model.CaseStay;
 import uk.gov.hmcts.sptribs.caseworker.model.CloseCase;
@@ -111,7 +111,7 @@ public class CaseData {
         label = "Case Location",
         access = {DefaultAccess.class, CaseworkerWithCAAAccess.class, CitizenAccess.class, GlobalSearchAccess.class}
     )
-    private CaseManagementLocation caseManagementLocation;
+    private CaseLocation caseLocation;
 
     @CCD(
         label = "Case Management Category",
