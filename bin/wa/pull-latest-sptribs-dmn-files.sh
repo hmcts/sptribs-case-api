6b4c72b05dbb8489fa91806a395df5b4920f6bb7
Tom Elliott <tomelliott@Toms-MacBook-Pro.local>
2024-07-04 11:23:51 +0100

    DTSSTCI-578: Import toolbelt/oathtool from hmctspublic to resolve toomanyrequests error

--- bin/wa/pull-latest-sptribs-dmn-files.sh
+++ bin/wa/pull-latest-sptribs-dmn-files.sh
@@ -2,7 +2,7 @@
 
 branchName=$1
 
-#Checkout specific branch of  camunda bpmn definition
+# Checkout specific branch of  camunda bpmn definition
 git clone https://github.com/hmcts/st-wa-task-configuration.git
 cd st-wa-task-configuration
 
@@ -10,7 +10,7 @@ echo "Switch to ${branchName} branch on st-wa-task-configuration"
 git checkout ${branchName}
 cd ..
 
-#Copy camunda folder which contains dmn files
+# Copy camunda folder which contains dmn files
 cp -r ./st-wa-task-configuration/src/main/resources .
 rm -rf ./st-wa-task-configuration
 
