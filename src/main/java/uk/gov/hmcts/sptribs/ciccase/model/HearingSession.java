2d13833376e3ce4d4dbd6aa1a62ba77bb33fe5b5
rajaram <rajaram.nagarajan@solirius.com>
2024-07-18 09:10:56 +0100

    DTSSTCI-908: Combined all Renovate PR changes as of 9th july

--- src/main/java/uk/gov/hmcts/sptribs/ciccase/model/HearingSession.java
+++ src/main/java/uk/gov/hmcts/sptribs/ciccase/model/HearingSession.java
@@ -3,11 +3,10 @@ package uk.gov.hmcts.sptribs.ciccase.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import uk.gov.hmcts.ccd.sdk.api.HasLabel;
 
 @Getter
 @AllArgsConstructor
-public enum HearingSession implements HasLabel {
+public enum HearingSession {
 
     @JsonProperty("morning")
     MORNING("Morning"),
