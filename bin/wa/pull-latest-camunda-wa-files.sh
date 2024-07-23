493b9c3b5bc660ebce65324aac3d53f50e1fc3fa
oleon23 <oliver.leon@solirius.com>
2024-07-23 18:34:33 +0100

    [DTSSTCI-578] Fix bpmn script

--- bin/wa/pull-latest-camunda-wa-files.sh
+++ bin/wa/pull-latest-camunda-wa-files.sh
@@ -16,4 +16,4 @@ cp -r ./wa-standalone-task-bpmn/src/main/resources/ .
 rm -rf ./wa-standalone-task-bpmn
 
 #upload bpmn files to environment
-./bin/wa/import-wa-bpmn-diagram.sh .
+./wa/import-wa-bpmn-diagram.sh .
