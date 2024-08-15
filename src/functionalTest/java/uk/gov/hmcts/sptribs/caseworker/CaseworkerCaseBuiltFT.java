930a4ba24b8a582de7e4865f677f156f3bff650f
Tom Elliott <tomelliott@Toms-MacBook-Pro.local>
2024-08-15 09:04:00 +0100

    DTSSTCI-974: Remove unused imports

--- src/functionalTest/java/uk/gov/hmcts/sptribs/caseworker/CaseworkerCaseBuiltFT.java
+++ src/functionalTest/java/uk/gov/hmcts/sptribs/caseworker/CaseworkerCaseBuiltFT.java
@@ -9,10 +9,7 @@ import uk.gov.hmcts.sptribs.testutil.FunctionalTestSuite;
 import java.util.Map;
 
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.http.HttpStatus.OK;
 import static uk.gov.hmcts.sptribs.testutil.CaseDataUtil.caseData;
-import static uk.gov.hmcts.sptribs.testutil.TestConstants.ABOUT_TO_SUBMIT_URL;
 import static uk.gov.hmcts.sptribs.testutil.TestConstants.SUBMITTED_URL;
 
 @SpringBootTest
@@ -21,7 +18,6 @@ public class CaseworkerCaseBuiltFT extends FunctionalTestSuite {
     private static final String REQUEST_GENERAL = "classpath:request/casedata/ccd-callback-casedata-general.json";
 
     private static final String CONFIRMATION_HEADER = "$.confirmation_header";
-    private static final String STATE = "$.state";
 
     @Test
     public void shouldTriggerSuccessfulResponseIfSubmitCallbackIsInvoked() throws Exception {
