ce061728694f52d590a3e6c24e648b2600a9457e
Rajaram <70951210+rajaram-nagarajan@users.noreply.github.com>
2024-08-19 14:19:46 +0100

    Update HighLevelDataSetupApp.java

--- src/main/java/uk/gov/hmcts/sptribs/common/ccd/HighLevelDataSetupApp.java
+++ src/main/java/uk/gov/hmcts/sptribs/common/ccd/HighLevelDataSetupApp.java
@@ -8,7 +8,6 @@ import uk.gov.hmcts.befta.dse.ccd.CcdRoleConfig;
 import uk.gov.hmcts.befta.dse.ccd.DataLoaderToDefinitionStore;
 import uk.gov.hmcts.befta.exception.ImportException;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -81,7 +80,6 @@ public class HighLevelDataSetupApp extends DataLoaderToDefinitionStore {
     @Override
     protected List<String> getAllDefinitionFilesToLoadAt(String definitionsPath) {
         String environmentName = environment.name().toLowerCase(Locale.UK);
-
         return List.of(
             "build/ccd-config/ccd-" + CcdServiceCode.ST_CIC.getCaseType().getCaseTypeName() + "-" + environmentName + ".xlsx"
         );
