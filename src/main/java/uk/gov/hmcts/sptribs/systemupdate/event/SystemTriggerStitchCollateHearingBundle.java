45820ffd944dcba55536db47622454f43018b139
oleon23 <oliver.leon@solirius.com>
2024-08-28 17:18:58 +0100

    [DTSSTCI-946] Add publishToCamunda flags for system events to create tasks

--- src/main/java/uk/gov/hmcts/sptribs/systemupdate/event/SystemTriggerStitchCollateHearingBundle.java
+++ src/main/java/uk/gov/hmcts/sptribs/systemupdate/event/SystemTriggerStitchCollateHearingBundle.java
@@ -1,9 +1,11 @@
 package uk.gov.hmcts.sptribs.systemupdate.event;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.ccd.sdk.api.Event;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
@@ -11,22 +13,32 @@ import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
 
 import static uk.gov.hmcts.sptribs.caseworker.model.YesNo.YES;
 import static uk.gov.hmcts.sptribs.ciccase.model.State.AwaitingHearing;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_WA_CONFIG_USER;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.SYSTEM_UPDATE;
+import static uk.gov.hmcts.sptribs.ciccase.model.access.Permissions.CREATE_READ_UPDATE;
 import static uk.gov.hmcts.sptribs.ciccase.model.access.Permissions.CREATE_READ_UPDATE_DELETE;
 
 @Component
 public class SystemTriggerStitchCollateHearingBundle implements CCDConfig<CaseData, State, UserRole> {
     public static final String SYSTEM_TRIGGER_STITCH_COLLATE_HEARING_BUNDLE = "system-trigger-stitch-collate-hearing-bundle";
 
+    @Value("${feature.wa.enabled}")
+    private boolean isWorkAllocationEnabled;
+
     @Override
     public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
-        configBuilder
+        Event.EventBuilder<CaseData, UserRole, State> eventBuilder = configBuilder
             .event(SYSTEM_TRIGGER_STITCH_COLLATE_HEARING_BUNDLE)
             .forState(AwaitingHearing)
             .name("Trigger stitch hearing bundle")
             .description("Trigger stitch hearing bundle")
             .aboutToSubmitCallback(this::aboutToSubmit)
             .grant(CREATE_READ_UPDATE_DELETE, SYSTEM_UPDATE);
+
+        if (isWorkAllocationEnabled) {
+            eventBuilder.publishToCamunda()
+                    .grant(CREATE_READ_UPDATE, ST_CIC_WA_CONFIG_USER);
+        }
     }
 
     public AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmit(final CaseDetails<CaseData, State> details,
