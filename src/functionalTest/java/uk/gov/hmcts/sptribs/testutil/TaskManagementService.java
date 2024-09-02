ffa1a0f3e6209b3c670d90f4df614381f8b2687b
gmmagruder <gabrielle.magruder@gmail.com>
2024-09-02 13:49:04 +0100

    Revert back to 60 seconds, cause is due to role assignments need to be refreshed.

--- src/functionalTest/java/uk/gov/hmcts/sptribs/testutil/TaskManagementService.java
+++ src/functionalTest/java/uk/gov/hmcts/sptribs/testutil/TaskManagementService.java
@@ -42,8 +42,8 @@ public class TaskManagementService {
         taskMonitorService.triggerTerminationJob();
         taskMonitorService.triggerReconfigurationJob();
 
-        // Wait 180 seconds to allow time for task to be created in database
-        Thread.sleep(180000);
+        // Wait 60 seconds to allow time for task to be created in database
+        Thread.sleep(60000);
 
         Map<String, Object> searchParameter = Map.of(
             "key", "caseId",
