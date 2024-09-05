dbaef95726e471b1151e19b0eafdbae88489dfb7
gmmagruder <gabrielle.magruder@gmail.com>
2024-09-05 15:07:52 +0100

    Add integration test for ReferToLegalOfficerReason

---
+++ src/integrationTest/java/uk/gov/hmcts/sptribs/caseworker/event/page/ReferToLegalOfficerReasonIT.java
@@ -0,0 +1,86 @@
+package uk.gov.hmcts.sptribs.caseworker.event.page;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import uk.gov.hmcts.sptribs.caseworker.model.ReferToLegalOfficer;
+import uk.gov.hmcts.sptribs.caseworker.model.ReferralReason;
+import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
+import uk.gov.hmcts.sptribs.common.config.WebMvcConfig;
+import uk.gov.hmcts.sptribs.testutil.IdamWireMock;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static uk.gov.hmcts.sptribs.caseworker.util.EventConstants.CASEWORKER_REFER_TO_LEGAL_OFFICER;
+import static uk.gov.hmcts.sptribs.testutil.TestConstants.AUTHORIZATION;
+import static uk.gov.hmcts.sptribs.testutil.TestConstants.REFER_TO_LEGAL_OFFICER_REASON_MID_EVENT_URL;
+import static uk.gov.hmcts.sptribs.testutil.TestConstants.SERVICE_AUTHORIZATION;
+import static uk.gov.hmcts.sptribs.testutil.TestConstants.TEST_AUTHORIZATION_TOKEN;
+import static uk.gov.hmcts.sptribs.testutil.TestDataHelper.callbackRequest;
+import static uk.gov.hmcts.sptribs.testutil.TestResourceUtil.expectedResponse;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@ContextConfiguration(initializers = {IdamWireMock.PropertiesInitializer.class})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class ReferToLegalOfficerReasonIT {
+    private static final String REFER_TO_LEGAL_OFFICER_REASON_RESPONSE =
+        "classpath:responses/refer-to-legal-officer-reason-mid-event-response.json";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private WebMvcConfig webMvcConfig;
+
+    @BeforeAll
+    static void setUp() {
+        IdamWireMock.start();
+    }
+
+    @AfterAll
+    static void tearDown() {
+        IdamWireMock.stopAndReset();
+    }
+
+    @Test
+    void shouldValidateJudgeReferralReasonInMidEvent() throws Exception {
+        CaseData caseData = CaseData.builder()
+            .referToLegalOfficer(ReferToLegalOfficer.builder()
+                .referralReason(ReferralReason.CORRECTIONS)
+                .build())
+            .build();
+
+        mockMvc.perform(post(REFER_TO_LEGAL_OFFICER_REASON_MID_EVENT_URL)
+                .contentType(APPLICATION_JSON)
+                .header(SERVICE_AUTHORIZATION, TEST_AUTHORIZATION_TOKEN)
+                .header(AUTHORIZATION, TEST_AUTHORIZATION_TOKEN)
+                .content(objectMapper.writeValueAsString(
+                    callbackRequest(
+                        caseData,
+                        CASEWORKER_REFER_TO_LEGAL_OFFICER,
+                        "CaseClosed")))
+                .accept(APPLICATION_JSON))
+            .andExpect(
+                status().isOk())
+            .andExpect(
+                content().json(expectedResponse(REFER_TO_LEGAL_OFFICER_REASON_RESPONSE))
+            );
+    }
+}
--- src/test/java/uk/gov/hmcts/sptribs/testutil/TestConstants.java
+++ src/test/java/uk/gov/hmcts/sptribs/testutil/TestConstants.java
@@ -35,6 +35,8 @@ public final class TestConstants {
 
     public static final String REFER_TO_JUDGE_REASON_MID_EVENT_URL = "/callbacks/mid-event?page=referToJudgeReason";
 
+    public static final String REFER_TO_LEGAL_OFFICER_REASON_MID_EVENT_URL = "/callbacks/mid-event?page=referToLegalOfficer";
+
     public static final String ISSUE_DECISION_MID_EVENT_URL = "/callbacks/mid-event?page=issueDecisionAddDocumentFooter";
 
     public static final String AUTH_HEADER_VALUE = "auth-header-value";
