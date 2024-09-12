6e75b04c9872ffce739ec1bccb206ff9336a69b6
gmmagruder <gabrielle.magruder@gmail.com>
2024-09-12 12:07:03 +0100

    Commit before switch of branch

---
+++ src/integrationTest/java/uk/gov/hmcts/sptribs/systemupdate/service/CaseDetailsUpdaterIT.java
@@ -0,0 +1,33 @@
+package uk.gov.hmcts.sptribs.systemupdate.service;
+
+import org.apache.commons.collections4.map.HashedMap;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
+import uk.gov.hmcts.sptribs.ciccase.task.CaseTask;
+
+import java.time.LocalDate;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class CaseDetailsUpdaterIT {
+
+    @Test
+    void shouldUpdateCaseData() {
+        final CaseTask caseTask = caseDetails -> {
+            caseDetails.getData().setDueDate(LocalDate.of(2023, 1, 2));
+            return caseDetails;
+        };
+
+        final StartEventResponse startEventResponse = StartEventResponse.builder()
+            .caseDetails(CaseDetails.builder()
+                .data(new HashedMap<>())
+                .build())
+            .build();
+    }
+}
