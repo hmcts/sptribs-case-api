f286d0a9af0acc30dff08507f1890c0ba6663e42
gmmagruder <gabrielle.magruder@gmail.com>
2024-09-25 09:15:43 +0100

    Change how case closed data is handled

--- src/functionalTest/java/uk/gov/hmcts/sptribs/wa/WAProcessSetAsideDirectionsFT.java
+++ src/functionalTest/java/uk/gov/hmcts/sptribs/wa/WAProcessSetAsideDirectionsFT.java
@@ -59,12 +59,12 @@ public class WAProcessSetAsideDirectionsFT extends FunctionalTestSuite {
 
         ccdCaseCreator.createInitialStartEventAndSubmit(CASEWORKER_EDIT_CASE, ST_CIC_JURISDICTION, ST_CIC_CASE_TYPE, newCaseId, caseData);
         ccdCaseCreator.createInitialStartEventAndSubmit(CASEWORKER_CASE_BUILT, ST_CIC_JURISDICTION, ST_CIC_CASE_TYPE, newCaseId, caseData);
-
-        final Map<String, Object> closedCaseData = ccdCaseCreator.createInitialStartEventAndSubmit(
+        ccdCaseCreator.createInitialStartEventAndSubmit(
             CASEWORKER_CLOSE_THE_CASE, ST_CIC_JURISDICTION, ST_CIC_CASE_TYPE, newCaseId, caseData);
 
-        closedCaseData.put("cicCaseReferralTypeForWA", "Set aside request");
-        closedCaseData.putAll(caseData(CASEWORKER_CREATE_DRAFT_ORDER_DATA));
+        caseData.put("cicCaseReferralTypeForWA", "Set aside request");
+        caseData.put("state", "CaseClosed");
+        caseData.putAll(caseData(CASEWORKER_CREATE_DRAFT_ORDER_DATA));
         ccdCaseCreator.createInitialStartEventAndSubmit(
             CASEWORKER_CREATE_DRAFT_ORDER, ST_CIC_JURISDICTION, ST_CIC_CASE_TYPE, newCaseId, caseData);
 
