4cbbb76a101861260eac8289b3c3bfa5f09ac411
Tom Elliott <tomelliott@Toms-MacBook-Pro.local>
2024-09-27 16:07:26 +0100

    DTSSTCI-1067: Map idam:caseworker-ras-validation correctly

--- src/main/java/uk/gov/hmcts/sptribs/ciccase/model/accessprofile/CICAccessProfile.java
+++ src/main/java/uk/gov/hmcts/sptribs/ciccase/model/accessprofile/CICAccessProfile.java
@@ -196,7 +196,10 @@ public class CICAccessProfile implements CCDConfig<CriminalInjuriesCompensationD
                 UserRolesForAccessProfiles.AC_CASEFLAGS_VIEWER.getRole())
             .build();
         configBuilder.caseRoleToAccessProfile(UserRolesForAccessProfiles.ST_CIC_WA_CONFIG_USER)
-            .accessProfiles(UserRolesForAccessProfiles.AC_ST_CIC_WA_CONFIG_USER.getRole(),
+            .accessProfiles(UserRolesForAccessProfiles.AC_ST_CIC_WA_CONFIG_USER.getRole())
+            .build();
+        configBuilder.caseRoleToAccessProfile(UserRolesForAccessProfiles.IDAM_RAS_CASEWORKER_VALIDATION)
+            .accessProfiles(UserRolesForAccessProfiles.RAS_CASEWORKER_VALIDATION.getRole(),
                 UserRolesForAccessProfiles.GS_PROFILE.getRole())
             .build();
     }
