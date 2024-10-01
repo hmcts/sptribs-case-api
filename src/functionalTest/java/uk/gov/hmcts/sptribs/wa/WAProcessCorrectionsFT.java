864947673c09a9a8b41491ebe209dbab2267a521
gmmagruder <gabrielle.magruder@gmail.com>
2024-10-01 15:21:46 +0100

    Fix roles

--- src/functionalTest/java/uk/gov/hmcts/sptribs/wa/WAProcessCorrectionsFT.java
+++ src/functionalTest/java/uk/gov/hmcts/sptribs/wa/WAProcessCorrectionsFT.java
@@ -42,7 +42,7 @@ public class WAProcessCorrectionsFT extends FunctionalTestSuite {
 
     private static final String TASK_TYPE = "processCorrections";
     private static final List<String> TASK_ROLES = Arrays.asList("regional-centre-admin", "regional-centre-team-leader", "task-supervisor",
-        "hearing-centre-admin", "hearing-centre-admin-team-leader", "ctsc", "ctsc-team-leader");
+        "hearing-centre-admin", "hearing-centre-team-leader", "ctsc", "ctsc-team-leader");
     private static final int DEFAULT_TIMEOUT_SECONDS = 300;
     private static final int DEFAULT_POLL_INTERVAL_SECONDS = 4;
 
