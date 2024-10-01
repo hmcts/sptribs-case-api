37e6a788f51eb8a15e40494d8459ca033374aff8
gmmagruder <gabrielle.magruder@gmail.com>
2024-10-01 16:19:19 +0100

    Fix num roles

--- src/functionalTest/java/uk/gov/hmcts/sptribs/wa/WAProcessFurtherEvidenceFT.java
+++ src/functionalTest/java/uk/gov/hmcts/sptribs/wa/WAProcessFurtherEvidenceFT.java
@@ -81,7 +81,7 @@ public class WAProcessFurtherEvidenceFT extends FunctionalTestSuite {
                     assertThat(taskType).isEqualTo(TASK_TYPE);
 
                     Response retrieveTaskRolePermissionsResponseBody =
-                        taskManagementService.retrieveTaskRolePermissions(taskId, 3, 200);
+                        taskManagementService.retrieveTaskRolePermissions(taskId, 7, 200);
 
                     if (retrieveTaskRolePermissionsResponseBody.asString().isBlank()) {
                         return false;
