b35e83d971772ddf9b530b0ba62831fc147ff089
gmmagruder <gabrielle.magruder@gmail.com>
2024-10-01 14:07:16 +0100

    Fix roles on tests

--- src/functionalTest/java/uk/gov/hmcts/sptribs/wa/WAProcessRule27DecisionFT.java
+++ src/functionalTest/java/uk/gov/hmcts/sptribs/wa/WAProcessRule27DecisionFT.java
@@ -40,7 +40,8 @@ public class WAProcessRule27DecisionFT extends FunctionalTestSuite {
     private RoleAssignmentService roleAssignmentService;
 
     private static final String TASK_TYPE = "processRule27Decision";
-    private static final List<String> TASK_ROLES = Arrays.asList("regional-centre-admin", "regional-centre-team-leader", "task-supervisor");
+    private static final List<String> TASK_ROLES = Arrays.asList("regional-centre-admin", "regional-centre-team-leader", "task-supervisor",
+        "hearing-centre-admin", "hearing-centre-admin-team-leader", "ctsc", "ctsc-team-leader");
     private static final int DEFAULT_TIMEOUT_SECONDS = 300;
     private static final int DEFAULT_POLL_INTERVAL_SECONDS = 4;
 
@@ -87,7 +88,7 @@ public class WAProcessRule27DecisionFT extends FunctionalTestSuite {
                     assertThat(taskType).isEqualTo(TASK_TYPE);
 
                     Response retrieveTaskRolePermissionsResponseBody =
-                        taskManagementService.retrieveTaskRolePermissions(taskId, 3, 200);
+                        taskManagementService.retrieveTaskRolePermissions(taskId, 7, 200);
 
                     if (retrieveTaskRolePermissionsResponseBody.asString().isBlank()) {
                         return false;
