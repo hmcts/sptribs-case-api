94f4ec821ec241bdcc7daaf89e509c5bd485ae59
oleon23 <oliver.leon@solirius.com>
2024-10-04 16:03:01 +0100

    [DTSSTCI-1122] Update expected roles in admin functional tests

--- src/functionalTest/java/uk/gov/hmcts/sptribs/wa/WAProcessSetAsideDirectionsFT.java
+++ src/functionalTest/java/uk/gov/hmcts/sptribs/wa/WAProcessSetAsideDirectionsFT.java
@@ -41,7 +41,8 @@ public class WAProcessSetAsideDirectionsFT extends FunctionalTestSuite {
     private RoleAssignmentService roleAssignmentService;
 
     private static final String TASK_TYPE = "processSetAsideDirections";
-    private static final List<String> TASK_ROLES = Arrays.asList("regional-centre-admin", "regional-centre-team-leader", "task-supervisor");
+    private static final List<String> TASK_ROLES = Arrays.asList("regional-centre-admin", "regional-centre-team-leader", "task-supervisor",
+            "hearing-centre-admin", "hearing-centre-team-leader", "ctsc", "ctsc-team-leader");
     private static final int DEFAULT_TIMEOUT_SECONDS = 300;
     private static final int DEFAULT_POLL_INTERVAL_SECONDS = 4;
 
