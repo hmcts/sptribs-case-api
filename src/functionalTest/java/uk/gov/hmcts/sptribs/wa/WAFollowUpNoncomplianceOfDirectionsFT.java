715cdb95fd85289b52338550df5ad2c247a7287c
Gabrielle Magruder <51362727+gmmagruder@users.noreply.github.com>
2024-10-07 10:10:01 +0100

    DTSSTCI-1121 (#1959)
    * Fix roles
    
    * Increase wait time to 90 seconds
    
    * Add functional test for ProcessStayDirections task
    
    * Try to run functional tests in parallel
    
    * Add condition to parallelise functional tests when WA is enabled
    
    * Test disabling parallelisation.
    
    * Enable paralleliseation when WA is enabled

--- src/functionalTest/java/uk/gov/hmcts/sptribs/wa/WAFollowUpNoncomplianceOfDirectionsFT.java
+++ src/functionalTest/java/uk/gov/hmcts/sptribs/wa/WAFollowUpNoncomplianceOfDirectionsFT.java
@@ -38,7 +38,8 @@ public class WAFollowUpNoncomplianceOfDirectionsFT extends FunctionalTestSuite {
     private RoleAssignmentService roleAssignmentService;
 
     private static final String TASK_TYPE = "followUpNoncomplianceOfDirections";
-    private static final List<String> TASK_ROLES = Arrays.asList("regional-centre-admin", "regional-centre-team-leader", "task-supervisor");
+    private static final List<String> TASK_ROLES = Arrays.asList("regional-centre-admin", "regional-centre-team-leader", "task-supervisor",
+        "hearing-centre-admin", "hearing-centre-team-leader", "ctsc", "ctsc-team-leader");
     private static final int DEFAULT_TIMEOUT_SECONDS = 300;
     private static final int DEFAULT_POLL_INTERVAL_SECONDS = 4;
 
@@ -79,7 +80,7 @@ public class WAFollowUpNoncomplianceOfDirectionsFT extends FunctionalTestSuite {
                     assertThat(taskType).isEqualTo(TASK_TYPE);
 
                     Response retrieveTaskRolePermissionsResponseBody =
-                        taskManagementService.retrieveTaskRolePermissions(taskId, 3, 200);
+                        taskManagementService.retrieveTaskRolePermissions(taskId, 7, 200);
 
                     if (retrieveTaskRolePermissionsResponseBody.asString().isBlank()) {
                         return false;
