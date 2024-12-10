e3e5ba0e067139069e39dedf1f6cbd371b30cac1
Tom Elliott <tomlaelliott@gmail.com>
2024-12-10 10:47:50 +0000

    Use /lease not /testing-support/lease for s2s DMN upload (#2006)
    Use /lease not /testing-support/lease
    
    Co-authored-by: Tom Elliott <tomelliott@Toms-MacBook-Pro.local>

--- bin/utils/idam-lease-service-token.sh
+++ bin/utils/idam-lease-service-token.sh
@@ -6,7 +6,7 @@ microservice=${1}
 oneTimePassword=${2}
 
 curl -v --insecure --fail --show-error --silent -X POST \
-  ${S2S_URL_BASE:-http://rpe-service-auth-provider-aat.service.core-compute-aat.internal}/testing-support/lease \
+  ${S2S_URL_BASE:-http://rpe-service-auth-provider-aat.service.core-compute-aat.internal}/lease \
   -H "Content-Type: application/json" \
   -d '{
     "microservice": "'${microservice}'",
