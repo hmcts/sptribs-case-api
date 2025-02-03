4b250bba36924acc998dea50e37846b66bffd3c7
Tom Elliott <tomlaelliott@gmail.com>
2025-02-03 10:21:54 +0000

    DTSSTCI-1228 (#2009)
    * DTSSTCI-1228: Add Create Due Date work allocation task configuration
    
    * DTSSTCI-1228: Enable wa ft if the label used
    
    * DTSSTCI-1228: revert label change in jenkins file
    
    * [DTSSTCI-1228] Update build definition to publish to camunda with pr-values:wa
    
    * [DTSSTCI-1228] Update Jenkinsfile for label with wa-ft
    
    * [DTSSTCI-1228] Fix sentence case and permissions for admin users
    
    * [DTSSTCI-1228] Fix jenkinsfile
    
    * [DTSSTCI-1228] Try workaround for failing camunda image
    
    * [DTSSTCI-1228] Try workaround for failing camunda image
    
    ---------
    
    Co-authored-by: Tom Elliott <tomelliott@Toms-MacBook-Pro.local>
    Co-authored-by: oleon23 <oliver.leon@solirius.com>

--- src/test/java/uk/gov/hmcts/sptribs/dmnutils/CamundaTaskConstants.java
+++ src/test/java/uk/gov/hmcts/sptribs/dmnutils/CamundaTaskConstants.java
@@ -101,6 +101,8 @@ public class CamundaTaskConstants {
     public static final String REVIEW_SPECIFIC_ACCESS_REQ_ADMIN_TASK = "reviewSpecificAccessRequestAdmin";
     public static final String REVIEW_SPECIFIC_ACCESS_REQ_CTSC_TASK = "reviewSpecificAccessRequestCTSC";
 
+    public static final String CREATE_DUE_DATE = "createDueDate";
+
     public static final String AUTO_COMPLETE_MODE = "Auto";
     public static final String DEFAULT_NONE_COMPLETE_MODE = "defaultNone";
 
