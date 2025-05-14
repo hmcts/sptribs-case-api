43a09f7ca18fdfae720409740efac0c07ddaecba
oleon23 <oliver.leon@solirius.com>
2025-05-14 16:57:18 +0100

    [DTSSTCI-1257] Revert changes to show case documents

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/page/ShowCaseDocuments.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/page/ShowCaseDocuments.java
@@ -5,7 +5,6 @@ import org.springframework.util.ObjectUtils;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.ccd.sdk.type.ListValue;
-import uk.gov.hmcts.sptribs.caseworker.model.CaseIssueDecision;
 import uk.gov.hmcts.sptribs.caseworker.model.CloseCase;
 import uk.gov.hmcts.sptribs.caseworker.model.DocumentManagement;
 import uk.gov.hmcts.sptribs.caseworker.model.HearingSummary;
@@ -36,9 +35,6 @@ public class ShowCaseDocuments implements CcdPageConfiguration {
             .done()
             .complex(CaseData::getCloseCase)
             .readonly(CloseCase::getDocuments)
-            .done()
-            .complex(CaseData::getCaseIssueDecision)
-            .readonly(CaseIssueDecision::getIssueDecisionDraft)
             .done()
             .complex(CaseData::getCicCase)
             .readonly(CicCase::getReinstateDocuments)
