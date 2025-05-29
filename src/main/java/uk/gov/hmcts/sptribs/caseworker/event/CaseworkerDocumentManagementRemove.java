b9f120a6b1f7a0cc2eb5253564f4c84fadc99781
oleon23 <150802367+oleon23@users.noreply.github.com>
2025-05-29 12:32:32 +0100

    [DTSSTCI-1257] Fix Decision and Final Decision Document Removal (#2068)
    * [DTSSTCI-1257] Add logging
    
    * [DTSSTCI-1257]Add issue decision and issue final decision fields to configuration of ShowCaseDocuments
    
    * [DTSSTCI-1257]Temporarily suppress CVE-2023-51775
    
    * [DTSSTCI-1257]Ammend fields ShowCaseDocuments
    
    * [DTSSTCI-1257] try rerun removal in aboutToSubmit
    
    * [DTSSTCI-1257] Temporary revert functionality, remove suppression
    
    * [DTSSTCI-1257] Temporary revert functionality
    
    * [DTSSTCI-1257] Temporary revert functionality
    
    * [DTSSTCI-1257] Revert changes to show case documents
    
    * [DTSSTCI-1257] Update remove decision and remove final decision documents
    
    * [DTSSTCI-1257] Remove unnecessary objectutils.isEmpty
    
    ---------
    
    Co-authored-by: jessieharrigan <144362161+jessieharrigan@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerDocumentManagementRemove.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerDocumentManagementRemove.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.sptribs.caseworker.event;
 
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
@@ -11,6 +12,7 @@ import uk.gov.hmcts.ccd.sdk.type.ListValue;
 import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import uk.gov.hmcts.sptribs.caseworker.event.page.ShowCaseDocuments;
 import uk.gov.hmcts.sptribs.caseworker.event.page.ShowRemovedCaseDocuments;
+import uk.gov.hmcts.sptribs.caseworker.util.DecisionDocumentListUtil;
 import uk.gov.hmcts.sptribs.caseworker.util.DocumentListUtil;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.CicCase;
@@ -92,9 +94,11 @@ public class CaseworkerDocumentManagementRemove implements CCDConfig<CaseData, S
 
     public AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmit(
         final CaseDetails<CaseData, State> details,
-        final CaseDetails<CaseData, State> beforeDetails
-    ) {
+        final CaseDetails<CaseData, State> beforeDetails) {
         var caseData = details.getData();
+        if (!ObjectUtils.isEmpty(caseData.getCicCase().getRemovedDocumentList())) {
+            removeCaseDocuments(caseData);
+        }
         List<ListValue<CaseworkerCICDocument>> listValues = new ArrayList<>();
         caseData.getCicCase().setRemovedDocumentList(listValues);
         return AboutToStartOrSubmitResponse.<CaseData, State>builder()
@@ -108,6 +112,14 @@ public class CaseworkerDocumentManagementRemove implements CCDConfig<CaseData, S
         return SubmittedCallbackResponse.builder()
             .confirmationHeader("# Case Updated")
             .build();
+    }
+
+    private void removeCaseDocuments(CaseData data) {
+        List<ListValue<CaseworkerCICDocument>> removedDocumentList = data.getCicCase().getRemovedDocumentList();
+        removedDocumentList.forEach(v -> {
+            DecisionDocumentListUtil.removeFinalDecisionDraftAndCICDocument(data, v);
+            DecisionDocumentListUtil.removeDecisionDraftAndCICDocument(data, v);
+        });
     }
 
 }
