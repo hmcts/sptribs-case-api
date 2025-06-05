a0d9339f3562044d4a0cb0e9d54d8596198292ea
Linus Norton <linusnorton@gmail.com>
2025-06-05 14:31:25 +0100

    DTSSTCI-1304 fix data loss issue (#2132)
    * DTSSTCI-1304 fix data loss issue
    
    * remove test

--- src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerEditCicaCaseDetailsTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerEditCicaCaseDetailsTest.java
@@ -7,7 +7,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.Event;
-import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
@@ -44,16 +43,19 @@ class CaseWorkerEditCicaCaseDetailsTest {
     void shouldSuccessfullySave() {
 
         //Given
-        final CaseDetails<CaseData, State> updatedCaseDetails = new CaseDetails<>();
-        final CaseDetails<CaseData, State> beforeDetails = new CaseDetails<>();
+        final CaseDetails<CaseData, State> updatedCaseDetails = CaseDetails.<CaseData, State>builder()
+            .id(12345L)
+            .data(CaseData.builder().build())
+            .state(State.CaseManagement)
+            .build();
+        final CaseDetails<CaseData, State> beforeDetails = CaseDetails.<CaseData, State>builder()
+            .id(12345L)
+            .data(CaseData.builder().build())
+            .state(State.CaseManagement)
+            .build();
 
 
         //When
-        AboutToStartOrSubmitResponse<CaseData, State> response =
-            caseWorkerEditCicaCaseDetails.aboutToSubmit(updatedCaseDetails, beforeDetails);
-        assertThat(response).isNotNull();
-
-
         SubmittedCallbackResponse draftCreatedResponse = caseWorkerEditCicaCaseDetails.submitted(updatedCaseDetails, beforeDetails);
         //Then
         assertThat(draftCreatedResponse).isNotNull();
