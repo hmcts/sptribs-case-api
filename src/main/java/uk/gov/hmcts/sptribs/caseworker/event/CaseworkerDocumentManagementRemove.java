1d73e499363d3bdb6029899099aff299b09b99df
oleon23 <oliver.leon@solirius.com>
2025-06-25 13:34:14 +0100

    [DTSSTCI-1301] undo permission change

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerDocumentManagementRemove.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerDocumentManagementRemove.java
@@ -69,10 +69,8 @@ public class CaseworkerDocumentManagementRemove implements CCDConfig<CaseData, S
                 CaseStayed)
             .name("Document management: Remove")
             .description("Document management: Remove")
-            .grant(Permission.C, ST_CIC_CASEWORKER)
-                .grant(Permission.D, ST_CIC_CASEWORKER)
             .grant(CREATE_READ_UPDATE_DELETE, SUPER_USER, ST_CIC_SENIOR_JUDGE, ST_CIC_SENIOR_CASEWORKER, ST_CIC_HEARING_CENTRE_TEAM_LEADER)
-            .grantHistoryOnly(ST_CIC_HEARING_CENTRE_ADMIN, ST_CIC_JUDGE)
+            .grantHistoryOnly(ST_CIC_CASEWORKER, ST_CIC_HEARING_CENTRE_ADMIN, ST_CIC_JUDGE)
             .aboutToStartCallback(this::aboutToStart)
             .aboutToSubmitCallback(this::aboutToSubmit)
             .submittedCallback(this::submitted));
