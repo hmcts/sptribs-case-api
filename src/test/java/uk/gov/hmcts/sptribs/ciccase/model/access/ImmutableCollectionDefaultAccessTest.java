365488535a18685ca251023bf1e67ece2bcfea8b
oleon23 <oliver.leon@solirius.com>
2025-06-25 10:35:50 +0100

    [DTSSTCI-1301] fix test

--- src/test/java/uk/gov/hmcts/sptribs/ciccase/model/access/ImmutableCollectionDefaultAccessTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/ciccase/model/access/ImmutableCollectionDefaultAccessTest.java
@@ -7,11 +7,7 @@ import uk.gov.hmcts.ccd.sdk.api.Permission;
 
 import static org.assertj.core.data.MapEntry.entry;
 import static org.assertj.guava.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-import static uk.gov.hmcts.ccd.sdk.api.Permission.C;
-import static uk.gov.hmcts.ccd.sdk.api.Permission.D;
 import static uk.gov.hmcts.ccd.sdk.api.Permission.R;
-import static uk.gov.hmcts.ccd.sdk.api.Permission.U;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.CREATOR;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_CASEWORKER;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_HEARING_CENTRE_ADMIN;
