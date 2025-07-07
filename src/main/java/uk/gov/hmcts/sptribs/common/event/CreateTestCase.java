0423603618b8af1a116b09901bc896f8f5dcfe84
oleon23 <oliver.leon@solirius.com>
2025-07-07 15:39:44 +0100

    [DTSSTCI-1322] Update test file IO

--- src/main/java/uk/gov/hmcts/sptribs/common/event/CreateTestCase.java
+++ src/main/java/uk/gov/hmcts/sptribs/common/event/CreateTestCase.java
@@ -61,7 +61,7 @@ public class CreateTestCase implements CCDConfig<CaseData, State, UserRole> {
     private static final String ENVIRONMENT_AAT = "aat";
     private static final String TEST_CREATE = "create-test-case";
     private static final String TEST_CASE_DATA_FILE = "classpath:data/st_cic_test_case.json";
-    private final Resource samplePdf = new ClassPathResource("data/sample_pdf.pdf");
+    private static final String SAMPLE_PDF_FILE = "classpath:data/sample_pdf.pdf";
 
     private final ObjectMapper objectMapper;
     private final CcdSupplementaryDataService ccdSupplementaryDataService;
@@ -123,7 +123,9 @@ public class CreateTestCase implements CCDConfig<CaseData, State, UserRole> {
 
         String authString = authorisationService.getAuthorisation();
         String serviceAuth = authTokenGenerator.generate();
-        InMemoryMultipartFile inMemoryMultipartFile = new InMemoryMultipartFile(samplePdf.getFile());
+
+        final DefaultResourceLoader resourceLoader = new DefaultResourceLoader();
+        InMemoryMultipartFile inMemoryMultipartFile = new InMemoryMultipartFile(resourceLoader.getResource(SAMPLE_PDF_FILE).getFile());
 
         DocumentUploadRequest documentUploadRequest = new DocumentUploadRequest(Classification.RESTRICTED.toString(),
                 caseType,
@@ -133,7 +135,6 @@ public class CreateTestCase implements CCDConfig<CaseData, State, UserRole> {
 
         UploadResponse uploadResponse = this.caseDocumentClientApi.uploadDocuments(user.getAuthToken(), serviceAuth, documentUploadRequest);
 
-        final DefaultResourceLoader resourceLoader = new DefaultResourceLoader();
         final String json = IOUtils.toString(
             resourceLoader.getResource(TEST_CASE_DATA_FILE).getInputStream(),
             Charset.defaultCharset()
