0e27dcf457634835adf7da078ad644eed2e900ed
oleon23 <oliver.leon@solirius.com>
2025-07-21 14:03:29 +0100

    [DTSSTCI-1246] Fix number of dmn tests

--- src/test/java/uk/gov/hmcts/sptribs/dmn/CamundaTaskTypesTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/dmn/CamundaTaskTypesTest.java
@@ -373,7 +373,7 @@ class CamundaTaskTypesTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(1));
         assertThat(logic.getOutputs().size(), is(2));
-        assertThat(logic.getRules().size(), is(64));
+        assertThat(logic.getRules().size(), is(66));
     }
 
     @ParameterizedTest(name = "retrieve all task type data")
