7d99f25516a6f9b426befbbc4bcc05412980d18b
oleon23 <oliver.leon@solirius.com>
2025-07-29 13:37:37 +0100

    [DTSSTCI-1293] fix checkstyle

--- src/test/java/uk/gov/hmcts/sptribs/services/cdam/CdamUrlDebuggerTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/services/cdam/CdamUrlDebuggerTest.java
@@ -6,7 +6,6 @@ import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.verify;
 
 class CdamUrlDebuggerTest {
 
