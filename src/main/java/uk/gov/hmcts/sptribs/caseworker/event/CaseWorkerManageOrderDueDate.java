da47d9f30983bb9305ce8568f061fabe929c4a1b
ed14537 <ellis.domengerboyce@yahoo.co.uk>
2025-07-30 08:45:52 +0100

    DTSSTCI-1352 - Remove WA feature flag (#2212)
    * Remove WA feature flag
    
    * Bumping chart version/ fixing aliases
    
    * Ignore tests to be deleted
    
    ---------
    
    Co-authored-by: EllisD-B <ellis.domengerboyce@hmcts.net>
    Co-authored-by: hmcts-jenkins-j-to-z <61242337+hmcts-jenkins-j-to-z[bot]@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerManageOrderDueDate.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerManageOrderDueDate.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.sptribs.caseworker.event;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
@@ -54,9 +53,6 @@ public class CaseWorkerManageOrderDueDate implements CCDConfig<CaseData, State, 
     @Autowired
     private OrderService orderService;
 
-    @Value("${feature.wa.enabled}")
-    private boolean isWorkAllocationEnabled;
-
     @Override
     public void configure(ConfigBuilder<CaseData, State, UserRole> configBuilder) {
         Event.EventBuilder<CaseData, UserRole, State> eventBuilder =
@@ -71,13 +67,9 @@ public class CaseWorkerManageOrderDueDate implements CCDConfig<CaseData, State, 
                 .submittedCallback(this::submitted)
                 .grant(CREATE_READ_UPDATE, SUPER_USER,
                     ST_CIC_CASEWORKER, ST_CIC_SENIOR_CASEWORKER, ST_CIC_HEARING_CENTRE_ADMIN,
-                    ST_CIC_HEARING_CENTRE_TEAM_LEADER, ST_CIC_SENIOR_JUDGE)
-                .grantHistoryOnly(ST_CIC_JUDGE);
-
-        if (isWorkAllocationEnabled) {
-            eventBuilder.publishToCamunda()
-                        .grant(CREATE_READ_UPDATE, ST_CIC_WA_CONFIG_USER);
-        }
+                    ST_CIC_HEARING_CENTRE_TEAM_LEADER, ST_CIC_SENIOR_JUDGE, ST_CIC_WA_CONFIG_USER)
+                .grantHistoryOnly(ST_CIC_JUDGE)
+                .publishToCamunda();
 
         PageBuilder pageBuilder = new PageBuilder(eventBuilder);
         manageSelectOrderTemplates.addTo(pageBuilder);
