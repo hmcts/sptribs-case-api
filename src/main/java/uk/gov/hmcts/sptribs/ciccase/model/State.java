2872919372674f38945c6c652b2b139a800ea166
pr00279 <89030768+pr00279@users.noreply.github.com>
2025-08-15 10:15:25 +0100

    DTSSTCI-1385: Accessibility - Heading structure on case events (#2242)
    DTSSTCI-1385: promote h3 to h2
    
    Co-authored-by: Phoebe Revolta <phoebe.revolta@solirius.com>

--- src/main/java/uk/gov/hmcts/sptribs/ciccase/model/State.java
+++ src/main/java/uk/gov/hmcts/sptribs/ciccase/model/State.java
@@ -18,98 +18,98 @@ public enum State {
 
     @CCD(
         label = "Application completed",
-        hint = "### ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
+        hint = "## ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
         access = {DefaultStateAccess.class, GlobalSearchAccess.class, CaseworkerRASValidationAccess.class}
     )
     Completed("Completed"),
 
     @CCD(
         label = "Application rejected",
-        hint = "### ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
+        hint = "## ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
         access = {DefaultStateAccess.class, GlobalSearchAccess.class, CaseworkerRASValidationAccess.class}
     )
     Rejected("Rejected"),
 
     @CCD(
         label = "Application Sent",
-        hint = "### ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
+        hint = "## ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
         access = {DefaultStateAccess.class, GlobalSearchAccess.class, CaseworkerRASValidationAccess.class}
     )
     Sent("Sent"),
 
     @CCD(
         label = "Application withdrawn",
-        hint = "### ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
+        hint = "## ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
         access = {DefaultStateAccess.class, GlobalSearchAccess.class, CaseworkerRASValidationAccess.class}
     )
     Withdrawn("Withdrawn"),
 
     @CCD(
         label = "Awaiting hearing",
-        hint = "### ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
+        hint = "## ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
         access = {DefaultStateAccessExcludingCAA.class, GlobalSearchAccess.class, CaseworkerRASValidationAccess.class}
     )
     AwaitingHearing("AwaitingHearing"),
 
     @CCD(
         label = "Awaiting outcome",
-        hint = "### ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
+        hint = "## ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
         access = {DefaultStateAccessExcludingCAA.class, GlobalSearchAccess.class, CaseworkerRASValidationAccess.class}
     )
     AwaitingOutcome("AwaitingOutcome"),
 
     @CCD(
         label = "Case closed",
-        hint = "### ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
+        hint = "## ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
         access = {DefaultStateAccessExcludingCAA.class, GlobalSearchAccess.class, CaseworkerRASValidationAccess.class}
     )
     CaseClosed("CaseClosed"),
 
     @CCD(
         label = "Case Concession",
-        hint = "### ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
+        hint = "## ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
         access = {DefaultStateAccessExcludingCAA.class, GlobalSearchAccess.class, CaseworkerRASValidationAccess.class}
     )
     Concession("Concession"),
 
     @CCD(
         label = "Case management",
-        hint = "### ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
+        hint = "## ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
         access = {DefaultStateAccessExcludingCAA.class, GlobalSearchAccess.class, CaseworkerRASValidationAccess.class}
     )
     CaseManagement("CaseManagement"),
 
     @CCD(
         label = "Case stayed",
-        hint = "### ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
+        hint = "## ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
         access = {DefaultStateAccessExcludingCAA.class, GlobalSearchAccess.class, CaseworkerRASValidationAccess.class}
     )
     CaseStayed("CaseStayed"),
 
     @CCD(
         label = "Case Strike Out",
-        hint = "### ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
+        hint = "## ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
         access = {DefaultStateAccessExcludingCAA.class, GlobalSearchAccess.class, CaseworkerRASValidationAccess.class}
     )
     StrikeOut("StrikeOut"),
 
     @CCD(
         label = "Consent Order",
-        hint = "### ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
+        hint = "## ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
         access = {DefaultStateAccessExcludingCAA.class, GlobalSearchAccess.class, CaseworkerRASValidationAccess.class}
     )
     ConsentOrder("ConsentOrder"),
 
     @CCD(
         label = "Death of Appellant",
-        hint = "### ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
+        hint = "## ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
         access = {DefaultStateAccessExcludingCAA.class}
     )
     DeathOfAppellant("DeathOfAppellant"),
 
     @CCD(
         label = "Draft",
-        hint = "### ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
+        hint = "## ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
         access = {DefaultStateAccessExcludingCAA.class, SolicitorAccess.class,
             GlobalSearchAccess.class, CaseworkerRASValidationAccess.class}
     )
@@ -117,35 +117,35 @@ public enum State {
 
     @CCD(
         label = "DSS-Draft",
-        hint = "### ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
+        hint = "## ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
         access = {DefaultStateAccessExcludingCAA.class, GlobalSearchAccess.class, CaseworkerRASValidationAccess.class}
     )
     DSS_Draft("DSS-Draft"),
 
     @CCD(
         label = "DSS-Expired",
-        hint = "### ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
+        hint = "## ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
         access = {DefaultStateAccessExcludingCAA.class}
     )
     DSS_Expired("DSS-Expired"),
 
     @CCD(
         label = "DSS-Submitted",
-        hint = "### ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
+        hint = "## ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
         access = {DefaultStateAccessExcludingCAA.class, GlobalSearchAccess.class, CaseworkerRASValidationAccess.class}
     )
     DSS_Submitted("DSS-Submitted"),
 
     @CCD(
         label = "New case received",
-        hint = "### ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
+        hint = "## ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
         access = {DefaultStateAccessExcludingCAA.class, GlobalSearchAccess.class, CaseworkerRASValidationAccess.class}
     )
     NewCaseReceived("NewCaseReceived"),
 
     @CCD(
         label = "Ready to list",
-        hint = "### ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
+        hint = "## ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
         access = {DefaultStateAccessExcludingCAA.class, CaseworkerWithCAAAccess.class,
             GlobalSearchAccess.class, CaseworkerRASValidationAccess.class}
     )
@@ -153,14 +153,14 @@ public enum State {
 
     @CCD(
         label = "Rule 27",
-        hint = "### ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
+        hint = "## ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
         access = {DefaultStateAccessExcludingCAA.class, GlobalSearchAccess.class, CaseworkerRASValidationAccess.class}
     )
     Rule27("Rule27"),
 
     @CCD(
         label = "Submitted",
-        hint = "### ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
+        hint = "## ${cicCaseFullName}\nCase number: ${hyphenatedCaseRef}",
         access = {DefaultStateAccessExcludingCAA.class, GlobalSearchAccess.class, CaseworkerRASValidationAccess.class}
     )
     Submitted("Submitted");
