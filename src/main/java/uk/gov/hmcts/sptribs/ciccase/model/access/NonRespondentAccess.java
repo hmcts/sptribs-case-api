4675a512790f28b52298412f988a46c74cad5d4f
ed14537 <ellis.domengerboyce@yahoo.co.uk>
2025-08-28 09:04:06 +0100

    SNI-6983 - Add new access profile for restricting resp access to case notes (#2238)
    * Add new access profile for restricting resp access to case notes
    
    * ignore access profile for sonar cube
    
    * ignore access profile for sonar cube
    
    * Add role to High Level Data Set Up
    
    * remove duplicate
    
    * improve name to show non-respondent user
    
    * Create NonRespondentAccess and give read to case notes
    
    * test non respondent access
    
    * remove non-respondent role from respondent
    
    * Omit History for Respondent Roles
    
    ---------
    
    Co-authored-by: EllisD-B <ellis.domengerboyce@hmcts.net>

---
+++ src/main/java/uk/gov/hmcts/sptribs/ciccase/model/access/NonRespondentAccess.java
@@ -0,0 +1,22 @@
+package uk.gov.hmcts.sptribs.ciccase.model.access;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.SetMultimap;
+import uk.gov.hmcts.ccd.sdk.api.HasAccessControl;
+import uk.gov.hmcts.ccd.sdk.api.HasRole;
+import uk.gov.hmcts.ccd.sdk.api.Permission;
+
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.NON_RESPONDENT_USER;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.SUPER_USER;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.SYSTEM_UPDATE;
+
+public class NonRespondentAccess implements HasAccessControl {
+    @Override
+    public SetMultimap<HasRole, Permission> getGrants() {
+        SetMultimap<HasRole, Permission> grants = HashMultimap.create();
+        grants.putAll(SUPER_USER, Permissions.CREATE_READ_UPDATE_DELETE);
+        grants.putAll(SYSTEM_UPDATE, Permissions.CREATE_READ_UPDATE_DELETE);
+        grants.putAll(NON_RESPONDENT_USER, Permissions.READ);
+        return grants;
+    }
+}
