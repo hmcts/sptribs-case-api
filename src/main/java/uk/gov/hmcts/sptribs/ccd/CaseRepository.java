0a6add65339f01f125463e8ba7e5cda26e4483ff
Alex McAusland <a.m@user.com>
2025-09-10 17:57:27 +0100

    bump tooling

--- src/main/java/uk/gov/hmcts/sptribs/ccd/CaseRepository.java
+++ src/main/java/uk/gov/hmcts/sptribs/ccd/CaseRepository.java
@@ -6,7 +6,7 @@ import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 @Component
 public class CaseRepository implements uk.gov.hmcts.ccd.sdk.CaseRepository<CaseData> {
     @Override
-    public CaseData getCase(long caseRef, CaseData data) {
+    public CaseData getCase(long caseRef, String state, CaseData data) {
         return data;
     }
 }
