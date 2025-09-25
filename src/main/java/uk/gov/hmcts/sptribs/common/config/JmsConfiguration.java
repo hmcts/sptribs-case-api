9681c7d26c7578e97534d53397a52f66e29a5708
Alex McAusland <a.m@user.com>
2025-09-25 09:52:07 +0100

    fix: restore auto-configured service bus JMS factory

--- src/main/java/uk/gov/hmcts/sptribs/common/config/JmsConfiguration.java
+++ src/main/java/uk/gov/hmcts/sptribs/common/config/JmsConfiguration.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.sptribs.common.config;
 
-import com.azure.spring.jms.ServiceBusJmsConnectionFactory;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -9,8 +8,6 @@ import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import jakarta.jms.ConnectionFactory;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,20 +19,8 @@ import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
 import uk.gov.hmcts.sptribs.common.servicebus.CcdMessageConverter;
 
-import static java.lang.System.getenv;
-
 @Configuration
-@Slf4j
 public class JmsConfiguration {
-
-    @Value("${spring.jms.servicebus.connection-string}")
-    private String connectionString;
-
-    @Value("${azure.servicebus.ccd-case-events.preview-connection-string}")
-    private String previewConnectionString;
-
-    private static final String ENDPOINT_PREFIX = "Endpoint=sb://";
-    private static final String AMQPS_PREFIX = "amqps://";
 
     @Bean
     public ObjectMapper defaultObjectMapper() {
@@ -44,17 +29,6 @@ public class JmsConfiguration {
             .registerModule(new ParameterNamesModule(JsonCreator.Mode.PROPERTIES))
             .registerModule(new JavaTimeModule()).disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
             .enable(JsonParser.Feature.STRICT_DUPLICATE_DETECTION);
-    }
-
-    @Bean
-    @ConditionalOnProperty(name = "spring.jms.servicebus.enabled")
-    @Primary
-    public ConnectionFactory connectionFactory() {
-        final String env = getenv().getOrDefault("ENVIRONMENT", "preview");
-        log.info("Current environment is {}", env);
-        String conn = env.equals("preview") ? previewConnectionString : connectionString;
-        log.info("Creating ConnectionFactory with connection string {}", conn);
-        return new ServiceBusJmsConnectionFactory(conn);
     }
 
     @Bean
