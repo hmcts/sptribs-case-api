65eeafaca5a298ad6ea703402be513c5a942ee9b
Phoebe Revolta <phoebe.revolta@solirius.com>
2025-10-22 14:27:03 +0100

    DTSSTCI-1418: remove unused import

--- src/main/java/uk/gov/hmcts/sptribs/notification/model/Correspondence.java
+++ src/main/java/uk/gov/hmcts/sptribs/notification/model/Correspondence.java
@@ -13,7 +13,6 @@ import uk.gov.hmcts.sptribs.ciccase.model.access.CaseworkerWithCAAAccess;
 import uk.gov.hmcts.sptribs.ciccase.model.access.DefaultAccess;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
 
 import static uk.gov.hmcts.ccd.sdk.type.FieldType.TextArea;
 
