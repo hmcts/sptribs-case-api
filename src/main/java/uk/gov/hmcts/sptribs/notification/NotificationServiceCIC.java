645bcce7ca1908c671fb0c7c2d99033888f2e6d9
Phoebe Revolta <phoebe.revolta@solirius.com>
2025-10-23 16:23:05 +0100

    DTSSTCI-1418: create new getPDF method to create pdf of correspondence

--- src/main/java/uk/gov/hmcts/sptribs/notification/NotificationServiceCIC.java
+++ src/main/java/uk/gov/hmcts/sptribs/notification/NotificationServiceCIC.java
@@ -2,25 +2,31 @@ package uk.gov.hmcts.sptribs.notification;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
 import uk.gov.hmcts.ccd.sdk.type.Document;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
+import uk.gov.hmcts.reform.ccd.document.am.model.Classification;
+import uk.gov.hmcts.reform.ccd.document.am.model.DocumentUploadRequest;
+import uk.gov.hmcts.reform.ccd.document.am.util.InMemoryMultipartFile;
 import uk.gov.hmcts.reform.idam.client.models.User;
-import uk.gov.hmcts.sptribs.ciccase.model.LanguagePreference;
+import uk.gov.hmcts.reform.pdf.service.client.PDFServiceClient;
+import uk.gov.hmcts.sptribs.cdam.model.UploadResponse;
 import uk.gov.hmcts.sptribs.ciccase.model.NotificationResponse;
 import uk.gov.hmcts.sptribs.ciccase.model.NotificationType;
 import uk.gov.hmcts.sptribs.common.config.EmailTemplatesConfigCIC;
 import uk.gov.hmcts.sptribs.common.repositories.CorrespondenceRepository;
-import uk.gov.hmcts.sptribs.document.CaseDataDocumentService;
 import uk.gov.hmcts.sptribs.document.DocumentClient;
 import uk.gov.hmcts.sptribs.document.SerializableDocument;
 import uk.gov.hmcts.sptribs.idam.IdamService;
 import uk.gov.hmcts.sptribs.notification.exception.NotificationException;
 import uk.gov.hmcts.sptribs.notification.model.NotificationRequest;
 import uk.gov.hmcts.sptribs.notification.persistence.CorrespondenceRecord;
+import uk.gov.hmcts.sptribs.services.cdam.CaseDocumentClientApi;
 import uk.gov.service.notify.NotificationClient;
 import uk.gov.service.notify.NotificationClientException;
 import uk.gov.service.notify.SendEmailResponse;
@@ -29,11 +35,13 @@ import uk.gov.service.notify.SendLetterResponse;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 
+import static java.util.Collections.singletonList;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static uk.gov.hmcts.sptribs.common.CommonConstants.DOC_AVAILABLE;
 import static uk.gov.hmcts.sptribs.common.config.ControllerConstants.BEARER_PREFIX;
@@ -41,8 +49,6 @@ import static uk.gov.hmcts.sptribs.common.config.ControllerConstants.BEARER_PREF
 @Service
 @Slf4j
 public class NotificationServiceCIC {
-    @Autowired
-    private final CaseDataDocumentService caseDataDocumentService;
 
     @Autowired
     private final CorrespondenceRepository correspondenceRepository;
@@ -65,13 +71,20 @@ public class NotificationServiceCIC {
     @Autowired
     private final DocumentClient caseDocumentClient;
 
+    @Autowired
+    private final CaseDocumentClientApi caseDocumentClientApi;
+
+    @Autowired
+    private final PDFServiceClient pdfServiceClient;
+
     public NotificationServiceCIC(NotificationClient notificationClient,
                                   EmailTemplatesConfigCIC emailTemplatesConfig,
                                   IdamService idamService, HttpServletRequest request,
                                   AuthTokenGenerator authTokenGenerator,
                                   DocumentClient caseDocumentClient,
-                                  CaseDataDocumentService caseDataDocumentService,
-                                  CorrespondenceRepository correspondenceRepository) {
+                                  CorrespondenceRepository correspondenceRepository,
+                                  CaseDocumentClientApi caseDocumentClientApi,
+                                  PDFServiceClient pdfServiceClient) {
 
         this.notificationClient = notificationClient;
         this.emailTemplatesConfig = emailTemplatesConfig;
@@ -79,98 +92,68 @@ public class NotificationServiceCIC {
         this.request = request;
         this.authTokenGenerator = authTokenGenerator;
         this.caseDocumentClient = caseDocumentClient;
-        this.caseDataDocumentService = caseDataDocumentService;
         this.correspondenceRepository = correspondenceRepository;
+        this.caseDocumentClientApi = caseDocumentClientApi;
+        this.pdfServiceClient = pdfServiceClient;
     }
 
-    public void saveEmailCorrespondence(String templateId,
-                                        Map<String, Object> templateVars,
-                                        String templateName,
+    public void saveEmailCorrespondence(String templateName,
                                         SendEmailResponse sendEmailResponse,
                                         String sentTo,
                                         String caseReferenceNumber) {
 
         Long longCaseRef = Long.parseLong(caseReferenceNumber.replace("-", ""));
-
         final LocalDateTime sentAt = LocalDateTime.now();
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-y-HH-mm");
-        final String formattedSentOn = sentAt.format(formatter)
-            .replace(" ", "_").replace(":", "-");
-
-        String correspondenceDocumentFilename = templateName + "_" + longCaseRef + "_" + formattedSentOn + ".pdf";
-
-        Document correspondenceDocument = caseDataDocumentService.renderDocument(templateVars, longCaseRef, templateId,
-            LanguagePreference.ENGLISH, correspondenceDocumentFilename, request);
-
-        SerializableDocument serializedCorrespondenceDocument = SerializableDocument.builder()
-            .url(correspondenceDocument.getUrl())
-            .filename(correspondenceDocument.getFilename())
-            .binaryUrl(correspondenceDocument.getBinaryUrl())
-            .categoryId(correspondenceDocument.getCategoryId())
-            .build();
-
         String sentFrom = "Criminal Injuries Compensation Tribunal";
 
         if (sendEmailResponse.getFromEmail().isPresent()) {
             sentFrom = sendEmailResponse.getFromEmail().get();
         }
 
-        CorrespondenceRecord correspondence = CorrespondenceRecord.builder()
-            .id(sendEmailResponse.getNotificationId())
-            .eventType(templateName)
-            .caseReferenceNumber(longCaseRef)
-            .sentAt(sentAt.atOffset(java.time.ZoneOffset.UTC))
-            .sentFrom(sentFrom)
-            .sentTo(sentTo)
-            .documentUrl(serializedCorrespondenceDocument)
-            .correspondenceType("Email")
-            .build();
+        try {
+            CorrespondenceRecord correspondence = CorrespondenceRecord.builder()
+                .id(sendEmailResponse.getNotificationId())
+                .eventType(templateName)
+                .caseReferenceNumber(longCaseRef)
+                .sentAt(sentAt.atOffset(java.time.ZoneOffset.UTC))
+                .sentFrom(sentFrom)
+                .sentTo(sentTo)
+                .documentUrl(this.getPDF(sendEmailResponse, null, longCaseRef, sentAt, sentFrom, sentTo, templateName))
+                .correspondenceType("Email")
+                .build();
 
-        correspondenceRepository.save(correspondence);
+            correspondenceRepository.save(correspondence);
+        } catch (java.io.IOException e) {
+            log.error("Failed to store pdf document", e);
+        }
+
     }
 
-    public void saveLetterCorrespondence(String templateId,
-                                         Map<String, Object> templateVars,
-                                         String templateName,
+    public void saveLetterCorrespondence(String templateName,
                                          SendLetterResponse sendLetterResponse,
                                          String sentTo,
                                          String caseReferenceNumber) {
 
         Long longCaseRef = Long.parseLong(caseReferenceNumber.replace("-", ""));
-
         final LocalDateTime sentAt = LocalDateTime.now();
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-y-HH-mm");
-        final String formattedSentOn = sentAt.format(formatter)
-            .replace(" ", "_").replace(":", "-");
-
-        String correspondenceDocumentFilename = templateName + "_" + longCaseRef + "_" + formattedSentOn + ".pdf";
-
-        Document correspondenceDocument = caseDataDocumentService.renderDocument(templateVars, longCaseRef, templateId,
-                LanguagePreference.ENGLISH, correspondenceDocumentFilename, request);
-
-        SerializableDocument serializedCorrespondenceDocument = SerializableDocument.builder()
-            .url(correspondenceDocument.getUrl())
-            .filename(correspondenceDocument.getFilename())
-            .binaryUrl(correspondenceDocument.getBinaryUrl())
-            .categoryId(correspondenceDocument.getCategoryId())
-            .build();
-
         String sentFrom = "Criminal Injuries Compensation Tribunal";
 
-        CorrespondenceRecord correspondence = CorrespondenceRecord.builder()
-            .id(sendLetterResponse.getNotificationId())
-            .eventType(templateName)
-            .caseReferenceNumber(longCaseRef)
-            .sentAt(sentAt.atOffset(java.time.ZoneOffset.UTC))
-            .sentFrom(sentFrom)
-            .sentTo(sentTo)
-            .documentUrl(serializedCorrespondenceDocument)
-            .correspondenceType("Letter")
-            .build();
+        try {
+            CorrespondenceRecord correspondence = CorrespondenceRecord.builder()
+                .id(sendLetterResponse.getNotificationId())
+                .eventType(templateName)
+                .caseReferenceNumber(longCaseRef)
+                .sentAt(sentAt.atOffset(java.time.ZoneOffset.UTC))
+                .sentFrom(sentFrom)
+                .sentTo(sentTo)
+                .documentUrl(this.getPDF(null, sendLetterResponse, longCaseRef, sentAt, sentFrom, sentTo, templateName))
+                .correspondenceType("Letter")
+                .build();
 
-        correspondenceRepository.save(correspondence);
+            correspondenceRepository.save(correspondence);
+        } catch (java.io.IOException e) {
+            log.error("Failed to store pdf document", e);
+        }
     }
 
     public NotificationResponse sendEmail(NotificationRequest notificationRequest, String caseReferenceNumber) {
@@ -198,8 +181,6 @@ public class NotificationServiceCIC {
                 );
 
             this.saveEmailCorrespondence(
-                templateId,
-                templateVars,
                 templateName,
                 sendEmailResponse,
                 destinationAddress,
@@ -247,8 +228,6 @@ public class NotificationServiceCIC {
                 );
 
             this.saveLetterCorrespondence(
-                templateId,
-                templateVars,
                 templateName,
                 sendLetterResponse,
                 notificationRequest.getDestinationAddress(),
@@ -349,5 +328,77 @@ public class NotificationServiceCIC {
             .createdAtTime(LocalDateTime.now())
             .status("Received")
             .build();
+    }
+
+    private SerializableDocument getPDF(SendEmailResponse sendEmailResponse,
+                                        SendLetterResponse sendLetterResponse,
+                                        Long longCaseRef,
+                                        LocalDateTime sentAt,
+                                        String sentFrom,
+                                        String sentTo,
+                                        String templateName) throws IOException {
+        Map<String, Object> placeholders = new HashMap<>();
+        String correspondenceType = "Email";
+        if (sendEmailResponse != null) {
+            placeholders.put("body", sendEmailResponse.getBody());
+            placeholders.put("subject", sendEmailResponse.getSubject());
+        } else if (sendLetterResponse != null) {
+            placeholders.put("body", sendLetterResponse.getBody());
+            placeholders.put("subject", sendLetterResponse.getSubject());
+            correspondenceType = "Letter";
+        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-y-HH-mm");
+        placeholders.put("sentOn", sentAt.format(formatter));
+        placeholders.put("from", sentFrom);
+        placeholders.put("to", sentTo);
+
+        byte[] template;
+        try {
+            template = IOUtils.toByteArray(Objects.requireNonNull(
+                getClass().getResourceAsStream("/templates/sent_notification.html")
+            ));
+        } catch (IOException e) {
+            log.info("Failed to get resource with exception {}", e.getMessage());
+            throw e;
+        }
+
+        byte[] pdf = pdfServiceClient.generateFromHtml(template, placeholders);
+
+        final String formattedSentOn = sentAt.format(formatter)
+            .replace(" ", "_").replace(":", "-");
+
+        String correspondenceDocumentFilename = templateName + "_" + longCaseRef + "_" + formattedSentOn + ".pdf";
+
+        final InMemoryMultipartFile inMemoryMultipartFile = new InMemoryMultipartFile(correspondenceDocumentFilename, pdf);
+
+        try {
+            log.info("Storing file {} of type {} into secure docstore",
+                correspondenceDocumentFilename, correspondenceType);
+
+            String serviceToken = authTokenGenerator.generate();
+            String authorizationHeader = request.getHeader(AUTHORIZATION);
+
+            DocumentUploadRequest documentUploadRequest = new DocumentUploadRequest(
+                Classification.RESTRICTED.toString(),
+                "CriminalInjuriesCompensation",
+                "ST_CIC",
+                singletonList(inMemoryMultipartFile)
+            );
+
+            UploadResponse uploadResponse = caseDocumentClientApi.uploadDocuments(authorizationHeader, serviceToken, documentUploadRequest);
+            Document uploadedPDF = new Document();
+            uploadedPDF.setBinaryUrl(uploadResponse.getDocuments().getFirst().links.binary.href);
+            uploadedPDF.setFilename(correspondenceDocumentFilename);
+            uploadedPDF.setUrl(uploadResponse.getDocuments().getFirst().links.self.href);
+
+            return SerializableDocument.builder()
+                    .url(uploadedPDF.getUrl())
+                    .filename(uploadedPDF.getFilename())
+                    .binaryUrl(uploadedPDF.getBinaryUrl())
+                    .build();
+        } catch (RestClientException e) {
+            log.error("Failed to store pdf document but carrying on [" + correspondenceDocumentFilename + "]", e);
+            return null;
+        }
     }
 }
