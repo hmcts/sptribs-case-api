4574319141f8a584b975af47c34194ed9b41f879
oleon23 <oliver.leon@solirius.com>
2025-10-28 16:41:42 +0000

    [DTSSTCI-1422] add base anonymity tests

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/page/ApplyAnonymity.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/page/ApplyAnonymity.java
@@ -1,7 +1,10 @@
 package uk.gov.hmcts.sptribs.caseworker.event.page;
 
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.CicCase;
+import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.common.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
 
@@ -9,11 +12,21 @@ public class ApplyAnonymity implements CcdPageConfiguration {
 
     @Override
     public void addTo(PageBuilder pageBuilder) {
-        pageBuilder.page("caseworkerApplyAnonymity")
+        pageBuilder.page("caseworkerApplyAnonymity", this::midEvent)
             .pageLabel("Anonymity")
             .label("LabelCaseworkerApplyAnonymity","")
             .complex(CaseData::getCicCase)
             .mandatory(CicCase::getAnonymiseYesOrNo)
             .done();
+    }
+
+    public AboutToStartOrSubmitResponse<CaseData, State> midEvent(CaseDetails<CaseData, State> caseDetails, CaseDetails<CaseData, State> caseDetails1) {
+        final CaseData caseData = caseDetails.getData();
+
+        // call to db to create anonymized name
+        caseData.getCicCase().setAnonymisedAppellantName("Anonymized Name");
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .data(caseData)
+            .build();
     }
 }
