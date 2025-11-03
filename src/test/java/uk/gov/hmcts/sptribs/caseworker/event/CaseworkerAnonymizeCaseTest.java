ec076a936fe6cc4a205f88d1f353af9041f1dafb
oleon23 <oliver.leon@solirius.com>
2025-11-03 14:51:48 +0000

    [DTSSTCI-1422] add base for CaseworkerAnonymizeCaseTest

---
+++ src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerAnonymizeCaseTest.java
@@ -0,0 +1,28 @@
+package uk.gov.hmcts.sptribs.caseworker.event;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
+import uk.gov.hmcts.sptribs.ciccase.model.State;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+public class CaseworkerAnonymizeCaseTest {
+    @InjectMocks
+    private CaseworkerAnonymizeCase caseworkerAnonymizeCase;
+
+    @Test
+    void shouldAnonymizeCaseSuccessfully() {
+        final CaseData caseData = CaseData.builder().build();
+        final CaseDetails<CaseData, State> details = new CaseDetails<>();
+        details.setData(caseData);
+
+        var response = caseworkerAnonymizeCase.aboutToSubmit(details, details);
+
+        assertThat(response).isNotNull();
+    }
+}
