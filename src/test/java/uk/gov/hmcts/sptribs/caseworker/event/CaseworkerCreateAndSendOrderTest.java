1b45385bbb73eb5093f0315691bbbeef3dddc21d
oleon23 <oliver.leon@solirius.com>
2025-11-03 14:25:48 +0000

    [DTSSTCI-1422] add base create and send order tests

--- src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerCreateAndSendOrderTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerCreateAndSendOrderTest.java
@@ -5,11 +5,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
+import uk.gov.hmcts.sptribs.caseworker.model.CreateAndSendIssueType;
+import uk.gov.hmcts.sptribs.caseworker.model.DraftOrderContentCIC;
 import uk.gov.hmcts.sptribs.caseworker.util.EventConstants;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
+import uk.gov.hmcts.sptribs.ciccase.model.CicCase;
+import uk.gov.hmcts.sptribs.ciccase.model.OrderTemplate;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 
-import static io.restassured.RestAssured.when;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
@@ -30,15 +34,54 @@ public class CaseworkerCreateAndSendOrderTest {
     }
 
     @Test
-    void shouldSuccessfullyCreateAndSendNewOrder() {
+    void shouldSuccessfullyCreateAndSendNewAnonymisedOrder() {
         // test new order creation and sending logic
         final CaseDetails<CaseData, State> details = new CaseDetails<>();
         final CaseData caseData = CaseData.builder().build();
         details.setData(caseData);
+        final CicCase cicCase = CicCase.builder()
+            .anonymiseYesOrNo(YesOrNo.YES)
+            .anonymisedAppellantName("Anonymised Name")
+            .createAndSendIssuingTypes(CreateAndSendIssueType.CREATE_AND_SEND_NEW_ORDER)
+            .build();
+        caseData.setCicCase(cicCase);
 
-//        when()
+        DraftOrderContentCIC draftOrderContentCIC = DraftOrderContentCIC.builder()
+            .orderTemplate(OrderTemplate.CIC3_RULE_27)
+            .mainContent("Sample main content")
+            .orderSignature("Test Judge")
+            .build();
+
+        caseData.setDraftOrderContentCIC(draftOrderContentCIC);
+
         final var response = caseworkerCreateAndSendOrder.aboutToSubmit(details, details);
 
         assertThat(response).isNotNull();
     }
+
+    @Test
+    void shouldSuccessfullyCreateAndSendNewNonAnonymisedOrder() {
+        // test new order creation and sending logic
+        final CaseDetails<CaseData, State> details = new CaseDetails<>();
+        final CaseData caseData = CaseData.builder().build();
+        details.setData(caseData);
+        final CicCase cicCase = CicCase.builder()
+                .anonymiseYesOrNo(YesOrNo.NO)
+                .createAndSendIssuingTypes(CreateAndSendIssueType.CREATE_AND_SEND_NEW_ORDER)
+                .build();
+        caseData.setCicCase(cicCase);
+
+        DraftOrderContentCIC draftOrderContentCIC = DraftOrderContentCIC.builder()
+                .orderTemplate(OrderTemplate.CIC3_RULE_27)
+                .mainContent("Sample main content")
+                .orderSignature("Test Judge")
+                .build();
+        caseData.setDraftOrderContentCIC(draftOrderContentCIC);
+
+        final var response = caseworkerCreateAndSendOrder.aboutToSubmit(details, details);
+
+        assertThat(response).isNotNull();
+    }
+
+
 }
