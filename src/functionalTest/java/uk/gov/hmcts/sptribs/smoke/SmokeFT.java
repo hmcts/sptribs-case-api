28f65a3cea56038303b7d8ae43fc7a7f0d031e4b
Alex McAusland <a.m@user.com>
2025-11-04 12:42:53 +0000

    avoid loading spring boot context in smoke tests when not needed

--- src/functionalTest/java/uk/gov/hmcts/sptribs/smoke/SmokeFT.java
+++ src/functionalTest/java/uk/gov/hmcts/sptribs/smoke/SmokeFT.java
@@ -2,14 +2,17 @@ package uk.gov.hmcts.sptribs.smoke;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest()
+import java.util.Optional;
+
 public class SmokeFT {
 
-    @Value("${test.url:http://localhost:4013}")
-    private String testUrl;
+    private static final String TEST_URL_PROPERTY = "test.url";
+    private static final String TEST_URL_ENV = "TEST_URL";
+    private static final String DEFAULT_TEST_URL = "http://localhost:4013";
+
+    private final String testUrl = Optional.ofNullable(System.getProperty(TEST_URL_PROPERTY))
+        .orElseGet(() -> Optional.ofNullable(System.getenv(TEST_URL_ENV)).orElse(DEFAULT_TEST_URL));
 
     @Test
     public void testHealthEndpoint() {
