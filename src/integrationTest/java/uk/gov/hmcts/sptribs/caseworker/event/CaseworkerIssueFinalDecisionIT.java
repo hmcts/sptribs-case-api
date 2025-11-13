3f563322d71df185fb89eb0d07d3ea03c66b6e35
banderous <alex.mcausland@gmail.com>
2025-11-13 09:10:37 +0000

    remove unneeded @DirtiesContext to improve test speed (#2325)
    Co-authored-by: Alex McAusland <a.m@user.com>

--- src/integrationTest/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerIssueFinalDecisionIT.java
+++ src/integrationTest/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerIssueFinalDecisionIT.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -68,7 +67,6 @@ import static uk.gov.hmcts.sptribs.testutil.TestResourceUtil.expectedResponse;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @ContextConfiguration(initializers = {IdamWireMock.PropertiesInitializer.class})
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class CaseworkerIssueFinalDecisionIT {
     private static final String CASEWORKER_ISSUE_FINAL_DECISION_MID_EVENT_RESPONSE =
         "classpath:responses/caseworker-issue-final-decision-mid-event-response.json";
--- src/integrationTest/java/uk/gov/hmcts/sptribs/citizen/event/CicDssUpdateCaseEventIT.java
+++ src/integrationTest/java/uk/gov/hmcts/sptribs/citizen/event/CicDssUpdateCaseEventIT.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -52,7 +51,6 @@ import static uk.gov.hmcts.sptribs.testutil.TestResourceUtil.expectedResponse;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @ContextConfiguration(initializers = {IdamWireMock.PropertiesInitializer.class})
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class CicDssUpdateCaseEventIT {
 
     private static final String CITIZEN_DSS_UPDATE_CASE_ABOUT_TO_SUBMIT_RESPONSE =
