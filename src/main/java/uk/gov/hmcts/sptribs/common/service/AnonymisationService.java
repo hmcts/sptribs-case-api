87a08ef52a6f394a9854cd84b1bd332f294b1def
oleon23 <oliver.leon@solirius.com>
2025-11-14 10:58:30 +0000

    [DTSSTCI-1422] update database, and use anonymisation sequence

--- src/main/java/uk/gov/hmcts/sptribs/common/service/AnonymisationService.java
+++ src/main/java/uk/gov/hmcts/sptribs/common/service/AnonymisationService.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.sptribs.common.service;
 
+import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,11 +12,16 @@ import uk.gov.hmcts.sptribs.common.repositories.AnonymisationRepository;
 public class AnonymisationService {
 
     private final AnonymisationRepository anonymisationRepository;
+    private final EntityManager entityManager;
 
     @Transactional
     public AnonymisationEntity getOrCreateAnonymisation(Long caseReference) {
-        return anonymisationRepository.findByCaseReference(caseReference)
-                .orElseGet(() -> anonymisationRepository.saveAndFlush(new AnonymisationEntity()));
+        return  anonymisationRepository.findByCaseReference(caseReference)
+            .orElseGet(() -> {
+                AnonymisationEntity entity = anonymisationRepository.saveAndFlush(new AnonymisationEntity(caseReference));
+                entityManager.refresh(entity);
+                return entity;
+            });
     }
 
     public String generateAnonymisedName(AnonymisationEntity anonymisationEntity) {
@@ -25,10 +31,14 @@ public class AnonymisationService {
     private String sequenceToString(Long seq) {
         StringBuilder stringBuilder = new StringBuilder();
         while (seq > 0) {
-            seq --;
+            seq--;
             stringBuilder.insert(0, (char) ('A' + (seq % 26)));
             seq /= 26;
         }
+        if (stringBuilder.length() == 1) {
+            stringBuilder.insert(0, 'A');
+        }
+
         return stringBuilder.toString();
     }
 }
