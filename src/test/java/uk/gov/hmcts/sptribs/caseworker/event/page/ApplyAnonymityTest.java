d6041249bbd303f108b9c4f6d5221008b245048f
oleon23 <oliver.leon@solirius.com>
2025-11-17 17:15:13 +0000

    [DTSSTCI-1422] fix checkstyle

--- src/test/java/uk/gov/hmcts/sptribs/caseworker/event/page/ApplyAnonymityTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/caseworker/event/page/ApplyAnonymityTest.java
@@ -18,7 +18,6 @@ import uk.gov.hmcts.sptribs.common.service.AnonymisationService;
 import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
