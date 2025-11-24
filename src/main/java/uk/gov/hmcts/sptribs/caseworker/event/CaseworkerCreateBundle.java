867f8beaa1b9c726f06532d7e8aff1dca152fc46
Phoebe Revolta <phoebe.revolta@solirius.com>
2025-11-24 10:59:18 +0000

    DTSSTCI-1486: display 'Bundle: Create a bundle' for 'Ready To List' state

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerCreateBundle.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerCreateBundle.java
@@ -30,8 +30,7 @@ import java.util.Optional;
 import static java.util.Collections.emptyList;
 import static uk.gov.hmcts.sptribs.caseworker.util.DocumentListUtil.getAllCaseDocumentsExcludingInitialCicaUpload;
 import static uk.gov.hmcts.sptribs.caseworker.util.EventConstants.CREATE_BUNDLE;
-import static uk.gov.hmcts.sptribs.ciccase.model.State.AwaitingHearing;
-import static uk.gov.hmcts.sptribs.ciccase.model.State.CaseManagement;
+import static uk.gov.hmcts.sptribs.ciccase.model.State.*;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_CASEWORKER;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_HEARING_CENTRE_ADMIN;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_HEARING_CENTRE_TEAM_LEADER;
@@ -55,7 +54,7 @@ public class CaseworkerCreateBundle implements CCDConfig<CaseData, State, UserRo
         Event.EventBuilder<CaseData, UserRole, State> eventBuilder =
             configBuilder
                 .event(CREATE_BUNDLE)
-                .forStates(CaseManagement, AwaitingHearing)
+                .forStates(CaseManagement, AwaitingHearing, ReadyToList)
                 .name("Bundle: Create a bundle")
                 .description("Bundle: Create a bundle")
                 .showSummary()
