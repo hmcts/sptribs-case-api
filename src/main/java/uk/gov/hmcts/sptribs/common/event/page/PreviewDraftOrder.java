2f295199b5e71d22866c362df95d616cd73c3cc4
oleon23 <oliver.leon@solirius.com>
2025-11-24 18:30:19 +0000

    [DTSSTCI-1422] add preview for uploaded docs

--- src/main/java/uk/gov/hmcts/sptribs/common/event/page/PreviewDraftOrder.java
+++ src/main/java/uk/gov/hmcts/sptribs/common/event/page/PreviewDraftOrder.java
@@ -7,12 +7,6 @@ import uk.gov.hmcts.sptribs.ciccase.model.CicCase;
 import uk.gov.hmcts.sptribs.common.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
 
-import static uk.gov.hmcts.sptribs.caseworker.util.PageShowConditionsUtil.ORDER_EVENT_CREATE_AND_SEND_NEW;
-import static uk.gov.hmcts.sptribs.caseworker.util.PageShowConditionsUtil.ORDER_EVENT_CREATE_AND_SEND_UPLOAD;
-import static uk.gov.hmcts.sptribs.caseworker.util.PageShowConditionsUtil.ORDER_EVENT_CREATE_DRAFT;
-import static uk.gov.hmcts.sptribs.caseworker.util.PageShowConditionsUtil.ORDER_EVENT_EDIT_DRAFT;
-
-
 @Slf4j
 @Component
 public class PreviewDraftOrder implements CcdPageConfiguration {
@@ -24,17 +18,12 @@ public class PreviewDraftOrder implements CcdPageConfiguration {
             .pageLabel("Preview order")
             .label("LabelPreviewOrdersDocuments", "")
             .complex(CaseData::getCicCase)
-            .readonly(CicCase::getOrderTemplateIssued, ORDER_EVENT_CREATE_DRAFT + " OR " + ORDER_EVENT_EDIT_DRAFT
-                    + " OR (" + ORDER_EVENT_CREATE_AND_SEND_NEW + ")")
-            .label("make Changes", """
-                To make changes, choose ‘Previous’ and navigate back to the Edit Order page.'
+                .readonly(CicCase::getOrderTemplateIssued)
+                .label("make Changes", """
+                    To make changes, choose ‘Previous’ and navigate back to the Edit Order page.'
 
-                If you are happy, continue to the next screen.""", ORDER_EVENT_CREATE_DRAFT + " OR " + ORDER_EVENT_EDIT_DRAFT
-                    + " OR (" + ORDER_EVENT_CREATE_AND_SEND_NEW + ")")
-            .done()
-            .complex(CaseData::getCicCase)
-            .readonly(CicCase::getOrderFile, ORDER_EVENT_CREATE_AND_SEND_UPLOAD)
-            .done();
+                    If you are happy, continue to the next screen.""")
+                .done();
     }
 }
 
