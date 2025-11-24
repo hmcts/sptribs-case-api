bdb20195fdf4e59acb3c7de6ae5a2c2226ca3079
Phoebe Revolta <phoebe.revolta@solirius.com>
2025-11-24 17:31:18 +0000

    DTSSTCI-1418: revert change (so far) to correspondencerepo jparepo extension

--- src/main/java/uk/gov/hmcts/sptribs/common/repositories/CorrespondenceRepository.java
+++ src/main/java/uk/gov/hmcts/sptribs/common/repositories/CorrespondenceRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface CorrespondenceRepository extends JpaRepository<CorrespondenceEntity, UUID> {
+public interface CorrespondenceRepository extends JpaRepository<CorrespondenceEntity, Long> {
     List<CorrespondenceEntity> findAllByCaseReferenceNumberOrderBySentOnDesc(Long caseReferenceNumber);
 }
 
