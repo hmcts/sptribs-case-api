adfbb3e3784cfc03a1667b3268c0fe7cb4e7ecd2
Phoebe Revolta <phoebe.revolta@solirius.com>
2025-11-25 11:36:13 +0000

    DTSSTCI-1418: reflect composite key in jpa

--- src/main/java/uk/gov/hmcts/sptribs/notification/persistence/CorrespondenceEntity.java
+++ src/main/java/uk/gov/hmcts/sptribs/notification/persistence/CorrespondenceEntity.java
@@ -3,31 +3,39 @@ package uk.gov.hmcts.sptribs.notification.persistence;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.time.OffsetDateTime;
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
 @Table(name = "case_correspondences")
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
+@IdClass(CorrespondenceEntityId.class)
 public class CorrespondenceEntity {
 
     @Id
     private UUID id;
 
+    @Id
+    private Long caseReferenceNumber;
+
     @Column(name = "event_type", nullable = false)
     private String eventType;
-
-    @Column(name = "case_reference_number", nullable = false)
-    private Long caseReferenceNumber;
 
     @Column(name = "sent_on", nullable = false)
     private OffsetDateTime sentOn;
@@ -49,4 +57,30 @@ public class CorrespondenceEntity {
 
     @Column(name = "document_binary_url", nullable = false)
     private String documentBinaryUrl;
+
+    @Override
+    public final boolean equals(Object correspondenceEntityObject) {
+        if (this == correspondenceEntityObject) {
+            return true;
+        }
+        if (correspondenceEntityObject == null) {
+            return false;
+        }
+        Class<?> correspondenceEntityObjectEffectiveClass =
+            correspondenceEntityObject instanceof HibernateProxy ? ((HibernateProxy) correspondenceEntityObject)
+            .getHibernateLazyInitializer().getPersistentClass() : correspondenceEntityObject.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this)
+            .getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != correspondenceEntityObjectEffectiveClass) {
+            return false;
+        }
+        CorrespondenceEntity that = (CorrespondenceEntity) correspondenceEntityObject;
+        return getId() != null && Objects.equals(getId(), that.getId())
+            && getCaseReferenceNumber() != null && Objects.equals(getCaseReferenceNumber(), that.getCaseReferenceNumber());
+    }
+
+    @Override
+    public final int hashCode() {
+        return Objects.hash(id, caseReferenceNumber);
+    }
 }
