9be7a659ee3385456cd52a4ae51bb277f7dfad30
oleon23 <150802367+oleon23@users.noreply.github.com>
2025-11-26 17:57:53 +0000

    [DTSSTCI-1465] Prepare to shutter service for feature release (#2319)
    * [DTSSTCI-1465] Prepare to shutter service for feature release
    
    * [DTSSTCI-1465] fix checkstyle spacing
    
    * Shutter for all except super users
    
    ---------
    
    Co-authored-by: EllisD-B <ellis.domengerboyce@hmcts.net>
    Co-authored-by: ed14537 <ellis.domengerboyce@yahoo.co.uk>

--- src/main/java/uk/gov/hmcts/sptribs/ciccase/CriminalInjuriesCompensation.java
+++ src/main/java/uk/gov/hmcts/sptribs/ciccase/CriminalInjuriesCompensation.java
@@ -14,6 +14,26 @@ import uk.gov.hmcts.sptribs.common.ccd.CcdServiceCode;
 
 import java.util.List;
 
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.AC_CASE_FLAGS_ADMIN;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.AC_CASE_FLAGS_VIEWER;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.CASEWORKER;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.CITIZEN;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.CREATOR;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.DISTRICT_JUDGE_CIC;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.GS_PROFILE;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.NON_RESPONDENT_USER;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.RAS_CASEWORKER_VALIDATION;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.RESPONDENT_CIC;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_CASEWORKER;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_HEARING_CENTRE_ADMIN;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_HEARING_CENTRE_TEAM_LEADER;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_JUDGE;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_RESPONDENT;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_SENIOR_CASEWORKER;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_SENIOR_JUDGE;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_WA_CONFIG_USER;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.SYSTEM_UPDATE;
+
 @Component
 @Slf4j
 public class CriminalInjuriesCompensation implements CCDConfig<CriminalInjuriesCompensationData, State, UserRole> {
@@ -33,14 +53,17 @@ public class CriminalInjuriesCompensation implements CCDConfig<CriminalInjuriesC
         configBuilder.jurisdiction(CcdJurisdiction.CRIMINAL_INJURIES_COMPENSATION.getJurisdictionId(),
             CcdJurisdiction.CRIMINAL_INJURIES_COMPENSATION.getJurisdictionName(), CcdServiceCode.ST_CIC.getCcdServiceDescription());
 
-        configBuilder.omitHistoryForRoles(UserRole.ST_CIC_RESPONDENT, UserRole.RESPONDENT_CIC);
+        configBuilder.omitHistoryForRoles(ST_CIC_RESPONDENT, RESPONDENT_CIC);
 
         ConfigBuilderHelper.configure(configBuilder, cfgs);
 
         ConfigBuilderHelper.configureWithTestEvent(configBuilder);
 
         // to shutter the service within xui uncomment this line
-        // configBuilder.shutterService();
+        configBuilder.shutterService(SYSTEM_UPDATE, CASEWORKER, CREATOR, DISTRICT_JUDGE_CIC, RESPONDENT_CIC, ST_CIC_CASEWORKER,
+            ST_CIC_SENIOR_CASEWORKER, ST_CIC_HEARING_CENTRE_ADMIN, ST_CIC_HEARING_CENTRE_TEAM_LEADER, ST_CIC_SENIOR_JUDGE, ST_CIC_JUDGE,
+            ST_CIC_RESPONDENT, AC_CASE_FLAGS_ADMIN, CITIZEN, ST_CIC_WA_CONFIG_USER, NON_RESPONDENT_USER, AC_CASE_FLAGS_VIEWER,
+            RAS_CASEWORKER_VALIDATION, GS_PROFILE);
         log.info("Building definition for " + System.getenv().getOrDefault("ENVIRONMENT", ""));
     }
 }
