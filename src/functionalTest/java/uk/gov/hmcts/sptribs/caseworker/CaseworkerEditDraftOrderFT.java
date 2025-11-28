4eaaaed4d9267c76d8a44bb0378930f36f51c2f5
oleon23 <oliver.leon@solirius.com>
2025-11-28 08:42:15 +0000

    [DTSSTCI-1422] Update edit draft, create draft and send order FT

--- src/functionalTest/java/uk/gov/hmcts/sptribs/caseworker/CaseworkerEditDraftOrderFT.java
+++ src/functionalTest/java/uk/gov/hmcts/sptribs/caseworker/CaseworkerEditDraftOrderFT.java
@@ -13,8 +13,10 @@ import static net.javacrumbs.jsonunit.core.Option.IGNORING_ARRAY_ORDER;
 import static net.javacrumbs.jsonunit.core.Option.IGNORING_EXTRA_FIELDS;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.HttpStatus.OK;
+import static uk.gov.hmcts.sptribs.caseworker.util.EventConstants.CASEWORKER_CREATE_DRAFT_ORDER;
 import static uk.gov.hmcts.sptribs.caseworker.util.EventConstants.CASEWORKER_EDIT_DRAFT_ORDER;
 import static uk.gov.hmcts.sptribs.testutil.CaseDataUtil.caseData;
+import static uk.gov.hmcts.sptribs.testutil.TestConstants.ABOUT_TO_START_URL;
 import static uk.gov.hmcts.sptribs.testutil.TestConstants.ABOUT_TO_SUBMIT_URL;
 import static uk.gov.hmcts.sptribs.testutil.TestConstants.EDIT_DRAFT_ORDER_MID_EVENT_URL;
 import static uk.gov.hmcts.sptribs.testutil.TestConstants.SUBMITTED_URL;
@@ -27,10 +29,11 @@ public class CaseworkerEditDraftOrderFT extends FunctionalTestSuite {
         "classpath:request/casedata/ccd-callback-casedata-caseworker-edit-draft-order-mid-event.json";
     private static final String CALLBACK_REQUEST =
         "classpath:request/casedata/ccd-callback-casedata-caseworker-edit-draft-order-about-to-submit.json";
-
+    private static final String GENERAL_REQUEST =
+        "classpath:request/casedata/ccd-callback-casedata-general.json";
     private static final String RESPONSE_MID_EVENT = "classpath:responses/response-caseworker-edit-draft-order-mid-event.json";
     private static final String RESPONSE_ABOUT_TO_SUBMIT = "classpath:responses/response-caseworker-edit-draft-order-about-to-submit.json";
-
+    private static final String RESPONSE_ABOUT_TO_START = "classpath:responses/response-caseworker-edit-draft-order-about-to-start.json";
     private static final String CONFIRMATION_HEADER = "$.confirmation_header";
 
     @Test
@@ -46,6 +49,16 @@ public class CaseworkerEditDraftOrderFT extends FunctionalTestSuite {
             .isEqualTo(json(expectedResponse(RESPONSE_MID_EVENT)));
     }
 
+    @Test
+    public void shouldSetCurrentEventInAboutToStartCallback() throws Exception {
+        final Map<String, Object> caseData = caseData(GENERAL_REQUEST);
+        final Response response = triggerCallback(caseData, CASEWORKER_EDIT_DRAFT_ORDER, ABOUT_TO_START_URL);
+
+        assertThat(response.getStatusCode()).isEqualTo(OK.value());
+        assertThatJson(response.asString())
+                .when(IGNORING_EXTRA_FIELDS)
+                .isEqualTo(json(expectedResponse(RESPONSE_ABOUT_TO_START)));
+    }
     @Test
     public void shouldSuccessFullyInitialiseDraftOrderListWhenAboutToSubmitCallbackIsInvoked() throws Exception {
         final Map<String, Object> caseData = caseData(CALLBACK_REQUEST);
