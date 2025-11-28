db3b475a22ac3be53059eeaee55878a7b59be0fa
oleon23 <oliver.leon@solirius.com>
2025-11-28 08:59:48 +0000

    [DTSSTCI-1422] fix FT checkstyle

--- src/functionalTest/java/uk/gov/hmcts/sptribs/caseworker/CaseworkerEditDraftOrderFT.java
+++ src/functionalTest/java/uk/gov/hmcts/sptribs/caseworker/CaseworkerEditDraftOrderFT.java
@@ -13,7 +13,6 @@ import static net.javacrumbs.jsonunit.core.Option.IGNORING_ARRAY_ORDER;
 import static net.javacrumbs.jsonunit.core.Option.IGNORING_EXTRA_FIELDS;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.HttpStatus.OK;
-import static uk.gov.hmcts.sptribs.caseworker.util.EventConstants.CASEWORKER_CREATE_DRAFT_ORDER;
 import static uk.gov.hmcts.sptribs.caseworker.util.EventConstants.CASEWORKER_EDIT_DRAFT_ORDER;
 import static uk.gov.hmcts.sptribs.testutil.CaseDataUtil.caseData;
 import static uk.gov.hmcts.sptribs.testutil.TestConstants.ABOUT_TO_START_URL;
@@ -59,6 +58,7 @@ public class CaseworkerEditDraftOrderFT extends FunctionalTestSuite {
                 .when(IGNORING_EXTRA_FIELDS)
                 .isEqualTo(json(expectedResponse(RESPONSE_ABOUT_TO_START)));
     }
+
     @Test
     public void shouldSuccessFullyInitialiseDraftOrderListWhenAboutToSubmitCallbackIsInvoked() throws Exception {
         final Map<String, Object> caseData = caseData(CALLBACK_REQUEST);
