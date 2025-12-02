453f1aab36db0ef79000e0920ab9fc4a02fd632c
Phoebe Revolta <phoebe.revolta@solirius.com>
2025-12-02 12:25:09 +0000

    DTSSTCI-1368: remove unused import

--- src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerCreateBundleTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerCreateBundleTest.java
@@ -29,7 +29,6 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
