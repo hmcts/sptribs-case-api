f7634e90a0c51ec4fccd8cd6d7f2d60b1a95ef4d
EllisD-B <ellis.domengerboyce@hmcts.net>
2025-12-08 11:48:44 +0000

    Fix issue with SearchCriteria

--- src/main/java/uk/gov/hmcts/sptribs/ciccase/model/access/GlobalSearchAccess.java
+++ src/main/java/uk/gov/hmcts/sptribs/ciccase/model/access/GlobalSearchAccess.java
@@ -6,7 +6,22 @@ import uk.gov.hmcts.ccd.sdk.api.HasAccessControl;
 import uk.gov.hmcts.ccd.sdk.api.HasRole;
 import uk.gov.hmcts.ccd.sdk.api.Permission;
 
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.AC_CASE_FLAGS_ADMIN;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.CITIZEN;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.CREATOR;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.DISTRICT_JUDGE_CIC;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.GS_PROFILE;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.RESPONDENT_CIC;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_CASEWORKER;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_HEARING_CENTRE_ADMIN;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_JUDGE;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_RESPONDENT;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_SENIOR_CASEWORKER;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_SENIOR_JUDGE;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_WA_CONFIG_USER;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.SUPER_USER;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.SYSTEM_UPDATE;
+import static uk.gov.hmcts.sptribs.ciccase.model.access.Permissions.DELETE;
 import static uk.gov.hmcts.sptribs.ciccase.model.access.Permissions.READ;
 
 public class GlobalSearchAccess implements HasAccessControl {
@@ -15,6 +30,21 @@ public class GlobalSearchAccess implements HasAccessControl {
     public SetMultimap<HasRole, Permission> getGrants() {
         SetMultimap<HasRole, Permission> grants = HashMultimap.create();
         grants.putAll(GS_PROFILE, READ);
+        grants.putAll(SUPER_USER, DELETE);
+        grants.putAll(SYSTEM_UPDATE, DELETE);
+        grants.putAll(CREATOR, DELETE);
+        grants.putAll(DISTRICT_JUDGE_CIC, DELETE);
+        grants.putAll(RESPONDENT_CIC, DELETE);
+        grants.putAll(ST_CIC_CASEWORKER, DELETE);
+        grants.putAll(ST_CIC_SENIOR_CASEWORKER, DELETE);
+        grants.putAll(ST_CIC_HEARING_CENTRE_ADMIN, DELETE);
+        grants.putAll(ST_CIC_SENIOR_JUDGE, DELETE);
+        grants.putAll(ST_CIC_JUDGE, DELETE);
+        grants.putAll(ST_CIC_RESPONDENT, DELETE);
+        grants.putAll(AC_CASE_FLAGS_ADMIN, DELETE);
+        grants.putAll(CITIZEN, DELETE);
+        grants.putAll(ST_CIC_WA_CONFIG_USER, DELETE);
+
 
         return grants;
     }
--- src/test/java/uk/gov/hmcts/sptribs/ciccase/model/access/GlobalSearchAccessTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/ciccase/model/access/GlobalSearchAccessTest.java
@@ -7,8 +7,23 @@ import uk.gov.hmcts.ccd.sdk.api.Permission;
 
 import static org.assertj.core.data.MapEntry.entry;
 import static org.assertj.guava.api.Assertions.assertThat;
+import static uk.gov.hmcts.ccd.sdk.api.Permission.D;
 import static uk.gov.hmcts.ccd.sdk.api.Permission.R;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.AC_CASE_FLAGS_ADMIN;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.CITIZEN;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.CREATOR;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.DISTRICT_JUDGE_CIC;
 import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.GS_PROFILE;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.RESPONDENT_CIC;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_CASEWORKER;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_HEARING_CENTRE_ADMIN;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_JUDGE;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_RESPONDENT;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_SENIOR_CASEWORKER;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_SENIOR_JUDGE;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.ST_CIC_WA_CONFIG_USER;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.SUPER_USER;
+import static uk.gov.hmcts.sptribs.ciccase.model.UserRole.SYSTEM_UPDATE;
 
 class GlobalSearchAccessTest {
 
@@ -17,9 +32,23 @@ class GlobalSearchAccessTest {
         final SetMultimap<HasRole, Permission> grants = new GlobalSearchAccess().getGrants();
 
         assertThat(grants)
-            .hasSize(1)
+            .hasSize(15)
             .contains(
-                entry(GS_PROFILE, R)
+                entry(GS_PROFILE, R),
+                entry(SUPER_USER, D),
+                entry(SYSTEM_UPDATE, D),
+                entry(CREATOR, D),
+                entry(DISTRICT_JUDGE_CIC, D),
+                entry(RESPONDENT_CIC, D),
+                entry(ST_CIC_CASEWORKER, D),
+                entry(ST_CIC_SENIOR_CASEWORKER, D),
+                entry(ST_CIC_HEARING_CENTRE_ADMIN, D),
+                entry(ST_CIC_SENIOR_JUDGE, D),
+                entry(ST_CIC_JUDGE, D),
+                entry(ST_CIC_RESPONDENT, D),
+                entry(AC_CASE_FLAGS_ADMIN, D),
+                entry(CITIZEN, D),
+                entry(ST_CIC_WA_CONFIG_USER, D)
             );
     }
 }
