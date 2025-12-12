72806cc57a2db8fd9411d846a776503a7149e631
oleon23 <oliver.leon@solirius.com>
2025-12-12 21:35:49 +0000

    [DTSSTCI-1492] wip demonstrate broken pageshowcondition injection

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/page/SendOrderAddDraftOrder.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/page/SendOrderAddDraftOrder.java
@@ -6,15 +6,23 @@ import uk.gov.hmcts.sptribs.ciccase.model.CicCase;
 import uk.gov.hmcts.sptribs.common.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
 
+import java.util.Map;
+
 
 public class SendOrderAddDraftOrder implements CcdPageConfiguration {
+
+    private final  Map<String, String> pageShowCondition;
+
+    public SendOrderAddDraftOrder(Map<String, String> pageShowCondition) {
+        this.pageShowCondition = pageShowCondition;
+    }
 
     @Override
     public void addTo(PageBuilder pageBuilder) {
         pageBuilder.page("caseworkerSendOrderSelectDraftOrder")
             .pageLabel("Select order")
             .label("LabelSelectOrder", "")
-            .pageShowConditions(PageShowConditionsUtil.createAndSendOrderConditions())
+            .pageShowConditions(this.pageShowCondition)
             .complex(CaseData::getCicCase)
             .mandatory(CicCase::getDraftOrderDynamicList)
             .done();
--- src/main/java/uk/gov/hmcts/sptribs/common/event/page/CreateNewOrder.java
+++ src/main/java/uk/gov/hmcts/sptribs/common/event/page/CreateNewOrder.java
@@ -1,10 +1,10 @@
 package uk.gov.hmcts.sptribs.common.event.page;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.sptribs.caseworker.model.DraftOrderContentCIC;
 import uk.gov.hmcts.sptribs.caseworker.util.EventUtil;
+import uk.gov.hmcts.sptribs.caseworker.util.PageShowConditionsUtil;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.CicCase;
 import uk.gov.hmcts.sptribs.ciccase.model.OrderTemplate;
@@ -12,37 +12,28 @@ import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.common.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
 
-import java.util.Map;
-
 import static org.apache.commons.lang3.StringUtils.isEmpty;
 
-public class CreateDraftOrder implements CcdPageConfiguration {
+public class CreateNewOrder implements CcdPageConfiguration {
 
     private static final String NEVER_SHOW = "orderContentOrderTemplate=\"NEVER_SHOW\"";
 
-    private Map<String, String> pageShowCondition;
-
-    @Autowired
-    public CreateDraftOrder(Map<String, String> pageShowCondition) {
-        this.pageShowCondition = pageShowCondition;
-    }
-
     @Override
     public void addTo(PageBuilder pageBuilder) {
-        pageBuilder.page("createDraftOrder", this::midEvent)
-            .pageLabel("Create order")
-            .pageShowConditions(this.pageShowCondition)
-            .label("LabelCreateDraftOrder", "")
-            .label("createDraftOrder", "Draft to be created")
-            .complex(CaseData::getDraftOrderContentCIC)
-                .mandatory(DraftOrderContentCIC::getOrderTemplate)
-                .done()
-            .readonly(CaseData::getCurrentEvent, NEVER_SHOW)
-            .complex(CaseData::getCicCase)
-                .readonly(CicCase::getReferralTypeForWA, NEVER_SHOW)
-                .readonly(CicCase::getCreateAndSendIssuingTypes, NEVER_SHOW)
-                .done()
-            .done();
+        pageBuilder
+                .page("createDraftOrder", this::midEvent)
+                .pageLabel("Create order")
+                .pageShowConditions(PageShowConditionsUtil.createAndSendOrderConditionsNew())
+                .label("LabelCreateDraftOrder", "")
+                .label("createDraftOrder", "Draft to be created")
+                .complex(CaseData::getDraftOrderContentCIC)
+                    .mandatory(DraftOrderContentCIC::getOrderTemplate)
+                    .done()
+                .readonly(CaseData::getCurrentEvent)
+                .complex(CaseData::getCicCase)
+                    .readonly(CicCase::getReferralTypeForWA, NEVER_SHOW)
+                    .done()
+                .done();
     }
 
     public AboutToStartOrSubmitResponse<CaseData, State> midEvent(CaseDetails<CaseData, State> details,
@@ -57,9 +48,8 @@ public class CreateDraftOrder implements CcdPageConfiguration {
         }
 
         return AboutToStartOrSubmitResponse.<CaseData, State>builder()
-            .data(caseData)
-            .build();
+                .data(caseData)
+                .build();
     }
-
 
 }
