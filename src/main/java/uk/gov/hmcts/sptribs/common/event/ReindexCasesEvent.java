47b462bca0dccd3d47cbfaf90ca966dc08558a04
Alex McAusland <a.m@user.com>
2025-12-15 21:56:31 +0000

    add test

--- src/main/java/uk/gov/hmcts/sptribs/common/event/ReindexCasesEvent.java
+++ src/main/java/uk/gov/hmcts/sptribs/common/event/ReindexCasesEvent.java
@@ -2,12 +2,12 @@ package uk.gov.hmcts.sptribs.common.event;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import uk.gov.hmcts.ccd.sdk.CaseReindexingService;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
-import uk.gov.hmcts.ccd.sdk.CaseReindexingService;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
