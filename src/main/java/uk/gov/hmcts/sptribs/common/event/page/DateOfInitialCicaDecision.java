822bd1f59cbd3dc57dc769c0a3096e679f58540a
P Revolta <phoebe.revolta@solirius.com>
2026-01-12 14:29:11 +0000

    DTSSTCI-1416: amend page label to 'When was the CICA initial review decision letter received?'

--- src/main/java/uk/gov/hmcts/sptribs/common/event/page/DateOfInitialCicaDecision.java
+++ src/main/java/uk/gov/hmcts/sptribs/common/event/page/DateOfInitialCicaDecision.java
@@ -10,7 +10,7 @@ public class DateOfInitialCicaDecision implements CcdPageConfiguration {
     public void addTo(PageBuilder pageBuilder) {
         pageBuilder
             .page("initialDecisionDateObjects")
-            .pageLabel("Date of CICA initial review decision letter")
+            .pageLabel("When was the CICA initial review decision letter received?")
             .complex(CaseData::getCicCase)
             .mandatoryWithLabel(CicCase::getInitialCicaDecisionDate, "Date of CICA initial review decision letter")
             .done();
