1e9370cf377e9ac4093386a6fcf5238253965ac8
oleon23 <oliver.leon@solirius.com>
2026-02-19 10:26:07 +0000

    [DTSSTCI-1509] fix checkstyle test

--- src/test/java/uk/gov/hmcts/sptribs/bankholidays/service/BankHolidayServiceTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/bankholidays/service/BankHolidayServiceTest.java
@@ -18,7 +18,6 @@ import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.times;
