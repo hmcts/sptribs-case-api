56b696635e1765fa4b7e5620add9e736a81642d7
P Revolta <phoebe.revolta@solirius.com>
2026-02-26 09:39:57 +0000

    DTSSTCI-1330: add test for aboutToStart method

--- src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerRemoveBundlesTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerRemoveBundlesTest.java
@@ -165,4 +165,90 @@ class CaseworkerRemoveBundlesTest {
         assertThat(response.getData().getCicCase().getRemoveBundlesList().getValue()).isNull();
     }
 
+    @Test
+    void shouldSuccessfullyPrepareBundleLabelsOnAboutToStart() {
+        //Given
+        String bundleUUID1 = UUID.randomUUID().toString();
+        String bundleUUID2 = UUID.randomUUID().toString();
+        String bundleUUID3 = UUID.randomUUID().toString();
+
+        final LocalDateTime bundleTimestamp1 = LocalDateTime.now();
+        final LocalDateTime bundleTimestamp2 = LocalDateTime.now().minusDays(1);
+        final LocalDateTime bundleTimestamp3 = LocalDateTime.now().minusDays(2);
+
+        List<ListValue<Bundle>> existingBundles = new ArrayList<>();
+        existingBundles.add(ListValue.<Bundle>builder()
+            .id("1")
+            .value(Bundle.builder()
+                .id(bundleUUID1)
+                .dateAndTime(bundleTimestamp1)
+                .fileName("1-cicBundle.pdf")
+                .build())
+            .build());
+        existingBundles.add(ListValue.<Bundle>builder()
+            .id("2")
+            .value(Bundle.builder()
+                .id(bundleUUID2)
+                .dateAndTime(bundleTimestamp2)
+                .fileName("2-cicBundle.pdf")
+                .build())
+            .build());
+        existingBundles.add(ListValue.<Bundle>builder()
+            .id("3")
+            .value(Bundle.builder()
+                .id(bundleUUID3)
+                .dateAndTime(bundleTimestamp3)
+                .fileName("3-cicBundle.pdf")
+                .build())
+            .build());
+
+        final CaseData caseData = caseData();
+
+        caseData.setCaseBundles(existingBundles);
+
+        List<ListValue<BundleIdAndTimestamp>> bundleIdAndTimestamps = new ArrayList<>();
+        bundleIdAndTimestamps.add(ListValue.<BundleIdAndTimestamp>builder()
+            .id("1")
+            .value(BundleIdAndTimestamp.builder()
+                .bundleId(bundleUUID1)
+                .dateAndTime(bundleTimestamp1)
+                .build())
+            .build());
+        bundleIdAndTimestamps.add(ListValue.<BundleIdAndTimestamp>builder()
+            .id("2")
+            .value(BundleIdAndTimestamp.builder()
+                .bundleId(bundleUUID2)
+                .dateAndTime(bundleTimestamp2)
+                .build())
+            .build());
+        bundleIdAndTimestamps.add(ListValue.<BundleIdAndTimestamp>builder()
+            .id("3")
+            .value(BundleIdAndTimestamp.builder()
+                .bundleId(bundleUUID3)
+                .dateAndTime(bundleTimestamp3)
+                .build())
+            .build());
+
+        caseData.setCaseBundleIdsAndTimestamps(bundleIdAndTimestamps);
+
+        final CaseDetails<CaseData, State> updatedCaseDetails = new CaseDetails<>();
+        updatedCaseDetails.setData(caseData);
+        updatedCaseDetails.setId(TEST_CASE_ID);
+        updatedCaseDetails.setCreatedDate(LOCAL_DATE_TIME);
+
+        //When
+        AboutToStartOrSubmitResponse<CaseData, State> response =
+            caseworkerRemoveBundles.aboutToStart(updatedCaseDetails);
+
+        //Then
+        assertThat(response.getData().getCicCase().getRemoveBundlesList().getListItems().size()).isEqualTo(3);
+        assertThat(response.getData().getCicCase().getRemoveBundlesList().getListItems().getFirst().getLabel())
+            .isEqualTo(bundleTimestamp1 + " -- 1-cicBundle.pdf");
+        assertThat(response.getData().getCicCase().getRemoveBundlesList().getListItems().get(1).getLabel())
+            .isEqualTo(bundleTimestamp2 + " -- 2-cicBundle.pdf");
+        assertThat(response.getData().getCicCase().getRemoveBundlesList().getListItems().get(2).getLabel())
+            .isEqualTo(bundleTimestamp3 + " -- 3-cicBundle.pdf");
+        assertThat(response.getData().getCicCase().getRemoveBundlesList().getValue()).isNull();
+    }
+
 }
