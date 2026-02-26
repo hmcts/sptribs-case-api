6be87fcd714d4f48e984795e1623fd40b517aeb7
P Revolta <phoebe.revolta@solirius.com>
2026-02-26 10:09:49 +0000

    DTSSTCI-1330: refactor repeated test code

--- src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerRemoveBundlesTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerRemoveBundlesTest.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.sptribs.caseworker.event;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -11,6 +12,7 @@ import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.ccd.sdk.type.DynamicListElement;
 import uk.gov.hmcts.ccd.sdk.type.DynamicMultiSelectList;
 import uk.gov.hmcts.ccd.sdk.type.ListValue;
+import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
@@ -36,23 +38,18 @@ class CaseworkerRemoveBundlesTest {
     @InjectMocks
     private CaseworkerRemoveBundles caseworkerRemoveBundles;
 
-    @Test
-    void shouldAddConfigurationToConfigBuilder() {
-        //Given
-        final ConfigBuilderImpl<CaseData, State, UserRole> configBuilder = createCaseDataConfigBuilder();
+    private CaseData caseData;
 
-        //When
-        caseworkerRemoveBundles.configure(configBuilder);
+    private CaseDetails<CaseData, State> updatedCaseDetails;
 
-        //Then
-        assertThat(getEventsFrom(configBuilder).values())
-            .extracting(Event::getId)
-            .contains(REMOVE_BUNDLES);
-    }
+    @BeforeEach
+    void setUp() {
+        caseData = caseData();
+        updatedCaseDetails = new CaseDetails<>();
 
-    @Test
-    void shouldSuccessfullyDeleteBundlesOnAboutToSubmit() {
-        //Given
+        updatedCaseDetails.setId(TEST_CASE_ID);
+        updatedCaseDetails.setCreatedDate(LOCAL_DATE_TIME);
+
         String bundleUUID1 = UUID.randomUUID().toString();
         String bundleUUID2 = UUID.randomUUID().toString();
         String bundleUUID3 = UUID.randomUUID().toString();
@@ -87,8 +84,6 @@ class CaseworkerRemoveBundlesTest {
                 .build())
             .build());
 
-        final CaseData caseData = caseData();
-
         caseData.setCaseBundles(existingBundles);
 
         List<ListValue<BundleIdAndTimestamp>> bundleIdAndTimestamps = new ArrayList<>();
@@ -115,6 +110,27 @@ class CaseworkerRemoveBundlesTest {
             .build());
 
         caseData.setCaseBundleIdsAndTimestamps(bundleIdAndTimestamps);
+
+        updatedCaseDetails.setData(caseData);
+    }
+
+    @Test
+    void shouldAddConfigurationToConfigBuilder() {
+        //Given
+        final ConfigBuilderImpl<CaseData, State, UserRole> configBuilder = createCaseDataConfigBuilder();
+
+        //When
+        caseworkerRemoveBundles.configure(configBuilder);
+
+        //Then
+        assertThat(getEventsFrom(configBuilder).values())
+            .extracting(Event::getId)
+            .contains(REMOVE_BUNDLES);
+    }
+
+    @Test
+    void shouldSuccessfullyDeleteBundlesOnAboutToSubmit() {
+        //Given
         UUID dynamicListElementCode1 = UUID.randomUUID();
         UUID dynamicListElementCode2 = UUID.randomUUID();
         UUID dynamicListElementCode3 = UUID.randomUUID();
@@ -122,26 +138,26 @@ class CaseworkerRemoveBundlesTest {
         List<DynamicListElement> removeBundleLabels = List.of(
             DynamicListElement.builder()
                 .code(dynamicListElementCode1)
-                .label(bundleTimestamp1 + " -- 1-cicBundle.pdf")
+                .label(caseData.getCaseBundles().getFirst().getValue().getDateAndTime() + " -- 1-cicBundle.pdf")
                 .build(),
             DynamicListElement.builder()
                 .code(dynamicListElementCode2)
-                .label(bundleTimestamp2 + " -- 2-cicBundle.pdf")
+                .label(caseData.getCaseBundles().get(1).getValue().getDateAndTime() + " -- 2-cicBundle.pdf")
                 .build(),
             DynamicListElement.builder()
                 .code(dynamicListElementCode3)
-                .label(bundleTimestamp3 + " -- 3-cicBundle.pdf")
+                .label(caseData.getCaseBundles().get(2).getValue().getDateAndTime() + " -- 3-cicBundle.pdf")
                 .build()
         );
 
         List<DynamicListElement> selectedRemoveBundleLabels = List.of(
             DynamicListElement.builder()
                 .code(dynamicListElementCode1)
-                .label(bundleTimestamp1 + " -- 1-cicBundle.pdf")
+                .label(caseData.getCaseBundles().getFirst().getValue().getDateAndTime() + " -- 1-cicBundle.pdf")
                 .build(),
             DynamicListElement.builder()
                 .code(dynamicListElementCode2)
-                .label(bundleTimestamp2 + " -- 2-cicBundle.pdf")
+                .label(caseData.getCaseBundles().get(1).getValue().getDateAndTime() + " -- 2-cicBundle.pdf")
                 .build()
         );
 
@@ -167,75 +183,6 @@ class CaseworkerRemoveBundlesTest {
 
     @Test
     void shouldSuccessfullyPrepareBundleLabelsOnAboutToStart() {
-        //Given
-        String bundleUUID1 = UUID.randomUUID().toString();
-        String bundleUUID2 = UUID.randomUUID().toString();
-        String bundleUUID3 = UUID.randomUUID().toString();
-
-        final LocalDateTime bundleTimestamp1 = LocalDateTime.now();
-        final LocalDateTime bundleTimestamp2 = LocalDateTime.now().minusDays(1);
-        final LocalDateTime bundleTimestamp3 = LocalDateTime.now().minusDays(2);
-
-        List<ListValue<Bundle>> existingBundles = new ArrayList<>();
-        existingBundles.add(ListValue.<Bundle>builder()
-            .id("1")
-            .value(Bundle.builder()
-                .id(bundleUUID1)
-                .dateAndTime(bundleTimestamp1)
-                .fileName("1-cicBundle.pdf")
-                .build())
-            .build());
-        existingBundles.add(ListValue.<Bundle>builder()
-            .id("2")
-            .value(Bundle.builder()
-                .id(bundleUUID2)
-                .dateAndTime(bundleTimestamp2)
-                .fileName("2-cicBundle.pdf")
-                .build())
-            .build());
-        existingBundles.add(ListValue.<Bundle>builder()
-            .id("3")
-            .value(Bundle.builder()
-                .id(bundleUUID3)
-                .dateAndTime(bundleTimestamp3)
-                .fileName("3-cicBundle.pdf")
-                .build())
-            .build());
-
-        final CaseData caseData = caseData();
-
-        caseData.setCaseBundles(existingBundles);
-
-        List<ListValue<BundleIdAndTimestamp>> bundleIdAndTimestamps = new ArrayList<>();
-        bundleIdAndTimestamps.add(ListValue.<BundleIdAndTimestamp>builder()
-            .id("1")
-            .value(BundleIdAndTimestamp.builder()
-                .bundleId(bundleUUID1)
-                .dateAndTime(bundleTimestamp1)
-                .build())
-            .build());
-        bundleIdAndTimestamps.add(ListValue.<BundleIdAndTimestamp>builder()
-            .id("2")
-            .value(BundleIdAndTimestamp.builder()
-                .bundleId(bundleUUID2)
-                .dateAndTime(bundleTimestamp2)
-                .build())
-            .build());
-        bundleIdAndTimestamps.add(ListValue.<BundleIdAndTimestamp>builder()
-            .id("3")
-            .value(BundleIdAndTimestamp.builder()
-                .bundleId(bundleUUID3)
-                .dateAndTime(bundleTimestamp3)
-                .build())
-            .build());
-
-        caseData.setCaseBundleIdsAndTimestamps(bundleIdAndTimestamps);
-
-        final CaseDetails<CaseData, State> updatedCaseDetails = new CaseDetails<>();
-        updatedCaseDetails.setData(caseData);
-        updatedCaseDetails.setId(TEST_CASE_ID);
-        updatedCaseDetails.setCreatedDate(LOCAL_DATE_TIME);
-
         //When
         AboutToStartOrSubmitResponse<CaseData, State> response =
             caseworkerRemoveBundles.aboutToStart(updatedCaseDetails);
@@ -244,12 +191,20 @@ class CaseworkerRemoveBundlesTest {
         assertThat(response.getData().getCicCase().getRemoveBundlesList().getListItems())
             .hasSize(3);
         assertThat(response.getData().getCicCase().getRemoveBundlesList().getListItems().getFirst().getLabel())
-            .isEqualTo(bundleTimestamp1 + " -- 1-cicBundle.pdf");
+            .isEqualTo(caseData.getCaseBundles().getFirst().getValue().getDateAndTime() + " -- 1-cicBundle.pdf");
         assertThat(response.getData().getCicCase().getRemoveBundlesList().getListItems().get(1).getLabel())
-            .isEqualTo(bundleTimestamp2 + " -- 2-cicBundle.pdf");
+            .isEqualTo(caseData.getCaseBundles().get(1).getValue().getDateAndTime() + " -- 2-cicBundle.pdf");
         assertThat(response.getData().getCicCase().getRemoveBundlesList().getListItems().get(2).getLabel())
-            .isEqualTo(bundleTimestamp3 + " -- 3-cicBundle.pdf");
+            .isEqualTo(caseData.getCaseBundles().get(2).getValue().getDateAndTime() + " -- 3-cicBundle.pdf");
         assertThat(response.getData().getCicCase().getRemoveBundlesList().getValue()).isNull();
+    }
+
+    @Test
+    void shouldSuccessfullySubmit() {
+        final CaseDetails<CaseData, State> beforeDetails = new CaseDetails<>();
+
+        SubmittedCallbackResponse response = caseworkerRemoveBundles.submitted(updatedCaseDetails, beforeDetails);
+        assertThat(response.getConfirmationHeader()).isEqualTo("# Case Updated");
     }
 
 }
