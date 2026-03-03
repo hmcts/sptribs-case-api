a5e6e05b5d1f8714e40c95d00b30d8c483bc4392
scottbdavey1993 <scott.davey@solirius.com>
2026-03-03 15:52:41 +0000

    review coments and sonar

--- src/main/java/uk/gov/hmcts/sptribs/systemupdate/event/SystemCleanDeletedDocumentsCase.java
+++ src/main/java/uk/gov/hmcts/sptribs/systemupdate/event/SystemCleanDeletedDocumentsCase.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
-import uk.gov.hmcts.ccd.sdk.api.Event;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.ccd.sdk.type.ListValue;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
