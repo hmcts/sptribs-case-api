4e864462361d59b4f3f5b53c8390ab5961739965
oleon23 <oliver.leon@solirius.com>
2026-03-19 09:41:24 +0000

    [DTSSTCI-1685] fix checkstyle test

--- src/test/java/uk/gov/hmcts/sptribs/systemupdate/event/SystemRestoreOrdersTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/systemupdate/event/SystemRestoreOrdersTest.java
@@ -25,7 +25,6 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
