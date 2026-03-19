dbe87130a03fe173cf6202e2cba23ff0b4c2cc0d
Oliver Leon <150802367+oleon23@users.noreply.github.com>
2026-03-19 14:43:42 +0000

    [DTSSTCI-1685] Add event to restore order documents (#2483)
    * adding task
    
    * [DTSSTCI-1685] add case event repository to query events and restore ordersList
    
    * [DTSSTCI-1685] update method to restore orders from previous event history
    
    * [DTSSTCI-1685] fix checkstyle test
    
    * [DTSSTCI-1685] fix sonar issues
    
    * [DTSSTCI-1685] fix order restore and add tests
    
    * [DTSSTCI-1685] add further tests
    
    * [DTSSTCI-1685] update users and dates for event after testing
    
    * [DTSSTCI-1685] update end date verified with when PR 2474 was merged
    
    * [DTSSTCI-1685] update tests
    
    * [DTSSTCI-1685] fix sonar issues
    
    * [DTSSTCI-1685] fix checkstyle test
    
    ---------
    
    Co-authored-by: scottbdavey1993 <scott.davey@solirius.com>

---
+++ src/main/java/uk/gov/hmcts/sptribs/common/dto/RemoveEventWithPrecedingData.java
@@ -0,0 +1,20 @@
+package uk.gov.hmcts.sptribs.common.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class RemoveEventWithPrecedingData {
+    private Long caseDataId;
+    private Long reference;
+    private String currentEvent;
+    private LocalDateTime currentEventDate;
+    private CaseData currentEventData;
+    private String precedingEventId;
+    private LocalDateTime precedingEventDate;
+    private CaseData precedingEventData;
+}
