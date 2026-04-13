0c43b11797d8d51061626645019c18dc5883c45d
Ben Middlehurst <ben.middlehurst@solirius.com>
2026-04-13 10:14:42 +0100

    removed disabled tag to check FT

--- src/functionalTest/java/uk/gov/hmcts/sptribs/citizen/event/CicCreateCaseEventFT.java
+++ src/functionalTest/java/uk/gov/hmcts/sptribs/citizen/event/CicCreateCaseEventFT.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.sptribs.citizen.event;
 
 import io.restassured.response.Response;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import uk.gov.hmcts.sptribs.testutil.FunctionalTestSuite;
