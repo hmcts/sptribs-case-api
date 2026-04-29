3481347b9a78950d2c8b876a1e5f2d55d06beab0
oleon23 <oliver.leon@solirius.com>
2026-04-29 15:30:27 +0100

    [SNI-8773] update YesOrNo to YesNo

--- src/test/java/uk/gov/hmcts/sptribs/systemupdate/event/SystemMigrateInitialCaseDocumentsTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/systemupdate/event/SystemMigrateInitialCaseDocumentsTest.java
@@ -10,7 +10,7 @@ import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.Event;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.ccd.sdk.type.ListValue;
-import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
+import uk.gov.hmcts.sptribs.caseworker.model.YesNo;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
@@ -79,7 +79,7 @@ class SystemMigrateInitialCaseDocumentsTest {
 
         CaseData data = response.getData();
         assertThat(data).isEqualTo(caseData);
-        assertThat(data.getNewBundleOrderEnabled()).isEqualTo(YesOrNo.YES);
+        assertThat(data.getNewBundleOrderEnabled()).isEqualTo(YesNo.YES);
     }
 
     @Test
