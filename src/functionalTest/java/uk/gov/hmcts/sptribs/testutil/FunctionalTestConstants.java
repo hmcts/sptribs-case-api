86740cf2b858152b94b2ee22327d7db462813aaf
scottbdavey1993 <scott.davey@solirius.com>
2026-05-18 13:12:59 +0100

    Dtsstci 1704 ft clear down test final (#2514)
    * WIP clear down functional tests
    
    * simplified FT clear down
    
    * refactor
    
    * added @value and changed to component
    
    * updated values appl-funct.yaml
    
    * fixing FT
    
    * added build local-env file and added to git ignore
    
    * trying to solve rerun - keep helm - FT failures
    
    * working on FT fix
    
    * added connection log info
    
    * added debug logger
    
    * added preview db config to jenkins file
    
    * added preview db config to jenkins file
    
    * jenkins update
    
    * FT
    
    * FT
    
    * ft
    
    * trying to fix FT
    
    * resolved comments
    
    * testing removing loadEnv on pipeline
    
    * removed commented out build code
    
    * updating gradle
    
    * updating readme
    
    * fix az command
    
    * fix build
    
    * overidding new values in stiching service to allow our domain
    
    * remove redundant object
    
    ---------
    
    Co-authored-by: Ben Middlehurst <ben.middlehurst@solirius.com>

---
+++ src/functionalTest/java/uk/gov/hmcts/sptribs/testutil/FunctionalTestConstants.java
@@ -0,0 +1,22 @@
+package uk.gov.hmcts.sptribs.testutil;
+
+public final class FunctionalTestConstants {
+
+    public static final String TABLE_CASE_DATA = "ccd.case_data";
+
+    public static final String TABLE_CASE_EVENT = "ccd.case_event";
+
+    public static final String TABLE_CASE_CORRESPONDENCES = "public.case_correspondences";
+
+    public static final String KEY_CASE_DATA_REFERENCE = "reference";
+
+    public static final String KEY_CASE_EVENT_REFERENCE = "case_data_id";
+
+    public static final String KEY_CASE_CORRESPONDENCES_REFERENCE = "case_reference_number";
+
+    public static final String KEY_CASE_DATA_ID = "id";
+
+    private FunctionalTestConstants() {
+
+    }
+}
