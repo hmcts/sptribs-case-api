c063961ed463cb76c0649ba56e1ce898c630e67f
P Revolta <phoebe.revolta@solirius.com>
2026-05-27 11:59:47 +0100

    DTSSTCI-1704: remove unused imports

--- src/functionalTest/java/uk/gov/hmcts/sptribs/caseworker/CaseworkerDocumentManagementAmendFT.java
+++ src/functionalTest/java/uk/gov/hmcts/sptribs/caseworker/CaseworkerDocumentManagementAmendFT.java
@@ -2,9 +2,7 @@ package uk.gov.hmcts.sptribs.caseworker;
 
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import uk.gov.hmcts.sptribs.testutil.FunctionalTestDataManager;
 import uk.gov.hmcts.sptribs.testutil.FunctionalTestSuite;
 
 import java.util.Map;
