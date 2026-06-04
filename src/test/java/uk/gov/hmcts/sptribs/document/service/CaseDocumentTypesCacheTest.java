b7a154cd1f2bed26ab23db5d82c1629d916180ac
scottbdavey1993 <scott.davey@solirius.com>
2026-06-04 19:19:56 +0100

    cache teat

---
+++ src/test/java/uk/gov/hmcts/sptribs/document/service/CaseDocumentTypesCacheTest.java
@@ -0,0 +1,86 @@
+package uk.gov.hmcts.sptribs.document.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+import uk.gov.hmcts.sptribs.common.repositories.CaseDocumentTypesRepository;
+import uk.gov.hmcts.sptribs.document.model.CaseDocumentType;
+import uk.gov.hmcts.sptribs.document.model.CaseDocumentTypesEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class CaseDocumentTypesCacheTest {
+
+    @Mock
+    private CaseDocumentTypesRepository repository;
+
+    @InjectMocks
+    private CaseDocumentTypesCache caseDocumentTypesCache;
+
+    @Test
+    void shouldLoadAndReturnDocumentTypeIds() {
+        //given
+        long id = 1L;
+        List<CaseDocumentTypesEntity> entities = new ArrayList<>();
+
+        for (CaseDocumentType type : CaseDocumentType.values()) {
+            entities.add(buildDocumentTypeEntity(id++, type));
+        }
+
+        when(repository.findAll()).thenReturn(entities);
+        caseDocumentTypesCache.reload();
+
+        // when
+        Long documentTypeId = caseDocumentTypesCache.getId(CaseDocumentType.values()[0]);
+
+        // then
+        assertThat(documentTypeId).isEqualTo(1L);
+        assertThat(caseDocumentTypesCache.getId(CaseDocumentType.values()[0]))
+            .isEqualTo(1L);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenDocumentTypeMissing() {
+        //given
+        CaseDocumentType firstType = CaseDocumentType.values()[0];
+        CaseDocumentTypesEntity entity = new CaseDocumentTypesEntity();
+        entity.setId(1L);
+        entity.setCode(firstType.name());
+
+        //when
+        when(repository.findAll()).thenReturn(List.of(entity));
+
+        //then
+        assertThatThrownBy(() -> caseDocumentTypesCache.reload())
+            .isInstanceOf(IllegalStateException.class)
+            .hasMessageContaining("Missing document type in database");
+    }
+
+    @Test
+    void shouldThrowExceptionWhenDocumentTypeIsUnknown() {
+        ReflectionTestUtils.setField(caseDocumentTypesCache, "caseDocumentTypeIdMap", Map.of());
+
+        CaseDocumentType type = CaseDocumentType.values()[0];
+
+        assertThatThrownBy(() -> caseDocumentTypesCache.getId(type))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("Unknown document type code: " + type);
+    }
+
+    private CaseDocumentTypesEntity buildDocumentTypeEntity(long id, CaseDocumentType type) {
+        CaseDocumentTypesEntity entity = new CaseDocumentTypesEntity();
+        entity.setId(id);
+        entity.setCode(type.name());
+        return entity;
+    }
+}
