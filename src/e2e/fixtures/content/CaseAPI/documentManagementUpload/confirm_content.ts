1170111d61cbdb91cc5842989c567bbd6e14090a
Will Orchard <will.orchard@solirius.com>
2026-06-09 16:04:37 +0100

    Add document upload fixtures

---
+++ src/e2e/fixtures/content/CaseAPI/documentManagementUpload/confirm_content.ts
@@ -0,0 +1,7 @@
+const confirm_content = {
+  pageHint: "Document management: Upload",
+  caseReference: "Case number: ",
+  pageTitle: "Case Updated",
+};
+
+export default confirm_content;
---
+++ src/e2e/fixtures/content/CaseAPI/documentManagementUpload/submit_content.ts
@@ -0,0 +1,14 @@
+const submit_content = {
+  pageTitle: "Document management: Upload",
+  caseReference: "Case number: ",
+  subTitle1: "Check your answers",
+  textOnPage1: "Check the information below carefully.",
+  textOnPage2: "Documents",
+  textOnPage3: "Document Category",
+  textOnPage4: "Description",
+  textOnPage5: "File",
+  category: "TG - Other",
+  message: "Lorem ipsum text TG - Other",
+};
+
+export default submit_content;
