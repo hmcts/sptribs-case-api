fd48fc9be9f65d24d5e0eb80684ba6208d8bbaf6
P Revolta <phoebe.revolta@solirius.com>
2026-06-15 11:56:34 +0100

    DTSSTCI-1771: revert timestamp used for updatedAt in documentsRepository

--- src/main/java/uk/gov/hmcts/sptribs/common/repositories/DocumentsRepository.java
+++ src/main/java/uk/gov/hmcts/sptribs/common/repositories/DocumentsRepository.java
@@ -27,7 +27,7 @@ public interface DocumentsRepository extends JpaRepository<DocumentEntity, Integ
         update DocumentEntity d
         set d.categoryId = :categoryId,
             d.documentTypeId = :documentTypeId,
-            d.updatedAt = current_timestamp
+            d.updatedAt = current_instant()
         where d.documentBinaryUrl = :documentBinaryUrl
         """)
     int setCategoryIdAndDocumentTypeIdByDocumentBinaryUrl(
@@ -37,7 +37,7 @@ public interface DocumentsRepository extends JpaRepository<DocumentEntity, Integ
     );
 
     @Modifying
-    @Query("update DocumentEntity d set d.isDraft = false, d.updatedAt = current_timestamp where d.documentBinaryUrl = :documentBinaryUrl")
+    @Query("update DocumentEntity d set d.isDraft = false, d.updatedAt = current_instant() where d.documentBinaryUrl = :documentBinaryUrl")
     void setIsDraftToFalseByDocumentBinaryUrl(@Param("documentBinaryUrl") String documentBinaryUrl);
 
     @Query("""
