946af8f14da0ab61dc25a9cf99758ef807d43c7c
scottbdavey1993 <scott.davey@solirius.com>
2026-06-16 11:05:05 +0100

    add logs to test bundle

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerBundleStitchComplete.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerBundleStitchComplete.java
@@ -4,11 +4,17 @@ import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.ccd.sdk.type.ListValue;
+import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
 import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
+import uk.gov.hmcts.sptribs.document.bundling.model.Bundle;
+
+import java.util.List;
 
 import static uk.gov.hmcts.sptribs.caseworker.util.EventConstants.ASYNC_STITCH_COMPLETE;
 import static uk.gov.hmcts.sptribs.ciccase.model.State.AwaitingHearing;
@@ -40,11 +46,35 @@ public class CaseworkerBundleStitchComplete implements CCDConfig<CaseData, State
             .description("Bundle: Async Stitching Comp")
             .showCondition(ALWAYS_HIDE)
             .showSummary()
+            .submittedCallback(this::submitted)
             .grant(CREATE_READ_UPDATE, SUPER_USER, ST_CIC_CASEWORKER, ST_CIC_SENIOR_CASEWORKER,
                 ST_CIC_HEARING_CENTRE_ADMIN, ST_CIC_HEARING_CENTRE_TEAM_LEADER)
             .grantHistoryOnly(ST_CIC_SENIOR_JUDGE, ST_CIC_JUDGE))
             .page("createBundle")
             .pageLabel("Create a bundle")
             .done();
+    }
+
+    public SubmittedCallbackResponse submitted(CaseDetails<CaseData, State> details,
+                                               CaseDetails<CaseData, State> beforeDetails) {
+
+        log.info("Submitting bundle stitch adding doc to db start");
+
+        CaseData caseData = details.getData();
+
+        List<ListValue<Bundle>> caseBundles = caseData.getCaseBundles();
+
+        for (ListValue<Bundle> caseBundle : caseBundles) {
+
+            log.info("Bundle info name = {} with binary url {}",
+                caseBundle.getValue().getStitchedDocument().getFilename(), caseBundle.getValue().getStitchedDocument().getBinaryUrl());
+
+
+        }
+
+        log.info("Submitting bundle stitch add doc to db complete");
+
+
+
     }
 }
