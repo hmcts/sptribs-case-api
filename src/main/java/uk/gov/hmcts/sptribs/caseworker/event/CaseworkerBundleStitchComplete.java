3f00761c1bdb1f2931e8987eed897afe8892d0d4
Ben Middlehurst <ben.middlehurst@solirius.com>
2026-06-22 12:40:23 +0100

    pulling in latest changes

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerBundleStitchComplete.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerBundleStitchComplete.java
@@ -2,20 +2,13 @@ package uk.gov.hmcts.sptribs.caseworker.event;
 
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
-import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
-import uk.gov.hmcts.ccd.sdk.type.Document;
-import uk.gov.hmcts.ccd.sdk.type.ListValue;
-import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import uk.gov.hmcts.sptribs.ciccase.model.CaseData;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
 import uk.gov.hmcts.sptribs.common.ccd.PageBuilder;
-import uk.gov.hmcts.sptribs.document.bundling.model.Bundle;
-import uk.gov.hmcts.sptribs.document.service.DocumentsService;
 
 import static uk.gov.hmcts.sptribs.caseworker.util.EventConstants.ASYNC_STITCH_COMPLETE;
 import static uk.gov.hmcts.sptribs.ciccase.model.State.AwaitingHearing;
@@ -38,9 +31,6 @@ public class CaseworkerBundleStitchComplete implements CCDConfig<CaseData, State
 
     private static final String ALWAYS_HIDE = "[STATE]=\"ALWAYS_HIDE\"";
 
-    @Autowired
-    private DocumentsService documentsService;
-
     @Override
     public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
         new PageBuilder(configBuilder
@@ -50,44 +40,11 @@ public class CaseworkerBundleStitchComplete implements CCDConfig<CaseData, State
             .description("Bundle: Async Stitching Comp")
             .showCondition(ALWAYS_HIDE)
             .showSummary()
-            .submittedCallback(this::submitted)
             .grant(CREATE_READ_UPDATE, SUPER_USER, ST_CIC_CASEWORKER, ST_CIC_SENIOR_CASEWORKER,
                 ST_CIC_HEARING_CENTRE_ADMIN, ST_CIC_HEARING_CENTRE_TEAM_LEADER)
             .grantHistoryOnly(ST_CIC_SENIOR_JUDGE, ST_CIC_JUDGE))
             .page("createBundle")
             .pageLabel("Create a bundle")
             .done();
-    }
-
-    public SubmittedCallbackResponse submitted(CaseDetails<CaseData, State> details,
-                                               CaseDetails<CaseData, State> beforeDetails) {
-
-        Long caseId = details.getId();
-
-        log.info("Starting insert for latest case bundle for caseId = {}", caseId);
-
-        CaseData caseData = details.getData();
-
-        ListValue<Bundle> latestCaseBundle = caseData.getCaseBundles().getFirst();
-        Document stitchedDocument = latestCaseBundle.getValue().getStitchedDocument();
-
-        if (stitchedDocument != null) {
-            log.info("Inserting latest bundle document for caseId = {}", caseId);
-
-            documentsService.buildAndSaveNewDocumentEntity(
-                stitchedDocument,
-                caseId,
-                false,
-                true
-            );
-
-            log.info("Successfully inserted latest case bundle document for caseId = {}", caseId);
-        } else {
-            log.info("No stitched document found for caseId = {}, skipping insert", caseId);
-        }
-
-        return SubmittedCallbackResponse.builder()
-            .confirmationHeader("# documents added successfully")
-            .build();
     }
 }
