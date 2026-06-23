1b203ada875f97ad29ff620dfcc85be36856e0ac
scottbdavey1993 <scott.davey@solirius.com>
2026-06-23 13:49:10 +0100

    refactoring coments

--- src/main/java/uk/gov/hmcts/sptribs/document/model/CaseDocumentTypesEntity.java
+++ src/main/java/uk/gov/hmcts/sptribs/document/model/CaseDocumentTypesEntity.java
@@ -4,21 +4,16 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "case_document_types")
 @Getter
-@Setter
-@ToString
-@RequiredArgsConstructor
 @AllArgsConstructor
-@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CaseDocumentTypesEntity {
 
     @Id
