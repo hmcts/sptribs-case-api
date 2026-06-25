cbc09fe33a50416f54d3f61b21be188bc16e770e
Mihai Tranca <mihai.tranca@justice.gov.uk>
2026-06-25 11:23:41 +0000

    fix to satisfy checkstyle

--- src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerCreateAndSendOrderTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerCreateAndSendOrderTest.java
@@ -30,8 +30,8 @@ import uk.gov.hmcts.sptribs.ciccase.model.PartiesCIC;
 import uk.gov.hmcts.sptribs.ciccase.model.State;
 import uk.gov.hmcts.sptribs.ciccase.model.UserRole;
 import uk.gov.hmcts.sptribs.document.model.CICDocument;
-import uk.gov.hmcts.sptribs.notification.dispatcher.NewOrderIssuedNotification;
 import uk.gov.hmcts.sptribs.notification.dispatcher.AnonymityAppliedNotification;
+import uk.gov.hmcts.sptribs.notification.dispatcher.NewOrderIssuedNotification;
 import uk.gov.hmcts.sptribs.notification.exception.NotificationException;
 
 import java.time.LocalDate;
