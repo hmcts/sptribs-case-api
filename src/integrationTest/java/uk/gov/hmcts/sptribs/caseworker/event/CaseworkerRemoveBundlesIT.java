62c3004cab5d88869faa1f53998b234dafcca3bc
Ben Middlehurst <ben.middlehurst@solirius.com>
2026-06-26 15:52:24 +0100

    test fix

--- src/integrationTest/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerRemoveBundlesIT.java
+++ src/integrationTest/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerRemoveBundlesIT.java
@@ -318,7 +318,7 @@ public class CaseworkerRemoveBundlesIT extends IntegrationTestBase {
 
         assertThat(removeBundlesLabels).hasSize(3);
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
 
         assertThat(removeBundlesLabels.getFirst().getLabel())
             .isEqualTo(bundleTimestamp1.format(formatter) + " -- " + "1-cicBundle.pdf");
