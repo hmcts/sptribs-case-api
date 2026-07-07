9353509ad03d63b820ba42f4fdff7c6108214ce1
Mihai Tranca <mihai.tranca@justice.gov.uk>
2026-07-07 12:45:15 +0000

    import rather than in place import for Autowired

--- src/functionalTest/java/uk/gov/hmcts/sptribs/testutil/FunctionalTestDataManager.java
+++ src/functionalTest/java/uk/gov/hmcts/sptribs/testutil/FunctionalTestDataManager.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.sptribs.testutil;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
@@ -51,7 +52,7 @@ public class FunctionalTestDataManager {
     @Value("${core_case_data.api.url}")
     private String ccdDataStoreUrl;
 
-    @org.springframework.beans.factory.annotation.Autowired
+    @Autowired
     private ServiceAuthenticationGenerator serviceAuthenticationGenerator;
 
     public void connectToDB() {
