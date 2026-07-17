23f20dc385abd3d8d39ddf52879c6e1b81470ab5
Ben Middlehurst <ben.middlehurst@solirius.com>
2026-07-17 08:43:49 +0100

    preview test

--- src/main/java/uk/gov/hmcts/sptribs/systemupdate/SystemUpdateController.java
+++ src/main/java/uk/gov/hmcts/sptribs/systemupdate/SystemUpdateController.java
@@ -5,13 +5,12 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.sptribs.systemupdate.schedule.migration.SystemMigrateCaseDocumentsDocumentTableTask;
 
 @Profile("preview")
 @RestController
-@RequestMapping("/migration")
+//@RequestMapping("/migration")
 @RequiredArgsConstructor
 public class SystemUpdateController {
 
