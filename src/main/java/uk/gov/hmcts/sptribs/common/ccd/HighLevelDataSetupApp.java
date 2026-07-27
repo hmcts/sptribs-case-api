97c8a692b9a46b46af5377ff966766d97bcbf84c
P Revolta <89030768+pr00279@users.noreply.github.com>
2026-07-27 09:25:50 +0100

    DTSSTCI-1784: AAT functional tests point to the actual environment not the new code.  (#2590)
    * DTSSTCI-1784: mock event to test aat env in jenkins pipeline
    
    * DTSSTCI-1784: add unit test for sonar coverage
    
    * DTSSTCI-1784: add show condition & restrict grants
    
    * DTSSTCI-1784: remove unused imports
    
    * DTSSTCI-1784: revert blank event
    
    * DTSSTCI-1784: amend CASE_API_URL in onMaster section of jenkinsfile_CNP
    
    * revert 'staging' to 'aat' in Jenkinsfile_CNP
    
    * DTSSTCI-1784: revert staging to aat
    
    * DTSSTCI-1784: amend CASE_API_URL to be staging address rather than aat internal
    
    * DTSSTCI-1784: upgrade gradle & owasp dependencycheck
    
    * DTSSTCI-1784: set yarnEnabled to false to try and fix security error
    
    * DTSSTCI-1784: use sonar instead of sonarqube
    
    * DTSSTCI-1784: upgrade sonarqube
    
    * DTSSTCI-1784: revert attempted fixes
    
    * DTSSTCI-1784: revert nodeAudit in gradle.build
    
    * DTSSTCI-1784: REVERT changes to CASE_API_URL in Jenkinsfile_CNP
    
    * DTSSTCI-1784: REVERT again
    
    * DTSSTCI-1784: upload ccd config after assigning staging env and after fts pass
    
    * DTSSTCI-1784: rollback
    
    * DTSSTCI-1784: readd case api url into onMaster
    
    * DTSSTCI-1784: use different CCD_DEF_NAME for staging & aat config files
    
    * DTSSTCI-1784: add echos to log when the url changes and when the files are generated
    
    * DTSSTCI-1784: fix indentation
    
    * DTSSTCI-1784: remove staging suffix & keep as aat suffix
    
    * DTSSTCI-1784: remove --rerun-tasks arg
    
    * DTSSTCI-1784: revert entire jenkinsfile_cnp
    
    * DTSSTCI-1784: remove existing aat file to rebuild with aat address rather than staging address
    
    * DTSSTCI-1784: create script to delete ccd config file
    
    * DTSSTCI-1784: only write to aat xlsx if ccd def name not assigned to prod already
    
    * DTSSTCI-1784: remove config from highlevedatasetup as this reads from DEF_NAME
    
    * DTSSTCI-1784: rollback changes
    
    * DTSSTCI-1784: rollback changes
    
    * DTSSTCI-1784: create manual highleveldatasetup pipeline event
    
    * DTSSTCI-1784: rollback
    
    * DTSSTCI-1784: generate blank aat and staging files for pipeline to populate
    
    * DTSSTCI-1784: include definition file for staging in highleveldatasetup
    
    * DTSSTCI-1784: remove attempted change to highleveldatasetup before statement
    
    * DTSSTCI-1784: revert
    
    * DTSSTCI-1784: revert changes
    
    * DTSSTCI-1784: abstract definition file generation outside of aat high level data setup
    
    * DTSSTCI-1784: remove builder from methods
    
    * DTSSTCI-1784: add echo for staging URL indication
    
    * DTSSTCI-1784: include adding staging xlsx to highleveldatasetup app
    
    * DTSSTCI-1784: rollback
    
    * DTSSTCI-1784: only generate definition files where node context is established
    
    * DTSSTCI-1784: fix whitespace
    
    * DTSSTCI-1784: include aat data setup method
    
    * DTSSTCI-1784: include change to definition file generation
    
    * DTSSTCO-1784: revert
    
    * DTSSTCI-1784: add TEST_DATA_LOAD_SKIP_PERIOD=0 so aat ccd def can be regenerated once FTs pass
    
    * DTSSTCI-1784: re-add adding of staging file in highleveldatasetuppapp
    
    ---------
    
    Co-authored-by: P Revolta <phoebe.revolta@solirius.com>

--- src/main/java/uk/gov/hmcts/sptribs/common/ccd/HighLevelDataSetupApp.java
+++ src/main/java/uk/gov/hmcts/sptribs/common/ccd/HighLevelDataSetupApp.java
@@ -7,6 +7,7 @@ import uk.gov.hmcts.befta.dse.ccd.CcdRoleConfig;
 import uk.gov.hmcts.befta.dse.ccd.DataLoaderToDefinitionStore;
 import uk.gov.hmcts.befta.exception.ImportException;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -79,8 +80,16 @@ public class HighLevelDataSetupApp extends DataLoaderToDefinitionStore {
     @Override
     protected List<String> getAllDefinitionFilesToLoadAt(String definitionsPath) {
         String environmentName = environment.name().toLowerCase(Locale.UK);
-        return List.of(
-            "build/ccd-config/ccd-" + CcdServiceCode.ST_CIC.getCaseType().getCaseTypeName() + "-" + environmentName + ".xlsx"
+
+        List<String> files = new ArrayList<>(
+            List.of("build/ccd-config/ccd-" + CcdServiceCode.ST_CIC.getCaseType().getCaseTypeName() + "-" + environmentName + ".xlsx")
         );
+
+        if ("aat".equals(environmentName)) {
+            files.add("build/ccd-config/ccd-" + CcdServiceCode.ST_CIC.getCaseType().getCaseTypeName()
+                + "-staging-" + environmentName + ".xlsx");
+        }
+
+        return files;
     }
 }
