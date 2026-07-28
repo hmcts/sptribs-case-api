eb00474a2439c05129feea13abe67f5689cb1d9d
Mihai Tranca <mihai.tranca@justice.gov.uk>
2026-07-28 14:44:51 +0000

    initial attempt

--- src/test/java/uk/gov/hmcts/sptribs/dmn/CamundaTaskWaConfigurationTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/dmn/CamundaTaskWaConfigurationTest.java
@@ -46,6 +46,7 @@ import static uk.gov.hmcts.sptribs.dmnutils.CamundaTaskConstants.LOCATION;
 import static uk.gov.hmcts.sptribs.dmnutils.CamundaTaskConstants.LOCATION_NAME;
 import static uk.gov.hmcts.sptribs.dmnutils.CamundaTaskConstants.MAJOR_PRIORITY;
 import static uk.gov.hmcts.sptribs.dmnutils.CamundaTaskConstants.MINOR_PRIORITY;
+import static uk.gov.hmcts.sptribs.dmnutils.CamundaTaskConstants.NEXT_HEARING_DATE;
 import static uk.gov.hmcts.sptribs.dmnutils.CamundaTaskConstants.PRIORITY_DATE_ORIGIN_REF;
 import static uk.gov.hmcts.sptribs.dmnutils.CamundaTaskConstants.PRIORITY_WORK_TYPE;
 import static uk.gov.hmcts.sptribs.dmnutils.CamundaTaskConstants.PROCESS_CASE_WITHDRAWAL_DIR_LISTED_TASK;
@@ -134,6 +135,41 @@ class CamundaTaskWaConfigurationTest extends DmnDecisionTableBaseUnitTest {
 
     static Stream<Arguments> scenarioProvider() throws IOException {
         return Stream.of(
+
+            Arguments.of(
+                PROCESS_CASE_WITHDRAWAL_DIR_TASK,
+                CaseDataBuilder.defaultCase()
+                    .withHearingDate(LocalDate.of(2026, 7, 28))
+                    .build(),
+                ConfigurationExpectationBuilder.defaultExpectations()
+                    .expectedValue(MINOR_PRIORITY, DEFAULT_MINOR_PRIORITY, true)
+                    .expectedValue(MAJOR_PRIORITY, "3000", true)
+                    .expectedValue(WORK_TYPE, ROUTINE_WORK_TYPE, true)
+                    .expectedValue(ROLE_CATEGORY, ROLE_CATEGORY_ADMIN, true)
+                    .expectedValue(DUE_DATE_INTERVAL_DAYS, "7", true)
+                    .expectedValue(DESCRIPTION, "[Orders: Send order](/cases/case-details"
+                        + "/${[CASE_REFERENCE]}/trigger/caseworker-send-order)", true)
+                    .expectedValue(DUE_DATE_ORIGIN, ZonedDateTime.now(), false)
+                    .expectedValue(NEXT_HEARING_DATE, "2026-07-28", true)
+                    .build()
+            ),
+            Arguments.of(
+                PROCESS_CASE_WITHDRAWAL_DIR_TASK,
+                CaseDataBuilder.defaultCase()
+                    .withDate(LocalDate.of(2026, 7, 28))
+                    .build(),
+                ConfigurationExpectationBuilder.defaultExpectations()
+                    .expectedValue(MINOR_PRIORITY, DEFAULT_MINOR_PRIORITY, true)
+                    .expectedValue(MAJOR_PRIORITY, "3000", true)
+                    .expectedValue(WORK_TYPE, ROUTINE_WORK_TYPE, true)
+                    .expectedValue(ROLE_CATEGORY, ROLE_CATEGORY_ADMIN, true)
+                    .expectedValue(DUE_DATE_INTERVAL_DAYS, "7", true)
+                    .expectedValue(DESCRIPTION, "[Orders: Send order](/cases/case-details"
+                        + "/${[CASE_REFERENCE]}/trigger/caseworker-send-order)", true)
+                    .expectedValue(DUE_DATE_ORIGIN, ZonedDateTime.now(), false)
+                    .expectedValue(NEXT_HEARING_DATE, "2026-07-28", true)
+                    .build()
+            ),
 
             Arguments.of(
                 PROCESS_CASE_WITHDRAWAL_DIR_TASK,
@@ -1332,7 +1368,7 @@ class CamundaTaskWaConfigurationTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(2));
         assertThat(logic.getOutputs().size(), is(3));
-        assertEquals(44, logic.getRules().size());
+        assertEquals(45, logic.getRules().size());
     }
 
     @ParameterizedTest(name = "task type: {0} case data: {1}")
