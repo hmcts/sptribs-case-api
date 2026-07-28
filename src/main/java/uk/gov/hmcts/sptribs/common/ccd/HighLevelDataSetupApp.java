162744e7f6c5d9b5959f770c619efc6bf39dab21
P Revolta <phoebe.revolta@solirius.com>
2026-07-28 12:04:06 +0100

    DTSSTCI-1784: rollback

--- src/main/java/uk/gov/hmcts/sptribs/common/ccd/HighLevelDataSetupApp.java
+++ src/main/java/uk/gov/hmcts/sptribs/common/ccd/HighLevelDataSetupApp.java
@@ -7,7 +7,6 @@ import uk.gov.hmcts.befta.dse.ccd.CcdRoleConfig;
 import uk.gov.hmcts.befta.dse.ccd.DataLoaderToDefinitionStore;
 import uk.gov.hmcts.befta.exception.ImportException;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -80,16 +79,8 @@ public class HighLevelDataSetupApp extends DataLoaderToDefinitionStore {
     @Override
     protected List<String> getAllDefinitionFilesToLoadAt(String definitionsPath) {
         String environmentName = environment.name().toLowerCase(Locale.UK);
-
-        List<String> files = new ArrayList<>(
-            List.of("build/ccd-config/ccd-" + CcdServiceCode.ST_CIC.getCaseType().getCaseTypeName() + "-" + environmentName + ".xlsx")
+        return List.of(
+            "build/ccd-config/ccd-" + CcdServiceCode.ST_CIC.getCaseType().getCaseTypeName() + "-" + environmentName + ".xlsx"
         );
-
-        if ("aat".equals(environmentName)) {
-            files.add("build/ccd-config/ccd-" + CcdServiceCode.ST_CIC.getCaseType().getCaseTypeName()
-                + "-staging-" + environmentName + ".xlsx");
-        }
-
-        return files;
     }
 }
