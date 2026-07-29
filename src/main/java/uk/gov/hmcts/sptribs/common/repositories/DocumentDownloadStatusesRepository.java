74865282083ddde8a2d3b503f1a4f8d6ee0fae80
Mihai Tranca <mihai.tranca@justice.gov.uk>
2026-07-29 13:46:39 +0000

    remove unnecessary transactional tag

--- src/main/java/uk/gov/hmcts/sptribs/common/repositories/DocumentDownloadStatusesRepository.java
+++ src/main/java/uk/gov/hmcts/sptribs/common/repositories/DocumentDownloadStatusesRepository.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.sptribs.common.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import uk.gov.hmcts.sptribs.document.model.DocumentDownloadStatusEntity;
 import uk.gov.hmcts.sptribs.notification.model.Party;
 
@@ -21,6 +20,5 @@ public interface DocumentDownloadStatusesRepository extends JpaRepository<Docume
     Optional<DocumentDownloadStatusEntity> findByDocumentIdAndParty(long documentId, Party party);
 
     @Modifying
-    @Transactional
     void deleteByCaseReferenceNumberAndParty(Long caseReferenceNumber, Party party);
 }
