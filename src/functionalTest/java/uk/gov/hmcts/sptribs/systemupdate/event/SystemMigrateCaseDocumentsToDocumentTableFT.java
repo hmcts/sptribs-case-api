674c35dd5216e2c344236de1282503e13f7b534c
Ben Middlehurst <ben.middlehurst@solirius.com>
2026-07-31 10:04:13 +0100

    added FT

---
+++ src/functionalTest/java/uk/gov/hmcts/sptribs/systemupdate/event/SystemMigrateCaseDocumentsToDocumentTableFT.java
@@ -0,0 +1,41 @@
+package uk.gov.hmcts.sptribs.systemupdate.event;
+
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import uk.gov.hmcts.sptribs.testutil.FunctionalTestSuite;
+
+import java.util.Map;
+
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.json;
+import static net.javacrumbs.jsonunit.core.Option.IGNORING_EXTRA_FIELDS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpStatus.OK;
+import static uk.gov.hmcts.sptribs.systemupdate.event.SystemMigrateCaseDocumentsToDocTable.SYSTEM_MIGRATE_CASE_DOCUMENTS_TO_TABLE;
+import static uk.gov.hmcts.sptribs.testutil.CaseDataUtil.caseData;
+import static uk.gov.hmcts.sptribs.testutil.TestConstants.ABOUT_TO_SUBMIT_URL;
+import static uk.gov.hmcts.sptribs.testutil.TestResourceUtil.expectedResponse;
+
+@SpringBootTest
+public class SystemMigrateCaseDocumentsToDocumentTableFT extends FunctionalTestSuite {
+
+    private static final String REQUEST =
+        "classpath:request/casedata/ccd-callback-casedata-system-migrate-documents-to-table-about-to-submit.json";
+    private static final String RESPONSE =
+        "classpath:responses/response-system-migrate-documents-to-table-about-to-submit.json";
+
+    @Test
+    public void shouldMigrateCaseFlagsOnAboutToSubmit() throws Exception {
+        final Map<String, Object> caseData = caseData(REQUEST);
+        final Response response = triggerCallback(caseData, SYSTEM_MIGRATE_CASE_DOCUMENTS_TO_TABLE, ABOUT_TO_SUBMIT_URL, false);
+
+        assertThat(response.getStatusCode()).isEqualTo(OK.value());
+        assertThatJson(response.asString())
+            .when(IGNORING_EXTRA_FIELDS)
+            .whenIgnoringPaths("data.hyphenatedCaseRef",
+                "data.cicCaseDraftOrderCICList[*].value.templateGeneratedDocument.document_filename")
+            .node("data")
+            .isEqualTo(json(expectedResponse(RESPONSE)));
+    }
+}
