d863f9de323998b69d55e8468bcae1016ba7cb80
Mihai Tranca <mihai.tranca@justice.gov.uk>
2026-08-12 08:00:22 +0000

    remove useless indentation

--- src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerCreateBundle.java
+++ src/main/java/uk/gov/hmcts/sptribs/caseworker/event/CaseworkerCreateBundle.java
@@ -244,8 +244,8 @@ public class CaseworkerCreateBundle implements CCDConfig<CaseData, State, UserRo
     }
 
     private List<ListValue<Bundle>> getConfiguredCaseBundles(CaseData caseData,
-                                                              BundleCallback bundleCallback,
-                                                              List<ListValue<Bundle>> existingBundles) {
+                                                             BundleCallback bundleCallback,
+                                                             List<ListValue<Bundle>> existingBundles) {
         List<ListValue<Bundle>> caseBundles = bundlingService.buildBundleListValues(bundlingService.createBundle(bundleCallback,
             Long.parseLong(caseData.getCaseNumber())));
 
