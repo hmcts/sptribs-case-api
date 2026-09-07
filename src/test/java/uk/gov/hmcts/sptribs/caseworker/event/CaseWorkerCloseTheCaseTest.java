759b5030f29e4693d1e974bb23c796db33acfabe
Mihai Tranca <mihai.tranca@justice.gov.uk>
2026-09-07 13:58:26 +0000

    remove unused import from CaseWorkerCloseTheCaseTest

--- src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerCloseTheCaseTest.java
+++ src/test/java/uk/gov/hmcts/sptribs/caseworker/event/CaseWorkerCloseTheCaseTest.java
@@ -42,7 +42,6 @@ import java.util.Set;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
