71b47ed4e81110a492338f925c6519b4da70aae2
P Revolta <phoebe.revolta@solirius.com>
2026-09-09 11:18:36 +0100

    DTSSTCI-546: attempt to resolve field not recognised error w/ jsonignore

--- src/main/java/uk/gov/hmcts/sptribs/document/bundling/model/Bundle.java
+++ src/main/java/uk/gov/hmcts/sptribs/document/bundling/model/Bundle.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.sptribs.document.bundling.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
@@ -97,6 +98,7 @@ public class Bundle {
         ignore = true,
         access = {DefaultAccess.class, CaseworkerWithCAAAccess.class}
     )
+    @JsonIgnore
     private String stitchingFailureMessage;
 
     @CCD(
