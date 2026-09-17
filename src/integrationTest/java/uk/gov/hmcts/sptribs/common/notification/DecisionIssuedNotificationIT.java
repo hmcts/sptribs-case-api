b1a43955ccb0499471e2d18a91d555d129700f2e
P Revolta <phoebe.revolta@solirius.com>
2026-09-17 11:44:49 +0100

    DTSSTCI-1736: remove broken verification from IT

--- src/integrationTest/java/uk/gov/hmcts/sptribs/common/notification/DecisionIssuedNotificationIT.java
+++ src/integrationTest/java/uk/gov/hmcts/sptribs/common/notification/DecisionIssuedNotificationIT.java
@@ -113,7 +113,6 @@ public class DecisionIssuedNotificationIT {
 
             verify(notificationServiceCIC)
                 .sendEmail(notificationRequestCaptor.capture(), anyList(), eq(TEST_CASE_ID.toString()), eq(null));
-            verify(notificationServiceCIC).sendEmail(notificationRequestCaptor.capture(), eq(TEST_CASE_ID.toString()), eq(null));
 
             NotificationRequest notificationRequest = notificationRequestCaptor.getValue();
 
