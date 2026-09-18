87a3af43f5793d4b651d74616b3d1099d9e61c4c
Ben Middlehurst <ben.middlehurst@solirius.com>
2026-09-18 13:17:53 +0100

    testing removing if prod statement

--- bin/wa/import-dmn-diagram.sh
+++ bin/wa/import-dmn-diagram.sh
@@ -8,9 +8,9 @@ product=${4}
 
 s2sSecret=${S2S_SECRET:-AABBCCDDEEFFGGHH}
 
-if [[ "${ENVIRONMENT}" == 'prod' ]]; then
-  s2sSecret=${S2S_SECRET_PROD}
-fi
+#if [[ "${ENVIRONMENT}" == 'prod' ]]; then
+#  s2sSecret=${S2S_SECRET_PROD}
+#fi
 
 serviceToken=$($(realpath $workspace)/bin/utils/idam-lease-service-token.sh sptribs_case_api \
   $(docker run --rm toolbelt/oathtool --totp -b ${s2sSecret}))
