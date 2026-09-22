93936a0a6776d761c74cd39cfc0328b11a4c617b
Ben Middlehurst <ben.middlehurst@solirius.com>
2026-09-22 11:30:58 +0100

    removed unused imports

--- src/functionalTest/java/uk/gov/hmcts/sptribs/testutil/IdamTokenGenerator.java
+++ src/functionalTest/java/uk/gov/hmcts/sptribs/testutil/IdamTokenGenerator.java
@@ -2,8 +2,6 @@ package uk.gov.hmcts.sptribs.testutil;
 
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
