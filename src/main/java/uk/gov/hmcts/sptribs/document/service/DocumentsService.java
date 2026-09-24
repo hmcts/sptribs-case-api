3652c9ac2455d41ecd65757abcacddd07eda3eb9
P Revolta <phoebe.revolta@solirius.com>
2026-09-24 16:02:57 +0100

    DTSSTCI-1736: amend logging

--- src/main/java/uk/gov/hmcts/sptribs/document/service/DocumentsService.java
+++ src/main/java/uk/gov/hmcts/sptribs/document/service/DocumentsService.java
@@ -119,8 +119,11 @@ public class DocumentsService {
 
         try {
             Long orderDocumentTypeId = caseDocumentTypesCache.getId(CaseDocumentType.ORDER);
-            log.info("Draft order updated {} document to non draft case document type successfully for url: {}",
-                documentsRepository.updateCaseDocumentTypeIdByDocumentBinaryUrl(documentBinaryUrl, orderDocumentTypeId), documentBinaryUrl);
+            if (documentsRepository.updateCaseDocumentTypeIdByDocumentBinaryUrl(documentBinaryUrl, orderDocumentTypeId) == 0) {
+                throw new DataAccessException("No document found with binary URL: " + documentBinaryUrl) {};
+            } else {
+                log.info("Draft order updated document to non draft case document type successfully for url: {}", documentBinaryUrl);
+            }
         } catch (DataAccessException e) {
             throw new DocumentUpdateException("Error updating case document type from draft order to order", e);
         }
